e4ae81b507b7cf294532021b6d4e3b77c07587c8
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2019-08-22 12:20:43 +0200

    daemon/c_cgroups.c: allow pseudo ttys for containers

--- daemon/c_cgroups.c
+++ daemon/c_cgroups.c
@@ -135,7 +135,7 @@ static const char *c_cgroups_devices_generic_whitelist[] = {
 	/* alternate tty devices - seem to be necessary for android logwrapper */
 	//"c 5:0 rwm", // tty
 	//"c 5:1 rwm", // console
-	//"c 5:2 rwm", // ptmx
+	"c 5:2 rwm", // ptmx
 
 	//"c 7:* rwm", // Virtual console capture devices
 
@@ -162,7 +162,7 @@ static const char *c_cgroups_devices_generic_whitelist[] = {
 	//"c 108:* rwm",
 
 	/* Unix98 PTY Slaves (majors 136-143) */
-	//"c 136:* rwm", // seems to be necessary for android logwrapper
+	"c 136:* rwm", // seems to be necessary for android logwrapper
 
 	/* USB */
 	//"c 180:* rwm", // USB
