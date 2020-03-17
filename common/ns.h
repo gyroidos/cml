c774ca8aeae5d9bb8949421f996336e3ba53f2ef
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2020-03-17 13:41:29 +0100

    fixed code formatting

--- common/ns.h
+++ common/ns.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 int
-namespace_exec(pid_t ns_pid, const char * const *namespaces, const size_t ns_len, int (*func)(void **), const void **data);
+namespace_exec(pid_t ns_pid, const char *const *namespaces, const size_t ns_len,
+	       int (*func)(void **), const void **data);
 
 #endif //NS_H
