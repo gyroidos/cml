6efae31068bda7d5e41fdba16849199bcd70e7c6
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2020-05-06 15:10:26 +0200

    Moved code for namespace joining to common

--- common/ns.c
+++ common/ns.c
@@ -3,74 +3,165 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+#include <sched.h>
+
+#include <grp.h>
 
 #include "macro.h"
 #include "mem.h"
 
-//#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
-
-/**
- * This function forks a new child in the given target namespaces and executes the given function
- */
 int
-namespace_exec(pid_t ns_pid, const char *const *namespaces, const size_t ns_len,
-	       int (*func)(const void **), const void **data)
+namespace_setuid0()
 {
-	TRACE("Executing function in namespace with pid %d", ns_pid);
-
-	if (ns_pid < 1) {
-		ERROR("Invalid namespace PID given: %d", ns_pid);
+	TRACE("uid %d, euid %d", getuid(), geteuid());
+	if (setuid(0) < 0) {
+		ERROR_ERRNO("Could not become root 'setuid(0)' in new userns");
 		return -1;
 	}
+	if (setgid(0) < 0) {
+		ERROR_ERRNO("Could not set gid to '0' in new userns");
+		return -1;
+	}
+	if (setgroups(0, NULL) < 0) {
+		ERROR_ERRNO("Could not setgroups to '0' in new userns");
+		return -1;
+	}
+	TRACE("uid %d, euid %d", getuid(), geteuid());
+	return 0;
+}
 
-	int status;
+int
+do_join_namespace(const char *namespace, const int pid)
+{
+	char *target_namespace_path = NULL;
+	char *target_namespace_id = mem_alloc0(40);
+	char *current_namespace_path = NULL;
+	char *current_namespace_id = mem_alloc0(40);
+	ssize_t len = -1;
+	int ns_fd = -1;
 
-	TRACE("Forking child to join namespaces");
+	target_namespace_path = mem_printf("/proc/%d/ns/%s", pid, namespace);
+	current_namespace_path = mem_printf("/proc/self/ns/%s", namespace);
+
+	len = readlink(target_namespace_path, target_namespace_id, 40);
+
+	if (len < 0 || len > 39) {
+		ERROR("Failed to read namespace identifier");
+		goto error;
+	}
+
+	target_namespace_id[len] = 0;
+
+	len = readlink(current_namespace_path, current_namespace_id, 40);
+
+	if (len < 0 || len > 39) {
+		WARN("Failed to read current identifier");
+		len = 0;
+	}
+
+	current_namespace_id[len] = 0;
+
+	DEBUG("Joining namespace with identifier %s, current namespace identifier is %s",
+	      target_namespace_id, current_namespace_id);
+
+	if (-1 == (ns_fd = open(target_namespace_path, O_RDONLY))) {
+		TRACE_ERRNO("Could not open ns file %s!", target_namespace_path);
+		goto error;
+	}
+
+	if (setns(ns_fd, 0) == -1) {
+		TRACE_ERRNO("Could not join %s namespace of pid %d!", target_namespace_path, pid);
+
+		goto error;
+	}
+
+	mem_free(target_namespace_path);
+	mem_free(target_namespace_id);
+	mem_free(current_namespace_path);
+	mem_free(current_namespace_id);
+
+	return 0;
+
+error:
+	mem_free(target_namespace_path);
+	mem_free(target_namespace_id);
+	mem_free(current_namespace_path);
+	mem_free(current_namespace_id);
+
+	return -1;
+}
+
+int
+namespace_exec(pid_t namespace_pid, const int namespaces, bool become_root,
+	       int (*func)(const void **), const void **data)
+{
+	if (namespace_pid < 1) {
+		ERROR("Invalid namespace PID given: %d", namespace_pid);
+		return -1;
+	}
 
 	pid_t pid = fork();
 
 	if (pid == -1) {
-		ERROR_ERRNO("Could not fork for switching to namespaces of %d", ns_pid);
+		ERROR_ERRNO("Could not fork for switching to namespaces of %d", namespace_pid);
 		return -1;
 	} else if (pid == 0) {
 		TRACE("Child to join namespaces forked");
 
-		for (size_t i = 0; i < ns_len; i++) {
-			char *ns_path = mem_printf("/proc/%d/ns/%s", ns_pid, namespaces[i]);
-			int ns_fd = open(ns_path, O_RDONLY);
-
-			TRACE("Joining namespace %s", ns_path);
-
-			if (ns_fd == -1) {
-				TRACE_ERRNO("Could not open ns file %s!", ns_path);
-				return -1;
-			}
-
-			if (setns(ns_fd, 0) == -1) {
-				TRACE_ERRNO("Could not join %s namespace of pid %d!", ns_path,
-					    ns_pid);
-				return -1;
-			}
-
-			//TODO handle userns
-
-			mem_free(ns_path);
+		if (namespaces & CLONE_NEWCGROUP) {
+			TRACE("Join cgroup namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("cgroup", namespace_pid), -1);
 		}
+		if (namespaces & CLONE_NEWIPC) {
+			TRACE("Join ipc namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("ipc", namespace_pid), -1);
+		}
+		if (namespaces & CLONE_NEWNET) {
+			TRACE("Join net namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("net", namespace_pid), -1);
+		}
+		if (namespaces & CLONE_NEWUTS) {
+			TRACE("Join uts namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("uts", namespace_pid), -1);
+		}
+		if (namespaces & CLONE_NEWPID) {
+			TRACE("Join pid namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("pid", namespace_pid), -1);
+		}
+		if (namespaces & CLONE_NEWUSER) {
+			TRACE("Join user namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("user", namespace_pid), -1);
+		}
+		//after joining the mount namespace the container init process has pid 1 in procfs
+		if (namespaces & CLONE_NEWNS) {
+			TRACE("Join mnt namespace");
+			IF_TRUE_RETVAL_ERROR(do_join_namespace("mnt", namespace_pid), -1);
+		}
+		if (become_root) {
+			TRACE("Becoming root in target namespace");
+			IF_TRUE_RETVAL(namespace_setuid0(), -1);
+		}
+
+		TRACE("Executing namespaced function");
 
 		int ret = func(data);
 
 		TRACE("Namespaced function returned %d", ret);
 		exit(ret);
 	} else {
-		ERROR("Waiting for namespace child to exit");
+		int status;
+
+		DEBUG("Waiting for namespace child %i to exit", pid);
 		if (waitpid(pid, &status, 0) != pid) {
-			ERROR_ERRNO("Could not waitpid for '%d'", pid);
-		} else if (!WIFEXITED(status)) {
-			ERROR("Namespaced child %d terminated abnormally", pid);
+			if (!WIFEXITED(status))
+				ERROR_ERRNO("Namespaced child %d did not exit cleanly", pid);
+			else
+				ERROR_ERRNO("Could not waitpid for '%d'", pid);
 		} else {
 			int estatus = WEXITSTATUS(status);
 			int ret = -1;
-			ret = WEXITSTATUS(estatus) ? -1 : 0;
+			ret = WIFEXITED(estatus) ? 0 : -1;
 
 			TRACE("Namespace exec child exited with status %d, returning %d", estatus,
 			      ret);
--- common/ns.h
+++ common/ns.h
@@ -1,10 +1,56 @@
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
+/**
+ * @file ns.h
+ * This module provides functions to execute a given function in the namespaces of a given process.
+ */
+
 #ifndef NS_H
 #define NS_H
 
 #include <unistd.h>
 
+/**
+ * This function tries to gain root priviledges.
+ * @returns 0 if root priviledges were sucessfully aquired
+ */
 int
-namespace_exec(pid_t ns_pid, const char *const *namespaces, const size_t ns_len,
-	       int (*func)(void **), const void **data);
+namespace_setuid0();
+
+/**
+ * This function forks a new child, joins the given namespaces and executes the given function
+ * The namespaces to join are specified using the flags of the clone call
+ *
+ * @param ns_pid pid of the namespace to join
+ * @param namespaces clone-flags of the namespaces to join
+ * @param become_root indicates if setuid(0) should be executed after joinin the namespaces
+ * @param func function pointer that gets executed inside the given namespaces
+ * @param data array that is passed as a parameter to func
+ * @returns 0 if the forked child exited cleanly, -1 otherwise.
+ */
+int
+namespace_exec(pid_t ns_pid, const int namespaces, bool become_root, int (*func)(void **),
+	       const void **data);
 
 #endif //NS_H
