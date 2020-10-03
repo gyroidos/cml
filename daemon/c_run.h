1bc35b7a0d55ba8594c15732242be35b807cf026
Michael Weiß <weiss@m4st3rnet.de>
2020-10-03 16:23:54 +0200

    daemon/c_run: added copyright headers

--- daemon/c_run.h
+++ daemon/c_run.h
@@ -1,3 +1,26 @@
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
 #ifndef C_RUN_H
 #define C_RUN_H
 
@@ -24,4 +47,4 @@ c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char *
 int
 c_run_get_console_sock_cmld(const c_run_t *run, int session_fd);
 
-#endif //end C_RUN_H
+#endif /* C_RUN_H */
