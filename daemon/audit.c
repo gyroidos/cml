7391914204feba63890a36a426036194bbb6c7b9
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2021-03-14 16:34:00 +0100

    common/audit: moved in general functionality form daemon/audit
    Refactored audit subsystem from daemon to common. Especially,
    functionality to log through the kernel nl interface was moved here.
    Thus, in all CML and other user space programmes running in containers,
    it would be feasible to log audit messages to the cmld.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/audit.c
+++ daemon/audit.c
@@ -29,6 +29,8 @@
 
 #include "cmld.h"
 #include "smartcard.h"
+
+#include "common/audit.h"
 #include "common/mem.h"
 #include "common/uuid.h"
 #include "common/str.h"
@@ -65,12 +67,6 @@
 #define AUDIT_LOGDIR "/data/audit"
 
 uint64_t AUDIT_STORAGE = 0;
-
-const char *evcategory[] = { "SUA", "FUA", "SSA", "FSA", "RLE" };
-const char *evclass[] = { "GUESTOS_MGMT",	 "TOKEN_MGMT", "CONTAINER_MGMT",
-			  "CONTAINER_ISOLATION", "TPM_COMM",   "KAUDIT" };
-const char *component[] = { "CMLD", "SCD", "TPM2D" };
-const char *result[] = { "SUCCESS", "FAIL" };
 
 static AUDIT_MODE LOGMODE = CONTAINER;
 
@@ -129,24 +125,6 @@ audit_remaining_storage(const char *uuid)
 	return AUDIT_STORAGE - size;
 }
 
-static const char *
-audit_category_to_string(AUDIT_CATEGORY c)
-{
-	return evcategory[c];
-}
-
-static const char *
-audit_evclass_to_string(AUDIT_EVENTCLASS c)
-{
-	return evclass[c];
-}
-
-static const char *
-audit_component_to_string(AUDIT_COMPONENT c)
-{
-	return component[c];
-}
-
 static void
 audit_send_record_cb(const char *hash_string, const char *hash_file,
 		     UNUSED smartcard_crypto_hashalgo_t hash_algo, void *data)
@@ -199,36 +177,6 @@ audit_send_record_cb(const char *hash_string, const char *hash_file,
 
 out:
 	mem_free(buf);
-}
-
-static AuditRecord *
-audit_record_new(AUDIT_CATEGORY category, AUDIT_COMPONENT component, AUDIT_EVENTCLASS evclass,
-		 const char *evtype, const char *subject_id, int meta_length,
-		 AuditRecord__Meta **metas)
-{
-	AuditRecord *s = mem_new0(AuditRecord, 1);
-
-	audit_record__init(s);
-
-	char *type = mem_printf("%s.%s.%s.%s", audit_category_to_string(category),
-				audit_component_to_string(component),
-				audit_evclass_to_string(evclass), evtype);
-	s->timestamp = time(NULL);
-
-	if (s->timestamp == (time_t)-1 && EFAULT == errno) {
-		ERROR_ERRNO("Failed to get current time");
-		return NULL;
-	}
-
-	s->type = type;
-
-	if (subject_id)
-		s->subject_id = mem_strdup(subject_id);
-
-	s->n_meta = meta_length;
-	s->meta = metas;
-
-	return s;
 }
 
 static AuditRecord *
@@ -671,15 +619,6 @@ audit_set_size(uint32_t size)
 	return 0;
 }
 
-#ifndef NETLINK_AUDIT
-#define NETLINK_AUDIT 9
-#endif
-
-#define MAX_AUDIT_MESSAGE_LENGTH 8970
-#define AUDIT_FIRST_EVENT 1300
-#define AUDIT_INTEGRITY_LAST_MSG 1899
-#define AUDIT_TRUSTED_APP 1121 /* Trusted app msg - freestyle text */
-
 static void
 audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, void *data)
 {
@@ -732,7 +671,7 @@ audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, v
 					uuid_string(container_get_uuid(c)), 2, "msg", log_record);
 			mem_free(record_type);
 		}
-		INFO("audit: type=%d %s", type, log_record);
+		TRACE("audit: type=%d %s", type, log_record);
 	} else if (type == AUDIT_KERNEL ||
 		   (type >= AUDIT_FIRST_EVENT && type <= AUDIT_INTEGRITY_LAST_MSG)) {
 		log_record = NLMSG_DATA(nlmsg);
@@ -740,102 +679,6 @@ audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, v
 	}
 err:
 	mem_free(buf);
-}
-
-static int
-audit_kernel_send(nl_sock_t *audit_sock, int type, const void *data, size_t len)
-{
-	int ret;
-	nl_msg_t *audit_msg = nl_msg_new();
-
-	IF_NULL_RETVAL(audit_msg, -1);
-
-	nl_msg_set_type(audit_msg, type);
-	nl_msg_set_flags(audit_msg, NLM_F_REQUEST | NLM_F_ACK);
-	nl_msg_set_buf_unaligned(audit_msg, (char *)data, len);
-
-	ret = nl_msg_send_kernel_verify(audit_sock, audit_msg);
-
-	nl_msg_free(audit_msg);
-	return ret;
-}
-
-int
-audit_kernel_log_record(const AuditRecord *msg)
-{
-	int ret = 0;
-	char *msg_text = protobuf_c_text_to_string((ProtobufCMessage *)msg, NULL);
-
-	IF_NULL_RETVAL(msg_text, -1);
-
-	DEBUG("kernel log msg='%s'", msg_text);
-
-	/* Open audit netlink socket */
-	nl_sock_t *audit_sock;
-	if (!(audit_sock = nl_sock_default_new(NETLINK_AUDIT))) {
-		ERROR("Failed to allocate audit netlink socket");
-		mem_free(msg_text);
-		return -1;
-	}
-	/* send msg to kernel framwork */
-	if (-1 ==
-	    audit_kernel_send(audit_sock, AUDIT_TRUSTED_APP, msg_text, strlen(msg_text) + 1)) {
-		ERROR("Failed to send log record to kernel!");
-		ret = -1;
-	}
-	/* Close netlink connection */
-	nl_sock_free(audit_sock);
-	mem_free(msg_text);
-	return ret;
-}
-
-int
-audit_kernel_log_event(AUDIT_CATEGORY category, AUDIT_COMPONENT component, AUDIT_EVENTCLASS evclass,
-		       const char *evtype, const char *subject_id, int meta_count, ...)
-{
-	AuditRecord *record = NULL;
-	AuditRecord__Meta **metas = NULL;
-	int ret = 0;
-
-	if (0 < meta_count) {
-		if (0 != (meta_count % 2)) {
-			ERROR("Odd number of variadic arguments, aborting...");
-			return -1;
-		}
-
-		va_list ap;
-
-		va_start(ap, meta_count);
-
-		metas = mem_alloc0((meta_count / 2) * sizeof(AuditRecord__Meta *));
-		for (int i = 0; i < meta_count / 2; i++) {
-			metas[i] = mem_alloc0(sizeof(AuditRecord__Meta));
-
-			audit_record__meta__init(metas[i]);
-
-			metas[i]->key = mem_strdup(va_arg(ap, const char *));
-			metas[i]->value = mem_strdup(va_arg(ap, const char *));
-		}
-
-		va_end(ap);
-		meta_count /= 2;
-	}
-
-	record = audit_record_new(category, component, evclass, evtype, subject_id, meta_count,
-				  metas);
-
-	if (!record) {
-		ERROR("Failed to create audit record");
-		mem_free(metas);
-		goto out;
-	}
-
-	ret = audit_kernel_log_record(record);
-
-out:
-	protobuf_free_message((ProtobufCMessage *)record);
-
-	return ret;
 }
 
 int
