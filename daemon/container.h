6b8f51c77994c1a52bffaaf2700f1eddc18e4333
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2021-03-15 08:44:56 +0100

    daemon: Removed deprecated adb debugging interface
    Removed the ability to configure an eth interface for adb debugging,
    including setup of correpsonding port forwarding.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/container.h
+++ daemon/container.h
@@ -187,12 +187,11 @@ container_t *
 container_new_internal(const uuid_t *uuid, const char *name, container_type_t type, bool ns_usr,
 		       bool ns_net, bool privileged, const guestos_t *os,
 		       const char *config_filename, const char *images_folder, mount_t *mnt,
-		       unsigned int ram_limit, uint32_t color, uint16_t adb_port,
-		       bool allow_autostart, list_t *feature_enabled, const char *dns_server,
-		       list_t *net_ifaces, char **allowed_devices, char **assigned_devices,
-		       list_t *vnet_cfg_list, list_t *usbdev_list, char **init_env,
-		       size_t init_env_len, list_t *fifo_list, container_token_type_t ttype,
-		       bool usb_pin_entry);
+		       unsigned int ram_limit, uint32_t color, bool allow_autostart,
+		       list_t *feature_enabled, const char *dns_server, list_t *net_ifaces,
+		       char **allowed_devices, char **assigned_devices, list_t *vnet_cfg_list,
+		       list_t *usbdev_list, char **init_env, size_t init_env_len, list_t *fifo_list,
+		       container_token_type_t ttype, bool usb_pin_entry);
 
 /**
  * Creates a new container container object. There are three different cases
