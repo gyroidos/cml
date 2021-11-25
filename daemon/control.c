c27bb5b1e331b7ad75f789a17564792fa532590b
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2021-11-25 10:49:08 +0100

    daemon/control: use cmld_reboot_device function for reboot requests
    Instead of just triggering a hard reboot on device reboot request at the
    control interface, we now call the new cmld_reboot_device() function
    which tries to properly stop containers before rebooting.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/control.c
+++ daemon/control.c
@@ -1000,9 +1000,8 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 	} break;
 
 	case CONTROLLER_TO_DAEMON__COMMAND__REBOOT_DEVICE: {
-		res = reboot_reboot(REBOOT);
-		control_send_message(res ? CONTROL_RESPONSE_CMD_FAILED : CONTROL_RESPONSE_CMD_OK,
-				     fd);
+		cmld_reboot_device();
+		control_send_message(CONTROL_RESPONSE_CMD_OK, fd);
 	} break;
 
 	case CONTROLLER_TO_DAEMON__COMMAND__PULL_DEVICE_CSR: {
