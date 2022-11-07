c7cb17818f7be2d5e8d47e0b57edc851accaace7
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2022-11-07 12:37:18 +0100

    daemon/c_idmapped: Allow to compile with -Werror=main
    kernel_version_check() was using main as variable which will trigger
    a warning. This would be treated as error in case -Werror=main is set.
    We renamed main to _main and for consistency also major to _major.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/c_idmapped.c
+++ daemon/c_idmapped.c
@@ -450,14 +450,14 @@ kernel_version_check(char *version)
 {
 	struct utsname buf;
 	char ignore[65];
-	int main, major, main_to_check, major_to_check;
+	int _main, _major, main_to_check, major_to_check;
 
 	uname(&buf);
 
 	ASSERT(sscanf(version, "%d.%d%s", &main_to_check, &major_to_check, ignore) >= 2);
-	ASSERT(sscanf(buf.release, "%d.%d.%s", &main, &major, ignore) == 3);
+	ASSERT(sscanf(buf.release, "%d.%d.%s", &_main, &_major, ignore) == 3);
 
-	return (main == main_to_check) ? major >= major_to_check : main >= main_to_check;
+	return (_main == main_to_check) ? _major >= major_to_check : _main >= main_to_check;
 }
 
 static int
