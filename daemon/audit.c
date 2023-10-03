gyroidos/cml

commit 9d50cd381b34cf60f01dce562f6f00642c476f9c

daemon/audit: avoid 'warning: the use of `mktemp' is dangerous'
We switched from mktemp() to mkstemp() for generating the temporary
file name in audit_log_record(). This avoids the corresponding gcc
warning.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -463,7 +463,7 @@ audit_do_send_record(const container_t *c)
 		return -1;
 	}
 
-	if (!strcmp("", mktemp(tmpfile))) {
+	if (-1 == mkstemp(tmpfile)) {
 		ERROR_ERRNO("Failed to generate temporary filename");
 		return -1;
 	}

commit d3ca979aac0feb54a184fa02b4aaf81daf06f383

daemon/audit: Fixed missing close of opened fd by mkstemp()
In commit 9d50cd381b34cf60f01dce562f6f00642c476f9c, mktemp() was
replaced by mkstemp(). However, the returned fd of mkstemp was not
used and also not closed. Thus, we use the returned fd by fd_write()
instead of reopen a new fd by using file_write() on the path.
Of course the fd opened by mkstemp is closed properly now, too.

Fixes: 9d50cd381b34 ("daemon/audit: avoid 'warning: the use of `mktemp' is dangerous'")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -456,6 +456,7 @@ audit_do_send_record(const container_t *c)
 	uint8_t *packed = NULL;
 	uint32_t packed_len = 0;
 	int ret = -1;
+	int fd = -1;
 
 	char tmpfile[strlen(AUDIT_LOGDIR) + 14];
 	if (0 >= sprintf(tmpfile, "%s/%s", AUDIT_LOGDIR, "audit_XXXXXX")) {
@@ -463,7 +464,7 @@ audit_do_send_record(const container_t *c)
 		return -1;
 	}
 
-	if (-1 == mkstemp(tmpfile)) {
+	if (-1 == (fd = mkstemp(tmpfile))) {
 		ERROR_ERRNO("Failed to generate temporary filename");
 		return -1;
 	}
@@ -485,7 +486,7 @@ audit_do_send_record(const container_t *c)
 		goto out;
 	}
 
-	if (-1 == file_write(tmpfile, (char *)packed, packed_len)) {
+	if (-1 == fd_write(fd, (char *)packed, packed_len)) {
 		ERROR("Failed to write packed message to file.");
 		goto out;
 	}
@@ -509,6 +510,8 @@ audit_do_send_record(const container_t *c)
 out:
 	if (ret < 0)
 		ERROR("Failed to send next stored audit record");
+
+	close(fd);
 
 	mem_free0(packed);
 	protobuf_free_message((ProtobufCMessage *)message_proto);