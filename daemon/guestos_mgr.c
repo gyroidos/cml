gyroidos/cml

commit 4e2c5c91236cb728814f691c9bfb6f654dbfaf19

daemon/guestos_mgr: only purge GuestOSes if not in use anymore
guestos_mgr_purge_obsolete() only was checking the version of GuestOSes
and kept the latest. This function was called internaly on
guestos_mgr_init() where containers are not yet initialized. Obviously
GuestOSes needs to be available before the containers could be loaded
since they need a reference to the corresponding GuestOS.
Thus, just adding a check by calling guestos_mgr_used_by_container()
is not sufficient since at this point no containers are loaded.

We add this check but also make guestos_mgr_purge_obsolete() externally
callable. We move the internal call in guestos_mgr_load_operatingsystems()
during guestos_mgr_init() to the cmld module after contaienr configs are
loaded.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -67,23 +67,6 @@ static bool guestos_mgr_allow_locally_signed = false;
 
 /******************************************************************************/
 
-static void
-guestos_mgr_purge_obsolete(void)
-{
-	INFO("Looking for obsolete GuestOSes to purge...");
-	for (list_t *l = guestos_list; l;) {
-		list_t *next = l->next;
-		guestos_t *os = l->data;
-		guestos_t *latest = guestos_mgr_get_latest_by_name(guestos_get_name(os), true);
-		if ((latest && guestos_get_version(os) < guestos_get_version(latest))) {
-			guestos_list = list_unlink(guestos_list, l);
-			guestos_purge(os);
-			guestos_free(os);
-		}
-		l = next;
-	}
-}
-
 /**
  * This function verifies the guestos configuration file at load time
  * as part of TSF.CML.SecureCompartmentInit
@@ -164,8 +147,6 @@ guestos_mgr_load_operatingsystems(void)
 		return -1;
 	}
 
-	guestos_mgr_purge_obsolete();
-
 	return 0;
 }
 
@@ -235,6 +216,24 @@ guestos_mgr_delete(guestos_t *os)
 	guestos_free(os);
 
 	return 0;
+}
+
+void
+guestos_mgr_purge_obsolete(void)
+{
+	INFO("Looking for obsolete GuestOSes to purge...");
+	for (list_t *l = guestos_list; l;) {
+		list_t *next = l->next;
+		guestos_t *os = l->data;
+		guestos_t *latest = guestos_mgr_get_latest_by_name(guestos_get_name(os), true);
+		if (latest && guestos_get_version(os) < guestos_get_version(latest) &&
+		    !guestos_mgr_is_guestos_used_by_containers(guestos_get_name(os))) {
+			guestos_list = list_unlink(guestos_list, l);
+			guestos_purge(os);
+			guestos_free(os);
+		}
+		l = next;
+	}
 }
 
 /******************************************************************************/

daemon/cmld.c
@@ -1551,6 +1551,9 @@ cmld_init(const char *path)
 	if (cmld_load_containers(containers_path) < 0)
 		FATAL("Could not load containers");
 
+	// check for unused superseded GuestOSes and remove them
+	guestos_mgr_purge_obsolete();
+
 	if (cmld_start_c0(cmld_containers_get_c0()) < 0)
 		FATAL("Could not start c0");
 

daemon/guestos_mgr.h
@@ -71,6 +71,16 @@ guestos_mgr_add_from_file(const char *file, guestos_verify_result_t verify_resul
 int
 guestos_mgr_delete(guestos_t *os);
 
+/**
+ * Purges all old GuestOSes from disk. It keeps older versions of operating
+ * systems if they are still in use by a container. Thus, be careful to call this
+ * function not too early in startup process. Container configs must be loaded
+ * before calling this. Otherwise the container list is empty which means no OS
+ * would be in use and only the latest version would be kept.
+ */
+void
+guestos_mgr_purge_obsolete(void);
+
 /******************************************************************************/
 
 /**

commit c5a144e0cf05b4fa899246a8975ccfb6c60c91f0

daemon/guestos_mgr: check exact version of guestos is used on purge
Instead of calling guestos_mgr_is_guestos_used_by_container() which
is based on the os_name and returns true if any version of the
guestos is used, we now call a new function which checks if the
guestos instance itself is used by any container.

Fixes: 4e2c5c91236c ("daemon/guestos_mgr: only purge GuestOSes if not in use anymore")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -165,6 +165,20 @@ guestos_mgr_is_guestos_used_by_containers(const char *os_name)
 	return false;
 }
 
+static bool
+guestos_mgr_is_this_guestos_used_by_containers(guestos_t *os)
+{
+	ASSERT(os);
+
+	int n = cmld_containers_get_count();
+	for (int i = 0; i < n; i++) {
+		container_t *c = cmld_container_get_by_index(i);
+		if (os == container_get_guestos(c))
+			return true;
+	}
+	return false;
+}
+
 /******************************************************************************/
 
 int
@@ -227,7 +241,7 @@ guestos_mgr_purge_obsolete(void)
 		guestos_t *os = l->data;
 		guestos_t *latest = guestos_mgr_get_latest_by_name(guestos_get_name(os), true);
 		if (latest && guestos_get_version(os) < guestos_get_version(latest) &&
-		    !guestos_mgr_is_guestos_used_by_containers(guestos_get_name(os))) {
+		    !guestos_mgr_is_this_guestos_used_by_containers(os)) {
 			guestos_list = list_unlink(guestos_list, l);
 			guestos_purge(os);
 			guestos_free(os);