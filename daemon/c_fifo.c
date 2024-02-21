gyroidos/cml

commit 35b7617b1f4f93bd5cb9053152af3634104f5364

daemon/c_fifo: register forwarders at compartment sigchld handler
If forwarding children gets killed externally or crashes somehow, it
would not be reaped properly. Thus remove the explicit waitpid() call in
the cleanup() and kill(<child_pid>) and register child_pid
at by container_wait_for_child() at compartments sigchld handler.
That way if the compartment gets killed (internally or externally) the
forwarders get reaped.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_fifo.c
@@ -127,7 +127,6 @@ c_fifo_cleanup(void *fifop, UNUSED bool is_rebooting)
 
 		DEBUG("Stopping forwarder %d", *pid);
 		kill(*pid, SIGKILL);
-		proc_waitpid(*pid, NULL, 0);
 
 		mem_free(pid);
 		fifo->forwarder_list = list_unlink(fifo->forwarder_list, fifo->forwarder_list);
@@ -330,7 +329,7 @@ c_fifo_start_post_clone(void *fifop)
 		goto error;
 	}
 
-	//fork FIFO forwarding childs
+	// fork FIFO forwarding children
 	for (list_t *elem = fifo->fifo_list; elem != NULL; elem = elem->next) {
 		char *current_fifo = elem->data;
 		char *current_fifo_c0 = mem_printf("%s/%s", fifo_path_c0, current_fifo);
@@ -350,7 +349,6 @@ c_fifo_start_post_clone(void *fifop)
 
 		} else if (pid == 0) {
 			DEBUG("Preparing forwarding for FIFO \'%s\'", current_fifo);
-
 			DEBUG("Forwarding from %s to %s", current_fifo_c0, current_fifo_container);
 
 			event_reset();
@@ -367,6 +365,11 @@ c_fifo_start_post_clone(void *fifop)
 		*mpid = pid;
 		DEBUG("Appending %d to forwarder list", *mpid);
 		fifo->forwarder_list = list_append(fifo->forwarder_list, mpid);
+
+		// register at sigchild handler for helper forwarding clone
+		char *forwarder_name = mem_printf("%s-forwarder", current_fifo);
+		container_wait_for_child(fifo->container, forwarder_name, *mpid);
+		mem_free0(forwarder_name);
 
 		DEBUG("Forked FIFO forwarding child %d for %s", pid, current_fifo);
 	}

commit 56edebcf8bfde47396568e0fd1c755ab9c3bd7cd

daemon/c_fifo: Fixed still running forwarder on compartment kill
If compartment gets killed, e.g., during boot timeout, the fifo forwarders
are not stopped. The stop() hooks are only called if a regular stop is
triggered by the user through the control interface. We now explicitly
kill the remaining forwarders on cleanup again.

Fixes: 35b7617b1f4f ("daemon/c_fifo: register forwarders at compartment sigchld handler")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_fifo.c
@@ -128,8 +128,15 @@ c_fifo_cleanup(void *fifop, bool is_rebooting)
 
 	while (fifo->forwarder_list) {
 		pid_t *pid = fifo->forwarder_list->data;
-		if (pid)
+		if (pid) {
+			// stop hooks are not called if compartment is killed;
+			// thus, kill forwarders here.
+			if (*pid > 0) {
+				DEBUG("Kill still running forwarder %d", *pid);
+				kill(*pid, SIGKILL);
+			}
 			mem_free(pid);
+		}
 		fifo->forwarder_list = list_unlink(fifo->forwarder_list, fifo->forwarder_list);
 	}
 
@@ -412,6 +419,7 @@ c_fifo_stop(void *fifop)
 
 		DEBUG("Stopping forwarder %d", *pid);
 		kill(*pid, SIGKILL);
+		*pid = -1;
 	}
 
 	return 0;