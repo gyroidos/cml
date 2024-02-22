a0a994e58b764065713012b30cb4dad061d89742
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2024-02-22 20:58:07 +0100

    daemon/c_uevent: removed automount feature from c_uevent
    Since automount does not depend on uevents anymore, but just uses
    inotify events on '/dev', this feature will be relocated to a new
    submodule c_automount in a followup commit. We remove this here
    now.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/c_uevent.c
+++ daemon/c_uevent.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of GyroidOS
- * Copyright(c) 2013 - 2022 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2024 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -35,18 +35,13 @@
 
 #include "container.h"
 
-#include <unistd.h>
 #include <libgen.h>
-#include <sys/inotify.h>
-#include <sys/mount.h>
 #include <sys/sysmacros.h>
-
-#define C_UEVENT_AUTO_MOUNT_RETRIES 10
+#include <unistd.h>
 
 typedef struct c_uevent {
 	container_t *container;
 	uevent_uev_t *uev;
-	event_inotify_t *inotify_dev;
 } c_uevent_t;
 
 static int
@@ -87,117 +82,6 @@ shift:
 err:
 	mem_free0(path_dirname);
 	return -1;
-}
-
-struct c_uevent_mount_timer_data {
-	char *path;
-	container_t *container;
-};
-
-static void
-c_uevent_mount_timer_cb(event_timer_t *timer, void *data)
-{
-	static int retries = 0;
-	ASSERT(data);
-
-	struct c_uevent_mount_timer_data *tdata = data;
-	int ret = 0;
-
-	char *fstypes[] = { "vfat", "ext4", "btrfs", "ext2", "ext3" };
-	char *basename_path = mem_strdup(tdata->path);
-	char *devname = basename(basename_path);
-	char *mount_path = mem_printf("%s/media/external/%s",
-				      container_get_rootdir(tdata->container), devname);
-
-	if (dir_mkdir_p(mount_path, 0755)) {
-		ERROR("Could not create path for external storage mount point");
-		goto out;
-	}
-
-	for (int i = 0; i < 5; ++i) {
-		char *mount_data = NULL;
-		if (!strcmp(fstypes[i], "vfat")) {
-			int uid = container_get_uid(tdata->container);
-			mount_data = mem_printf("uid=%d, gid=%d", uid, uid);
-		}
-
-		ret = mount(tdata->path, mount_path, fstypes[i], MS_RELATIME, mount_data);
-
-		if (mount_data)
-			mem_free0(mount_data);
-
-		if (ret == 0) {
-			INFO("Mounting %s to %s fstype = %s in container %s!", tdata->path,
-			     mount_path, fstypes[i],
-			     uuid_string(container_get_uuid(tdata->container)));
-			break;
-		} else {
-			DEBUG_ERRNO("Failed mounting %s to %s fstype = %s!", tdata->path,
-				    mount_path, fstypes[i]);
-		}
-	}
-
-out:
-	mem_free0(basename_path);
-	mem_free0(mount_path);
-
-	IF_TRUE_RETURN(ret && retries++ < C_UEVENT_AUTO_MOUNT_RETRIES);
-
-	retries = 0;
-	mem_free0(tdata->path);
-	mem_free0(tdata);
-
-	event_remove_timer(timer);
-	event_timer_free(timer);
-}
-
-static void
-c_uevent_mount_watch_dev_dir_cb(const char *path, uint32_t mask, UNUSED event_inotify_t *inotify,
-				void *data)
-{
-	ASSERT(data);
-	c_uevent_t *uevent = data;
-
-	IF_FALSE_RETURN(mask & IN_CREATE);
-
-	struct stat dev_stat;
-	mem_memset(&dev_stat, 0, sizeof(dev_stat));
-
-	if (stat(path, &dev_stat) == -1) {
-		WARN_ERRNO("Could not stat %s", path);
-		return;
-	}
-
-	IF_FALSE_RETURN(S_ISBLK(dev_stat.st_mode));
-
-	DEBUG("blk in container %s: %s (create)", container_get_description(uevent->container),
-	      path);
-
-	char type;
-	if (S_ISBLK(dev_stat.st_mode)) {
-		type = 'b';
-	} else if (S_ISCHR(dev_stat.st_mode)) {
-		type = 'c';
-	} else {
-		return;
-	}
-
-	unsigned int major = major(dev_stat.st_rdev);
-	unsigned int minor = minor(dev_stat.st_rdev);
-
-	if (!container_is_device_allowed(uevent->container, type, major, minor)) {
-		TRACE("skip not allowed device (%c %d:%d) for container %s", type, major, minor,
-		      container_get_name(uevent->container));
-		return;
-	}
-
-	// give device some time to get ready
-	struct c_uevent_mount_timer_data *tdata = mem_new0(struct c_uevent_mount_timer_data, 1);
-	tdata->path = mem_strdup(path);
-	tdata->container = uevent->container;
-	event_timer_t *e =
-		event_timer_new(1000, EVENT_TIMER_REPEAT_FOREVER, c_uevent_mount_timer_cb, tdata);
-	event_add_timer(e);
 }
 
 static void
@@ -288,10 +172,6 @@ c_uevent_new(compartment_t *compartment)
 			       UEVENT_ACTION_ADD | UEVENT_ACTION_CHANGE | UEVENT_ACTION_REMOVE,
 			       c_uevent_handle_event_cb, uevent);
 
-	// watch /dev for device nodes to appear in filesystem
-	uevent->inotify_dev =
-		event_inotify_new("/dev", IN_CREATE, &c_uevent_mount_watch_dev_dir_cb, uevent);
-
 	return uevent;
 }
 
@@ -302,9 +182,6 @@ c_uevent_free(void *ueventp)
 	ASSERT(uevent);
 
 	uevent_uev_free(uevent->uev);
-
-	event_inotify_free(uevent->inotify_dev);
-	uevent->inotify_dev = NULL;
 
 	mem_free0(uevent);
 }
@@ -345,45 +222,6 @@ c_uevent_boot_complete_cb(container_t *container, container_callback_t *cb, void
 }
 
 static int
-c_uevent_start_child_early(void *ueventp)
-{
-	c_uevent_t *uevent = ueventp;
-	ASSERT(uevent);
-
-	char *mnt_media = mem_printf("%s/media", container_get_rootdir(uevent->container));
-
-	INFO("Mounting tmpfs to %s", mnt_media);
-
-	if (mkdir(mnt_media, 0755) < 0 && errno != EEXIST) {
-		ERROR_ERRNO("Could not mkdir %s", mnt_media);
-		goto error;
-	}
-
-	if (mount("tmpfs", mnt_media, "tmpfs", MS_RELATIME | MS_NOSUID, NULL) < 0) {
-		ERROR_ERRNO("Could not mount %s", mnt_media);
-		goto error;
-	}
-
-	if (mount(NULL, mnt_media, NULL, MS_SHARED, NULL) < 0) {
-		ERROR_ERRNO("Could not apply MS_SHARED to %s", mnt_media);
-		goto error;
-	} else {
-		DEBUG("Applied MS_SHARED to %s", mnt_media);
-	}
-
-	if (container_shift_ids(uevent->container, mnt_media, mnt_media, NULL)) {
-		ERROR_ERRNO("Could not shift ids for dev on '%s'", mnt_media);
-		goto error;
-	}
-
-	mem_free0(mnt_media);
-	return 0;
-error:
-	mem_free0(mnt_media);
-	return -COMPARTMENT_ERROR;
-}
-
-static int
 c_uevent_start_post_exec(void *ueventp)
 {
 	c_uevent_t *uevent = ueventp;
@@ -392,13 +230,6 @@ c_uevent_start_post_exec(void *ueventp)
 	// register uevent handling for this c_uevent container submodule
 	if (uevent_add_uev(uevent->uev))
 		return -COMPARTMENT_ERROR;
-
-	/* start watching device nodes for automount */
-	int error = event_add_inotify(uevent->inotify_dev);
-	if (error && error != -EEXIST) {
-		ERROR("Could not register inotify event for automount events!");
-		return -COMPARTMENT_ERROR;
-	}
 
 	/* register an observer to wait for the container to be running */
 	if (!container_register_observer(uevent->container, &c_uevent_boot_complete_cb, uevent)) {
@@ -417,8 +248,6 @@ c_uevent_stop(void *ueventp)
 
 	uevent_remove_uev(uevent->uev);
 
-	event_remove_inotify(uevent->inotify_dev);
-
 	return 0;
 }
 
@@ -428,11 +257,9 @@ c_uevent_cleanup(void *ueventp, UNUSED bool rebooting)
 	c_uevent_t *uevent = ueventp;
 	ASSERT(uevent);
 
-	char *mnt_media = mem_printf("%s/media", container_get_rootdir(uevent->container));
-	if (umount(mnt_media) < 0)
-		WARN_ERRNO("Could not umount %s", mnt_media);
-
-	mem_free0(mnt_media);
+	// if killed, stop hook may not be called. Thus remove
+	// uev if still in list if not this will not harm.
+	uevent_remove_uev(uevent->uev);
 }
 
 static compartment_module_t c_uevent_module = {
@@ -441,7 +268,7 @@ static compartment_module_t c_uevent_module = {
 	.compartment_free = c_uevent_free,
 	.compartment_destroy = NULL,
 	.start_post_clone_early = NULL,
-	.start_child_early = c_uevent_start_child_early,
+	.start_child_early = NULL,
 	.start_pre_clone = NULL,
 	.start_post_clone = NULL,
 	.start_pre_exec = NULL,
