gyroidos/cml

commit 3c8fd49d582ffd9d9ac608ed07f91b67ae784a48

daemon/guestos_mgr: Check available disk space
Check whether enough disk space is left before creating a new guestos.

Signed-off-by: Maximilian Peisl <maximilian.peisl@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -433,7 +433,38 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-failed", os_name, 0);
 		ERROR("Signature verification failed (%d) for pushed GuestOS config buffer, skipping.",
 		      verify_result);
-		goto err;
+		goto cleanup_os;
+	}
+
+	// disk space check
+	mount_t *mnt = mount_new();	   // need to get "mounts" to get image URLs... feels wrong
+	guestos_fill_mount(os, mnt);	   // append mounts to be checked
+	guestos_fill_mount_setup(os, mnt); // append setup mode mounts to be check
+
+	off_t disk_space_required = mount_get_disk_usage_guestos(mnt);
+	if (disk_space_required < 0) {
+		ERROR("Function mount_get_disk_usage_guestos failed");
+		goto cleanup_mnt;
+	}
+
+	off_t disk_space_available = file_disk_space_free(guestos_get_dir(os));
+	if (disk_space_available < 0) {
+		ERROR("Function file_disk_space_free failed");
+		goto cleanup_mnt;
+	}
+
+	off_t min_free_space = file_disk_space(guestos_get_dir(os));
+	if (min_free_space < 0) {
+		ERROR("Function file_disk_space failed");
+		goto cleanup_mnt;
+	}
+	min_free_space *= CMLD_STORAGE_FREE_THRESHOLD;
+
+	if (disk_space_required > disk_space_available ||
+	    (disk_space_available - disk_space_required) < min_free_space) {
+		// not enough space left
+		ERROR("Not enough disk space left");
+		goto cleanup_mnt;
 	}
 
 	uint64_t os_ver = guestos_get_version(os);
@@ -455,7 +486,7 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 			WARN("Skipping update of GuestOS %s version %" PRIu64
 			     " to older/same version %" PRIu64 ".",
 			     os_name, old_ver, os_ver);
-			goto cleanup_os;
+			goto cleanup_mnt;
 		} else if (os_ver == old_ver) {
 			DEBUG("Retrigger download of GuestOS images for %s of v%" PRIu64 ".",
 			      os_name, os_ver);
@@ -517,6 +548,8 @@ trigger_download:
 
 cleanup_purge:
 	guestos_purge(os);
+cleanup_mnt:
+	mount_free(mnt);
 cleanup_os:
 	guestos_free(os);
 err:

commit 55a66e3e220930fe27a54812029f1977ca17584d

daemon/guestos_mgr: Use guestos_basepath for disk space check
Commit 3c8fd49d582f "daemon/guestos_mgr: Check available disk space"
introduced disk space check during update procedure of guestoses.
The check was made on guest_os_dir(os). However this dir does not
yet exists at this early stage of the update procedure.

Check against 'guestos_basepath' instead.

This fixes the followng error:
[448] <ERROR> guestos_mgr.c+452: Function file_disk_space_free failed

Fixes: 3c8fd49d582f ("daemon/guestos_mgr: Check available disk space")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -447,13 +447,13 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 		goto cleanup_mnt;
 	}
 
-	off_t disk_space_available = file_disk_space_free(guestos_get_dir(os));
+	off_t disk_space_available = file_disk_space_free(guestos_basepath);
 	if (disk_space_available < 0) {
 		ERROR("Function file_disk_space_free failed");
 		goto cleanup_mnt;
 	}
 
-	off_t min_free_space = file_disk_space(guestos_get_dir(os));
+	off_t min_free_space = file_disk_space(guestos_basepath);
 	if (min_free_space < 0) {
 		ERROR("Function file_disk_space failed");
 		goto cleanup_mnt;
@@ -463,7 +463,7 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 	if (disk_space_required > disk_space_available ||
 	    (disk_space_available - disk_space_required) < min_free_space) {
 		// not enough space left
-		ERROR("Not enough disk space left");
+		ERROR("Not enough disk space left on %s", guestos_basepath);
 		goto cleanup_mnt;
 	}
 