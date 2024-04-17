gyroidos/cml

commit 7230fa0fc824053c7e037c13ae4c6ebe26bc27db

daemon/container: added setter for name in container_pnet_cfg_t
We introduce a setter for the interface name field in the
contaier_pnet_cfg_t struct.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/container.c
@@ -683,7 +683,16 @@ container_pnet_cfg_free(container_pnet_cfg_t *pnet_cfg)
 		mem_free0(mac);
 	}
 	list_delete(pnet_cfg->mac_whitelist);
+
 	mem_free0(pnet_cfg);
+}
+
+void
+container_pnet_cfg_set_pnet_name(container_pnet_cfg_t *pnet_cfg, const char *pnet_name)
+{
+	IF_NULL_RETURN(pnet_cfg);
+
+	pnet_cfg->pnet_name = mem_strdup(pnet_name);
 }
 
 void

daemon/container.h
@@ -278,6 +278,12 @@ void
 container_pnet_cfg_free(container_pnet_cfg_t *pnet_cfg);
 
 /**
+ * Update name attribute of pnet_cfg data structure
+ */
+void
+container_pnet_cfg_set_pnet_name(container_pnet_cfg_t *pnet_cfg, const char *pnet_name);
+
+/**
  * Get the list of usb devices which are set in container config.
  */
 list_t *

commit e8886bc16cc87249b705a21076c664ce25101bb4

daemon/container: fixed mem leak in container_pnet_cfg_set_pnet_name()
Properly free the current pnet_name before setting the new name with
mem_strdup().

Fixes: 7230fa0fc824 ("daemon/container: added setter for name in container_pnet_cfg_t")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/container.c
@@ -837,6 +837,9 @@ container_pnet_cfg_set_pnet_name(container_pnet_cfg_t *pnet_cfg, const char *pne
 {
 	IF_NULL_RETURN(pnet_cfg);
 
+	if (pnet_cfg->pnet_name)
+		mem_free0(pnet_cfg->pnet_name);
+
 	pnet_cfg->pnet_name = mem_strdup(pnet_name);
 }
 