gyroidos/cml

commit 723c5d14d91688e27c68348d0a63fbba5fcf6ef0

daemon/control: Provide device stats through control interface
Introduce a new protobuf message command DEVICE_STATS to retrieve
statistics about memory and disk utilization of the CML.
This could be used in c0 to monitor if container space may run out or
system disk is getting full due to any reasons.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/control.c
@@ -49,6 +49,7 @@
 #include "common/file.h"
 #include "common/dir.h"
 #include "common/str.h"
+#include "common/proc.h"
 
 #include <unistd.h>
 #include <inttypes.h>
@@ -1217,6 +1218,47 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 		mem_free0(cuuid_str);
 		protobuf_free_message((ProtobufCMessage *)ccfg[0]);
 		mem_free0(ccfg);
+	} break;
+
+	case CONTROLLER_TO_DAEMON__COMMAND__GET_DEVICE_STATS: {
+		DaemonToController out = DAEMON_TO_CONTROLLER__INIT;
+		out.code = DAEMON_TO_CONTROLLER__CODE__DEVICE_STATS;
+
+		DeviceStats *device_stats = mem_new(DeviceStats, 1);
+		device_stats__init(device_stats);
+
+		device_stats->disk_system = file_disk_space(cmld_get_cmld_dir());
+		device_stats->disk_system_free = file_disk_space_free(cmld_get_cmld_dir());
+		device_stats->disk_system_used = file_disk_space_used(cmld_get_cmld_dir());
+
+		if (!file_on_same_fs(cmld_get_cmld_dir(), cmld_get_containers_dir())) {
+			device_stats->has_disk_containers = true;
+			device_stats->has_disk_containers_free = true;
+			device_stats->has_disk_containers_used = true;
+			device_stats->disk_containers = file_disk_space(cmld_get_containers_dir());
+			device_stats->disk_containers_free =
+				file_disk_space_free(cmld_get_containers_dir());
+			device_stats->disk_containers_used =
+				file_disk_space_used(cmld_get_containers_dir());
+		}
+
+		proc_meminfo_t *meminfo = proc_meminfo_new();
+		if (meminfo) {
+			device_stats->has_mem_total = true;
+			device_stats->has_mem_free = true;
+			device_stats->has_mem_available = true;
+			device_stats->mem_total = proc_get_mem_total(meminfo);
+			device_stats->mem_free = proc_get_mem_free(meminfo);
+			device_stats->mem_available = proc_get_mem_available(meminfo);
+		}
+
+		out.device_stats = device_stats;
+
+		if (protobuf_send_message(fd, (ProtobufCMessage *)&out) < 0)
+			WARN("Could not send provisioned state");
+
+		proc_meminfo_free(meminfo);
+		protobuf_free_message((ProtobufCMessage *)device_stats);
 	} break;
 
 	// Container-specific commands:

daemon/cc_mode/control.proto
@@ -65,6 +65,9 @@ message ControllerToDaemon {
 		// Also fills [container_uuids] with the corresponding container UUIDs.
 		GET_CONTAINER_CONFIG = 4;	// [container_uuid] -> [container_config]
 
+		// Retrive device statistics about mem and storage
+		GET_DEVICE_STATS = 6;
+
 		/**********************************************************************
 		 * @exclude
 		 * Commands (global) that modify the system 
@@ -103,7 +106,7 @@ message ControllerToDaemon {
 		 * Commands that operate on a specific container
 		**********************************************************************/
 
-	// The UUID of the container must be in the container_uuids field.
+		// The UUID of the container must be in the container_uuids field.
 
 		// Starts a container. Also needs [start_params].
 		CONTAINER_START = 101;
@@ -138,7 +141,7 @@ message ControllerToDaemon {
 	optional bytes container_config_signature = 18;		// signature of the container config file
 	optional bytes container_config_certificate = 19;	// sw signing certificate to verify the signature on the container config file
 	optional string newpin = 20;	// new pin for token  for CONTAINER_CHANGE_TOKEN_PIN
-	
+
 	// Daemon
 	optional bytes guestos_config_file = 30;	// new/updated GuestOS config for PUSH_GUESTOS_CONFIG
 	optional bytes guestos_config_signature = 31;	// signature of the GuestOS config file
@@ -159,6 +162,18 @@ message ContainerStartParams {
 message AssignInterfaceParams {
 	optional string iface_name = 1;
 	optional bool persistent = 2 [default = false];
+}
+
+message DeviceStats {
+	required uint64 disk_system = 1;
+	required uint64 disk_system_free = 2;
+	required uint64 disk_system_used = 3;
+	optional uint64 disk_containers = 4;
+	optional uint64 disk_containers_free = 5;
+	optional uint64 disk_containers_used = 6;
+	optional uint64 mem_total = 7;
+	optional uint64 mem_free = 8;
+	optional uint64 mem_available = 9;
 }
 
 /******************************************************************************
@@ -187,6 +202,8 @@ message DaemonToController {
 		CONTAINER_CMLD_HANDLES_PIN = 7; // -> [container_cmld_handles_pin]
 
 		RESPONSE = 13;			// -> [response]
+
+		DEVICE_STATS = 30;		// -> [device_stats]
 
 		DEVICE_CSR = 40;		// -> [device_csr]
 
@@ -234,6 +251,9 @@ message DaemonToController {
 	optional bool container_cmld_handles_pin = 11; 		// Indicate that CMLD handles pin input for container start and stop
 
 	optional Response response = 13;
+
+	optional DeviceStats device_stats = 20;		// device_stats for GET_DEVICE_STATS
+
 	optional bytes device_csr = 40;			// device_csr for DEVICE_CSR (provisioning)
 	optional bool device_is_provisioned = 41;	// device provisioned state (provisioning)
 }

daemon/control.proto
@@ -76,6 +76,9 @@ message ControllerToDaemon {
 		//Returns logfiles stored in LOGFILE_DIR
 		GET_LAST_LOG = 5;
 
+		// Retrive device statistics about mem and storage
+		GET_DEVICE_STATS = 6;
+
 		//////////////////////////////////////////////
 		// Commands (global) that modify the system //
 		//////////////////////////////////////////////
@@ -211,6 +214,18 @@ message ControllerToDaemon {
 // Outgoing messages //////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+message DeviceStats {
+	required uint64 disk_system = 1;
+	required uint64 disk_system_free = 2;
+	required uint64 disk_system_used = 3;
+	optional uint64 disk_containers = 4;
+	optional uint64 disk_containers_free = 5;
+	optional uint64 disk_containers_used = 6;
+	optional uint64 mem_total = 7;
+	optional uint64 mem_free = 8;
+	optional uint64 mem_available = 9;
+}
+
 /**
  * Control message sent from the cml-daemon on the device to the backend/cmdline tool/etc.
  */
@@ -238,6 +253,8 @@ message DaemonToController {
 		EXEC_END = 14;
 
 		EXEC_OUTPUT = 15;
+
+		DEVICE_STATS = 30;		// -> [device_stats]
 
 		DEVICE_CSR = 40;		// -> [device_csr]
 
@@ -294,6 +311,9 @@ message DaemonToController {
 	optional LogMessage log_message = 12;				// log message received because of OBSERVE_LOG_START
 
 	optional Response response = 13;
+
+	optional DeviceStats device_stats = 20;		// device_stats for GET_DEVICE_STATS
+
 	optional bytes device_csr = 40;			// device_csr for DEVICE_CSR (provisioning)
 	optional bool device_is_provisioned = 41;	// device provisioned state (provisioning)
 

commit 433dd56d1d84fee1443a2119af26f484f79bef63

daemon/control: Allow GET_DEVICE_STATS in CC mode
The protofile cc_mode/control.proto already provides the interface
definition for DEVICE_STATS message handling in CC mode. However the
command checker in the control module itself does not allow the
corresponding command. Thus add GET_DEVICE_STATS to the list there, too.

Fixes: 723c5d14d916 ("daemon/control: Provide device stats through control interface")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/control.c
@@ -801,6 +801,7 @@ control_check_command(control_t *control, const ControllerToDaemon *msg)
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__LIST_CONTAINERS) ||
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__GET_CONTAINER_STATUS) ||
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__GET_CONTAINER_CONFIG) ||
+	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__GET_DEVICE_STATS) ||
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__PUSH_GUESTOS_CONFIG) ||
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__CREATE_CONTAINER) ||
 	      (msg->command == CONTROLLER_TO_DAEMON__COMMAND__REMOVE_CONTAINER) ||
@@ -842,6 +843,7 @@ control_check_command(control_t *control, const ControllerToDaemon *msg)
 	// Device is in privileged provisioned mode, only allow subset of commands
 	if ((msg->command == CONTROLLER_TO_DAEMON__COMMAND__LIST_GUESTOS_CONFIGS) ||
 	    (msg->command == CONTROLLER_TO_DAEMON__COMMAND__LIST_CONTAINERS) ||
+	    (msg->command == CONTROLLER_TO_DAEMON__COMMAND__GET_DEVICE_STATS) ||
 	    (msg->command == CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_CHANGE_TOKEN_PIN) ||
 	    (msg->command == CONTROLLER_TO_DAEMON__COMMAND__CREATE_CONTAINER) ||
 	    (msg->command == CONTROLLER_TO_DAEMON__COMMAND__REBOOT_DEVICE) ||