gyroidos/cml

commit 2744ee01ad5559a77a95b55f7980de43c58fc28c

common/ssl_util: added helper to get uuid from a certificate/csr
Provide a new helper to get the uuid from the subject alternative name
field of a x509 certificate or csr file. This could be used to extract
the device uuid from a device certificate generated by the scd after
provisioning.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/ssl_util.c
@@ -319,6 +319,67 @@ error:
 	return -1;
 }
 
+char *
+ssl_get_uid_from_cert_new(const char *file)
+{
+	char *uuid = NULL;
+	int pos = 0;
+	FILE *fp = NULL;
+	X509 *cert = NULL;
+	X509_EXTENSION *ex;
+	BIO *ext_bio = NULL;
+	BUF_MEM *bptr = NULL;
+
+	if (!(fp = fopen(file, "r"))) {
+		ERROR("Error opening file %s", file);
+		return NULL;
+	}
+
+	if (!(cert = PEM_read_X509(fp, NULL, NULL, NULL))) {
+		ERROR("Error reading file %s", file);
+		goto error;
+	}
+
+	if (0 > (pos = X509_get_ext_by_NID(cert, NID_subject_alt_name, -1))) {
+		ERROR("Extension for UUID not found!");
+		goto error;
+	}
+
+	if (!(ex = X509_get_ext(cert, pos))) {
+		ERROR("Unable to get extension at pos=%d", pos);
+		goto error;
+	}
+
+	if (!(ext_bio = BIO_new(BIO_s_mem()))) {
+		ERROR("Unable to allocate mem for extension value BIO");
+		goto error;
+	}
+	if (!X509V3_EXT_print(ext_bio, ex, 0, 0)) {
+		ERROR("Failed to convert ASN1_STRING");
+		goto error;
+	}
+
+	BIO_get_mem_ptr(ext_bio, &bptr);
+	BIO_set_close(ext_bio, BIO_NOCLOSE); /* So BIO_free() leaves BUF_MEM bptr alone */
+
+	uuid = mem_new0(char, 36);
+
+	if (sscanf(bptr->data, "URI:UUID:%36c", uuid) != 1) {
+		ERROR("Failed parsing uuid!");
+		mem_free0(uuid);
+		goto error;
+	}
+
+error:
+	if (ext_bio)
+		BIO_free(ext_bio);
+	if (cert)
+		X509_free(cert);
+	fclose(fp);
+
+	return uuid;
+}
+
 static X509_REQ *
 ssl_mkreq(EVP_PKEY *pkeyp, const char *common_name, const char *uid, UNUSED bool tpmkey)
 {

common/ssl_util.h
@@ -56,6 +56,20 @@ ssl_create_csr(const char *req_file, const char *key_file, const char *passphras
 	       const char *common_name, const char *uid, bool tpmkey, rsa_padding_t rsa_padding);
 
 /**
+ * extracts the uid filed from the cert or csr file
+ * The ssl_create_csr() sets an extension similar to:
+ *
+ * X509v3 Subject Alternative Name:
+ *   URI:UUID:83131c82-ec1d-49ff-943b-1af411858dd0
+ *
+ * This function returns just the UUID value after URI:UUID:
+ *
+ * @return returns uuid on success, NULL in case of a failure.
+ */
+char *
+ssl_get_uid_from_cert_new(const char *file);
+
+/**
  * This function wraps a (symmetric) key plain_key of length plain_key_len into a wrapped key wrapped_key
  * of length wrapped_key_len using a public key pkey (unwrap works with the corresp. private key).
  * @return returns 0 on success, -1 in case of a failure. */

common/ssl_util.test.c
@@ -506,7 +506,7 @@ test_ssl_create_csr_pss(UNUSED const MunitParameter params[], UNUSED void *data)
 		FATAL("Could not create device uuid");
 	}
 
-	if (ssl_create_csr("testdata/munic-device.csr", "testdata/munit-private.key", NULL,
+	if (ssl_create_csr("testdata/munit-device.csr", "testdata/munit-private.key", NULL,
 			   "common_name", uid, false, RSA_PSS_PADDING) != 0) {
 		FATAL("Unable to create CSR");
 	}
@@ -517,6 +517,68 @@ test_ssl_create_csr_pss(UNUSED const MunitParameter params[], UNUSED void *data)
 	unlink("testdata/munit-private.key");
 
 	return 0;
+}
+
+static MunitResult
+test_get_uid_from_device_cert_default(UNUSED const MunitParameter params[], UNUSED void *data)
+{
+	uuid_t *dev_uuid = uuid_new(NULL);
+	munit_assert(NULL != dev_uuid);
+	const char *uid = uuid_string(dev_uuid);
+	munit_assert(NULL != uid);
+
+	munit_assert(0 == ssl_create_csr("testdata/munit-device.csr", "testdata/munit-private.key",
+					 NULL, "common_name", uid, false, RSA_SSA_PADDING));
+
+	munit_assert(0 == ssl_self_sign_csr("testdata/munit-device.csr",
+					    "testdata/munit-device.self.crt",
+					    "testdata/munit-private.key", false));
+
+	char *uuid_read = ssl_get_uid_from_cert_new("testdata/munit-device.self.crt");
+
+	munit_assert(NULL != uuid_read);
+
+	INFO("uid='%s', uuid_read='%s'", uid, uuid_read);
+	munit_assert(0 == memcmp(uid, uuid_read, strlen(uid)));
+
+	mem_free0(uuid_read);
+	uuid_free(dev_uuid);
+	unlink("testdata/munit-device.csr");
+	unlink("testdata/munit-device.self.crt");
+	unlink("testdata/munit-private.key");
+
+	return MUNIT_OK;
+}
+
+static MunitResult
+test_get_uid_from_device_cert_pss(UNUSED const MunitParameter params[], UNUSED void *data)
+{
+	uuid_t *dev_uuid = uuid_new(NULL);
+	munit_assert(NULL != dev_uuid);
+	const char *uid = uuid_string(dev_uuid);
+	munit_assert(NULL != uid);
+
+	munit_assert(0 == ssl_create_csr("testdata/munit-device.csr", "testdata/munit-private.key",
+					 NULL, "common_name", uid, false, RSA_PSS_PADDING));
+
+	munit_assert(0 == ssl_self_sign_csr("testdata/munit-device.csr",
+					    "testdata/munit-device.self.crt",
+					    "testdata/munit-private.key", false));
+
+	char *uuid_read = ssl_get_uid_from_cert_new("testdata/munit-device.self.crt");
+
+	munit_assert(NULL != uuid_read);
+
+	INFO("uid = '%s', uuid_read='%s'", uid, uuid_read);
+	munit_assert(0 == memcmp(uid, uuid_read, strlen(uid)));
+
+	mem_free0(uuid_read);
+	uuid_free(dev_uuid);
+	unlink("testdata/munit-device.csr");
+	unlink("testdata/munit-device.self.crt");
+	unlink("testdata/munit-private.key");
+
+	return MUNIT_OK;
 }
 
 static MunitResult
@@ -1303,6 +1365,10 @@ static MunitTest tests[] = {
 	  MUNIT_TEST_OPTION_NONE, NULL },
 	{ "ssl_create_csr_pss", test_ssl_create_csr_pss, setup, tear_down, MUNIT_TEST_OPTION_NONE,
 	  NULL },
+	{ "get_uid_from_device_cert_default", test_get_uid_from_device_cert_default, setup,
+	  tear_down, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "get_uid_from_device_cert_pss", test_get_uid_from_device_cert_pss, setup, tear_down,
+	  MUNIT_TEST_OPTION_NONE, NULL },
 	{ "test_ssl_read_pkcs12_token_ssa", test_ssl_read_pkcs12_token_ssa, setup, tear_down,
 	  MUNIT_TEST_OPTION_NONE, NULL },
 	{ "test_ssl_read_pkcs12_token_pss", test_ssl_read_pkcs12_token_pss, setup, tear_down,

commit 5f5ea3820173886ee36bef54791f3d7fbe96556a

common/ssl_util: Fix null terminator in ssl_get_uid_from_cert_new()
This commit fixes length of the returned UUID buffer to container 36
bytes for UUID + null terminator.

Fixes: 2744ee01ad55 ("common/ssl_util: added helper to get uuid from a certificate/csr")
Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

common/ssl_util.c
@@ -362,7 +362,8 @@ ssl_get_uid_from_cert_new(const char *file)
 	BIO_get_mem_ptr(ext_bio, &bptr);
 	BIO_set_close(ext_bio, BIO_NOCLOSE); /* So BIO_free() leaves BUF_MEM bptr alone */
 
-	uuid = mem_new0(char, 36);
+	// initialize 36 byte UUID + null terminator
+	uuid = mem_new0(char, 37);
 
 	if (sscanf(bptr->data, "URI:UUID:%36c", uuid) != 1) {
 		ERROR("Failed parsing uuid!");