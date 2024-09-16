gyroidos/cml

commit 84b9064405fec5f960d72197718a25a12298279f

dameon/c_seccomp: Moved c_seccomp.c to c_seccomp/seccomp.c
This is the beginning of restructuring the seccomp module to separate
files. The main seccomp handling should be take palce in the
'c_seccomp/seccomp.c' file while each syscall emulation code should
be moved into separate emulation specific files, e.g., mknod emulation
will be moved to c_seccomp/mknod.c.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_seccomp/seccomp.c
@@ -22,7 +22,7 @@
  */
 
 /**
- * @file c_seccomp.c
+ * @file c_seccomp/seccomp.c
  *
  * This module handles system call hooking and emulation through the seccomp notify kernel functionality.
  * It applies a seccomp filter matching relevant system calls before the container init process is executed.
@@ -35,18 +35,20 @@
 
 #define MOD_NAME "c_seccomp"
 
-#include "compartment.h"
-#include "audit.h"
+#include "../compartment.h"
+#include "../audit.h"
 
-#include "common/macro.h"
-#include "common/mem.h"
-#include "common/fd.h"
-#include "common/file.h"
-#include "common/event.h"
-#include "common/audit.h"
-#include "common/kernel.h"
-#include "common/proc.h"
-#include "common/ns.h"
+#include <common/macro.h>
+#include <common/mem.h>
+#include <common/fd.h>
+#include <common/file.h>
+#include <common/event.h>
+#include <common/audit.h>
+#include <common/kernel.h>
+#include <common/proc.h>
+#include <common/ns.h>
+
+#include "seccomp.h"
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -67,7 +69,6 @@
 
 #include <linux/capability.h>
 #include <linux/filter.h>
-#include <linux/seccomp.h>
 #include <linux/audit.h>
 #include <linux/netlink.h>
 #include <linux/module.h>
@@ -135,13 +136,13 @@
 #define MODULE_INIT_COMPRESSED_FILE 4
 #endif
 
-static int
+int
 pidfd_open(pid_t pid, unsigned int flags)
 {
 	return syscall(__NR_pidfd_open, pid, flags);
 }
 
-static int
+int
 pidfd_getfd(int pidfd, int targetfd, unsigned int flags)
 {
 	return syscall(__NR_pidfd_getfd, pidfd, targetfd, flags);
@@ -167,22 +168,12 @@ finit_module(int fd, const char *param_values, int flags)
 	return syscall(__NR_finit_module, fd, param_values, flags);
 }
 
-static int
+int
 capget(cap_user_header_t hdrp, cap_user_data_t datap)
 {
 	return syscall(__NR_capget, hdrp, datap);
 }
 /**************************/
-
-typedef struct c_seccomp {
-	compartment_t *compartment;
-	struct seccomp_notif_sizes *notif_sizes;
-	int notify_fd;
-	event_io_t *event;
-	unsigned int enabled_features;
-	container_t *container;
-	list_t *module_list; /* names of modules loaded by this compartment */
-} c_seccomp_t;
 
 static int
 c_seccomp_install_filter()
@@ -288,7 +279,7 @@ c_seccomp_is_pidfd_supported()
 	return kernel_version_check("5.10.0");
 }
 
-static bool
+bool
 c_seccomp_capable(pid_t pid, uint64_t cap)
 {
 	struct __user_cap_header_struct cap_header = { .version = _LINUX_CAPABILITY_VERSION_3,
@@ -304,31 +295,7 @@ c_seccomp_capable(pid_t pid, uint64_t cap)
 	return cap_data[CAP_TO_INDEX(cap)].effective & CAP_TO_MASK(cap);
 }
 
-static int
-c_seccomp_start_pre_exec_child_early(void *seccompp)
-{
-	ASSERT(seccompp);
-	c_seccomp_t *seccomp = seccompp;
-
-	int notify_fd = -1;
-
-	if (-1 == (notify_fd = c_seccomp_install_filter())) {
-		ERROR("Failed to install seccomp filter");
-		return -1;
-	}
-
-	DEBUG("Installed seccomp filter, sending notify fd %d to parent", notify_fd);
-
-	if (fd_write(compartment_get_sync_sock_child(seccomp->compartment), (char *)&notify_fd,
-		     sizeof(notify_fd)) < 0) {
-		WARN_ERRNO("Could not send notify fd number over sync socket");
-		return -1;
-	}
-
-	return 0;
-}
-
-static void *
+void *
 c_seccomp_fetch_vm_new(c_seccomp_t *seccomp, int pid, void *rbuf, uint64_t size)
 {
 	IF_NULL_RETVAL(rbuf, NULL);
@@ -950,6 +917,30 @@ c_seccomp_handle_notify(int fd, unsigned events, UNUSED event_io_t *io, void *da
 
 	mem_free(req);
 	mem_free(resp);
+}
+
+static int
+c_seccomp_start_pre_exec_child_early(void *seccompp)
+{
+	ASSERT(seccompp);
+	c_seccomp_t *seccomp = seccompp;
+
+	int notify_fd = -1;
+
+	if (-1 == (notify_fd = c_seccomp_install_filter())) {
+		ERROR("Failed to install seccomp filter");
+		return -1;
+	}
+
+	DEBUG("Installed seccomp filter, sending notify fd %d to parent", notify_fd);
+
+	if (fd_write(compartment_get_sync_sock_child(seccomp->compartment), (char *)&notify_fd,
+		     sizeof(notify_fd)) < 0) {
+		WARN_ERRNO("Could not send notify fd number over sync socket");
+		return -1;
+	}
+
+	return 0;
 }
 
 static int

daemon/Makefile
@@ -146,6 +146,9 @@ else
 	LOCAL_CFLAGS += -DCGROUPS_LEGACY
 endif
 
+SRC_CSECCOMP = \
+	c_seccomp/seccomp.c
+
 SRC_CMODULES += \
 	c_net.c \
 	c_vol.c \
@@ -156,7 +159,7 @@ SRC_CMODULES += \
 	c_audit.c \
 	c_cap.c \
 	c_hotplug.c \
-	c_seccomp.c
+	$(SRC_CSECCOMP)
 
 ifeq ($(AUTOMOUNT),y)
     SRC_CMODULES += c_automount.c

daemon/c_seccomp/seccomp.h
@@ -1,0 +1,59 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2013 - 2024 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+/**
+ * @file c_seccomp/seccomp.h
+ *
+ * Internal seccomp header file to define common interal structures
+ * of the c_seccomp module.
+ */
+
+#ifndef SECCOMP_H
+#define SECCOMP_H
+
+#include <common/event.h>
+#include <linux/seccomp.h>
+
+typedef struct c_seccomp {
+	compartment_t *compartment;
+	struct seccomp_notif_sizes *notif_sizes;
+	int notify_fd;
+	event_io_t *event;
+	unsigned int enabled_features;
+	container_t *container;
+	list_t *module_list; /* names of modules loaded by this compartment */
+} c_seccomp_t;
+
+bool
+c_seccomp_capable(pid_t pid, uint64_t cap);
+
+int
+pidfd_open(pid_t pid, unsigned int flags);
+
+int
+pidfd_getfd(int pidfd, int targetfd, unsigned int flags);
+
+void *
+c_seccomp_fetch_vm_new(c_seccomp_t *seccomp, int pid, void *rbuf, uint64_t size);
+
+#endif /* SECCOMP_H */

commit 123dd88f914bcbdef24af7557bf1a34455fd5e3c

daemon: added missing include linux/seccomp.h
Building the 'cmld' in buildroot results in the following compiler error:
  c_seccomp/seccomp.c:418:31: error: 'SECCOMP_USER_NOTIF_FLAG_CONTINUE' undeclared.
Just include the missing header 'linux/seccomp.h' to fix this issue.

Fixes: 84b9064405fe ("dameon/c_seccomp: Moved c_seccomp.c to c_seccomp/seccomp.c")
Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>
[ michael.weiss ] added commit description and fixes tag
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_seccomp/seccomp.c
@@ -67,6 +67,7 @@
 #include <linux/filter.h>
 #include <linux/audit.h>
 #include <linux/netlink.h>
+#include <linux/seccomp.h>
 
 //#undef LOGF_LOG_MIN_PRIO
 //#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE