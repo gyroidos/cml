gyroidos/cml

commit 526d6ccf99d30b268e1bc23ef8a1e2a9c41b5042

daemon/audit: locking of audit log file
In some cases where audit log is written in child_early hooks,
concurrent access to the audit logfile could happen. Thus,
we introduce file locks using flock() to protect access to the
audit log file.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -45,6 +45,7 @@
 
 #include <fcntl.h>
 #include <string.h>
+#include <sys/file.h>
 #include <time.h>
 #include <linux/audit.h>
 #include <inttypes.h>
@@ -220,21 +221,35 @@ audit_record_from_textfile_new(const char *filename, bool purge)
 
 	TRACE("audit record from textfile using log '%s'", filename);
 
-	FILE *file = fopen(filename, "r");
+	FILE *file = fopen(filename, purge ? "r+" : "r");
 	if (!file) {
-		WARN_ERRNO("Could not open file \"%s\" for reading.", filename);
+		WARN_ERRNO("Could not fopen file \"%s\" for reading.", filename);
 		return NULL;
 	}
 
-	ssize_t size = file_size(filename);
+	int fd = fileno(file);
+	if (fd < 0) {
+		WARN_ERRNO("Could not get fd for \"%s\" for reading.", filename);
+		fclose(file);
+		return NULL;
+	}
 
-	if (0 > size) {
+	int audit_file_lock = flock(fd, LOCK_EX);
+	if (audit_file_lock < 0) {
+		ERROR_ERRNO("Failed to get lock on audit log file: %s", filename);
+		fclose(file);
+		return NULL;
+	}
+
+	struct stat s;
+	if (fstat(fd, &s) && 0 > s.st_size) {
 		ERROR("Failed to retrieve size of audit record log '%s'", filename);
 		fclose(file);
 		return NULL;
 	}
 
 	AuditRecord *record = NULL;
+	ssize_t size = s.st_size;
 
 	// read file up to delimiter
 	char *buf = mem_alloc0(size + 1);
@@ -271,7 +286,6 @@ audit_record_from_textfile_new(const char *filename, bool purge)
 		read += current;
 		mem_free0(line);
 	}
-	fclose(file);
 
 	// parse record from file
 	if (0 == size) {
@@ -316,40 +330,41 @@ audit_record_from_textfile_new(const char *filename, bool purge)
 			goto out;
 		}
 
-		char *filebuf = file_read_new(filename, AUDIT_STORAGE);
-
-		if (!filebuf) {
-			ERROR("Failed to read audit log file");
+		rewind(file);
+		if (fread(buf, sizeof(char), size, file)) {
+			ERROR("Failed to read audit log file into memory");
 			goto out;
 		}
 
 		size_t offset = read + strlen(AUDIT_DELIMITER);
-		if (-1 == file_write(filename, filebuf + offset, strlen(filebuf + offset))) {
+		if (-1 == fd_write(fd, buf + offset, strlen(buf + offset))) {
 			ERROR_ERRNO("Failed to remove message from file: %s", filename);
 		}
-		mem_free0(filebuf);
 	}
 
 out:
 	if (buf)
 		mem_free0(buf);
 
+	if (audit_file_lock == 0 && flock(fd, LOCK_UN)) {
+		ERROR_ERRNO("Failed to release lock on audit log file: %s", filename);
+	}
+	fclose(file);
+
 	return (AuditRecord *)record;
 }
 
 static bool
-audit_log_check_delimiter(const char *log_file)
+audit_log_check_delimiter(int fd)
 {
-	int fd = -1;
 	char buf[sizeof(AUDIT_DELIMITER)];
-	size_t log_file_size = 0;
 	bool ret = false;
 
-	IF_TRUE_RETVAL_TRACE(0 >= (log_file_size = file_size(log_file)), true);
-	if (0 > (fd = open(log_file, O_RDONLY))) {
-		TRACE_ERRNO("open failed by returning fd=%d", fd);
-		return true;
-	}
+	struct stat s;
+	IF_TRUE_RETVAL(fstat(fd, &s) != 0, true);
+	IF_TRUE_RETVAL(s.st_size <= 0, true);
+
+	off_t current = lseek(fd, 0, SEEK_CUR);
 
 	IF_TRUE_GOTO_WARN(-1 == lseek(fd, -strlen(AUDIT_DELIMITER), SEEK_END), out);
 	IF_TRUE_GOTO_WARN(-1 == fd_read(fd, buf, strlen(AUDIT_DELIMITER)), out);
@@ -359,9 +374,10 @@ audit_log_check_delimiter(const char *log_file)
 	// if delimiter line was read, all is fine
 	if (!strcmp(AUDIT_DELIMITER, buf))
 		ret = true;
-
 out:
-	close(fd);
+	if (-1 == lseek(fd, current, SEEK_SET))
+		WARN_ERRNO("could not restore position!");
+
 	return ret;
 }
 
@@ -369,6 +385,8 @@ static int
 audit_write_file(const uuid_t *uuid, const AuditRecord *msg)
 {
 	int ret = -1;
+	int audit_file_lock = -1;
+	int fd = -1;
 
 	char *dir = mem_printf("%s/audit", AUDIT_LOGDIR);
 
@@ -400,27 +418,49 @@ audit_write_file(const uuid_t *uuid, const AuditRecord *msg)
 	}
 
 	TRACE("Logging audit record to file: %s", file);
-	if (!audit_log_check_delimiter(file)) {
+
+	int oflags = O_RDWR;
+	oflags |= file_exists(file) ? O_APPEND : (O_CREAT | O_TRUNC);
+
+	fd = open(file, oflags, 00600);
+	if (fd < 0) {
+		ERROR_ERRNO("Failed to open audit log file: %s", file);
+		goto out;
+	}
+
+	audit_file_lock = flock(fd, LOCK_EX);
+	if (audit_file_lock < 0) {
+		ERROR_ERRNO("Failed to get lock on audit log file: %s", file);
+		goto out;
+	}
+
+	if (!audit_log_check_delimiter(fd)) {
 		TRACE("Fixup audit delimiter to %s", file);
-		if (0 > file_write_append(file, AUDIT_DELIMITER, strlen(AUDIT_DELIMITER))) {
+		if (0 > fd_write(fd, AUDIT_DELIMITER, strlen(AUDIT_DELIMITER))) {
 			ERROR("Failed to fixup audit log file: %s", file);
 			goto out;
 		}
 	}
 
-	if (0 > file_write_append(file, msg_text, msg_len)) {
+	if (0 > fd_write(fd, msg_text, msg_len)) {
 		ERROR("Failed to log audit message to file: %s", file);
 		goto out;
 	}
 
-	if (0 > file_write_append(file, AUDIT_DELIMITER, strlen(AUDIT_DELIMITER))) {
-		ERROR("Failed to log audit message to file: %s", msg_text);
+	if (0 > fd_write(fd, AUDIT_DELIMITER, strlen(AUDIT_DELIMITER))) {
+		ERROR("Failed to log audit message %s to file: %s", msg_text, file);
 		goto out;
 	}
 
 	ret = 0;
 
 out:
+	if (audit_file_lock == 0 && flock(fd, LOCK_UN)) {
+		ERROR_ERRNO("Failed to release lock on audit log file: %s", file);
+	}
+
+	close(fd);
+
 	// ensure audit log goes to disk
 	file_syncfs(file);
 

commit 159d8a5d35fed878b0c9881dbe8a820371742e80

daemon/audit: truncate and properly seek logfile on message purge
We missed to truncate the logfile after the buffer with the purged
log message was written back to the log file. Further, missed to
rewind the file again after reading the whole file into memory before
writing. This caused logfile to grow until memory exhaustion.
Thus, add the missing second rewind().

This fixes following error trace:
 <ERROR> audit.c+154: Detected audit log overflow
 <ERROR> audit.c+416: Failed to store audit record: max. log size exceeded
 <ERROR> audit.c+656: Failed to store audit log for container 00000000-0000-0000-0000-000000000000 to file
 <DEBUG> mem.c+52: Allocating a large memory area of 17232822398 bytes
 <DEBUG> mem.c+52: Allocating a large memory area of 34465644670 bytes
 <FATAL> mem.c+54: Assertion `p' failed
 <FATAL> mem.c+54: Aborting...

Fixes: 526d6ccf99d3 ("daemon/audit: locking of audit log file")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -331,7 +331,12 @@ audit_record_from_textfile_new(const char *filename, bool purge)
 			goto out;
 		}
 
+		errno = 0;
 		rewind(file);
+		if (errno != 0) {
+			ERROR_ERRNO("Rewind for reading of file %s failed!", filename);
+			goto out;
+		}
 		size_t read_len;
 		if ((size_t)size != (read_len = fread(buf, sizeof(char), size, file))) {
 			ERROR("Failed to read audit log file into memory read %zu "
@@ -340,9 +345,20 @@ audit_record_from_textfile_new(const char *filename, bool purge)
 			goto out;
 		}
 
+		errno = 0;
+		rewind(file);
+		if (errno != 0) {
+			ERROR_ERRNO("Rewind for writing of file %s failed!", filename);
+			goto out;
+		}
 		size_t offset = read + strlen(AUDIT_DELIMITER);
-		if (-1 == fd_write(fd, buf + offset, strlen(buf + offset))) {
+		size_t write_len = read_len - offset;
+		if (-1 == fd_write(fd, buf + offset, write_len)) {
 			ERROR_ERRNO("Failed to remove message from file: %s", filename);
+			goto out;
+		}
+		if (ftruncate(fd, write_len)) {
+			ERROR_ERRNO("Failed to truncate size of file: %s", filename);
 		}
 	}
 