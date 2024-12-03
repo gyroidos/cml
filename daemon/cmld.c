gyroidos/cml

commit c122aea10436cacbd054408cf788f18870e2dfbc

daemon/c_seccomp/: Minor fixes for audit events
This commit fixes typos in audit events and a log message.

Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

daemon/cmld.c
@@ -852,7 +852,7 @@ cmld_audit_compartment_state_cb(container_t *container, container_callback_t *cb
 					uuid_string(container_get_uuid(container)), 0);
 		} else {
 			audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
-					"stop", uuid_string(container_get_uuid(container)), 0);
+					"container-stop", uuid_string(container_get_uuid(container)), 0);
 		}
 		container_unregister_observer(container, cb);
 		break;
@@ -1669,7 +1669,7 @@ cmld_container_stop(container_t *container)
 	// otherwise the container_stop command is regarded
 	// as failed
 	if (!container_is_stoppable(container)) {
-		WARN("Container is already in a stopped state. Aborting");
+		WARN("Container is not stoppable. Aborting");
 		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
 				"container-stop", uuid_string(container_get_uuid(container)), 0);
 		return -1;

daemon/c_seccomp/seccomp.c
@@ -486,7 +486,7 @@ c_seccomp_handle_notify(int fd, unsigned events, UNUSED event_io_t *io, void *da
 	}
 
 	if (-1 == seccomp_ioctl(fd, SECCOMP_IOCTL_NOTIF_SEND, resp)) {
-		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-send-respone",
+		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-send-response",
 				compartment_get_name(seccomp->compartment), 2, "errno",
 				strerror(errno));
 		ERROR_ERRNO("Failed to send seccomp notify response");

commit 78e3fd39bce55b38c921b14544505446dfe71e00

daemon/cmld: fixes formatting in cmld_audit_compartment_state_cb
Commit c122aea10436 ("daemon/c_seccomp/: Minor fixes for audit
events") introduced fixes for some log messages. However, it also
introduced a formatting issue in cmld_audit_compartment_state_cb().
This is fixed now.

Fixes: c122aea10436 ("daemon/c_seccomp/: Minor fixes for audit events")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -852,7 +852,8 @@ cmld_audit_compartment_state_cb(container_t *container, container_callback_t *cb
 					uuid_string(container_get_uuid(container)), 0);
 		} else {
 			audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
-					"container-stop", uuid_string(container_get_uuid(container)), 0);
+					"container-stop",
+					uuid_string(container_get_uuid(container)), 0);
 		}
 		container_unregister_observer(container, cb);
 		break;