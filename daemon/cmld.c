gyroidos/cml

commit c13ef819d042e90e44c1677b0058d7f523e41a43

daemon/cmld: cleanup observer handling
Do some cleanup of observer registration code, especially the
handling of cmld_container_config_sync_cb.
The config sync callback is registered at config load operation.

The registration in cmld_init_c0() is not necessary nor does it
make sense if no c0 config is provided and is removed.
Further, it was registered twice in the cmld_config_reload() and by
the callback itslefe since the sync callback reregisters itself on
the new created container object. Thus, the registration in
cmld_config_reaload is removed too.

Additionally some missleading debug prints about observers were
fixed.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -100,6 +100,8 @@
 #define DUMMY_KEY                                                                                  \
 	"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
+#define CMLD_C0_UUID "00000000-0000-0000-0000-000000000000"
+
 static const char *cmld_path = DEFAULT_BASE_PATH;
 static const char *cmld_container_path = NULL;
 static const char *cmld_wrapped_keys_path = NULL;
@@ -142,7 +144,7 @@ cmld_start_c0(container_t *new_c0);
 container_t *
 cmld_containers_get_c0()
 {
-	uuid_t *c0_uuid = uuid_new("00000000-0000-0000-0000-000000000000");
+	uuid_t *c0_uuid = uuid_new(CMLD_C0_UUID);
 	container_t *container = cmld_container_get_by_uuid(c0_uuid);
 	uuid_free(c0_uuid);
 	return container;
@@ -582,31 +584,29 @@ cmld_containers_add(container_t *container)
 static void
 cmld_container_config_sync_cb(container_t *container, container_callback_t *cb, UNUSED void *data)
 {
-	IF_FALSE_RETURN_ERROR(container_get_state(container) == COMPARTMENT_STATE_REBOOTING ||
-			      container_get_state(container) == COMPARTMENT_STATE_STOPPED);
+	IF_FALSE_RETURN(container_get_state(container) == COMPARTMENT_STATE_REBOOTING ||
+			container_get_state(container) == COMPARTMENT_STATE_STOPPED);
 
-	container_t *c = NULL;
-
-	if (!container_get_sync_state(container)) {
-		container_unregister_observer(container, cb);
-		DEBUG("Container is out of sync with its config. Reloading..");
-		if (!(c = cmld_reload_container(container_get_uuid(container),
-						cmld_get_containers_dir()))) {
-			ERROR("Failed to reload container on config update");
-		} else {
-			/* register observer on new container object */
-			if (!container_register_observer(c, &cmld_container_config_sync_cb, NULL)) {
-				WARN("Could not register container config sync observer callback for %s",
-				     container_get_description(c));
-				audit_log_event(container_get_uuid(c), FSA, CMLD, CONTAINER_MGMT,
-						"c0-reload", uuid_string(container_get_uuid(c)), 0);
-			}
-			WARN("Could not register container config sync observer callback for %s",
-			     container_get_description(c));
-		}
-	} else {
+	if (container_get_sync_state(container)) {
 		DEBUG("Container %s already in sync", container_get_description(container));
+		return;
 	}
+
+	// in error case reload may destroy container object thus dup uuid
+	uuid_t *c_uuid = uuid_new(uuid_string(container_get_uuid(container)));
+
+	container_unregister_observer(container, cb);
+	DEBUG("Container is out of sync with its config. Reloading..");
+
+	if (!cmld_reload_container(c_uuid, cmld_get_containers_dir())) {
+		ERROR("Failed to reload container on config update");
+		audit_log_event(c_uuid, FSA, CMLD, CONTAINER_MGMT, "reload", uuid_string(c_uuid),
+				0);
+		if (strncmp(uuid_string(c_uuid), CMLD_C0_UUID, strlen(CMLD_C0_UUID)))
+			FATAL("Could not reload C0!");
+	}
+
+	mem_free0(c_uuid);
 }
 
 container_t *
@@ -615,37 +615,44 @@ cmld_reload_container(const uuid_t *uuid, const char *path)
 	ASSERT(uuid);
 	ASSERT(path);
 
-	container_t *c = NULL;
+	container_t *c = NULL, *c_current = NULL;
 
 	// Will be destroyed on container_free
 	uuid_t *uuid_tmp = uuid_new(uuid_string(uuid));
 
-	c = cmld_container_get_by_uuid(uuid);
-	if (c) {
-		compartment_state_t state = container_get_state(c);
+	c_current = cmld_container_get_by_uuid(uuid);
+	if (c_current) {
+		compartment_state_t state = container_get_state(c_current);
 		if (state != COMPARTMENT_STATE_STOPPED) {
 			DEBUG("Refusing to reload already created and not stopped container %s.",
-			      container_get_name(c));
+			      container_get_name(c_current));
+			c = c_current;
 			goto cleanup;
 		}
-		DEBUG("Removing outdated created container %s for config update",
-		      container_get_name(c));
-
-		cmld_containers_list = list_remove(cmld_containers_list, c);
-		container_free(c);
 	}
 	c = cmld_container_new(path, uuid_tmp, NULL, 0, NULL, 0, NULL, 0);
 	if (!c) {
 		WARN("Could not create new container object");
+		c = c_current;
 		goto cleanup;
+	}
+
+	if (c_current) {
+		DEBUG("Removing outdated created container %s for config update",
+		      container_get_name(c_current));
+
+		cmld_containers_list = list_remove(cmld_containers_list, c_current);
+		container_free(c_current);
 	}
 
 	DEBUG("Loaded config for container %s", container_get_name(c));
 	cmld_containers_list = list_append(cmld_containers_list, c);
 
-	/* register observer for automatic config reload again
-	   CAUTION: This callback destroys the previous container object.
-	            It must be the last observer in the observer list */
+	/*
+	 * register observer for automatic config reload again
+	 * CAUTION: This callback destroys the previous container object.
+	 *          It must be the last observer in the observer list
+	 */
 	if (!container_register_observer(c, &cmld_container_config_sync_cb, NULL)) {
 		WARN("Could not register container config sync observer callback for %s",
 		     container_get_description(c));
@@ -908,9 +915,9 @@ cmld_container_register_observers(container_t *container)
 			     container_get_description(container));
 		}
 	}
-	/* register audit sync observer */
+	/* register an observer for auditing compartment state */
 	if (!container_register_observer(container, &cmld_audit_compartment_state_cb, NULL)) {
-		WARN("Could not register container audit sync observer callback for %s",
+		WARN("Could not register container audit state observer callback for %s",
 		     container_get_description(container));
 		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
 				"container-observer-error",
@@ -1280,16 +1287,6 @@ cmld_init_c0(const char *path, const char *c0os)
 	/* store c0 as first element of the cmld_containers_list */
 	cmld_containers_list = list_prepend(cmld_containers_list, new_c0);
 
-	/* register an observer for automatic config reload of c0
-	   CAUTION: This callback destroys the previous container object.
-	            It must be the last observer in the observer list */
-	if (!container_register_observer(new_c0, &cmld_container_config_sync_cb, NULL)) {
-		WARN("Could not register container config sync observer callback for c0");
-		audit_log_event(container_get_uuid(new_c0), FSA, CMLD, CONTAINER_MGMT, "c0-start",
-				uuid_string(container_get_uuid(new_c0)), 0);
-		return -1;
-	}
-
 	mem_free0(c0_images_folder);
 
 out:
@@ -1343,9 +1340,9 @@ cmld_start_c0(container_t *new_c0)
 				uuid_string(container_get_uuid(new_c0)), 0);
 		return -1;
 	}
-	/* register an observer for automatic config reload */
+	/* register an observer for auditing compartment state */
 	if (!container_register_observer(new_c0, &cmld_audit_compartment_state_cb, NULL)) {
-		WARN("Could not register container audit sync observer callback for %s",
+		WARN("Could not register container audit state observer callback for %s",
 		     container_get_description(new_c0));
 		audit_log_event(container_get_uuid(new_c0), FSA, CMLD, CONTAINER_MGMT, "c0-start",
 				uuid_string(container_get_uuid(new_c0)), 0);
@@ -1584,9 +1581,11 @@ cmld_container_create_from_config(const uint8_t *config, size_t config_len, uint
 		cmld_container_new(path, NULL, config, config_len, sig, sig_len, cert, cert_len);
 	if (c) {
 		cmld_containers_list = list_append(cmld_containers_list, c);
-		/* register an observer for automatic config reload
-	       CAUTION: This callback destroys the previous container object.
-	                It must be the last observer in the observer list */
+		/*
+		 * register an observer for automatic config reload
+		 * CAUTION: This callback destroys the previous container object.
+		 *          It must be the last observer in the observer list
+		 */
 		if (!container_register_observer(c, &cmld_container_config_sync_cb, NULL)) {
 			WARN("Could not register container config sync observer callback for %s",
 			     container_get_description(c));

commit 46d0db234cad938c52b8bf12acda9c98a6f736d4

daemon/cmld: reattach token on container reload
On reload the new container object is created with container_new()
and afterwards the previous object is deleted by a call to
container_free(). Makes sens to first try to create the new updated
container object and only if this succeeds, the old object is
destroyed. However on container_new() the token is attached to the
scd and on container_free() it is removed:

Thus we now have the following behavior:

1) We have the token attached in the scd. The config updated kicks
   in and we get a new container object created by container_new().
   A second attach is made in the scd:
      <INFO>  control.c+221: Token already exists.

2) That is ok, we just ignore that in the scd and the new updated
   container object is created successfully.

3) However afterwards the container_free(<outdated container object>)
   is made and also the scd resources are released by an internal
   call to usbtoken_free() in scd:
      <DEBUG> usbtoken.c+603: Closing CT interface (ctn=0) done.

4) Since both container objects map to the same token in the scd, we
   now have no token attached in the scd anymore.

5) An attempt to do anything token related in the cmld now results
   in that the token is not available:
      <ERROR> control.c+254: No token loaded, unlock failed

Simply fix this by reattaching the token after the container_free()
of the outdated container.

Fixes: c13ef819d042 ("daemon/cmld: cleanup observer handling")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -620,6 +620,8 @@ cmld_reload_container(const uuid_t *uuid, const char *path)
 
 		cmld_containers_list = list_remove(cmld_containers_list, c_current);
 		container_free(c_current);
+		// container_free() releases the token in scd, thus reattach it
+		container_token_attach(c);
 	}
 
 	DEBUG("Loaded config for container %s", container_get_name(c));