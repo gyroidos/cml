gyroidos/cml

commit 88f0970f41dc932f9d5e46fcd4a4a7ca0fc69a0b

daemon/c_user: avoid shifting out of range

daemon/c_user.c
@@ -40,8 +40,9 @@
 #include "common/dir.h"
 #include "container.h"
 
-#define UID_RANGE 10000
-#define UID_RANGES_START 30000
+#define UID_RANGE 100000
+#define UID_RANGES_START 100000
+#define UID_MAX 65535
 #define MAX_UID_RANGES ((int)((UINT_MAX - UID_RANGES_START) / UID_RANGE))
 
 /* Paths for controling mappings */
@@ -160,7 +161,7 @@ c_user_setup_mapping(const c_user_t *user)
 {
 	ASSERT(user);
 
-	char *uid_mapping = mem_printf(C_USER_MAP_FORMAT, 0, user->uid_start, UID_RANGE - 1);
+	char *uid_mapping = mem_printf(C_USER_MAP_FORMAT, 0, user->uid_start, UID_MAX);
 	INFO("mapping: '%s'", uid_mapping);
 
 	char *uid_map_path = mem_printf(C_USER_UID_MAP_PATH, container_get_pid(user->container));
@@ -261,6 +262,14 @@ c_user_chown_dev_cb(const char *path, const char *file, void *data)
 
 	uid_t uid = s.st_uid + user->uid_start;
 	gid_t gid = s.st_gid + user->uid_start;
+
+	// avoid shifting twice
+	uid_t uid_overflow = user->uid_start + UID_MAX;
+	if (uid > uid_overflow) {
+		mem_free(file_to_chown);
+		return 0;
+	}
+		
 
 	if (file_is_dir(file_to_chown)) {
 		TRACE("Path %s is dir", file_to_chown);

commit 6c86c6e972ca6bbba0d1dcfbe8a05608e7dacc6b

daemon/c_user,cmld: Increase and refactor UID_MAX
Refactor UID_MAX such that it is only defined in one location.
Set value to 65536 to assure that also 65535 is mapped as id.

Fixes: 88f0970f41dc ("daemon/c_user: avoid shifting out of range")
Signed-off-by: Maximilian Peisl <maximilian.peisl@aisec.fraunhofer.de>
[ michael.weiss ] formatted commit message + fixes tag
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_user.c
@@ -47,7 +47,7 @@
 
 #define UID_RANGE 100000
 #define UID_RANGES_START 100000
-#define UID_MAX 65535
+
 #define MAX_UID_RANGES ((int)((UINT_MAX - UID_RANGES_START) / UID_RANGE))
 
 /* Paths for controling mappings */

daemon/cmld.c
@@ -185,7 +185,6 @@ cmld_container_get_by_uuid(const uuid_t *uuid)
 	return NULL;
 }
 
-#define UID_MAX 65535
 container_t *
 cmld_container_get_by_uid(int uid)
 {

daemon/cmld.h
@@ -52,6 +52,8 @@
 
 #define CMLD_STORAGE_FREE_THRESHOLD 0.2 // 20% reserved space
 
+#define UID_MAX 65536
+
 /**
  * Enum represents different commands to control a container
  */