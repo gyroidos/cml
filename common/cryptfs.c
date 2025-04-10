gyroidos/cml

commit 79fc87debabab08be68a206174cc1a07bdbf07f6

common/cryptfs: switched authenticated encryption scheme to hmac,xts
Use AES256-XTS (512 bit key size) and HMAC(SHA256). Thus, we needed to
increase the key size to 96 byte in smartcard subsystem as well.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/cryptfs.c
@@ -56,8 +56,8 @@
 
 #define TABLE_LOAD_RETRIES 10
 #define INTEGRITY_TAG_SIZE 32
-#define CRYPTO_TYPE "capi:aegis128-random"
-#define CRYPTO_TYPE_LEGACY "aes-xts-plain64"
+#define CRYPTO_TYPE_AUTHENC "capi:authenc(hmac(sha256),xts(aes))-random"
+#define CRYPTO_TYPE "aes-xts-plain64"
 
 /* taken from vold */
 #define DEVMAPPER_BUFFER_SIZE 4096
@@ -232,7 +232,7 @@ load_crypto_mapping_table(int fd, const char *real_blk_name, const char *master_
 	char *extra_params = integrity ? mem_printf("1 integrity:%d:aead", INTEGRITY_TAG_SIZE) :
 					 mem_printf("1 allow_discards");
 
-	const char *crypto_type = integrity ? CRYPTO_TYPE : CRYPTO_TYPE_LEGACY;
+	const char *crypto_type = integrity ? CRYPTO_TYPE_AUTHENC : CRYPTO_TYPE;
 
 	int i;
 	int ioctl_ret;
@@ -582,11 +582,7 @@ cryptfs_setup_volume_integrity_new(const char *label, const char *real_blkdev,
 		DEBUG("Successfully created device node");
 	}
 
-	/* Use only the first 32 hex digits of master key for 128 bit aead modes */
-	char aead_key[33];
-	snprintf(aead_key, 33, "%s", key);
-
-	if (create_crypto_blk_dev(integrity_dev, aead_key, label, fs_size, true) < 0) {
+	if (create_crypto_blk_dev(integrity_dev, key, label, fs_size, true) < 0) {
 		ERROR("Could not create crypto block device");
 		return NULL;
 	}
@@ -655,7 +651,13 @@ cryptfs_setup_volume_new(const char *label, const char *real_blkdev, const char 
 							  fs_size);
 
 	// do dmcrypt device setup only
-	if (create_crypto_blk_dev(real_blkdev, key, label, fs_size, false) < 0)
+
+	/* Use only the first 64 hex digits of master key for 512 bit xts mode */
+	IF_FALSE_RETVAL(strlen(key) < 65, NULL);
+	char enc_key[65];
+	snprintf(enc_key, 65, "%s", key);
+
+	if (create_crypto_blk_dev(real_blkdev, enc_key, label, fs_size, false) < 0)
 		return NULL;
 
 	return create_device_node(label);

daemon/smartcard.c
@@ -51,7 +51,7 @@
 // clang-format on
 
 // TODO: centrally define key length in container or other module?
-#define TOKEN_KEY_LEN 64 // actual encryption key
+#define TOKEN_KEY_LEN 96 // actual encryption key + hmac key
 #define TOKEN_MAX_WRAPPED_KEY_LEN 4096
 
 #define MAX_PAIR_SEC_LEN 8

commit c3f11899c2b3d04383beaa35f3eb0dd7a359d244

common/cryptfs: Fixed key length truncation for crypt only device
The crypto key was truncated to 64 byte key length in hex representation.
This would result in a 256bit aes-xts key which in in the end results
in an AES 128 bit key for the actual en- and decryption.
Do not use the CRYPTFS_FDE_KEY_LEN define which is the length of the key,
but the newly introduced CRYPTO_HEXKEY_LEN which is twice the size for
the ascii key.

Fixes: 79fc87debabab ("common/cryptfs: switched authenticated encryption scheme to hmac,xts")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/cryptfs.c
@@ -55,6 +55,8 @@
 #define CRYPTO_TYPE_AUTHENC "capi:authenc(hmac(sha256),xts(aes))-random"
 #define CRYPTO_TYPE "aes-xts-plain64"
 
+#define CRYPTO_HEXKEY_LEN 2 * CRYPTFS_FDE_KEY_LEN
+
 /* taken from vold */
 #define DEVMAPPER_BUFFER_SIZE 4096
 #define DM_CRYPT_BUF_SIZE 4096
@@ -666,11 +668,11 @@ cryptfs_setup_volume_new(const char *label, const char *real_blkdev, const char 
 
 	// do dmcrypt device setup only
 
-	/* Use only the first 64 hex digits of master key for 512 bit xts mode */
-	IF_TRUE_RETVAL(strlen(key) < CRYPTFS_FDE_KEY_LEN, NULL);
-	char enc_key[CRYPTFS_FDE_KEY_LEN + 1];
-	memcpy(enc_key, key, CRYPTFS_FDE_KEY_LEN);
-	enc_key[CRYPTFS_FDE_KEY_LEN] = '\0';
+	/* Use the first 128 hex digits (64 byte) of master key for 512 bit xts mode */
+	IF_TRUE_RETVAL(strlen(key) < CRYPTO_HEXKEY_LEN, NULL);
+	char enc_key[CRYPTO_HEXKEY_LEN + 1];
+	memcpy(enc_key, key, CRYPTO_HEXKEY_LEN);
+	enc_key[CRYPTO_HEXKEY_LEN] = '\0';
 
 	if (create_crypto_blk_dev(real_blkdev, enc_key, label, fs_size, false) < 0)
 		return NULL;