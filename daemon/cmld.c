gyroidos/cml

commit 08b1ef393aa7757c8db08bfaa500b71414ade8c9

make token container specific and enable usage of usb token
cmld:
    - enable creation of a random pairing secret and persisting it
    - make token container specific i.e. every container is associated
      to one token

scd:
   - add support for usb token
   - add support to wrap container disk encryption keys with symmetric
     keys extracted from a usb token

control:
   - make change_pin command container specific

TODO:
   - evaluate if explicit commands to create/free scd internal token
     structures make the overall user interaction clearer
   - secure pairing_secret inside TPM

daemon/cmld.c
@@ -660,6 +660,18 @@ cmld_container_start(container_t *container, const char *key)
 }
 
 int
+cmld_container_change_token_pin(control_t *control, container_t *container, const char *passwd,
+				const char *newpasswd)
+{
+	ASSERT(container);
+	ASSERT(passwd);
+	ASSERT(newpasswd);
+
+	return smartcard_change_container_pin(cmld_smartcard, control, container, passwd,
+					      newpasswd);
+}
+
+int
 cmld_container_start_with_smartcard(control_t *control, container_t *container, const char *passwd)
 {
 	ASSERT(container);
@@ -667,12 +679,6 @@ cmld_container_start_with_smartcard(control_t *control, container_t *container, 
 	ASSERT(passwd);
 
 	return smartcard_container_start_handler(cmld_smartcard, control, container, passwd);
-}
-
-int
-cmld_change_device_pin(control_t *control, const char *passwd, const char *newpasswd)
-{
-	return smartcard_change_pin(cmld_smartcard, control, passwd, newpasswd);
 }
 
 void
@@ -1059,6 +1065,11 @@ cmld_init(const char *path)
 	char *tokens_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
 	cmld_smartcard = smartcard_new(tokens_path);
 	mem_free(tokens_path);
+
+	char *keys_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
+	if (mkdir(containers_path, 0700) < 0 && errno != EEXIST)
+		FATAL_ERRNO("Could not mkdir container keys directory %s", containers_path);
+	mem_free(keys_path);
 
 	if (cmld_smartcard == NULL)
 		FATAL("Could not connect to smartcard daemon");

common/mem.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of trust|me
- * Copyright(c) 2013 - 2017 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -80,6 +80,16 @@ mem_strndup(const char *str, size_t len)
 	DEBUG_THRESHOLD(len);
 	char *p = strndup(str, len);
 	ASSERT(p);
+	return p;
+}
+
+unsigned char *
+mem_memcpy(const unsigned char *mem, size_t size)
+{
+	ASSERT(mem);
+	unsigned char *p = mem_alloc0(size);
+	ASSERT(p);
+	memcpy(p, mem, size);
 	return p;
 }
 

common/mem.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of trust|me
- * Copyright(c) 2013 - 2017 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -87,6 +87,18 @@ mem_strdup(const char *str);
  */
 char *
 mem_strndup(const char *str, size_t len);
+
+/**
+ * Duplicates an array of unsigned char, but copies at most size bytes.
+ * Allocates sufficient memory.
+ * This is a wrapper for memcpy(3).
+ *
+ * @param mem The memory to duplicate.
+ * @param size The size of the memory.
+ * @return Pointer to the new array.
+ */
+unsigned char *
+mem_memcpy(const unsigned char *mem, size_t size);
 
 /**
  * Prints to a string allocated by this function.

common/mem.test.c
@@ -160,6 +160,38 @@ test_strdup_strndup(UNUSED const MunitParameter params[], UNUSED void *data)
 }
 
 static MunitResult
+test_mem_memcpy(UNUSED const MunitParameter params[], UNUSED void *data)
+{
+	unsigned char a[257];
+	munit_rand_memory(a, sizeof(a));
+
+	// memcpy duplicates memory
+	unsigned char *b = mem_memcpy(a, sizeof(a));
+	munit_assert_not_null(b);
+	munit_assert_memory_equal(sizeof(a), a, b);
+
+	// b is in a different memory chunk than a and can be modified
+	b[8] = ~b[8];
+	munit_assert_memory_not_equal(sizeof(a), a, b);
+	mem_free(b);
+
+	// memcpy copies entire memory
+	unsigned char *c = mem_memcpy(a, sizeof(a));
+	munit_assert_not_null(c);
+	munit_assert_memory_equal(sizeof(a), a, c);
+	mem_free(c);
+
+	// memcpy cuts
+	c = mem_memcpy(a, 129);
+	munit_assert_not_null(c);
+	munit_assert_memory_not_equal(129, a, c);
+	munit_assert_memory_equal(129, a, c);
+	mem_free(c);
+
+	return MUNIT_OK;
+}
+
+static MunitResult
 test_printf_dynamic_buffer(UNUSED const MunitParameter params[], UNUSED void *data)
 {
 	struct complex_t *ptr = mem_new0(struct complex_t, 1);
@@ -300,6 +332,14 @@ static MunitTest tests[] = {
 	{
 		"/strdup and strndup",	/* name */
 		test_strdup_strndup,	/* test */
+		setup,			/* setup */
+		tear_down,		/* tear_down */
+		MUNIT_TEST_OPTION_NONE, /* options */
+		NULL			/* parameters */
+	},
+	{
+		"/memcpy",		/* name */
+		test_mem_memcpy,	/* test */
 		setup,			/* setup */
 		tear_down,		/* tear_down */
 		MUNIT_TEST_OPTION_NONE, /* options */

control/control.c
@@ -82,7 +82,7 @@ print_usage(const char *cmd)
 	printf("   ca_register <ca.cert>\n        Registers a new certificate in trusted CA store for allowed GuestOS signatures.\n");
 	printf("   pull_csr <device.csr>\n        Pulls the device csr and stores it in <device.csr>.\n");
 	printf("   push_cert <device.cert>\n        Pushes back the device certificate provided by <device.cert>.\n");
-	printf("   change_pin\n        Change device token's pin which is used for container key wrapping. Prompts for password entry.\n");
+	printf("   change_pin\n        Change token pin which is used for container key wrapping. Prompts for password entry.\n");
 	printf("   assign_iface --iface <iface_name> <container-uuid> [--persistent]\n        Assign the specified network interface to the specified container. If the 'persistent' option is set, the container config file will be modified accordingly.\n");
 	printf("   unassign_iface --iface <iface_name> <container-uuid> [--persistent]\n        Unassign the specified network interface from the specified container. If the 'persistent' option is set, the container config file will be modified accordingly.\n");
 	printf("   ifaces <container-uuid>\n        Prints the list of network interfaces assigned to the specified container.\n");
@@ -366,20 +366,6 @@ main(int argc, char *argv[])
 		msg.device_cert.data = dev_cert;
 		goto send_message;
 	}
-	if (!strcasecmp(command, "change_pin")) {
-		char *newpin_verify = NULL;
-		has_response = true;
-		msg.command = CONTROLLER_TO_DAEMON__COMMAND__CHANGE_DEVICE_PIN;
-		msg.device_pin = get_password_new("Current Password: ");
-		msg.device_newpin = get_password_new("New Password: ");
-		newpin_verify = get_password_new("Re-enter New Password: ");
-
-		if (strcmp(msg.device_newpin, newpin_verify) != 0)
-			FATAL("Passwords don't match!");
-
-		mem_free(newpin_verify);
-		goto send_message;
-	}
 	if (!strcasecmp(command, "create")) {
 		has_response = true;
 		// need exactly one more argument (container config file)
@@ -571,6 +557,19 @@ main(int argc, char *argv[])
 		TRACE("[CLIENT] Done parsing arguments, got %d argsuments", argcount);
 		msg.n_exec_args = argcount;
 		TRACE("after set n_exec_args");
+
+	} else if (!strcasecmp(command, "change_pin")) {
+		char *newpin_verify = NULL;
+		has_response = true;
+		msg.command = CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_CHANGE_TOKEN_PIN;
+		msg.device_pin = get_password_new("Current Password: ");
+		msg.device_newpin = get_password_new("New Password: ");
+		newpin_verify = get_password_new("Re-enter New Password: ");
+
+		if (strcmp(msg.device_newpin, newpin_verify) != 0)
+			FATAL("Passwords don't match!");
+
+		mem_free(newpin_verify);
 	} else
 		print_usage(argv[0]);
 

daemon/cmld.h
@@ -200,16 +200,17 @@ const char *
 cmld_get_c0os(void);
 
 /**
- * Change the pin of the device token.
- * The request is sent asynchronously through lower communication layer.
+ * Change the pin of the token associated to a container.
  *
  * @parma control link to control for responses
+ * @param container container whose token's pin should be changed
  * @param passwd old passphrase/pin
  * @param newpasswd new passphrase/pin which is to be set
  * @return 0 on message delivered to lower levels, -1 message delivery failed
  */
 int
-cmld_change_device_pin(control_t *control, const char *passwd, const char *newpasswd);
+cmld_container_change_token_pin(control_t *control, container_t *container, const char *passwd,
+				const char *newpasswd);
 
 /**
  * Change the device cert during provisioning.

daemon/container.c
@@ -293,6 +293,13 @@ container_new_internal(const uuid_t *uuid, const char *name, container_type_t ty
 
 	container->images_dir = mem_strdup(images_dir);
 
+	/* create image_dir already so we can store a pseudo file as a
+	 * marker that the token aossicated with the container has been initialized
+	 */
+	if (mkdir(images_dir, 0755) < 0 && errno != EEXIST) {
+		ERROR_ERRNO("Cound not mkdir container directory %s", images_dir);
+	}
+
 	container->color = color;
 
 	container->allow_autostart = allow_autostart;
@@ -440,7 +447,7 @@ container_uuid_is_c0id(const uuid_t *uuid)
 }
 
 /**
- * Creates a new container container object. There are three different cases
+ * Creates a new container object. There are three different cases
  * depending on the combination of the given parameters:
  *
  * uuid && !config: In this case, a container with the given UUID must be already
@@ -2429,4 +2436,44 @@ container_get_uid(const container_t *container)
 {
 	ASSERT(container);
 	return c_user_get_uid(container->user);
+}
+
+container_token_type_t
+container_get_token_type(const container_t *container)
+{
+	ASSERT(container);
+
+	container_token_type_t ret;
+	container_config_t *conf = container_config_new(container->config_filename, NULL, 0);
+	ret = container_config_get_token_type(conf);
+	container_config_free(conf);
+	return ret;
+}
+
+char *
+container_get_usbtoken_serial(const container_t *container)
+{
+	ASSERT(container);
+
+	container_token_type_t tt;
+
+	tt = container_get_token_type(container);
+
+	if (tt != CONTAINER_TOKEN_TYPE_USB) {
+		ERROR("The container is not configured to use a usb token");
+		return NULL;
+	}
+
+	if (container->usbdev_list == NULL)
+		ERROR("No usbdev_list in container config");
+
+	for (list_t *l = container->usbdev_list; l; l = l->next) {
+		uevent_usbdev_t *ud = (uevent_usbdev_t *)l->data;
+		if (uevent_usbdev_get_type(ud) == UEVENT_USBDEV_TYPE_TOKEN) {
+			return (uevent_usbdev_get_i_serial(ud));
+		}
+	}
+
+	ERROR("Could not find usbdev with type \"TOKEN\"");
+	return NULL;
 }

daemon/container.h
@@ -70,6 +70,16 @@ typedef enum {
 } container_type_t;
 
 /**
+ * Represents the type of token which the container is associated with.
+ * The token is used to i.a. wrap the container's disk encryption key.
+ */
+typedef enum {
+	CONTAINER_TOKEN_TYPE_NONE = 1,
+	CONTAINER_TOKEN_TYPE_DEVICE,
+	CONTAINER_TOKEN_TYPE_USB,
+} container_token_type_t;
+
+/**
  * Structure to define the configuration for a virtual network
  * interface in a container. It defines the name and if cmld
  * should configure it in its c_net submodule.
@@ -790,5 +800,17 @@ container_get_rootdir(const container_t *container);
  */
 bool
 container_uuid_is_c0id(const uuid_t *uuid);
+
+/**
+ * Returns the type of the token that is used with the container
+ */
+container_token_type_t
+container_get_token_type(const container_t *container);
+
+/**
+ * Returns the i_serial number of the usb device that is the token reader
+ */
+char *
+container_get_usbtoken_serial(const container_t *container);
 
 #endif /* CONTAINER_H */

commit afb0842147edc76058f500d3fef0e92a682f2899

daemon/cmld: fix usage of correct variable to create keys dir
The 'container_path' variable was used as parameter for mkdir() of
the keys subdirectory. Use 'cmld_wrapped_keys_path' instead.

Fixes: 08b1ef393aa7 ("make token container specific and enable usage of usb token")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -1599,7 +1599,7 @@ cmld_init(const char *path)
 		FATAL_ERRNO("Could not mkdir containers directory %s", containers_path);
 
 	cmld_wrapped_keys_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
-	if (mkdir(containers_path, 0700) < 0 && errno != EEXIST)
+	if (mkdir(cmld_wrapped_keys_path, 0700) < 0 && errno != EEXIST)
 		FATAL_ERRNO("Could not mkdir container keys directory %s", containers_path);
 
 	if (cmld_init_c0(containers_path, device_config_get_c0os(device_config)) < 0)