c76bb412e47be9a703a9c218a5191448f8b9b403
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2025-10-07 17:19:22 +0200

    daemon/unit: added new u_perm submodule
    The u_perm submodule implements a reduced "/dev" inside units.
    Further, it provides API to allow and deny additional devices by
    a mknod wrapper unit_device_allow() and unit_device_deny(). This
    could later be used to allow, e.g., '/dev/tpm0' and several USB
    token devices to units.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

---
+++ daemon/u_perm.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "unit.h"
+#include "common/macro.h"
+
+typedef struct u_perm u_perm_t;
+
+#ifdef UNIT_MODULE_PERM
+int
+u_perm_allow_dev(u_perm_t *perm, char type, int major, int minor, const char *name);
+
+int
+u_perm_deny_dev(u_perm_t *perm, const char *name);
+#else
+inline int
+u_perm_allow_dev(UNUSED u_perm_t *perm, UNUSED char type, UNUSED int major, UNUSED int minor,
+		 UNUSED const char *name)
+{
+	return 0;
+}
+
+inline int
+u_perm_deny_dev(UNUSED u_perm_t *perm, UNUSED const char *name)
+{
+	return 0;
+}
+#endif /* UNIT_MODULE_PERM */
