a6fb017e12da659f2347858c6923df48ac709293
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2025-10-09 19:57:17 +0200

    daemon/u_user: mount tmpfs to socket inside unit
    Mount a tmpfs to "/run/socket/cml_unit/<unit_name>/" in cmld's mount
    namespace. This is move-mounted inside the unit to "/run/socket".
    The corresponding socket dir name can be retrieved by the unit
    submodule through API exposed by new 'u_user.h' header file.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

---
+++ daemon/u_user.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "unit.h"
+
+typedef struct u_user u_user_t;
+
+const char *
+u_user_get_sock_dir(const u_user_t *user);
