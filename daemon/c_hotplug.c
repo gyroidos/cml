81bb71d4864396d1bce374abe7aa29f8e965e569
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2025-10-17 20:53:28 +0200

    daemon/c_hotplug: initially allow usbtoken devce in scd unit
    If the token is already plugged at boot the corresponding device
    node has to be created initially in the scd_unit, too. Thus, not
    only on plug events we also allow access to the scd_unit initially
    in c_hotplug_new().
    
    Further, we have to ensure that the initial allow of the hotplug
    module is performed before the token is added to the scd by the
    c_smartcard module in c_smartcard_new(). Therefore, we have to
    reorder the module loading in the Makefile by putting c_hotplug.c
    before c_smartcard.c.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/c_hotplug.c
+++ daemon/c_hotplug.c
@@ -522,6 +522,22 @@ c_hotplug_new(compartment_t *compartment)
 		container_usbdev_t *usbdev = l->data;
 		if (container_usbdev_get_type(usbdev) == CONTAINER_USBDEV_TYPE_TOKEN) {
 			c_hotplug_token_list = list_append(c_hotplug_token_list, usbdev);
+			// and allow access to token by the scd unit
+			if (0 != c_hotplug_usbdev_set_sysfs_props(usbdev)) {
+				WARN("Failed to find usbdev in sysfs");
+				continue;
+			}
+			char *dev_name = container_usbdev_get_devpath_new(usbdev);
+			if (dev_name) {
+				unit_device_allow(scd_get_unit(), dev_name, 'c',
+						  container_usbdev_get_major(usbdev),
+						  container_usbdev_get_minor(usbdev));
+				mem_free0(dev_name);
+			} else {
+				WARN("Can't get dev name for device 'c %d:%d'.",
+				     container_usbdev_get_major(usbdev),
+				     container_usbdev_get_minor(usbdev));
+			}
 		}
 	}
 
@@ -542,6 +558,12 @@ c_hotplug_free(void *hotplugp)
 		container_usbdev_t *usbdev = l->data;
 		if (container_usbdev_get_type(usbdev) == CONTAINER_USBDEV_TYPE_TOKEN) {
 			c_hotplug_token_list = list_remove(c_hotplug_token_list, usbdev);
+			// and deny access to token by the scd unit
+			char *dev_name = container_usbdev_get_devpath_new(usbdev);
+			if (dev_name) {
+				unit_device_deny(scd_get_unit(), dev_name);
+				mem_free0(dev_name);
+			}
 		}
 	}
 
