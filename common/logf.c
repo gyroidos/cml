gyroidos/cml

commit 2703ace06ca8e38ceaef36af9ed96df3a7fdfb6c

common/logf: Creates symlink to identify current log.
Upon log file creation a .current symlink is linked to
the file in creation. Therefore the currently used logfile
can be identified.

Signed-off-by: David Sonntag <dsonntag@blackned.de>

common/logf.c
@@ -21,7 +21,7 @@
  * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
  */
 
-#include "macro.h"
+#include "file.h"
 #include "logf.h"
 #include "list.h"
 #include "mem.h"
@@ -300,6 +300,8 @@ logf_file_new_name(const char *name)
 void *
 logf_file_new(const char *name)
 {
+	char *current = mem_printf("%s%s", name, ".current");
+
 	FILE *f;
 	char *name_with_time_of_day = logf_file_new_name(name);
 	f = fopen(name_with_time_of_day, "w");
@@ -312,6 +314,15 @@ logf_file_new(const char *name)
 		f = NULL;
 	}
 
+	if (file_exists(current) && unlink(current)) {
+		WARN_ERRNO("could not unlink %s", current);
+	}
+
+	if (symlink(name_with_time_of_day, current) < 0) {
+		WARN_ERRNO("Could not create symlink %s to %s", name_with_time_of_day, current);
+	}
+
+	mem_free0(current);
 	mem_free0(name_with_time_of_day);
 
 	return f;

commit 13f42e6b4bcd16971b203295060067df39e1bc6b

common/logf: add condition to the unlink mechanism
If a symlink has a valid and existing target, it will be unlinked
on the daily log roll over or on restart. Then, a new symlink is
created.

If a symlink has no valid / existing target,  the symlink will be
not unlinked.

-> A broken / not valid symlink (.current file) remains and will
not be updated.

This is fixed by extending the condition for unlinking the current
symlink. If the symlink (.current file) is a link, but the target
file does not exist anymore, it will be nevertheless unlinked.

Fixes: 2703ace06ca8 ("common/logf: Creates symlink to identify current log.")
Signed-off-by: Elias Wachter <ewachter@blackned.de>
[michael.weiss: formatted commit message]
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/logf.c
@@ -413,8 +413,10 @@ logf_file_new(const char *name)
 		f = NULL;
 	}
 
-	if (file_exists(current) && unlink(current)) {
-		FATAL_ERRNO("could not unlink %s", current);
+	if ((file_is_link(current) && !file_exists(current)) || file_exists(current)) {
+		if (unlink(current)) {
+			FATAL_ERRNO("could not unlink %s", current);
+		}
 	}
 
 	if (symlink(name_with_time_of_day, current) < 0) {