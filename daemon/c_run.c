gyroidos/cml

commit 4a5fb1e9e8adc8fd4ec4b931d9c028f26e713f90

daemon/c_run: introduce session for simultaneous running commands
This patch allows to run several control run sessions through the
c_run subsystem. We reuse the control client fd as session identifier
for this purpose. Also some other cleanup and fixes were made to
the c_run subsystem and the handler functions in control.

daemon/c_run.c
@@ -11,7 +11,7 @@
 #include <linux/sockios.h>
 #include <sched.h>
 
-#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
+//#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
 #include <macro.h>
 
 #include "common/mem.h"
@@ -28,20 +28,24 @@
 //TODO define in container.h?
 #define CLONE_STACK_SIZE 8192
 
-struct c_run {
-	container_t *container;
+typedef struct c_run_session {
+	c_run_t *run;
+	int fd;
+	pid_t active_exec_pid;
 	int console_sock_cmld;
 	int console_sock_container;
 	int pty_master;
-	pid_t active_exec_pid;
-	event_io_t *pty_master_read_io;
-	event_io_t *pty_master_write_io;
 	char *pty_slave_name;
 	int pty_slave_fd;
 	int create_pty;
 	char *cmd;
 	ssize_t argc;
 	char **argv;
+} c_run_session_t;
+
+struct c_run {
+	container_t *container;
+	list_t *sessions;
 };
 
 c_run_t *
@@ -49,19 +53,78 @@ c_run_new(container_t *container)
 {
 	c_run_t *run = mem_new0(c_run_t, 1);
 	run->container = container;
-	run->console_sock_cmld = -1;
-	run->console_sock_container = -1;
-	run->pty_master = -1;
-	run->active_exec_pid = -1;
-	run->pty_master_read_io = NULL;
-	run->pty_master_write_io = NULL;
-	run->pty_slave_name = NULL;
-	run->pty_slave_fd = -1;
-	run->create_pty = -1;
-	run->cmd = NULL;
-	run->argc = 0;
-
+	run->sessions = NULL;
 	return run;
+}
+
+c_run_session_t *
+c_run_session_new(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv,
+		  int session_fd)
+{
+	if (argv == NULL) {
+		ERROR("No command was specified to execute.");
+		return NULL;
+	}
+
+	/* Create a socketpair for communication with the console task */
+	TRACE("Setting up sockets");
+	int cfd[2];
+
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, cfd)) {
+		ERROR_ERRNO("Could not create socketpair for communication with console task!");
+		return NULL;
+	}
+
+	c_run_session_t *session = mem_new0(c_run_session_t, 1);
+	session->fd = session_fd;
+	session->run = run;
+	session->pty_master = -1;
+	session->active_exec_pid = -1;
+	session->pty_slave_name = NULL;
+	session->pty_slave_fd = -1;
+
+	session->console_sock_cmld = cfd[0];
+	session->console_sock_container = cfd[1];
+
+	TRACE("Making cmld console socket nonblocking");
+	fd_make_non_blocking(session->console_sock_cmld);
+	fd_make_non_blocking(session->console_sock_container);
+
+	//Add NULL pointer to end of argv
+	session->argv = NULL;
+
+	if (argc > 0 && argv != NULL) {
+		ssize_t i = 0;
+		size_t total_len = ADD_WITH_OVERFLOW_CHECK(session->argc, (size_t)1);
+		total_len = MUL_WITH_OVERFLOW_CHECK(sizeof(char *), total_len);
+		session->argv = mem_alloc(total_len);
+
+		while (i < argc) {
+			TRACE("Got argument: %s", argv[i]);
+			session->argv[i] = mem_strdup(argv[i]);
+			i++;
+		}
+
+		session->argv[i] = NULL;
+	}
+
+	session->cmd = mem_strdup(cmd);
+	session->argc = argc;
+	session->create_pty = create_pty;
+
+	return session;
+}
+
+void
+c_run_session_free(c_run_session_t *session)
+{
+	ASSERT(session);
+	if (session->cmd)
+		mem_free(session->cmd);
+	if (session->pty_slave_name)
+		mem_free(session->pty_slave_name);
+	mem_free_array((void *)session->argv, session->argc);
+	mem_free(session);
 }
 
 void
@@ -71,48 +134,53 @@ c_run_free(c_run_t *run)
 	mem_free(run);
 }
 
-// to be called from exec event loop
 static void
-c_run_internal_cleanup(c_run_t *run)
+c_run_session_cleanup(c_run_session_t *session)
 {
-	ASSERT(run);
+	IF_NULL_RETURN(session);
 
-	TRACE("Called c_run cleanup, active_exec_pid: %d", run->active_exec_pid);
+	if (session->active_exec_pid != -1) {
+		TRACE("Cleanup exec'ed process: %d", session->active_exec_pid);
 
-	if (run->active_exec_pid != -1) {
-		TRACE("Cleanup exec'ed process: %d", run->active_exec_pid);
-
-		if (!kill(run->active_exec_pid, SIGKILL)) {
+		if (!kill(session->active_exec_pid, SIGKILL)) {
 			TRACE("Killed process injected by control run with PID: %d",
-			      run->active_exec_pid);
+			      session->active_exec_pid);
 		} else {
 			TRACE("Failed to kill process inside container");
 		}
-		run->active_exec_pid = -1;
 	}
-	if (run->pty_master != -1) {
-		TRACE("Shutting down PTY master: %d", run->pty_master);
-		shutdown(run->pty_master, SHUT_WR);
-		TRACE("Shuttind down read direction of console container socket: %d",
-		      run->console_sock_container);
-		shutdown(run->console_sock_container, SHUT_RD);
 
-		TRACE("Shutting down console_sock_container: %d", run->console_sock_container);
-		shutdown(run->console_sock_container, SHUT_RDWR);
-		TRACE("Shutting down console_sock_cmld: %d", run->console_sock_cmld);
-		shutdown(run->console_sock_cmld, SHUT_RDWR);
+	if (session->pty_master != -1) {
+		TRACE("Shutting down PTY master: %d", session->pty_master);
+		shutdown(session->pty_master, SHUT_WR);
+		TRACE("Shuttind down read direction of console container socket: %d",
+		      session->console_sock_container);
+		shutdown(session->console_sock_container, SHUT_RD);
+
+		TRACE("Shutting down console_sock_container: %d", session->console_sock_container);
+		shutdown(session->console_sock_container, SHUT_RDWR);
+		TRACE("Shutting down console_sock_cmld: %d", session->console_sock_cmld);
+		shutdown(session->console_sock_cmld, SHUT_RDWR);
 		TRACE("Finished socket shutdown. Exiting cleanup.");
 	} else {
 		TRACE("Shutting down console sockets");
-		shutdown(run->console_sock_container, SHUT_RDWR);
-		shutdown(run->console_sock_cmld, SHUT_RDWR);
+		shutdown(session->console_sock_container, SHUT_RDWR);
+		shutdown(session->console_sock_cmld, SHUT_RDWR);
 	}
 }
 
 void
 c_run_cleanup(c_run_t *run)
 {
-	c_run_internal_cleanup(run);
+	ASSERT(run);
+	TRACE("Called c_run cleanup");
+	for (list_t *l = run->sessions; l; l = l->next) {
+		c_run_session_t *session = l->data;
+		c_run_session_cleanup(session);
+		c_run_session_free(session);
+	}
+	list_delete(run->sessions);
+	run->sessions = NULL;
 }
 
 static int
@@ -130,19 +198,38 @@ do_clone(int (*func)(void *), unsigned long flags, void *data)
 	return clone(func, exec_stack_high, flags, data);
 }
 
-int
-c_run_get_console_sock_cmld(const c_run_t *run)
+static c_run_session_t *
+c_run_get_session_by_fd(const c_run_t *run, int session_fd)
 {
-	ASSERT(run);
-	return run->console_sock_cmld;
+	for (list_t *l = run->sessions; l; l = l->next) {
+		c_run_session_t *session = l->data;
+		if (session_fd == session->fd)
+			return session;
+	}
+	ERROR("Session for fd=%d does not exist!", session_fd);
+	return NULL;
 }
 
 int
-c_run_write_exec_input(c_run_t *run, char *exec_input)
+c_run_get_console_sock_cmld(const c_run_t *run, int session_fd)
 {
-	if (run->active_exec_pid != -1) {
-		TRACE("Write message \"%s\" to fd: %d", exec_input, run->console_sock_cmld);
-		return write(run->console_sock_cmld, exec_input, strlen(exec_input));
+	ASSERT(run);
+	c_run_session_t *session = c_run_get_session_by_fd(run, session_fd);
+	IF_NULL_RETVAL(session, -1);
+
+	return session->console_sock_cmld;
+}
+
+int
+c_run_write_exec_input(const c_run_t *run, char *exec_input, int session_fd)
+{
+	ASSERT(run);
+	c_run_session_t *session = c_run_get_session_by_fd(run, session_fd);
+	IF_NULL_RETVAL(session, -1);
+
+	if (session->active_exec_pid != -1) {
+		TRACE("Write message \"%s\" to fd: %d", exec_input, session->console_sock_cmld);
+		return write(session->console_sock_cmld, exec_input, strlen(exec_input));
 	} else {
 		WARN("Currently no process executing. Can't write input");
 		return -1;
@@ -152,9 +239,16 @@ c_run_write_exec_input(c_run_t *run, char *exec_input)
 void
 c_run_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 {
-	c_run_t *run = data;
+	c_run_session_t *session = data;
 
-	ASSERT(run);
+	c_run_t *run = session->run;
+	if (NULL == run) {
+		// cleanup of c_run subsystem already done just remove signal handler
+		TRACE("SIGCHLD handler called for already dead c_run session");
+		event_remove_signal(sig);
+		event_signal_free(sig);
+		return;
+	}
 
 	TRACE("SIGCHLD handler called for c_run injected process in container %s with PID %d",
 	      container_get_description(run->container), container_get_pid(run->container));
@@ -162,10 +256,10 @@ c_run_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 	 * Therefore wait for this PGID. If a descendant process call setsid it doesn't
 	 * get reaped by this handler. This enables the user to inject processes who
 	 * continue running after the command injected by control exits */
-	pid_t exec_pid = run->active_exec_pid;
+	pid_t exec_pid = session->active_exec_pid;
 	pid_t pid = 0;
 	int status = 0;
-	while ((pid = waitpid(run->active_exec_pid, &status, WNOHANG))) {
+	while ((pid = waitpid(session->active_exec_pid, &status, WNOHANG))) {
 		TRACE("Got exited child with PID: %d, exec pid: %d", pid, exec_pid);
 
 		//if (pid == exec_pid || pid == run->pty_master_read_pid || run->pty_master_write_pid) {
@@ -186,10 +280,11 @@ c_run_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 			event_remove_signal(sig);
 			event_signal_free(sig);
 
-			/* Close sockets */
-			c_run_internal_cleanup(run);
-
-			return;
+			/* Close sockets of the session */
+			run->sessions = list_remove(run->sessions, session);
+			c_run_session_cleanup(session);
+			c_run_session_free(session);
+			break;
 		} else {
 			DEBUG("Reaped a descendant process with PID %d of injected process in container %s",
 			      pid, container_get_description(run->container));
@@ -291,25 +386,9 @@ error:
 }
 
 static int
-do_exec(c_run_t *run)
+c_run_join_container(c_run_t *run)
 {
-	//Add NULL pointer to end of argv
-	char **exec_args = NULL;
-
-	if (run->argc > 0 && run->argv != NULL) {
-		ssize_t i = 0;
-		size_t total_len = ADD_WITH_OVERFLOW_CHECK(run->argc, (size_t)1);
-		total_len = MUL_WITH_OVERFLOW_CHECK(sizeof(char *), total_len);
-		exec_args = mem_alloc(total_len);
-
-		while (i < run->argc) {
-			TRACE("Got argument: %s", run->argv[i]);
-			exec_args[i] = mem_strdup(run->argv[i]);
-			i++;
-		}
-
-		exec_args[i] = NULL;
-	}
+	ASSERT(run);
 
 	if (container_add_pid_to_cgroups(run->container, getpid()) < 0) {
 		ERROR("Could not join container cgroups!");
@@ -328,27 +407,32 @@ do_exec(c_run_t *run)
 		goto error;
 	}
 
-	TRACE("[EXEC]: Executing command %s in process with PID: %d, PGID: %d, PPID: %d", run->cmd,
-	      getpid(), getpgid(getpid()), getppid());
+	return 0;
+error:
+	return -1;
+}
 
-	if (-1 == dup2(run->pty_slave_fd, STDIN_FILENO)) {
+static int
+do_exec(c_run_session_t *session)
+{
+	IF_TRUE_GOTO(-1 == c_run_join_container(session->run), error);
+
+	TRACE("[EXEC]: Executing command %s in process with PID: %d, PGID: %d, PPID: %d",
+	      session->cmd, getpid(), getpgid(getpid()), getppid());
+
+	if (-1 == dup2(session->pty_slave_fd, STDIN_FILENO)) {
 		ERROR("Failed to redirect stdin to cmld socket. Exiting...");
 		goto error;
 	}
 
-	if (-1 == dup2(run->pty_slave_fd, STDOUT_FILENO)) {
+	if (-1 == dup2(session->pty_slave_fd, STDOUT_FILENO)) {
 		ERROR("Failed to redirect stdout to cmld socket. Exiting...");
 		goto error;
 	}
 
-	if (-1 == dup2(run->pty_slave_fd, STDERR_FILENO)) {
+	if (-1 == dup2(session->pty_slave_fd, STDERR_FILENO)) {
 		ERROR("Failed to redirect stderr to cmld. Exiting...");
 		goto error;
-	}
-
-	if (NULL == exec_args) {
-		ERROR("No command was specified to execute. Exiting...");
-		exit(EXIT_FAILURE);
 	}
 
 	/*
@@ -356,13 +440,11 @@ do_exec(c_run_t *run)
 	 * the current process into the pidns but sets the childs to be created
 	 * in the new ns.
 	 */
-	IF_TRUE_GOTO(proc_fork_and_execvp((const char *const *)exec_args) < 0, error);
-	mem_free_array((void *)exec_args, run->argc);
+	IF_TRUE_GOTO(proc_fork_and_execvp((const char *const *)session->argv) < 0, error);
 	exit(EXIT_SUCCESS);
 
 error:
 	ERROR_ERRNO("An error occured while trying to execute command. Giving up...");
-	mem_free_array((void *)exec_args, run->argc);
 	exit(EXIT_FAILURE);
 }
 
@@ -370,17 +452,17 @@ static int
 do_pty_exec(void *data)
 {
 	ASSERT(data);
-	c_run_t *run = (c_run_t *)data;
+	c_run_session_t *session = data;
 
-	run->active_exec_pid = getpid();
+	session->active_exec_pid = getpid();
 
 	TRACE("[EXEC] Prepare command execution in process with PID: %d, PGID: %d", getpid(),
 	      getpgid(getpid()));
-	run->pty_slave_fd = -1;
+	session->pty_slave_fd = -1;
 
 	// open PTY slave
-	if (-1 == (run->pty_slave_fd = open(run->pty_slave_name, O_RDWR))) {
-		TRACE("Failed to open pty slave: %s\n", run->pty_slave_name);
+	if (-1 == (session->pty_slave_fd = open(session->pty_slave_name, O_RDWR))) {
+		TRACE("Failed to open pty slave: %s\n", session->pty_slave_name);
 		//TODO avoid access to pty master from child ?
 		goto error;
 	}
@@ -396,12 +478,12 @@ do_pty_exec(void *data)
 	// necessary for TIOCSCTTY
 	setsid();
 
-	if (-1 == ioctl(run->pty_slave_fd, TIOCSCTTY, NULL)) {
+	if (-1 == ioctl(session->pty_slave_fd, TIOCSCTTY, NULL)) {
 		ERROR("[EXEC] Failed to set controlling pty slave\n");
 		goto error;
 	}
 
-	do_exec(run);
+	do_exec(session);
 error:
 	TRACE("An error occurred. Exiting...");
 	exit(EXIT_FAILURE);
@@ -432,54 +514,58 @@ readloop(int from_fd, int to_fd)
 }
 
 static void
-c_run_cb_read_pty(int fd, unsigned events, event_io_t *io, void *data)
+c_run_cb_read_pty(int fd, unsigned events, UNUSED event_io_t *io, void *data)
 {
 	ASSERT(data);
-	c_run_t *run = (c_run_t *)data;
+	c_run_session_t *session = data;
 
 	if (events & EVENT_IO_EXCEPT) {
 		TRACE("Exception on reading pty fd %d", fd);
 		event_remove_io(io);
 		event_io_free(io);
-		run->pty_master_read_io = NULL;
 		close(fd);
 		return;
 	}
 
 	TRACE("Entering PTY master reading loop");
-	if (-1 == readloop(run->pty_master, run->console_sock_container)) {
+	if (-1 == readloop(session->pty_master, session->console_sock_container)) {
 		ERROR("Readloop returned an error, cleanup!");
-		c_run_internal_cleanup(run);
+		c_run_t *run = session->run;
+		run->sessions = list_remove(run->sessions, session);
+		c_run_session_cleanup(session);
+		c_run_session_free(session);
 	}
 }
 
 static void
-c_run_cb_write_pty(int fd, unsigned events, event_io_t *io, void *data)
+c_run_cb_write_pty(int fd, unsigned events, UNUSED event_io_t *io, void *data)
 {
 	ASSERT(data);
-	c_run_t *run = (c_run_t *)data;
+	c_run_session_t *session = data;
 
 	if (events & EVENT_IO_EXCEPT) {
 		TRACE("Exception on console socket fd %d", fd);
 		event_remove_io(io);
 		event_io_free(io);
-		run->pty_master_write_io = NULL;
 		close(fd);
 		return;
 	}
 
 	TRACE("Entering console sock reading loop");
-	if (-1 == readloop(run->console_sock_container, run->pty_master)) {
+	if (-1 == readloop(session->console_sock_container, session->pty_master)) {
 		ERROR("Readloop returned an error, cleanup!");
-		c_run_internal_cleanup(run);
+		c_run_t *run = session->run;
+		run->sessions = list_remove(run->sessions, session);
+		c_run_session_cleanup(session);
+		c_run_session_free(session);
 	}
 }
 
 static int
-c_run_prepare_exec(c_run_t *run)
+c_run_prepare_exec(c_run_session_t *session)
 {
 	//create new PTY
-	if (run->create_pty) {
+	if (session->create_pty) {
 		TRACE("[EXEC] Starting to create new pty");
 
 		int pty_master = 0;
@@ -500,33 +586,34 @@ c_run_prepare_exec(c_run_t *run)
 		}
 
 		//TODO get name to alloc sufficient memory?
-		run->pty_slave_name = mem_alloc(100);
+		session->pty_slave_name = mem_alloc(100);
 
-		ptsname_r(pty_master, run->pty_slave_name, 100);
+		ptsname_r(pty_master, session->pty_slave_name, 100);
 		TRACE("Created new pty with fd: %i, slave name: %s\n", pty_master,
-		      run->pty_slave_name);
+		      session->pty_slave_name);
 
 		TRACE("Storing PTY master fd to c_run_t: %d", pty_master);
-		run->pty_master = pty_master;
+		session->pty_master = pty_master;
 
-		fd_make_non_blocking(run->pty_master);
+		fd_make_non_blocking(session->pty_master);
 
 		DEBUG("Registering read callback for PTY master fd");
-		run->pty_master_write_io =
-			event_io_new(run->console_sock_container, EVENT_IO_READ | EVENT_IO_EXCEPT,
-				     c_run_cb_write_pty, run);
-		event_add_io(run->pty_master_write_io);
+		event_io_t *pty_master_write_io =
+			event_io_new(session->console_sock_container,
+				     EVENT_IO_READ | EVENT_IO_EXCEPT, c_run_cb_write_pty, session);
+		event_add_io(pty_master_write_io);
 
 		DEBUG("Registering read callback for console socket");
-		run->pty_master_read_io = event_io_new(
-			run->pty_master, EVENT_IO_READ | EVENT_IO_EXCEPT, c_run_cb_read_pty, run);
-		event_add_io(run->pty_master_read_io);
+		event_io_t *pty_master_read_io =
+			event_io_new(session->pty_master, EVENT_IO_READ | EVENT_IO_EXCEPT,
+				     c_run_cb_read_pty, session);
+		event_add_io(pty_master_read_io);
 
 		//clone child to execute command
 		TRACE("clone child process to execute command with PTY");
-		run->active_exec_pid = do_clone(do_pty_exec, SIGCHLD, (void *)run);
+		session->active_exec_pid = do_clone(do_pty_exec, SIGCHLD, (void *)session);
 
-		if (run->active_exec_pid == -1) {
+		if (session->active_exec_pid == -1) {
 			TRACE("Failed to fork() ...\n");
 			goto error;
 		}
@@ -538,9 +625,9 @@ c_run_prepare_exec(c_run_t *run)
 
 		//clone child to execute command
 		TRACE("Clone child process to execute command without PTY");
-		run->active_exec_pid = do_clone(do_pty_exec, SIGCHLD, (void *)run);
+		session->active_exec_pid = do_clone(do_pty_exec, SIGCHLD, (void *)session);
 
-		if (run->active_exec_pid == -1) {
+		if (session->active_exec_pid == -1) {
 			TRACE("Failed to fork() ...\n");
 			goto error;
 		}
@@ -554,48 +641,29 @@ error:
 }
 
 int
-c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv)
+c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv,
+		   int session_fd)
 {
 	TRACE("Trying to excute command \"%s\" inside container", cmd);
 	ASSERT(cmd);
 
-	if (run->active_exec_pid != -1) {
-		TRACE("Already executing command. Aborting request.");
-		return -1;
-	}
+	c_run_session_t *session = c_run_session_new(run, create_pty, cmd, argc, argv, session_fd);
+	IF_NULL_RETVAL(session, -1);
 
-	/* Create a socketpair for communication with the console task */
-	TRACE("Setting up sockets");
-	int cfd[2];
+	run->sessions = list_append(run->sessions, session);
 
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, cfd)) {
-		ERROR_ERRNO("Could not create socketpair for communication with console task!");
-		return -1;
-	}
-
-	run->console_sock_cmld = cfd[0];
-	run->console_sock_container = cfd[1];
-
-	TRACE("Making cmld console socket nonblocking");
-	fd_make_non_blocking(run->console_sock_cmld);
-	fd_make_non_blocking(run->console_sock_container);
-
-	// prepare c_run_t
-	run->create_pty = create_pty;
-	run->cmd = cmd;
-	run->argc = argc;
-	run->argv = argv;
-
-	IF_TRUE_GOTO(c_run_prepare_exec(run) < 0, error);
+	IF_TRUE_GOTO(c_run_prepare_exec(session) < 0, error);
 
 	TRACE("Registering SIGCHLD handler for injected processes");
-	event_signal_t *sig = event_signal_new(SIGCHLD, c_run_sigchld_cb, run);
+	event_signal_t *sig = event_signal_new(SIGCHLD, c_run_sigchld_cb, session);
 	event_add_signal(sig);
 
-	return 0;
+	return session->fd;
 
 error:
 	TRACE("An error occurred.");
-	c_run_internal_cleanup(run);
+	run->sessions = list_remove(run->sessions, session);
+	c_run_session_cleanup(session);
+	c_run_session_free(session);
 	return -1;
 }

daemon/c_run.h
@@ -15,14 +15,13 @@ void
 c_run_cleanup(c_run_t *run);
 
 int
-c_run_write_exec_input(c_run_t *run, char *exec_input);
+c_run_write_exec_input(const c_run_t *run, char *exec_input, int session_fd);
 
 int
-c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv);
+c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv,
+		   int session_fd);
 
 int
-c_run_get_console_sock_cmld(const c_run_t *run);
+c_run_get_console_sock_cmld(const c_run_t *run, int session_fd);
 
-int
-c_run_get_exec_loop_pid(const c_run_t *run);
 #endif //end C_RUN_H

daemon/container.c
@@ -826,10 +826,10 @@ container_set_cap_current_process(const container_t *container)
 }
 
 int
-container_get_console_sock_cmld(const container_t *container)
+container_get_console_sock_cmld(const container_t *container, int session_fd)
 {
 	ASSERT(container);
-	return c_run_get_console_sock_cmld(container->run);
+	return c_run_get_console_sock_cmld(container->run, session_fd);
 }
 
 int
@@ -1359,7 +1359,8 @@ error:
 }
 
 int
-container_run(container_t *container, int create_pty, char *cmd, ssize_t argc, char **argv)
+container_run(container_t *container, int create_pty, char *cmd, ssize_t argc, char **argv,
+	      int session_fd)
 {
 	ASSERT(container);
 	ASSERT(cmd);
@@ -1376,14 +1377,14 @@ container_run(container_t *container, int create_pty, char *cmd, ssize_t argc, c
 	}
 
 	TRACE("Forwarding request to c_run subsystem");
-	return c_run_exec_process(container->run, create_pty, cmd, argc, argv);
+	return c_run_exec_process(container->run, create_pty, cmd, argc, argv, session_fd);
 }
 
 int
-container_write_exec_input(container_t *container, char *exec_input)
+container_write_exec_input(container_t *container, char *exec_input, int session_fd)
 {
 	TRACE("Forwarding write request to c_run subsystem");
-	return c_run_write_exec_input(container->run, exec_input);
+	return c_run_write_exec_input(container->run, exec_input, session_fd);
 }
 
 int

daemon/container.h
@@ -322,7 +322,7 @@ container_get_color_rgb_string(const container_t *container);
  * by using the control run interface
  */
 int
-container_get_console_sock_cmld(const container_t *container);
+container_get_console_sock_cmld(const container_t *container, int session_fd);
 
 /**
  * Remove a container persistently from disk, i.e. remove its configuration and
@@ -380,12 +380,15 @@ container_resume(container_t *container);
 
 /**
  * Run a given command inside a container
+ *
+ * @return session id of the running session in the container, -1 on error
  */
 int
-container_run(container_t *container, int create_pty, char *cmd, ssize_t argc, char **argv);
+container_run(container_t *container, int create_pty, char *cmd, ssize_t argc, char **argv,
+	      int session_fd);
 
 int
-container_write_exec_input(container_t *container, char *exec_input);
+container_write_exec_input(container_t *container, char *exec_input, int session_fd);
 
 /**
  * Start the given container using the given key to decrypt its filesystem

daemon/control.c
@@ -340,6 +340,8 @@ control_read_send(int cfd, int fd)
 		if (protobuf_send_message(cfd, (ProtobufCMessage *)&out) < 0) {
 			WARN("Could not send exec output to MDM");
 		}
+	} else {
+		TRACE_ERRNO("[CONTROL] Read from console socket returned %zd", count);
 	}
 
 	return count;
@@ -356,15 +358,15 @@ control_cb_read_console(int fd, unsigned events, event_io_t *io, void *data)
 	if ((events & EVENT_IO_READ)) {
 		TRACE("Got output from exec'ed command, trying to read from console socket");
 
-		int i = 0, count = 0;
+		int count = 0;
 
 		// necessary to get all output from interactive commands
 		do {
 			TRACE("Trying to read all available data from socket");
 			count = control_read_send(*cfd, fd);
 
-			TRACE("Response from read was %d", count);
-		} while (i < 100 && count > 0);
+			TRACE_ERRNO("Response from read was %d", count);
+		} while (count > 0);
 	}
 
 	if ((events & EVENT_IO_EXCEPT)) {
@@ -1168,8 +1170,8 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 			ERROR("Missing command or exec_pty info");
 			break;
 		}
-		if ((!container) || container_run(container, msg->exec_pty, msg->exec_command,
-						  msg->n_exec_args, msg->exec_args) < 0) {
+		if (container_run(container, msg->exec_pty, msg->exec_command, msg->n_exec_args,
+				  msg->exec_args, fd) < 0) {
 			ERROR("Failed to exec");
 
 			DaemonToController out = DAEMON_TO_CONTROLLER__INIT;
@@ -1186,9 +1188,10 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 			DEBUG("Registering read callback for cmld console socket");
 			int *cfd = mem_new(int, 1);
 			*cfd = fd;
-			event_io_t *event = event_io_new(container_get_console_sock_cmld(container),
-							 EVENT_IO_READ | EVENT_IO_EXCEPT,
-							 control_cb_read_console, cfd);
+			event_io_t *event =
+				event_io_new(container_get_console_sock_cmld(container, fd),
+					     EVENT_IO_READ | EVENT_IO_EXCEPT,
+					     control_cb_read_console, cfd);
 			event_add_io(event);
 		}
 	} break;
@@ -1197,7 +1200,7 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 		IF_NULL_RETURN(container);
 		TRACE("Got input for exec'ed process. Sending message on fd");
 
-		int ret = container_write_exec_input(container, msg->exec_input);
+		int ret = container_write_exec_input(container, msg->exec_input, fd);
 		if (ret < 0) {
 			ERROR_ERRNO("Failed to write input to exec'ed process");
 		}

commit 27c03cfd67172098663c4c444b26db233de0178e

daemon/c_run: maintain by session write buffers for pending data
Maintain write buffers to console_sock_container and pty_master
in the session struct. Keep these buffers for pending writes if
the write syscall returns with EAGAIN (busy) in readloop(). This
allows for proper handling of large output data in c_run sessions.

Fixes: 4a5fb1e9e8ad ("daemon/c_run: introduce session for simultaneous running commands")
Reported-by: Harshal Gohel <hg@simonwunderlich.de>
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_run.c
@@ -50,8 +50,9 @@
 #include "container.h"
 #include "cmld.h"
 
-//TODO define in container.h?
 #define CLONE_STACK_SIZE 8192
+
+#define SESSION_WBUF_SIZE 1024
 
 typedef struct c_run {
 	container_t *container;
@@ -71,6 +72,10 @@ typedef struct c_run_session {
 	char *cmd;
 	ssize_t argc;
 	char **argv;
+	char *console_sock_container_wbuf;
+	int console_sock_container_wbuf_count;
+	char *pty_master_wbuf;
+	int pty_master_wbuf_count;
 } c_run_session_t;
 
 static void *
@@ -135,6 +140,11 @@ c_run_session_new(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **
 	}
 	session->argv[i] = NULL;
 
+	session->console_sock_container_wbuf = mem_new0(char, SESSION_WBUF_SIZE);
+	session->pty_master_wbuf = mem_new0(char, SESSION_WBUF_SIZE);
+	session->console_sock_container_wbuf_count = 0;
+	session->pty_master_wbuf_count = 0;
+
 	return session;
 }
 
@@ -147,6 +157,10 @@ c_run_session_free(c_run_session_t *session)
 	if (session->pty_slave_name)
 		mem_free0(session->pty_slave_name);
 	mem_free_array((void *)session->argv, session->argc);
+	if (session->console_sock_container_wbuf)
+		mem_free0(session->console_sock_container_wbuf);
+	if (session->pty_master_wbuf)
+		mem_free0(session->pty_master_wbuf);
 	mem_free0(session);
 }
 
@@ -191,6 +205,12 @@ c_run_session_cleanup(c_run_session_t *session)
 		shutdown(session->console_sock_container, SHUT_RDWR);
 		shutdown(session->console_sock_cmld, SHUT_RDWR);
 	}
+
+	session->console_sock_container_wbuf_count = 0;
+	session->pty_master_wbuf_count = 0;
+
+	mem_memset0(session->console_sock_container_wbuf, SESSION_WBUF_SIZE);
+	mem_memset0(session->pty_master_wbuf, SESSION_WBUF_SIZE);
 }
 
 static void
@@ -445,18 +465,32 @@ error:
 }
 
 static int
-readloop(int from_fd, int to_fd)
+readloop(int from_fd, int to_fd, char *buf, int count)
 {
 	TRACE("[EXEC] Starting read loop in process %d; from fd %d, to fd %d, PPID: %d", getpid(),
 	      from_fd, to_fd, getppid());
 
-	int count = 0;
-	char buf[1024];
+	// write pending data
+	if (count > 0) {
+		TRACE("[READLOOP] Pending %d bytes from fd: %d: %s", count, from_fd, buf);
+		IF_TRUE_RETVAL(count + 1 > SESSION_WBUF_SIZE, -1);
 
-	while (0 < (count = read(from_fd, &buf, sizeof(buf) - 1))) {
+		if (write(to_fd, buf, count + 1) < 0) {
+			if (errno == EAGAIN)
+				return 0; // try again;
+
+			TRACE_ERRNO("[READLOOP] write failed.");
+			return -1;
+		}
+	}
+
+	while (0 < (count = read(from_fd, buf, SESSION_WBUF_SIZE - 1))) {
 		buf[count] = 0;
 		TRACE("[READLOOP] Read %d bytes from fd: %d: %s", count, from_fd, buf);
 		if (write(to_fd, buf, count + 1) < 0) {
+			if (errno == EAGAIN)
+				return 0; // try again;
+
 			TRACE_ERRNO("[READLOOP] write failed.");
 			return -1;
 		}
@@ -483,7 +517,9 @@ c_run_cb_read_pty(int fd, unsigned events, UNUSED event_io_t *io, void *data)
 	}
 
 	TRACE("Entering PTY master reading loop");
-	if (-1 == readloop(session->pty_master, session->console_sock_container)) {
+	if (-1 == readloop(session->pty_master, session->console_sock_container,
+			   session->console_sock_container_wbuf,
+			   session->console_sock_container_wbuf_count)) {
 		ERROR("Readloop returned an error, cleanup!");
 		c_run_session_cleanup(session);
 	}
@@ -504,7 +540,8 @@ c_run_cb_write_pty(int fd, unsigned events, UNUSED event_io_t *io, void *data)
 	}
 
 	TRACE("Entering console sock reading loop");
-	if (-1 == readloop(session->console_sock_container, session->pty_master)) {
+	if (-1 == readloop(session->console_sock_container, session->pty_master,
+			   session->pty_master_wbuf, session->pty_master_wbuf_count)) {
 		ERROR("Readloop returned an error, cleanup!");
 		c_run_session_cleanup(session);
 	}