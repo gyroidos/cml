gyroidos/cml

commit b38cfa26928db4422ec5eeafe4a1b7095a58e218

scd/control: export CRYPTO_HASH_BUF through protobuf interface
Allow to use currently exported ssl_hash_buf() also through protobuf
interface, e.g., by the cmld. This is an asynchronous implementation
similar to the CRYPTO_HASH_FILE command.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

scd/control.c
@@ -494,6 +494,32 @@ scd_control_handle_message(const DaemonToToken *msg, int fd)
 		if (hash)
 			mem_free0(hash);
 	} break;
+	case DAEMON_TO_TOKEN__CODE__CRYPTO_HASH_BUF: {
+		TRACE("SCD: Handle messsage CRYPTO_HASH_BUF");
+		unsigned int hash_len;
+		const char *hash_algo;
+		unsigned char *hash = NULL;
+		TokenToDaemon out = TOKEN_TO_DAEMON__INIT;
+		out.code = TOKEN_TO_DAEMON__CODE__CRYPTO_HASH_ERROR;
+
+		hash_algo = switch_proto_hash_algo(msg->hash_algo);
+
+		if (hash_algo) {
+			if ((hash = ssl_hash_buf(msg->hash_buf.data, msg->hash_buf.len, &hash_len,
+						 hash_algo)) == NULL) {
+				ERROR("Hashing buffer failed");
+			} else {
+				out.has_hash_value = true;
+				out.hash_value.len = hash_len;
+				out.hash_value.data = hash;
+				out.code = TOKEN_TO_DAEMON__CODE__CRYPTO_HASH_OK;
+			}
+		}
+
+		protobuf_send_message(fd, (ProtobufCMessage *)&out);
+		if (hash)
+			mem_free0(hash);
+	} break;
 	/*
 	 * This case handles verify requests as part of TSF.CML.Updates
 	 */

daemon/scd.proto
@@ -62,6 +62,7 @@ message DaemonToToken {
 
 		// crypto commands unrelated to actual secure element (FIXME move elsewhere?!)
 		CRYPTO_HASH_FILE = 50;		// compute hash for file [hash_file]
+		CRYPTO_HASH_BUF = 51;		// compute hash for buffer [hash_buf]
 		CRYPTO_VERIFY_FILE = 60;	// verify certificate and signature on data given in [verify_*_file]
 		CRYPTO_VERIFY_BUF = 61;	// verify certificate and signature on data given in [verify_*_buf]
 
@@ -98,6 +99,7 @@ message DaemonToToken {
 
 	optional HashAlgo hash_algo = 50;	// determines hash algorithm for hashing
 	optional string hash_file = 51;		// the full path to the file to hash
+	optional bytes hash_buf = 52;		// buf with data to hash
 
 	optional string verify_data_file = 60;	// file with data to verify
 	optional string verify_sig_file = 61;	// file with signature for data file

commit d0344bc6646263782b0810ee5d2a716ef037eb3f

scd/control: ensure that hash_buf is set before using ssl_util
ssl_hash_buf() of ssl_util asserts that the input hash is set.
Thus, ensure that ssl_hash_buf() is not called if the protobuf
message contains no buffer, e.g., has_hash_buf is not set.

Since the hashing is done in a concurrent child it would just die
and no response would be sent to the protobuf caller. See error:

 [6] <FATAL> ssl_util.c+1151: Assertion `buf_to_hash' failed
 [6] <FATAL> ssl_util.c+1151: Aborting...
 [1] <WARN>  control.c+531: asyn crypto handler reurned with error

Also fixed the typo in the above WARN string 's/reurned/returned/'.

Fixes: b38cfa26928d ("scd/control: export CRYPTO_HASH_BUF through protobuf interface")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

scd/control.c
@@ -528,7 +528,7 @@ scd_crypto_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 		event_signal_free(sig);
 
 		if ((WIFEXITED(status) && WEXITSTATUS(status)) || WIFSIGNALED(status)) {
-			WARN("asyn crypto handler reurned with error");
+			WARN("asyn crypto handler returned with error");
 		}
 		mem_free0(pid);
 	}
@@ -609,7 +609,7 @@ scd_control_handle_crypto_message(const DaemonToToken *msg, int fd)
 
 		hash_algo = switch_proto_hash_algo(msg->hash_algo);
 
-		if (hash_algo) {
+		if (hash_algo && msg->has_hash_buf && msg->hash_buf.data) {
 			if ((hash = ssl_hash_buf(msg->hash_buf.data, msg->hash_buf.len, &hash_len,
 						 hash_algo)) == NULL) {
 				ERROR("Hashing buffer failed");