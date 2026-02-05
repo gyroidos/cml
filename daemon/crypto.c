gyroidos/cml

commit 814f6a8e1f0a6c42fbfd5da8811ec4b9ee8de1e9

daemon/crypto: provide crypto_hash_buf for hashing buffers
Use the new scd API for directly hashing buffers instead of hashing
files.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/crypto.c
@@ -128,10 +128,13 @@ crypto_verify_result_from_proto(TokenToDaemon__Code code)
 
 typedef struct crypto_callback_task {
 	crypto_hash_callback_t hash_complete;
+	crypto_hash_buf_callback_t hash_buf_complete;
 	crypto_verify_callback_t verify_complete;
 	crypto_verify_buf_callback_t verify_buf_complete;
 	void *data;
 	char *hash_file;
+	unsigned char *hash_buf;
+	size_t hash_buf_len;
 	crypto_hashalgo_t hash_algo;
 	char *verify_data_file;
 	char *verify_sig_file;
@@ -152,6 +155,21 @@ crypto_callback_hash_task_new(crypto_hash_callback_t cb, void *data, const char 
 	task->hash_complete = cb;
 	task->data = data;
 	task->hash_file = mem_strdup(hash_file);
+	task->hash_algo = hash_algo;
+	return task;
+}
+
+static crypto_callback_task_t *
+crypto_callback_hash_buf_task_new(crypto_hash_buf_callback_t cb, void *data,
+				  const unsigned char *hash_buf, size_t hash_buf_len,
+				  crypto_hashalgo_t hash_algo)
+{
+	crypto_callback_task_t *task = mem_new0(crypto_callback_task_t, 1);
+	task->hash_buf_complete = cb;
+	task->data = data;
+	task->hash_buf = mem_new0(unsigned char, hash_buf_len);
+	memcpy(task->hash_buf, hash_buf, hash_buf_len);
+	task->hash_buf_len = hash_buf_len;
 	task->hash_algo = hash_algo;
 	return task;
 }
@@ -200,6 +218,8 @@ crypto_callback_task_free(crypto_callback_task_t *task)
 	IF_NULL_RETURN(task);
 	if (task->hash_file)
 		mem_free0(task->hash_file);
+	if (task->hash_buf)
+		mem_free0(task->hash_buf);
 	if (task->verify_data_file)
 		mem_free0(task->verify_data_file);
 	if (task->verify_sig_file)
@@ -407,6 +427,32 @@ crypto_hash_file(const char *file, crypto_hashalgo_t hashalgo, crypto_hash_callb
 	out.has_hash_algo = true;
 	out.hash_algo = crypto_hashalgo_to_proto(hashalgo);
 	out.hash_file = task->hash_file;
+
+	TRACE("Requesting scd to hash file at %s", task->hash_file);
+
+	if (crypto_send_msg(&out, task) < 0) {
+		crypto_callback_task_free(task);
+		return -1;
+	}
+	return 0;
+}
+
+int
+crypto_hash_buf(const unsigned char *buf, size_t buf_len, crypto_hashalgo_t hashalgo,
+		crypto_hash_buf_callback_t cb, void *data)
+{
+	ASSERT(buf);
+	ASSERT(cb);
+
+	crypto_callback_task_t *task =
+		crypto_callback_hash_buf_task_new(cb, data, buf, buf_len, hashalgo);
+
+	DaemonToToken out = DAEMON_TO_TOKEN__INIT;
+	out.code = DAEMON_TO_TOKEN__CODE__CRYPTO_HASH_BUF;
+	out.has_hash_algo = true;
+	out.hash_algo = crypto_hashalgo_to_proto(hashalgo);
+	out.hash_buf.data = task->hash_buf;
+	out.hash_buf.len = task->hash_buf_len;
 
 	TRACE("Requesting scd to hash file at %s", task->hash_file);
 

daemon/crypto.h
@@ -32,10 +32,17 @@
 typedef enum crypto_hashalgo { SHA1, SHA256, SHA512 } crypto_hashalgo_t;
 
 /**
- * Callback function for receiving the result of a hash operation.
+ * Callback function for receiving the result of a hash operation. (file)
  */
 typedef void (*crypto_hash_callback_t)(const char *hash_string, const char *hash_file,
 				       crypto_hashalgo_t hash_algo, void *data);
+
+/**
+ * Callback function for receiving the result of a hash operation. (buffer)
+ */
+typedef void (*crypto_hash_buf_callback_t)(const char *hash_string, const unsigned char *hash_buf,
+					   size_t hash_buf_len, crypto_hashalgo_t hash_algo,
+					   void *data);
 
 /**
  * Requests the scd to hash the given file and report the hash to the given callback.
@@ -50,6 +57,19 @@ int
 crypto_hash_file(const char *file, crypto_hashalgo_t hashalgo, crypto_hash_callback_t cb,
 		 void *data);
 
+/**
+ * Requests the scd to hash the given buffer and report the hash to the given callback.
+ *
+ * @param buf the buffer to hash
+ * @param buf_len the size of the buffer to hash
+ * @param hashalgo the hash algorithm to use
+ * @param cb the callback to receive the result
+ * @param data custom data parameter to pass to the callback
+ * @return 0 if the hash request was sent and the callback is expected to be called, -1 otherwise
+ */
+int
+crypto_hash_buf(const unsigned char *buf, size_t buf_len, crypto_hashalgo_t hashalgo,
+		crypto_hash_buf_callback_t cb, void *data);
 /**
  * Requests the scd to hash the given file, wait for the result and directly return it.
  *

commit c15a79dd0a3947cf3be7a45047f6d535805b3a1e

daemon/crypto: corrected HASH_BUF related protobuf messages handling
We missed to set the filed has_hash_buf of the corresponding protobuf
message which is sent to the scd. This lead to empty hash buffer to
be transmitted. Further, in the response handler for HASH_OK and
HASH_ERR call the correct has complete callback depending on
hash_buf_complete or hash_complete is set in the crypto_callback_task.

This fixes following error in scd:

  [6] <FATAL> ssl_util.c+1151: Assertion `buf_to_hash' failed
  [6] <FATAL> ssl_util.c+1151: Aborting...
  [1] <WARN>  control.c+531: asyn crypto handler reurned with error

Fixes: 814f6a8e1f0a ("daemon/crypto: provide crypto_hash_buf for hashing buffers")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/crypto.c
@@ -271,8 +271,13 @@ crypto_cb(int fd, unsigned events, event_io_t *io, void *data)
 
 				TRACE("Received hash for file %s: %s",
 				      task->hash_file ? task->hash_file : "<empty>", hash);
-				task->hash_complete(hash, task->hash_file, task->hash_algo,
-						    task->data);
+				if (task->hash_complete)
+					task->hash_complete(hash, task->hash_file, task->hash_algo,
+							    task->data);
+				if (task->hash_buf_complete)
+					task->hash_buf_complete(hash, task->hash_buf,
+								task->hash_buf_len, task->hash_algo,
+								task->data);
 				if (hash != NULL) {
 					mem_free0(hash);
 				}
@@ -281,6 +286,12 @@ crypto_cb(int fd, unsigned events, event_io_t *io, void *data)
 			ERROR("Missing hash_value in CRYPTO_HASH_OK response!"); // fallthrough
 		case TOKEN_TO_DAEMON__CODE__CRYPTO_HASH_ERROR:
 			task->hash_complete(NULL, task->hash_file, task->hash_algo, task->data);
+			if (task->hash_complete)
+				task->hash_complete(NULL, task->hash_file, task->hash_algo,
+						    task->data);
+			if (task->hash_buf_complete)
+				task->hash_buf_complete(NULL, task->hash_buf, task->hash_buf_len,
+							task->hash_algo, task->data);
 			break;
 
 		// deal with CRYPTO_VERIFY_* cases
@@ -459,10 +470,11 @@ crypto_hash_buf(const unsigned char *buf, size_t buf_len, crypto_hashalgo_t hash
 	out.code = DAEMON_TO_TOKEN__CODE__CRYPTO_HASH_BUF;
 	out.has_hash_algo = true;
 	out.hash_algo = crypto_hashalgo_to_proto(hashalgo);
+	out.has_hash_buf = true;
 	out.hash_buf.data = task->hash_buf;
 	out.hash_buf.len = task->hash_buf_len;
 
-	TRACE("Requesting scd to hash file at %s", task->hash_file);
+	TRACE("Requesting scd to hash buf of len %zu", task->hash_buf_len);
 
 	if (crypto_send_msg(&out, task) < 0) {
 		crypto_callback_task_free(task);