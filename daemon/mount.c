gyroidos/cml

commit f8e91c7330ab08684f50e99398feea251665129a

daemon/mount: Introduce mount_idmapped()
Refactored idmapped mount generic syscall compat stuff mount module.
Provide a new mount_idmapped header which inludes most of the compat
code.

Use this for a generic mount_idmapped() implementation. This could
later be used in units as well as for a major refactoring in the
c_idmapped module.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/mount.c
@@ -25,12 +25,14 @@
 #include <sched.h>
 
 #include "mount.h"
+#include "mount_idmapped.h"
 #include "crypto.h"
 
 #include "common/macro.h"
 #include "common/mem.h"
 #include "common/list.h"
 #include "common/file.h"
+#include "common/kernel.h"
 
 #include <errno.h>
 #include <string.h>
@@ -534,4 +536,70 @@ error:
 	umount(MOUNT_CGROUPS_FOLDER "/systemd");
 	umount(MOUNT_CGROUPS_FOLDER);
 	return -1;
+}
+
+/**************************/
+int
+mount_setattr(int dirfd, const char *path, unsigned int flags, struct mount_attr *attr, size_t size)
+{
+	return syscall(__NR_mount_setattr, dirfd, path, flags, attr, size);
+}
+
+int
+open_tree(int dirfd, const char *path, unsigned int flags)
+{
+	return syscall(__NR_open_tree, dirfd, path, flags);
+}
+
+int
+move_mount(int from_dirfd, const char *from_path, int to_dirfd, const char *to_path,
+	   unsigned int flags)
+{
+	return syscall(__NR_move_mount, from_dirfd, from_path, to_dirfd, to_path, flags);
+}
+/**************************/
+
+bool
+mount_is_idmapping_supported(void)
+{
+	return kernel_version_check("6.3");
+}
+
+int
+mount_idmapped(char *src, char *dst, int userns_fd)
+{
+	ASSERT(src);
+
+	IF_TRUE_RETVAL(!mount_is_idmapping_supported(), -1);
+	IF_TRUE_RETVAL(userns_fd < 0, -1);
+
+	int mapped_tree_fd = -1;
+
+	struct mount_attr attr = { 0 };
+	attr.userns_fd = userns_fd;
+	attr.attr_set = MOUNT_ATTR_IDMAP;
+
+	if ((mapped_tree_fd =
+		     open_tree(-1, src, OPEN_TREE_CLONE | OPEN_TREE_CLOEXEC | AT_EMPTY_PATH)) < 0) {
+		ERROR_ERRNO("Could not open_tree path '%s'", src);
+		return -1;
+	}
+
+	if (mount_setattr(mapped_tree_fd, "", AT_EMPTY_PATH, &attr, sizeof(struct mount_attr))) {
+		ERROR_ERRNO("Could not setattr for src path '%s'", src);
+		close(mapped_tree_fd);
+		return -1;
+	}
+
+	INFO("Sucessfully applied idmapping from src path %s on fd=%d for %s", src, mapped_tree_fd,
+	     dst);
+
+	if (move_mount(mapped_tree_fd, "", -1, dst, MOVE_MOUNT_F_EMPTY_PATH) == -1) {
+		ERROR_ERRNO("Could not move_mount %s on %s!", src, dst);
+		return -1;
+	}
+
+	INFO("Successfully move_mount %s on %s.", src, dst);
+
+	return 0;
 }

daemon/c_idmapped.c
@@ -40,179 +40,11 @@
 #include "common/dir.h"
 #include "common/kernel.h"
 #include "container.h"
+#include "mount.h"
+#include "mount_idmapped.h"
 
 #define IDMAPPED_SRC_DIR "/tmp/idmapped_mnts"
 #define UID_RANGE 100000
-
-/**************************/
-#ifndef MOUNT_ATTR_RDONLY
-#define MOUNT_ATTR_RDONLY 0x00000001
-#endif
-
-#ifndef MOUNT_ATTR_NOSUID
-#define MOUNT_ATTR_NOSUID 0x00000002
-#endif
-
-#ifndef MOUNT_ATTR_NOEXEC
-#define MOUNT_ATTR_NOEXEC 0x00000008
-#endif
-
-#ifndef MOUNT_ATTR_NODIRATIME
-#define MOUNT_ATTR_NODIRATIME 0x00000080
-#endif
-
-#ifndef MOUNT_ATTR__ATIME
-#define MOUNT_ATTR__ATIME 0x00000070
-#endif
-
-#ifndef MOUNT_ATTR_RELATIME
-#define MOUNT_ATTR_RELATIME 0x00000000
-#endif
-
-#ifndef MOUNT_ATTR_NOATIME
-#define MOUNT_ATTR_NOATIME 0x00000010
-#endif
-
-#ifndef MOUNT_ATTR_STRICTATIME
-#define MOUNT_ATTR_STRICTATIME 0x00000020
-#endif
-
-#ifndef MOUNT_ATTR_IDMAP
-#define MOUNT_ATTR_IDMAP 0x00100000
-#endif
-
-#ifndef AT_RECURSIVE
-#define AT_RECURSIVE 0x8000
-#endif
-
-// clang-format off
-#ifndef __NR_mount_setattr
-	#if defined __alpha__
-		#define __NR_mount_setattr 552
-	#elif defined _MIPS_SIM
-		#if _MIPS_SIM == _MIPS_SIM_ABI32        /* o32 */
-			#define __NR_mount_setattr (442 + 4000)
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_NABI32       /* n32 */
-			#define __NR_mount_setattr (442 + 6000)
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_ABI64        /* n64 */
-			#define __NR_mount_setattr (442 + 5000)
-		#endif
-	#elif defined __ia64__
-		#define __NR_mount_setattr (442 + 1024)
-	#else
-		#define __NR_mount_setattr 442
-	#endif
-// clang-format on
-
-struct mount_attr {
-	__u64 attr_set;
-	__u64 attr_clr;
-	__u64 propagation;
-	__u64 userns_fd;
-};
-#endif
-
-#ifndef OPEN_TREE_CLONE
-#define OPEN_TREE_CLONE 1
-#endif
-
-#ifndef OPEN_TREE_CLOEXEC
-#define OPEN_TREE_CLOEXEC O_CLOEXEC
-#endif
-
-// clang-format off
-#ifndef __NR_open_tree
-	#if defined __alpha__
-		#define __NR_open_tree 538
-	#elif defined _MIPS_SIM
-		#if _MIPS_SIM == _MIPS_SIM_ABI32        /* o32 */
-			#define __NR_open_tree 4428
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_NABI32       /* n32 */
-			#define __NR_open_tree 6428
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_ABI64        /* n64 */
-			#define __NR_open_tree 5428
-		#endif
-	#elif defined __ia64__
-		#define __NR_open_tree (428 + 1024)
-	#else
-		#define __NR_open_tree 428
-	#endif
-#endif
-// clang-format on
-
-#ifndef MOVE_MOUNT_F_SYMLINKS
-#define MOVE_MOUNT_F_SYMLINKS 0x00000001
-#endif
-
-#ifndef MOVE_MOUNT_F_AUTOMOUNTS
-#define MOVE_MOUNT_F_AUTOMOUNTS 0x00000002
-#endif
-
-#ifndef MOVE_MOUNT_F_EMPTY_PATH
-#define MOVE_MOUNT_F_EMPTY_PATH 0x00000004
-#endif
-
-#ifndef MOVE_MOUNT_T_SYMLINKS
-#define MOVE_MOUNT_T_SYMLINKS 0x00000010
-#endif
-
-#ifndef MOVE_MOUNT_T_AUTOMOUNTS
-#define MOVE_MOUNT_T_AUTOMOUNTS 0x00000020
-#endif
-
-#ifndef MOVE_MOUNT_T_EMPTY_PATH
-#define MOVE_MOUNT_T_EMPTY_PATH 0x00000040
-#endif
-
-#ifndef MOVE_MOUNT__MASK
-#define MOVE_MOUNT__MASK 0x00000077
-#endif
-
-// clang-format off
-#ifndef __NR_move_mount
-	#if defined __alpha__
-		#define __NR_move_mount 539
-	#elif defined _MIPS_SIM
-		#if _MIPS_SIM == _MIPS_SIM_ABI32
-			#define __NR_move_mount 4429
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_NABI32
-			#define __NR_move_mount 6429
-		#endif
-		#if _MIPS_SIM == _MIPS_SIM_ABI64
-			#define __NR_move_mount 5429
-		#endif
-	#elif defined __ia64__
-		#define __NR_move_mount (428 + 1024)
-	#else
-		#define __NR_move_mount 429
-	#endif
-#endif
-// clang-format on
-
-int
-mount_setattr(int dirfd, const char *path, unsigned int flags, struct mount_attr *attr, size_t size)
-{
-	return syscall(__NR_mount_setattr, dirfd, path, flags, attr, size);
-}
-
-int
-open_tree(int dirfd, const char *path, unsigned int flags)
-{
-	return syscall(__NR_open_tree, dirfd, path, flags);
-}
-
-int
-move_mount(int from_dirfd, const char *from_path, int to_dirfd, const char *to_path,
-	   unsigned int flags)
-{
-	return syscall(__NR_move_mount, from_dirfd, from_path, to_dirfd, to_path, flags);
-}
-/**************************/
 
 struct c_idmapped_mnt {
 	char *target;
@@ -456,12 +288,6 @@ error:
 	return -1;
 }
 
-static bool
-is_idmapping_supported()
-{
-	return kernel_version_check("6.3");
-}
-
 static int
 c_idmapped_prepare_dir(c_idmapped_t *idmapped, struct c_idmapped_mnt *mnt, const char *dir)
 {
@@ -473,7 +299,7 @@ c_idmapped_prepare_dir(c_idmapped_t *idmapped, struct c_idmapped_mnt *mnt, const
 	struct statfs dir_statfs;
 	statfs(dir, &dir_statfs);
 
-	if (!is_idmapping_supported()) {
+	if (!mount_is_idmapping_supported()) {
 		if (dir_statfs.f_flags & MS_RDONLY) {
 			char *tmpfs_dir =
 				mem_printf("%s/%s/tmp%d", IDMAPPED_SRC_DIR,
@@ -532,7 +358,7 @@ c_idmapped_prepare_dir(c_idmapped_t *idmapped, struct c_idmapped_mnt *mnt, const
 		return -1;
 	}
 
-	if (!is_idmapping_supported())
+	if (!mount_is_idmapping_supported())
 		return 0;
 
 	/*
@@ -595,7 +421,7 @@ c_idmapped_mount_idmapped(c_idmapped_t *idmapped, const char *src, const char *d
 
 	if (ovl_lower) {
 		// mount ovl in rootns if kernel is to old
-		if (!is_idmapping_supported()) {
+		if (!mount_is_idmapping_supported()) {
 			if (c_idmapped_mount_ovl(src, src, ovl_lower, false)) {
 				ERROR("Failed to mount ovl '%s' (lower='%s') in rootns on '%s'",
 				      src, ovl_lower, dst);
@@ -749,7 +575,7 @@ c_idmapped_start_child(void *idmappedp)
 
 		// if explictly set to bind inchild (e.g. /dev on tmpfs) or
 		// kernel does not support idmapped mounts just do bind mount
-		if (mnt->bind_in_child || (!is_idmapping_supported())) {
+		if (mnt->bind_in_child || (!mount_is_idmapping_supported())) {
 			if (!file_exists(mnt->target))
 				dir_mkdir_p(mnt->target, 0755);
 

daemon/mount.h
@@ -264,4 +264,10 @@ mount_private_tmp(void);
 int
 mount_cgroups(list_t *cgroups_subsystems);
 
+bool
+mount_is_idmapping_supported(void);
+
+int
+mount_idmapped(char *src, char *dst, int userns_fd);
+
 #endif /* MOUNT_H */

daemon/mount_idmapped.h
@@ -1,0 +1,194 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#ifndef MOUNT_IDMAPPED_H
+#define MOUNT_IDMAPPED_H
+
+#include <fcntl.h>
+#include <linux/types.h>
+#include <stdint.h>
+#include <sys/mount.h>
+#include <unistd.h>
+
+/**************************/
+#ifndef MOUNT_ATTR_RDONLY
+#define MOUNT_ATTR_RDONLY 0x00000001
+#endif
+
+#ifndef MOUNT_ATTR_NOSUID
+#define MOUNT_ATTR_NOSUID 0x00000002
+#endif
+
+#ifndef MOUNT_ATTR_NOEXEC
+#define MOUNT_ATTR_NOEXEC 0x00000008
+#endif
+
+#ifndef MOUNT_ATTR_NODIRATIME
+#define MOUNT_ATTR_NODIRATIME 0x00000080
+#endif
+
+#ifndef MOUNT_ATTR__ATIME
+#define MOUNT_ATTR__ATIME 0x00000070
+#endif
+
+#ifndef MOUNT_ATTR_RELATIME
+#define MOUNT_ATTR_RELATIME 0x00000000
+#endif
+
+#ifndef MOUNT_ATTR_NOATIME
+#define MOUNT_ATTR_NOATIME 0x00000010
+#endif
+
+#ifndef MOUNT_ATTR_STRICTATIME
+#define MOUNT_ATTR_STRICTATIME 0x00000020
+#endif
+
+#ifndef MOUNT_ATTR_IDMAP
+#define MOUNT_ATTR_IDMAP 0x00100000
+
+struct mount_attr {
+	__u64 attr_set;
+	__u64 attr_clr;
+	__u64 propagation;
+	__u64 userns_fd;
+};
+#endif
+
+#ifndef AT_RECURSIVE
+#define AT_RECURSIVE 0x8000
+#endif
+
+// clang-format off
+#ifndef __NR_mount_setattr
+	#if defined __alpha__
+		#define __NR_mount_setattr 552
+	#elif defined _MIPS_SIM
+		#if _MIPS_SIM == _MIPS_SIM_ABI32        /* o32 */
+			#define __NR_mount_setattr (442 + 4000)
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_NABI32       /* n32 */
+			#define __NR_mount_setattr (442 + 6000)
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_ABI64        /* n64 */
+			#define __NR_mount_setattr (442 + 5000)
+		#endif
+	#elif defined __ia64__
+		#define __NR_mount_setattr (442 + 1024)
+	#else
+		#define __NR_mount_setattr 442
+	#endif
+// clang-format on
+#endif
+
+#ifndef OPEN_TREE_CLONE
+#define OPEN_TREE_CLONE 1
+#endif
+
+#ifndef OPEN_TREE_CLOEXEC
+#define OPEN_TREE_CLOEXEC O_CLOEXEC
+#endif
+
+// clang-format off
+#ifndef __NR_open_tree
+	#if defined __alpha__
+		#define __NR_open_tree 538
+	#elif defined _MIPS_SIM
+		#if _MIPS_SIM == _MIPS_SIM_ABI32        /* o32 */
+			#define __NR_open_tree 4428
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_NABI32       /* n32 */
+			#define __NR_open_tree 6428
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_ABI64        /* n64 */
+			#define __NR_open_tree 5428
+		#endif
+	#elif defined __ia64__
+		#define __NR_open_tree (428 + 1024)
+	#else
+		#define __NR_open_tree 428
+	#endif
+#endif
+// clang-format on
+
+#ifndef MOVE_MOUNT_F_SYMLINKS
+#define MOVE_MOUNT_F_SYMLINKS 0x00000001
+#endif
+
+#ifndef MOVE_MOUNT_F_AUTOMOUNTS
+#define MOVE_MOUNT_F_AUTOMOUNTS 0x00000002
+#endif
+
+#ifndef MOVE_MOUNT_F_EMPTY_PATH
+#define MOVE_MOUNT_F_EMPTY_PATH 0x00000004
+#endif
+
+#ifndef MOVE_MOUNT_T_SYMLINKS
+#define MOVE_MOUNT_T_SYMLINKS 0x00000010
+#endif
+
+#ifndef MOVE_MOUNT_T_AUTOMOUNTS
+#define MOVE_MOUNT_T_AUTOMOUNTS 0x00000020
+#endif
+
+#ifndef MOVE_MOUNT_T_EMPTY_PATH
+#define MOVE_MOUNT_T_EMPTY_PATH 0x00000040
+#endif
+
+#ifndef MOVE_MOUNT__MASK
+#define MOVE_MOUNT__MASK 0x00000077
+#endif
+
+// clang-format off
+#ifndef __NR_move_mount
+	#if defined __alpha__
+		#define __NR_move_mount 539
+	#elif defined _MIPS_SIM
+		#if _MIPS_SIM == _MIPS_SIM_ABI32
+			#define __NR_move_mount 4429
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_NABI32
+			#define __NR_move_mount 6429
+		#endif
+		#if _MIPS_SIM == _MIPS_SIM_ABI64
+			#define __NR_move_mount 5429
+		#endif
+	#elif defined __ia64__
+		#define __NR_move_mount (428 + 1024)
+	#else
+		#define __NR_move_mount 429
+	#endif
+#endif
+// clang-format on
+
+int
+mount_setattr(int dirfd, const char *path, unsigned int flags, struct mount_attr *attr,
+	      size_t size);
+
+int
+open_tree(int dirfd, const char *path, unsigned int flags);
+
+int
+move_mount(int from_dirfd, const char *from_path, int to_dirfd, const char *to_path,
+	   unsigned int flags);
+
+#endif /* MOUNT_IDMAPPED_H */

commit cf91aaa5f4a4b65ad9798c5bfb0a60623f21fd82

daemon/mount: Fixed typo in mount_idmapped()
s/Sucessfully/Successfully/ in INFO log message of mount_idmapped().

Fixes: f8e91c7330ab ("daemon/mount: Introduce mount_idmapped()")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/mount.c
@@ -591,7 +591,7 @@ mount_idmapped(char *src, char *dst, int userns_fd)
 		return -1;
 	}
 
-	INFO("Sucessfully applied idmapping from src path %s on fd=%d for %s", src, mapped_tree_fd,
+	INFO("Successfully applied idmapping from src path %s on fd=%d for %s", src, mapped_tree_fd,
 	     dst);
 
 	if (move_mount(mapped_tree_fd, "", -1, dst, MOVE_MOUNT_F_EMPTY_PATH) == -1) {