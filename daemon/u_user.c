gyroidos/cml

commit 5e8105d0c174ffca2f413c622065e781efebf480

daemon/u_user: mount tmpfs to socket inside unit
Mount a tmpfs to "/run/socket/cml_unit/<unit_name>/" in cmld's mount
namespace. This is move-mounted inside the unit to "/run/socket".
The corresponding socket dir name can be retrieved by the unit
submodule through API exposed by new 'u_user.h' header file.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_user.c
@@ -23,11 +23,17 @@
 
 #define MOD_NAME "c_user"
 
+#define _GNU_SOURCE
+#include <sched.h>
+#include <sys/mount.h>
+
 #include "common/macro.h"
 #include "common/mem.h"
+#include "common/dir.h"
 #include "common/file.h"
 #include "common/ns.h"
 #include "unit.h"
+#include "u_user.h"
 
 #define UID_RANGE 1
 #define UID_RANGES_START 65537
@@ -41,11 +47,12 @@
 #define U_USER_MAP_FORMAT "%d %d %d"
 
 /* User structure with specific usernamespace mappings */
-typedef struct u_user {
+struct u_user {
 	unit_t *unit;  //!< unit which the u_user struct is associated to
 	int offset;    //!< gives information about the uid mapping to be set
 	int uid_start; //!< this is the start of uids and gids in the root namespace
-} u_user_t;
+	char *sock_dir;
+};
 
 /**
  * bool array, which globally holds assigend ranges in order to
@@ -131,6 +138,7 @@ u_user_new(compartment_t *compartment)
 	user->unit = compartment_get_extension_data(compartment);
 
 	user->uid_start = 0;
+	user->sock_dir = mem_printf("/run/socket/cml_unit/%s", unit_get_name(user->unit));
 
 	TRACE("new u_user struct was allocated");
 
@@ -191,6 +199,7 @@ u_user_free(void *usr)
 	u_user_t *user = usr;
 	ASSERT(user);
 
+	mem_free0(user->sock_dir);
 	mem_free0(user);
 }
 
@@ -203,6 +212,28 @@ u_user_setuid0(void)
 	return namespace_setuid0();
 }
 
+/*
+ * Move mount of socket tmpfs to /run/socket in child namespace
+ */
+static int
+u_user_start_child_early(void *usr)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	if (unshare(CLONE_NEWNS) == -1) {
+		WARN_ERRNO("Could not unshare mount namespace!");
+		return -COMPARTMENT_ERROR_USER;
+	}
+
+	if (mount(user->sock_dir, "/run/socket", NULL, MS_MOVE, NULL) < 0) {
+		ERROR_ERRNO("Could not move mount '%s' to '/run/socket'!", user->sock_dir);
+		return -COMPARTMENT_ERROR_USER;
+	}
+
+	return 0;
+}
+
 static int
 u_user_start_child(void *usr)
 {
@@ -212,6 +243,12 @@ u_user_start_child(void *usr)
 	if (u_user_setuid0() < 0) {
 		return -COMPARTMENT_ERROR_USER;
 	}
+
+	if (mount("proc", "/proc", "proc", MS_RELATIME | MS_NOSUID, NULL) < 0) {
+		ERROR_ERRNO("Could not remount /proc in unit %s", unit_get_description(user->unit));
+		return -COMPARTMENT_ERROR_USER;
+	}
+
 	return 0;
 }
 
@@ -224,12 +261,31 @@ u_user_start_pre_clone(void *usr)
 	u_user_t *user = usr;
 	ASSERT(user);
 
+	int ret = 0;
+
 	// reserve a new mapping
 	if (u_user_set_next_uid_range_start(user)) {
 		ERROR("Reserving uid range for userns for unit %s", unit_get_name(user->unit));
 		return -COMPARTMENT_ERROR_USER;
 	}
-	return 0;
+
+	char *sock_dir_parent = mem_printf("/run/socket/cml_unit/%s", unit_get_name(user->unit));
+	if (dir_mkdir_p(sock_dir_parent, 00777) < 0) {
+		ERROR_ERRNO("Could not mkdir %s", sock_dir_parent);
+		ret = -COMPARTMENT_ERROR_USER;
+		goto out;
+	}
+
+	if (mount("tmpfs", sock_dir_parent, "tmpfs", MS_RELATIME | MS_NOSUID, NULL) < 0) {
+		ERROR_ERRNO("Could not mount %s in unit %s", sock_dir_parent,
+			    unit_get_description(user->unit));
+		ret = -COMPARTMENT_ERROR_USER;
+		goto out;
+	}
+
+out:
+	mem_free0(sock_dir_parent);
+	return ret;
 }
 
 /**
@@ -252,13 +308,19 @@ u_user_start_post_clone(void *usr)
 	return 0;
 }
 
+const char *
+u_user_get_sock_dir(const u_user_t *user)
+{
+	return user->sock_dir;
+}
+
 static compartment_module_t u_user_module = {
 	.name = MOD_NAME,
 	.compartment_new = u_user_new,
 	.compartment_free = u_user_free,
 	.compartment_destroy = NULL,
 	.start_post_clone_early = NULL,
-	.start_child_early = NULL,
+	.start_child_early = u_user_start_child_early,
 	.start_pre_clone = u_user_start_pre_clone,
 	.start_post_clone = u_user_start_post_clone,
 	.start_pre_exec = NULL,

daemon/u_user.h
@@ -1,0 +1,29 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "unit.h"
+
+typedef struct u_user u_user_t;
+
+const char *
+u_user_get_sock_dir(const u_user_t *user);

commit a73c85f1d32a17294bbbffedc802b590570797a2

daemon/u_user: Remove unnecessary local variable sock_dir_parent
In u_user_start_pre_clone() instead of building the socket_dir
string by unit's name, use u_user_t's attribute sock_dir which
contains the same string.

Fixes: 5e8105d0c174 ("daemon/u_user: mount tmpfs to socket inside unit")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_user.c
@@ -267,48 +267,40 @@ u_user_start_pre_clone(void *usr)
 	u_user_t *user = usr;
 	ASSERT(user);
 
-	int ret = 0;
-
 	// reserve a new mapping
 	if (u_user_set_next_uid_range_start(user)) {
 		ERROR("Reserving uid range for userns for unit %s", unit_get_name(user->unit));
 		return -COMPARTMENT_ERROR_USER;
 	}
 
-	char *sock_dir_parent = mem_printf("/run/socket/cml_unit/%s", unit_get_name(user->unit));
 	/*
 	 * set world rwx only if idmapped mount is not supported
 	 * otherwise only allow root to write sock_dir
 	 */
 	mode_t mode = mount_is_idmapping_supported() ? 00755 : 00777;
-	if (dir_mkdir_p(sock_dir_parent, mode) < 0) {
-		ERROR_ERRNO("Could not mkdir %s (%o)", sock_dir_parent, mode);
-		ret = -COMPARTMENT_ERROR_USER;
-		goto out;
+	if (dir_mkdir_p(user->sock_dir, mode) < 0) {
+		ERROR_ERRNO("Could not mkdir %s (%o)", user->sock_dir, mode);
+		return -COMPARTMENT_ERROR_USER;
 	}
 
-	if (mount("tmpfs", sock_dir_parent, "tmpfs", MS_RELATIME | MS_NOSUID, NULL) < 0) {
-		ERROR_ERRNO("Could not mount %s in unit %s", sock_dir_parent,
+	if (mount("tmpfs", user->sock_dir, "tmpfs", MS_RELATIME | MS_NOSUID, NULL) < 0) {
+		ERROR_ERRNO("Could not mount %s in unit %s", user->sock_dir,
 			    unit_get_description(user->unit));
-		ret = -COMPARTMENT_ERROR_USER;
-		goto out;
+		return -COMPARTMENT_ERROR_USER;
 	}
 
 	// skip chowning if idmapping is supported
 	if (mount_is_idmapping_supported())
-		goto out;
+		return 0;
 
 	if (dir_chown_folder(unit_get_data_path(user->unit), user->uid_start, user->uid_start) <
 	    0) {
 		ERROR("Could not chown %s to target uid:gid (%d:%d)",
 		      unit_get_data_path(user->unit), user->uid_start, user->uid_start);
-		ret = -COMPARTMENT_ERROR_USER;
-		goto out;
+		return -COMPARTMENT_ERROR_USER;
 	}
 
-out:
-	mem_free0(sock_dir_parent);
-	return ret;
+	return 0;
 }
 
 /**