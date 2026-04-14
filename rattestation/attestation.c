gyroidos/cml

commit 5c44157e7f3fe120008b38490dd17ea2e6943790

rattestation: Implemented verification process
Implemented nonce verification, PCR verification,
signature verification

Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

rattestation/attestation.c
@@ -22,6 +22,7 @@
  */
 
 #include "attestation.pb-c.h"
+#include "config.pb-c.h"
 
 #include "common/macro.h"
 #include "common/mem.h"
@@ -30,11 +31,17 @@
 #include "common/sock.h"
 #include "common/fd.h"
 
+#include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/random.h>
 
 #include "attestation.h"
+#include "config.h"
+#include "common/ssl_util.h"
+
+#include "ibmtss/Unmarshal_fp.h"
 
 #define TPM2D_SERVICE_PORT "9505"
 
@@ -42,6 +49,7 @@ struct attestation_resp_cb_data {
 	void (*resp_verified_cb)(bool);
 	size_t nonce_len;
 	uint8_t *nonce;
+	char *config_file;
 };
 
 static char *
@@ -51,6 +59,9 @@ convert_bin_to_hex_new(const uint8_t *bin, int length)
 	len = MUL_WITH_OVERFLOW_CHECK(len, sizeof(char));
 	len = ADD_WITH_OVERFLOW_CHECK(len, 1);
 	char *hex = mem_alloc0(len);
+	if (!hex) {
+		FATAL("ERROR: Failed to allocate memory");
+	}
 
 	for (int i = 0; i < length; ++i) {
 		// remember snprintf additionally writs a '0' byte
@@ -61,16 +72,161 @@ convert_bin_to_hex_new(const uint8_t *bin, int length)
 }
 
 static bool
-attestation_verify_resp(Tpm2dToRemote *resp, uint8_t *nonce, size_t nonce_len)
+attestation_verify_resp(Tpm2dToRemote *resp, const char *config_file, uint8_t *nonce,
+			size_t nonce_len)
 {
-	DEBUG("Got Response from TPM2D");
+	ASSERT(config_file);
+	ASSERT(nonce);
+	ASSERT(resp);
+
+	bool ret = false;
+	uint8_t quote[resp->quoted.len];
+	uint8_t sig[resp->signature.len];
+	uint8_t *q = quote; // Required as TSS functions manipulate pointer
+	uint8_t *s = sig;   // Required as TSS functions manipulate pointer
+	uint32_t quote_len = resp->quoted.len;
+	uint32_t sig_len = resp->signature.len;
+	char *pcr_strings[resp->n_pcr_values];
+
+	// TODO Right now, the "good" values are read from the configuration file.
+	// Later, different methods could be implemented
+	RAttestationConfig *config = rattestation_read_config_new(config_file);
+	if (!config) {
+		ERROR("Failed to read config file %s. The file has to be provided as a command line argument",
+		      config_file);
+		return false;
+	}
+
+	DEBUG("Got Response from TPM2D, Response size=%zu",
+	      protobuf_c_message_get_packed_size((ProtobufCMessage *)resp));
 	protobuf_dump_message(STDOUT_FILENO, (ProtobufCMessage *)resp);
+
+	// Verficication Process
+
+	if (resp->has_quoted) {
+		char *quote_str = convert_bin_to_hex_new(resp->quoted.data, resp->quoted.len);
+		DEBUG("Quote (Length %ld): %s", resp->quoted.len, quote_str);
+		mem_free(quote_str);
+	} else {
+		ERROR("Response does not contain quote to be verified");
+		ret = false;
+		goto err;
+	}
+
+	if (resp->has_signature) {
+		char *sig_str = convert_bin_to_hex_new(resp->signature.data, resp->signature.len);
+		DEBUG("Signature (Length %ld): %s\n", resp->signature.len, sig_str);
+		mem_free(sig_str);
+	} else {
+		ERROR("Response does not contain signature");
+		goto err;
+	}
+
+	// The TSS library manipulates the quote and signature buffers. Copy the buffers
+	// in order to enable a clean freeing of the protobuf response
+	memcpy(quote, resp->quoted.data, resp->quoted.len);
+	memcpy(sig, resp->signature.data, resp->signature.len);
+
+	// Convert to hex string representation to directly compare with specified values
+	// in the configuration file
+	for (size_t i = 0; i < resp->n_pcr_values; i++) {
+		pcr_strings[i] = convert_bin_to_hex_new(resp->pcr_values[i]->value.data,
+							resp->pcr_values[i]->value.len);
+	}
+
+	DEBUG("Verifying Response...");
+
+	DEBUG("Hash Algorithm: SHA%d", config->halg * 8);
+
+	// The pcrs are configured as hex strings
+	size_t pcr_string_len = config->halg * 2;
+
+	for (size_t i = 0; i < config->n_pcr_values; i++) {
+		if (strlen(pcr_strings[i]) != pcr_string_len) {
+			ERROR("Length of configured PCR value %ld invalid (%ld, must be %ld)", i,
+			      strlen(pcr_strings[i]), pcr_string_len);
+			ret = false;
+			goto err;
+		}
+		if (strlen(config->pcr_values[i]->value) != pcr_string_len) {
+			ERROR("Length of received PCR value %ld invalid (%ld, must be %ld)", i,
+			      strlen(config->pcr_values[i]->value), pcr_string_len);
+			ret = false;
+			goto err;
+		}
+		if (strncmp(pcr_strings[i], config->pcr_values[i]->value, pcr_string_len)) {
+			DEBUG("PCR_%ld: %s - VERIFICATION FAILED", i, pcr_strings[i]);
+			ret = false;
+			goto err;
+		}
+		DEBUG("PCR_%ld: %s - VERIFICATION SUCCESSFUL", i, pcr_strings[i]);
+	}
+
+	// The quote is sent as a TPMS_ATTEST and has to be unmarshalled
+	TPMS_ATTEST tpms_attest;
+	TPM_RC rc = TSS_TPMS_ATTEST_Unmarshalu(&tpms_attest, &q, &quote_len);
+	if (rc != TPM_RC_SUCCESS) {
+		printf("TSS_TPMS_ATTEST_Unmarshalu returned error code %u\n", rc);
+		ret = false;
+		goto err;
+	}
+
+	// Nonce verification
+	int ret_nonce = memcmp(tpms_attest.extraData.t.buffer, nonce, nonce_len);
+
 	char *nonce_str = convert_bin_to_hex_new(nonce, nonce_len);
-	INFO("Response for Nonce %s, Response size=%zu", nonce_str,
-	     protobuf_c_message_get_packed_size((ProtobufCMessage *)resp));
+	char *rcv_nonce_str = convert_bin_to_hex_new(tpms_attest.extraData.t.buffer,
+						     tpms_attest.extraData.t.size);
+	DEBUG("Nonce (sent %s, received %s) - %s", nonce_str, rcv_nonce_str,
+	      ret_nonce ? "VERIFICATION FAILED" : "VERIFICATION SUCCESSFUL");
 	mem_free(nonce_str);
-	//TODO implement verification process
-	return true;
+	mem_free(rcv_nonce_str);
+
+	// The signature is sent as a TPMT_SIGNATURE and has to be unmarshalled
+	TPMT_SIGNATURE tpmt_signature;
+	rc = TSS_TPMT_SIGNATURE_Unmarshalu(&tpmt_signature, &s, &sig_len, false);
+	if (rc != TPM_RC_SUCCESS) {
+		printf("TSS_TPMT_SIGNATURE_Unmarshalu returned error code %u\n", rc);
+		// TODO Error Handling
+		ret = false;
+		goto err;
+	}
+
+	// Verify signature
+	ssl_init(false, NULL);
+
+	int retssl = ssl_verify_signature_from_buf((const char *)resp->certificate.data,
+						   tpmt_signature.signature.rsapss.sig.t.buffer,
+						   tpmt_signature.signature.rsapss.sig.t.size,
+						   resp->quoted.data, resp->quoted.len, "SHA256");
+	if (retssl != 0) {
+		// TODO error handling
+		ERROR("VERIFY SIGNATURE FAILED");
+		ret = false;
+		goto err;
+	} else {
+		INFO("VERIFY SIGNATURE SUCCESSFUL");
+	}
+
+	// TODO Implement PCR10 and PCR11 verification of measurement lists (kernel module signatures
+	// and containers)
+	WARN("WARN: Measurement List Verification not yet implemented");
+
+	ret = true;
+
+err:
+	DEBUG("---------------------------");
+	DEBUG("REMOTE ATTESTATION: %s", ret ? "SUCCESSFUL" : "FAILED");
+	DEBUG("---------------------------");
+
+	// Free resources
+	for (size_t i = 0; i < resp->n_pcr_values; i++) {
+		mem_free(pcr_strings[i]);
+	}
+	protobuf_free_message((ProtobufCMessage *)config);
+	ssl_free();
+
+	return ret;
 }
 
 static void
@@ -90,7 +246,8 @@ attestation_response_recv_cb(int fd, unsigned events, event_io_t *io, void *data
 		(Tpm2dToRemote *)protobuf_recv_message(fd, &tpm2d_to_remote__descriptor);
 	IF_NULL_GOTO_ERROR(resp, cleanup);
 
-	verified = attestation_verify_resp(resp, resp_cb_data->nonce, resp_cb_data->nonce_len);
+	verified = attestation_verify_resp(resp, resp_cb_data->config_file, resp_cb_data->nonce,
+					   resp_cb_data->nonce_len);
 
 	protobuf_free_message((ProtobufCMessage *)resp);
 	INFO("Handled response on connection %d", fd);
@@ -109,15 +266,21 @@ cleanup:
 }
 
 int
-attestation_do_request(const char *host, void (*resp_verified_cb)(bool))
+attestation_do_request(const char *host, char *config_file, void (*resp_verified_cb)(bool))
 {
-	uint8_t nonce[] = { 0xde, 0xad, 0xbe, 0xef }; //FIXME generate a real nonce here
-	size_t nonce_len = 4;
+	// Set nonce
+	size_t nonce_len = 8;
+	uint8_t nonce[nonce_len];
+	if (getrandom(nonce, nonce_len, (unsigned int)0) != (ssize_t)nonce_len) {
+		ERROR("Failed to create attestation request: Failed to retrieve random nonce from /dev/urandom");
+		return -1;
+	}
 
 	// build RemoteToTpm2d message
 	RemoteToTpm2d msg = REMOTE_TO_TPM2D__INIT;
 
 	msg.code = REMOTE_TO_TPM2D__CODE__ATTESTATION_REQ;
+	msg.has_qualifyingdata = true;
 	msg.qualifyingdata.data = nonce;
 	msg.qualifyingdata.len = nonce_len;
 
@@ -129,6 +292,8 @@ attestation_do_request(const char *host, void (*resp_verified_cb)(bool))
 	ssize_t msg_size = protobuf_send_message(sock, (ProtobufCMessage *)&msg);
 	IF_TRUE_RETVAL(msg_size < 0, -1);
 
+	INFO("Send message with size %ld", msg_size);
+
 	char *nonce_str = convert_bin_to_hex_new(nonce, nonce_len);
 	INFO("Request with Nonce %s, Request size=%zd", nonce_str, msg_size);
 	mem_free(nonce_str);
@@ -139,6 +304,7 @@ attestation_do_request(const char *host, void (*resp_verified_cb)(bool))
 	resp_cb_data->nonce = mem_new0(uint8_t, nonce_len);
 	memcpy(resp_cb_data->nonce, nonce, nonce_len);
 	resp_cb_data->nonce_len = nonce_len;
+	resp_cb_data->config_file = config_file;
 
 	DEBUG("Register Response handler on sockfd=%d", sock);
 	fd_make_non_blocking(sock);

common/ssl_util.c
@@ -22,7 +22,6 @@
  */
 
 #include "ssl_util.h"
-#include "tpm2d_shared.h"
 
 #include "common/macro.h"
 #include "common/mem.h"

rattestation/Makefile
@@ -26,7 +26,7 @@ CC ?= gcc
 DEVELOPMENT_BUILD ?= y
 AGGRESSIVE_WARNINGS ?= n
 SANITIZERS ?= n
-STATIC = -static
+STATIC = # -static TODO OpenSSL causes segfault with -static
 
 LOCAL_CFLAGS := -std=gnu99 -Icommon -I.. -I../include -pedantic -O2
 LOCAL_CFLAGS += -Wall -Wextra -Wcast-align -Wformat -Wformat-security -fstack-protector-all -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -fpic -pie
@@ -41,11 +41,11 @@ endif
 ifeq ($(AGGRESSIVE_WARNINGS),y)
     # on CI (and also for well-behaved developers) warnings should be
     # converted to errors; this helps us redistribute the code base without any pain;
-    # pure builds are better than polluted builds. 
+    # pure builds are better than polluted builds.
     LOCAL_CFLAGS += -Werror
 endif
 ifeq ($(SANITIZERS),y)
-    # if requested, we enable sanitization for easier debugging 
+    # if requested, we enable sanitization for easier debugging
     # this requires libasan libasan-static libubsan libubsan-static
     # to be installed on the build host
     LOCAL_CFLAGS += -fsanitize=address -fsanitize=undefined
@@ -56,7 +56,8 @@ endif
 
 
 PROTO_SRC := \
-	attestation.pb-c.c
+	attestation.pb-c.c \
+	config.pb-c.c
 
 SRC_FILES := \
 	common/protobuf.c \
@@ -64,13 +65,15 @@ SRC_FILES := \
 	common/sock.c \
 	common/file.c \
 	attestation.c \
+    common/ssl_util.c \
+    config.c \
 	main.c
 
 .PHONY: all
 all: rattestation
 
 protobuf: attestation.proto
-	protoc-c --c_out=. attestation.proto
+	protoc-c --c_out=. attestation.proto config.proto
 	$(MAKE) -C common protobuf
 
 $(PROTO_SRC): protobuf
@@ -79,7 +82,9 @@ libcommon:
 	$(MAKE) -C common libcommon
 
 rattestation: libcommon $(SRC_FILES) $(PROTO_SRC)
-	$(CC) $(STATIC) $(LOCAL_CFLAGS) $(SRC_FILES) $(PROTO_SRC) -lprotobuf-c -lprotobuf-c-text -Lcommon -lcommon -o $@
+	$(CC) $(STATIC) $(LOCAL_CFLAGS) $(SRC_FILES) $(PROTO_SRC) -lprotobuf-c -lprotobuf-c-text -Lcommon -lcommon -lssl -lcrypto -libmtss -o $@
+
+
 
 
 .PHONY: clean

rattestation/README
@@ -1,8 +1,0 @@
-Simple Attestation client which connects to a remote tpm2d
-on its TCP/IP service socket (port 9505) and asks for an attest.
-
-compile:
-	make
-run:
-	./attestation <remote host>
-	default is 127.0.0.1

rattestation/attestation.h
@@ -10,5 +10,5 @@
  * provided resp_verivied_cb function.
  */
 int
-attestation_do_request(const char *host, void (*resp_verified_cb)(bool));
+attestation_do_request(const char *host, char *config_file, void (*resp_verified_cb)(bool));
 #endif /* IP_AGENT_ATTESTATION_H */

rattestation/config.c
@@ -34,37 +34,42 @@
 RAttestationConfig *
 rattestation_read_config_new(const char *file)
 {
-    RAttestationConfig *config = NULL;
+	RAttestationConfig *config = NULL;
 
-    off_t conf_len = 0;
-    uint8_t *buf = NULL;
-    ASSERT(file);
+	off_t conf_len = 0;
+	uint8_t *buf = NULL;
+	ASSERT(file);
 
-    IF_FALSE_GOTO(file_exists(file), err);
+	DEBUG("Checking if config file %s exists", file);
 
-    size_t file_len = strlen(file);
+	IF_FALSE_GOTO(file_exists(file), err);
 
-    IF_TRUE_GOTO(file_len < 5 || strcmp(file + file_len - 5, ".conf"), err);
+	DEBUG("File exists");
 
-    conf_len = file_size(file);
-    if (conf_len <= 0) {
-        WARN("Failed to get file-size of configuration file");
-        goto err;
-    }
+	size_t file_len = strlen(file);
 
-    buf = mem_alloc(conf_len);
-    if (-1 == file_read(file, (char *)buf, conf_len)) {
-        WARN("Failed to read rattestation configuration file");
-        goto err;
-    }
+	IF_TRUE_GOTO(file_len < 5 || strcmp(file + file_len - 5, ".conf"), err);
 
-    config = (RAttestationConfig *)protobuf_message_new_from_buf(buf, conf_len, &rattestation_config__descriptor);
+	conf_len = file_size(file);
+	if (conf_len <= 0) {
+		WARN("Failed to get file-size of configuration file");
+		goto err;
+	}
 
-    if(!config) {
-        WARN("Failed to load rattestation configuration");
-    }
+	buf = mem_alloc(conf_len);
+	if (-1 == file_read(file, (char *)buf, conf_len)) {
+		WARN("Failed to read rattestation configuration file");
+		goto err;
+	}
+
+	config = (RAttestationConfig *)protobuf_message_new_from_buf(
+		buf, conf_len, &rattestation_config__descriptor);
+
+	if (!config) {
+		WARN("Failed to load rattestation configuration");
+	}
 
 err:
-    mem_free(buf);
-    return config;
+	mem_free(buf);
+	return config;
 }

rattestation/main.c
@@ -33,6 +33,18 @@
 
 #include "attestation.h"
 
+#include <openssl/err.h>
+#include <openssl/sha.h>
+#include <openssl/pkcs12.h>
+#include <openssl/rand.h>
+#include <openssl/pem.h>
+#include <openssl/conf.h>
+#include <openssl/x509v3.h>
+#include <openssl/rsa.h>
+#include <openssl/engine.h>
+#include <openssl/bio.h>
+#include <openssl/x509_vfy.h>
+
 #define LOGFILE_DIR "/data/logs"
 #define LOGFILE_PATH LOGFILE_DIR "/rattestation"
 
@@ -81,6 +93,7 @@ main(int argc, char **argv)
 	logf_handler_set_prio(ipagent_logfile_handler_stdout, LOGF_PRIO_TRACE);
 
 	char *rhost = (argc < 2) ? "127.0.0.1" : argv[1];
+	char *config_file = (argc < 3) ? "rattestation.conf" : argv[2];
 
 	event_init();
 
@@ -92,7 +105,7 @@ main(int argc, char **argv)
 	 * do attestation and register the main_retrun_result_and_exit handler
 	 * as callback when the response has been validated
 	 */
-	if (-1 == attestation_do_request(rhost, main_return_result_and_exit)) {
+	if (-1 == attestation_do_request(rhost, config_file, main_return_result_and_exit)) {
 		ERROR("Connection to remote host %s failed!", rhost);
 		main_return_result_and_exit(false);
 	}

tpm2d/attestation.proto
@@ -83,10 +83,11 @@ message Tpm2dToRemote {
 	// the hash algorith used to sign the quoted information
 	optional HashAlgLen halg = 3;
 
-	// the quoted information (a TPM2B_ATTEST data structure)
+	// the quoted information (a TPMS_ATTEST data structure)
 	optional bytes quoted = 4;
 
-	// the signature over 'quoted' (a TPMT_SIGNATURE data structure)
+	// the signature over 'quoted' (a TPMT_SIGNATURE data structure with an
+	// TPMS_SIGNATURE_RSAPSS signature algorithm)
 	optional bytes signature = 5;
 
 	// the explicit PCR values

commit 1329c915d5fbff2a7e59f47b837301402be22a77

rattestation/attestation: allocate quote, signature and pcr on heap
In attestation_verify_resp(), allocate buffers on heap for quote,
signature and pcr instead of using stack allocated variable length
arrays. Further, make the allocations after the check if the protobuf
resp has the actual length parameters set.

Fixes: 5c44157e7f3f ("rattestation: Implemented verification process")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

rattestation/attestation.c
@@ -66,13 +66,13 @@ attestation_verify_resp(Tpm2dToRemote *resp, RAttestationConfig *config, uint8_t
 	ASSERT((config->halg == SHA256_DIGEST_LENGTH) || (config->halg == SHA_DIGEST_LENGTH));
 
 	bool ret = false;
-	uint8_t quote[resp->quoted.len];
-	uint8_t sig[resp->signature.len];
-	uint8_t *q = quote; // Required as TSS functions manipulate pointer
-	uint8_t *s = sig;   // Required as TSS functions manipulate pointer
-	uint32_t quote_len = resp->quoted.len;
-	uint32_t sig_len = resp->signature.len;
-	uint8_t pcr[resp->n_pcr_values][config->halg];
+	uint8_t *quote = NULL;
+	uint8_t *q = NULL;
+	uint8_t *sig = NULL;
+	uint8_t *s = NULL;
+	uint32_t quote_len;
+	uint32_t sig_len;
+	uint8_t **pcr = NULL;
 
 	if (resp->has_quoted) {
 		INFO("Response contains quote (Length %zu)", resp->quoted.len);
@@ -91,6 +91,15 @@ attestation_verify_resp(Tpm2dToRemote *resp, RAttestationConfig *config, uint8_t
 		goto err;
 	}
 
+	quote_len = resp->quoted.len;
+	sig_len = resp->signature.len;
+
+	quote = mem_alloc0(resp->quoted.len);
+	sig = mem_alloc0(resp->signature.len);
+
+	q = quote; // Required as TSS functions manipulate pointer
+	s = sig;   // Required as TSS functions manipulate pointer
+
 	// The TSS library manipulates the quote and signature buffers. Copy the buffers
 	// in order to enable a clean freeing of the protobuf response
 	memcpy(quote, resp->quoted.data, resp->quoted.len);
@@ -105,6 +114,8 @@ attestation_verify_resp(Tpm2dToRemote *resp, RAttestationConfig *config, uint8_t
 		ret = false;
 		goto err;
 	}
+
+	pcr = mem_alloc0(MUL_WITH_OVERFLOW_CHECK(resp->n_pcr_values, config->halg));
 
 	bool ret_pcr = true;
 	for (size_t i = 0; i < config->n_pcr_values; i++) {
@@ -300,6 +311,9 @@ err:
 
 	// Free resources
 	ssl_free();
+	mem_free0(quote);
+	mem_free0(sig);
+	mem_free0(pcr);
 
 	return ret;
 }