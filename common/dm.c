gyroidos/cml

commit 3e431b6ec6660ec42aff6f4ead2ff050b8e648ab

common: Refactoring
Refactored common functionality from cryptfs into
new module 'dm' to be used by a future module for
dm-verity. Refactored loopdev functions.

Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

common/dm.c
@@ -1,0 +1,183 @@
+
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2022 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
+#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
+
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <linux/ioctl.h>
+#include <linux/unistd.h>
+#include <linux/dm-ioctl.h>
+#include <libgen.h>
+#include <stdlib.h>
+#include <sys/mount.h>
+#include <errno.h>
+#include <stdint.h>
+
+#include "macro.h"
+#include "mem.h"
+#include "dm.h"
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+struct dm_cmd_table cmd_table[] = {
+	{ DM_DEV_CREATE, { 4, 0, 0 } },	  { DM_TABLE_LOAD, { 4, 0, 0 } },
+	{ DM_DEV_REMOVE, { 4, 0, 0 } },	  { DM_REMOVE_ALL, { 4, 0, 0 } },
+	{ DM_DEV_SUSPEND, { 4, 0, 0 } },  { DM_DEV_STATUS, { 4, 0, 0 } },
+	{ DM_TABLE_DEPS, { 4, 0, 0 } },	  { DM_DEV_RENAME, { 4, 0, 0 } },
+	{ DM_VERSION, { 4, 0, 0 } },	  { DM_TABLE_STATUS, { 4, 0, 0 } },
+	{ DM_DEV_WAIT, { 4, 0, 0 } },	  { DM_LIST_DEVICES, { 4, 0, 0 } },
+	{ DM_TABLE_CLEAR, { 4, 0, 0 } },  { DM_LIST_VERSIONS, { 4, 1, 0 } },
+	{ DM_TARGET_MSG, { 4, 2, 0 } },	  { DM_DEV_SET_GEOMETRY, { 4, 6, 0 } },
+	{ DM_DEV_ARM_POLL, { 4, 36, 0 } }
+};
+
+#ifndef __GNU_LIBRARY__
+/*
+ * non glibc std libraries such as musl provide ioctl (int, int, ...)
+ * wrapper. However, dm integrity requests are 'unsigned long int' and
+ * would overflow on a cast to int. Thus, we directly provide a wrapper
+ * here instead of using the ioctl wrapper of the std library.
+ */
+int
+dm_ioctl(int fd, unsigned long int request, ...)
+{
+	void *args;
+	va_list ap;
+	int result;
+	va_start(ap, request);
+	args = va_arg(ap, void *);
+	result = syscall(__NR_ioctl, fd, request, args);
+	va_end(ap);
+	return result;
+}
+#endif
+
+int
+dm_ioctl_init(struct dm_ioctl *io, enum dm_cmd_index idx, size_t data_size, const char *name,
+	      const char *uuid, unsigned flags, unsigned long long dev, unsigned int target_count,
+	      unsigned int event_nr)
+{
+	if (idx > ARRAY_SIZE(cmd_table)) {
+		ERROR("Failed to lookup ioctl command");
+		return -1;
+	}
+
+	mem_memset(io, 0, data_size);
+	io->data_size = data_size;
+	io->data_start = sizeof(struct dm_ioctl);
+	io->version[0] = cmd_table[idx].version[0];
+	io->version[1] = cmd_table[idx].version[1];
+	io->version[2] = cmd_table[idx].version[2];
+	io->flags = flags;
+	io->dev = dev;
+	io->target_count = target_count;
+	io->event_nr = event_nr;
+
+	if (name)
+		strncpy(io->name, name, sizeof(io->name) - 1);
+
+	if (uuid)
+		strncpy(io->uuid, uuid, sizeof(io->name) - 1);
+
+	return 0;
+}
+
+uint64_t
+dm_get_blkdev_size64(int fd)
+{
+	uint64_t size;
+
+	if ((dm_ioctl(fd, BLKGETSIZE64, &size)) == -1) {
+		ERROR_ERRNO("BLKGETSIZE64 ioctl failed");
+		size = 0;
+	}
+	return size;
+}
+
+int
+dm_get_blkdev_sector_size(int fd)
+{
+	int sec_size;
+
+	if ((dm_ioctl(fd, BLKSSZGET, &sec_size)) == -1) {
+		ERROR_ERRNO("BLKSSZGET ioctl failed");
+		sec_size = -1;
+	}
+	return sec_size;
+}
+
+int
+dm_get_blkdev_readonly(int fd)
+{
+	int read_only;
+
+	if ((dm_ioctl(fd, BLKROGET, &read_only)) == -1) {
+		ERROR_ERRNO("BLKROGET ioctl failed");
+		read_only = -1;
+	}
+	return read_only;
+}
+
+int
+dm_read_version(void)
+{
+	int control_fd = -1;
+	uint8_t buf[16384] = { 0 };
+	struct dm_ioctl *dmi = NULL;
+
+	dmi = (struct dm_ioctl *)buf;
+	dm_ioctl_init(dmi, INDEX_DM_VERSION, sizeof(buf), NULL, NULL, DM_EXISTS_FLAG, 0, 0, 0);
+	int ioctl_ret = dm_ioctl(control_fd, cmd_table[INDEX_DM_VERSION].cmd, dmi);
+	if (ioctl_ret != 0) {
+		ERROR_ERRNO("DM_VERSION ioctl returned %d", ioctl_ret);
+		close(control_fd);
+		return -1;
+	}
+
+	TRACE("DM Version: %d.%d.%d", dmi->version[0], dmi->version[1], dmi->version[2]);
+	return 0;
+}
+
+int
+dm_list_versions(void)
+{
+	int control_fd = -1;
+	uint8_t buf[16384] = { 0 };
+	struct dm_ioctl *dmi = NULL;
+
+	dmi = (struct dm_ioctl *)buf;
+	dm_ioctl_init(dmi, INDEX_DM_LIST_VERSIONS, sizeof(buf), NULL, NULL, DM_EXISTS_FLAG, 0, 0,
+		      0);
+	int ioctl_ret = dm_ioctl(control_fd, cmd_table[INDEX_DM_LIST_VERSIONS].cmd, dmi);
+	if (ioctl_ret != 0) {
+		ERROR_ERRNO("DM_VERSION ioctl returned %d", ioctl_ret);
+		close(control_fd);
+		return -1;
+	}
+
+	return 0;
+}

common/Makefile
@@ -87,6 +87,7 @@ OBJS_COMMON_FULL := \
 OBJS_COMMON_EXT := \
 	$(OBJS_COMMON_FULL) \
 	cryptfs.o \
+	dm.o \
 	hex.o \
 	reboot.o \
 	ssl_util.o \

common/cryptfs.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of trust|me
- * Copyright(c) 2013 - 2021 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2022 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -39,20 +39,14 @@
 #include <sys/mount.h>
 #include <errno.h>
 #include <linux/kdev_t.h>
+#include <stdint.h>
 
 #include "cryptfs.h"
 #include "macro.h"
 #include "mem.h"
 #include "proc.h"
 #include "file.h"
-
-#ifdef ANDROID
-#define DEV_MAPPER "/dev/device-mapper"
-#define CRYPT_PATH_PREFIX "/dev/block/dm-"
-#else
-#define DEV_MAPPER "/dev/mapper/control"
-#define CRYPT_PATH_PREFIX "/dev/mapper/"
-#endif
+#include "dm.h"
 
 #define TABLE_LOAD_RETRIES 10
 #define INTEGRITY_TAG_SIZE 32
@@ -74,59 +68,10 @@
 static unsigned long
 get_provided_data_sectors(const char *real_blk_name);
 
-#ifdef __GNU_LIBRARY__
-#define dm_ioctl(...) ioctl(__VA_ARGS__)
-#else
-/*
- * non glibc std libraries such as musl provide ioctl (int, int, ...)
- * wrapper. However, dm integrity requests are 'unsigned long int' and
- * would overflow on a cast to int. Thus, we directly provide a wrapper
- * here instead of using the ioctl wrapper of the std library.
- */
-static int
-dm_ioctl(int fd, unsigned long int request, ...)
-{
-	void *args;
-	va_list ap;
-	int result;
-	va_start(ap, request);
-	args = va_arg(ap, void *);
-	result = syscall(__NR_ioctl, fd, request, args);
-	va_end(ap);
-	return result;
-}
-#endif
-
-static void
-ioctl_init(struct dm_ioctl *io, size_t dataSize, const char *name, unsigned flags)
-{
-	mem_memset(io, 0, dataSize);
-	io->data_size = dataSize;
-	io->data_start = sizeof(struct dm_ioctl);
-	io->version[0] = 4;
-	io->version[1] = 0;
-	io->version[2] = 0;
-	io->flags = flags;
-
-	if (name)
-		strncpy(io->name, name, sizeof(io->name) - 1);
-}
-
-static unsigned long
-get_blkdev_size(int fd)
-{
-	unsigned long nr_sec;
-
-	if ((dm_ioctl(fd, BLKGETSIZE, &nr_sec)) == -1) {
-		nr_sec = 0;
-	}
-	return nr_sec;
-}
-
 char *
 cryptfs_get_device_path_new(const char *label)
 {
-	return mem_printf("%s%s", CRYPT_PATH_PREFIX, label);
+	return mem_printf("%s%s", DM_PATH_PREFIX, label);
 }
 
 #if 0
@@ -176,7 +121,8 @@ load_integrity_mapping_table(int fd, const char *real_blk_name, const char *meta
 	tgt = (struct dm_target_spec *)&mapping_buffer[sizeof(struct dm_ioctl)];
 
 	// Configure parameters for ioctl
-	ioctl_init(mapping_io, DM_INTEGRITY_BUF_SIZE, name, 0);
+	dm_ioctl_init(mapping_io, INDEX_DM_TABLE_LOAD, DM_INTEGRITY_BUF_SIZE, name, NULL, 0, 0, 0,
+		      0);
 	mapping_io->target_count = 1;
 	tgt->status = 0;
 	tgt->sector_start = 0;
@@ -245,7 +191,8 @@ load_crypto_mapping_table(int fd, const char *real_blk_name, const char *master_
 	/* Load the mapping table for this device */
 	tgt = (struct dm_target_spec *)&buffer[sizeof(struct dm_ioctl)];
 
-	ioctl_init(io, DM_CRYPT_BUF_SIZE, name, DM_EXISTS_FLAG);
+	dm_ioctl_init(io, INDEX_DM_TABLE_LOAD, DM_CRYPT_BUF_SIZE, name, NULL, DM_EXISTS_FLAG, 0, 0,
+		      0);
 	io->target_count = 1;
 	tgt->status = 0;
 	tgt->sector_start = 0;
@@ -281,29 +228,15 @@ load_crypto_mapping_table(int fd, const char *real_blk_name, const char *master_
 	}
 }
 
-// This function does:
-//		Create an integrity block device with IOCTL(DM_DEV_CREATE)
-//		Next load the mapping table of this device with IOCTL(DM_TABLE_LOAD)
-//		Lastly resume the device with IOCTL(DM_DEV_SUSPEND)
-//		Only then the crypto device can be mounted ontop of the integrity device
-//
-//	This general functionality is adapted from [1,2] and strace of the dmsetup setup
-//	When executing this functionality on its on (in a separate .c file), on a host system it works
-//	Verified by:
-//		user@host:~/$ dmsetup ls --tree
-//			00000000-0000-0000-0000-000000000000-etc (252:1)
-//			 └─00000000-0000-0000-0000-000000000000-etc-integrity (252:0)
-//			    └─ (7:3)
-
-//
-//	And:
-//      user@host:~/$ sudo dmsetup table
-//            dmsetup table
-//				0[...]0-etc-integrity: 0 1677721 integrity 7:3 0 32 J 5 journal_sectors:16368 interleave_sectors:32768 buffer_sectors:128 journal_watermark:50 commit_time:10000
-//				0[...]0-etc: 0 1677721 crypt capi:aegis128-random 00000000000000000000000000000000 0 252:0 0 1 integrity:32:aead
-//
-// [1] https://www.kernel.org/doc/html/latest/admin-guide/device-mapper/dm-integrity.html
-// [2] https://wiki.gentoo.org/wiki/Device-mapper#Integrity
+/**
+ * Creates an integrity block device with DM_DEV_CREATE ioctl, reloads the mapping
+ * table with DM_TABLE_LOADE ioctl and resumes the device with DM_DEV_SUSPEND ioctl
+ *
+ * [1] https://www.kernel.org/doc/html/latest/admin-guide/device-mapper/dm-integrity.html
+ * [2] https://wiki.gentoo.org/wiki/Device-mapper#Integrity
+ *
+ * @return int 0 if successful, otherwise -1
+ */
 static int
 create_integrity_blk_dev(const char *real_blk_name, const char *meta_blk_name, const char *name,
 			 const unsigned long fs_size)
@@ -316,14 +249,15 @@ create_integrity_blk_dev(const char *real_blk_name, const char *meta_blk_name, c
 	int create_counter;
 
 	// Open device mapper
-	if ((fd = open(DEV_MAPPER, O_RDWR)) < 0) {
+	if ((fd = open(DM_CONTROL, O_RDWR)) < 0) {
 		ERROR_ERRNO("Cannot open device-mapper");
 	}
 
 	// Create blk device
 	// Initialize create_io struct
 	create_io = (struct dm_ioctl *)create_buffer;
-	ioctl_init(create_io, DM_INTEGRITY_BUF_SIZE, name, 0);
+	dm_ioctl_init(create_io, INDEX_DM_DEV_CREATE, DM_INTEGRITY_BUF_SIZE, name, NULL, 0, 0, 0,
+		      0);
 
 	for (create_counter = 0; create_counter < TABLE_LOAD_RETRIES; create_counter++) {
 		ioctl_ret = dm_ioctl(fd, DM_DEV_CREATE, create_io);
@@ -353,7 +287,8 @@ create_integrity_blk_dev(const char *real_blk_name, const char *meta_blk_name, c
 
 	// Resume this device to activate it
 	DEBUG("Resuming the blk device");
-	ioctl_init(create_io, DM_INTEGRITY_BUF_SIZE, name, 0);
+	dm_ioctl_init(create_io, INDEX_DM_DEV_SUSPEND, DM_INTEGRITY_BUF_SIZE, name, NULL, 0, 0, 0,
+		      0);
 
 	ioctl_ret = dm_ioctl(fd, DM_DEV_SUSPEND, create_io);
 	if (ioctl_ret != 0) {
@@ -384,13 +319,13 @@ create_crypto_blk_dev(const char *real_blk_name, const char *master_key, const c
 	int ioctl_ret;
 
 	DEBUG("Creating crypto blk device");
-	if ((fd = open(DEV_MAPPER, O_RDWR)) < 0) {
+	if ((fd = open(DM_CONTROL, O_RDWR)) < 0) {
 		ERROR("Cannot open device-mapper\n");
 		goto errout;
 	}
 
 	io = (struct dm_ioctl *)buffer;
-	ioctl_init(io, DM_CRYPT_BUF_SIZE, name, 0);
+	dm_ioctl_init(io, INDEX_DM_DEV_CREATE, DM_CRYPT_BUF_SIZE, name, NULL, 0, 0, 0, 0);
 
 	for (i = 0; i < TABLE_LOAD_RETRIES; i++) {
 		ioctl_ret = dm_ioctl(fd, DM_DEV_CREATE, io);
@@ -418,7 +353,7 @@ create_crypto_blk_dev(const char *real_blk_name, const char *master_key, const c
 	}
 
 	/* Resume this device to activate it */
-	ioctl_init(io, DM_CRYPT_BUF_SIZE, name, 0);
+	dm_ioctl_init(io, INDEX_DM_DEV_SUSPEND, DM_CRYPT_BUF_SIZE, name, NULL, 0, 0, 0, 0);
 
 	if (dm_ioctl(fd, DM_DEV_SUSPEND, io)) {
 		ERROR_ERRNO("Cannot resume the dm-crypt device\n");
@@ -435,14 +370,14 @@ errout:
 }
 
 /* TODO:
- * maybe we need to wait a bit before device is created 
+ * maybe we need to wait a bit before device is created
  */
 static char *
 create_device_node(const char *name)
 {
 	char *buffer = mem_new(char, DEVMAPPER_BUFFER_SIZE);
 
-	int fd = open(DEV_MAPPER, O_RDWR);
+	int fd = open(DM_CONTROL, O_RDWR);
 	if (fd < 0) {
 		ERROR_ERRNO("Error opening devmapper");
 		mem_free0(buffer);
@@ -451,7 +386,7 @@ create_device_node(const char *name)
 
 	struct dm_ioctl *io = (struct dm_ioctl *)buffer;
 
-	ioctl_init(io, DEVMAPPER_BUFFER_SIZE, name, 0);
+	dm_ioctl_init(io, INDEX_DM_DEV_STATUS, DEVMAPPER_BUFFER_SIZE, name, NULL, 0, 0, 0, 0);
 	if (dm_ioctl(fd, DM_DEV_STATUS, io)) {
 		if (errno != ENXIO) {
 			ERROR_ERRNO("DM_DEV_STATUS ioctl failed for lookup");
@@ -486,7 +421,7 @@ delete_integrity_blk_dev(const char *name)
 	struct dm_ioctl *io;
 	int ret = -1;
 
-	fd = open(DEV_MAPPER, O_RDWR);
+	fd = open(DM_CONTROL, O_RDWR);
 	if (fd < 0) {
 		ERROR_ERRNO("Cannot open device-mapper");
 		goto error;
@@ -494,7 +429,7 @@ delete_integrity_blk_dev(const char *name)
 
 	io = (struct dm_ioctl *)buffer;
 
-	ioctl_init(io, DM_INTEGRITY_BUF_SIZE, name, 0);
+	dm_ioctl_init(io, INDEX_DM_DEV_REMOVE, DM_INTEGRITY_BUF_SIZE, name, NULL, 0, 0, 0, 0);
 	if (dm_ioctl(fd, DM_DEV_REMOVE, io) < 0) {
 		ret = errno;
 		if (errno != ENXIO)
@@ -627,22 +562,28 @@ cryptfs_setup_volume_new(const char *label, const char *real_blkdev, const char 
 			 const char *meta_blkdev)
 {
 	int fd;
-	unsigned long fs_size;
+	// The file system size in sectors
+	uint64_t fs_size;
 
 	/* Update the fs_size field to be the size of the volume */
 	if ((fd = open(real_blkdev, O_RDONLY)) < 0) {
 		ERROR("Cannot open volume %s", real_blkdev);
 		return NULL;
 	}
-	fs_size = get_blkdev_size(fd);
+	// BLKGETSIZE64 returns size in bytes, we require size in sectors
+	int sector_size = dm_get_blkdev_sector_size(fd);
+	if (!(sector_size > 0)) {
+		ERROR("dm_get_blkdev_sector_size returned %d\n", sector_size);
+		return NULL;
+	}
+	fs_size = dm_get_blkdev_size64(fd) / sector_size;
 	close(fd);
 
 	if (fs_size == 0) {
 		ERROR("Cannot get size of volume %s", real_blkdev);
 		return NULL;
-	} else {
-		DEBUG("Crypto blk device size: %lu", fs_size);
 	}
+	DEBUG("Crypto blk device size: %lu", fs_size);
 
 	if (meta_blkdev)
 		return cryptfs_setup_volume_integrity_new(label, real_blkdev, meta_blkdev, key,
@@ -670,7 +611,7 @@ cryptfs_delete_blk_dev(const char *name)
 	struct dm_ioctl *io;
 	int ret = -1;
 
-	fd = open(DEV_MAPPER, O_RDWR);
+	fd = open(DM_CONTROL, O_RDWR);
 	if (fd < 0) {
 		ERROR("Cannot open device-mapper");
 		goto error;
@@ -678,7 +619,7 @@ cryptfs_delete_blk_dev(const char *name)
 
 	io = (struct dm_ioctl *)buffer;
 
-	ioctl_init(io, DM_CRYPT_BUF_SIZE, name, 0);
+	dm_ioctl_init(io, INDEX_DM_DEV_REMOVE, DM_CRYPT_BUF_SIZE, name, NULL, 0, 0, 0, 0);
 	if (dm_ioctl(fd, DM_DEV_REMOVE, io) < 0) {
 		ret = errno;
 		if (errno != ENXIO)

common/cryptfs.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of trust|me
- * Copyright(c) 2013 - 2017 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2022 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it

common/dm.h
@@ -1,0 +1,139 @@
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2022 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
+#ifndef DM_H
+#define DM_H
+
+#define DM_NAME_LEN 128
+#define DM_UUID_LEN 129
+
+#define DM_EXISTS_FLAG 0x00000004
+
+#ifdef ANDROID
+#define DM_CONTROL "/dev/device-mapper"
+#define DM_PATH_PREFIX "/dev/block/dm-"
+#else
+#define DM_CONTROL "/dev/mapper/control"
+#define DM_PATH_PREFIX "/dev/mapper/"
+#endif
+
+enum dm_cmd_index {
+	INDEX_DM_DEV_CREATE,
+	INDEX_DM_TABLE_LOAD,
+	INDEX_DM_DEV_REMOVE,
+	INDEX_DM_REMOVE_ALL,
+	// Suspend is also used for resume
+	INDEX_DM_DEV_SUSPEND,
+	INDEX_DM_DEV_STATUS,
+	INDEX_DM_TABLE_DEPS,
+	INDEX_DM_DEV_RENAME,
+	INDEX_DM_VERSION,
+	INDEX_DM_TABLE_STATUS,
+	INDEX_DM_DEV_WAIT,
+	INDEX_DM_LIST_DEVICES,
+	INDEX_DM_TABLE_CLEAR,
+	INDEX_DM_LIST_VERSIONS,
+	INDEX_DM_TARGET_MSG,
+	INDEX_DM_DEV_SET_GEOMETRY,
+	INDEX_DM_DEV_ARM_POLL,
+	INDEX_DM_GET_TARGET_VERSION
+};
+
+struct dm_cmd_table {
+	const unsigned cmd;
+	const int version[3];
+};
+
+#ifdef __GNU_LIBRARY__
+#define dm_ioctl(...) ioctl(__VA_ARGS__)
+#else
+/*
+ * non glibc std libraries such as musl provide ioctl (int, int, ...)
+ * wrapper. However, dm integrity requests are 'unsigned long int' and
+ * would overflow on a cast to int. Thus, we directly provide a wrapper
+ * here instead of using the ioctl wrapper of the std library.
+ */
+int
+dm_ioctl(int fd, unsigned long int request, ...);
+#endif
+
+/**
+ * Initializes the device-mapper ioctl data structure dm_ioctl
+ * whith its parameters. See linux/dm-ioctl.h for details of the
+ * struct parameters
+ */
+int
+dm_ioctl_init(struct dm_ioctl *io, enum dm_cmd_index idx, size_t dataSize, const char *name,
+	      const char *uuid, unsigned flags, unsigned long long dev, unsigned int target_count,
+	      unsigned int event_nr);
+
+/**
+ * Get the size of a Linux special block device in bytes
+ *
+ * This function uses BLKGETSIZE64, which returns the size in bytes,
+ * compared to the deprecated BLKGETSIZE, which returns the size as
+ * a number of 512-byte blocks. To get size in blocks similar to the
+ * deprecated command, the value must be divided by the result of
+ * dm_get_blkdev_sector_size.
+ *
+ * @param fd The file descriptor of the block device
+ * @return uint64_t The size of the block device in bytes
+ */
+uint64_t
+dm_get_blkdev_size64(int fd);
+
+/**
+ * Get the sector size of a Linux block device
+ *
+ * @param fd The file descriptor of the block device
+ * @return int The sector size
+ */
+int
+dm_get_blkdev_sector_size(int fd);
+
+/**
+ * Check if block device is read-only
+ *
+ * @param fd The file descriptor of the block device to be checked
+ * @return int 0 if read-write, 1 if read-only, -1 on error
+ */
+int
+dm_get_blkdev_readonly(int fd);
+
+/**
+ * Read dm-verity version via the DM_VERSION ioctl
+ *
+ * @return int 0 in case of success, -1 in case of failure
+ */
+int
+dm_read_version(void);
+
+/**
+ * List dm-verity versions via the DM_LIST_VERSIONS ioctl
+ *
+ * @return int 0 in case of success, -1 in case of failure
+ */
+int
+dm_list_versions(void);
+
+#endif // DM_H

common/fd.c
@@ -21,13 +21,14 @@
  * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
  */
 
-#include "fd.h"
-
-#include "macro.h"
-
+#include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+
+#include "mem.h"
+#include "macro.h"
+#include "fd.h"
 
 int
 fd_write(int fd, const char *buf, size_t len)
@@ -86,6 +87,59 @@ fd_read(int fd, char *buf, size_t len)
 	}
 
 	return len - remain;
+}
+
+ssize_t
+fd_read_blockwise(int fd, void *buf, size_t len, size_t block_size, size_t alignment)
+{
+	ASSERT(buf);
+	ASSERT(fd > 0);
+	ASSERT(block_size > 0);
+	ASSERT(alignment > 0);
+
+	void *fragment_buf = NULL;
+	void *p = NULL;
+	ssize_t ret = -1;
+
+	size_t fragment_len = len % block_size;
+	size_t blocks_len = len - fragment_len;
+
+	if ((size_t)buf & (alignment - 1)) {
+		int r = posix_memalign(&p, alignment, len);
+		if (r) {
+			ERROR("posix_memalign returned %d", r);
+			return -1;
+		}
+	} else {
+		p = buf;
+	}
+
+	if (fd_read(fd, p, blocks_len) != (int)blocks_len) {
+		goto out;
+	}
+
+	if (fragment_len) {
+		int r = posix_memalign(&fragment_buf, alignment, block_size);
+		if (r) {
+			ERROR("posix_memalign returned %d", r);
+			goto out;
+		}
+		if (fd_read(fd, fragment_buf, block_size) < (int)fragment_len) {
+			goto out;
+		}
+
+		memcpy((char *)p + blocks_len, fragment_buf, fragment_len);
+	}
+	ret = len;
+out:
+	mem_free(fragment_buf);
+	if (p != buf) {
+		if (ret != -1) {
+			memcpy(buf, p, len);
+		}
+		mem_free(p);
+	}
+	return ret;
 }
 
 int

common/fd.h
@@ -55,6 +55,21 @@ fd_write(const int fd, const char *buf, size_t len);
 int
 fd_read(int fd, char *buf, size_t len);
 
+/*
+ * Reads blockwise from the given file descriptor to the given buffer, calling fd_read
+ * internally.
+ *
+ * @param fd The file descriptor to read from
+ * @param buf The buffer to read into
+ * @param Number of bytes to read
+ * @param block_size Size of the blocks to read
+ * @param alignment Alignment to read with
+ *
+ * @return number of bytes read on success, otherwise -1
+ */
+ssize_t
+fd_read_blockwise(int fd, void *buf, size_t len, size_t block_size, size_t alignment);
+
 /**
  * Makes the given file descriptor non-blocking by setting the O_NONBLOCK flag.
  *

common/loopdev.c
@@ -27,6 +27,7 @@
 #include <linux/loop.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <errno.h>
 
 #include "loopdev.h"
 
@@ -45,10 +46,19 @@
 
 #define LOOP_CONTROL "/dev/loop-control"
 
-char *
+#define SECTOR_SHIFT 9
+#define SECTOR_SIZE (1 << SECTOR_SHIFT)
+
+/**
+ * Get a free loop device.
+ * @return The path of the loop device or NULL in case of an error.
+ */
+static char *
 loopdev_new(void)
 {
 	int fd, i;
+	char dev[64];
+	struct stat st;
 
 	fd = open(LOOP_CONTROL, O_RDONLY);
 	if (fd < 0) {
@@ -56,7 +66,6 @@ loopdev_new(void)
 		return NULL;
 	}
 
-	//i = ioctl(loop_fd, LOOP_CTL_ADD);
 	i = ioctl(fd, LOOP_CTL_GET_FREE);
 	close(fd);
 	if (i < 0) {
@@ -64,88 +73,107 @@ loopdev_new(void)
 		return NULL;
 	}
 
-	/* TODO: Handle creation of new loop devices dynamically?
-	 */
+	if (sprintf(dev, "%s%d", LOOP_DEV_PREFIX, i) < 0) {
+		ERROR("Failed to create loopdev name");
+		return NULL;
+	}
 
-	return mem_printf("%s%d", LOOP_DEV_PREFIX, i);
+	if (stat(dev, &st)) {
+		ERROR("Failed to check device info");
+		return NULL;
+	}
+
+	if (!S_ISBLK(st.st_mode)) {
+		ERROR("Device is not a block device");
+		return NULL;
+	}
+
+	return mem_strdup(dev);
+}
+
+char *
+loopdev_create_new(int *loop_fd, const char *img, int readonly, size_t blocksize)
+{
+	struct loop_info64 info;
+	mem_memset(&info, 0, sizeof(info));
+	int img_fd;
+	char *loop_dev = NULL;
+
+	img_fd = open(img, (readonly ? O_RDONLY : O_RDWR) | O_EXCL);
+	if (img_fd < 0) {
+		ERROR_ERRNO("Could not open image file %s with readonly = %d", img, readonly);
+		goto error;
+	}
+
+	// Set file name
+	strncpy((char *)info.lo_file_name, img, sizeof(info.lo_file_name) - 1);
+	// Do not require detach after umount
+	info.lo_flags |= LO_FLAGS_AUTOCLEAR;
+
+	do {
+		loop_dev = loopdev_new();
+		if (!loop_dev) {
+			ERROR("Could not get free loop device for %s", img);
+			goto error;
+		}
+
+		*loop_fd = open(loop_dev, readonly ? O_RDONLY : O_RDWR);
+		if (*loop_fd < 0) {
+			ERROR_ERRNO("Could not open device %s", loop_dev);
+			goto error;
+		}
+
+		if (ioctl(*loop_fd, LOOP_SET_FD, img_fd) < 0) {
+			if (errno != EBUSY) {
+				ERROR_ERRNO("LOOP_SET_FD ioctl failed");
+				goto error;
+			}
+			mem_free(loop_dev);
+			loop_dev = NULL;
+			close(*loop_fd);
+			*loop_fd = -1;
+		}
+	} while (*loop_fd < 0);
+
+	if (blocksize > SECTOR_SIZE) {
+		ioctl(*loop_fd, LOOP_SET_BLOCK_SIZE, (unsigned long)blocksize);
+	}
+
+	// TODO For kernel 5.8 and later, LOOP_CONFIGURE could be used instead
+	// (https://man7.org/linux/man-pages/man4/loop.4.html)
+	if (ioctl(*loop_fd, LOOP_SET_STATUS64, &info) < 0) {
+		ERROR_ERRNO("Failed to set AUTOCLEAR for loop device %s", loop_dev);
+		goto error;
+	}
+
+	memset(&info, 0x0, sizeof(info));
+	if (ioctl(*loop_fd, LOOP_GET_STATUS64, &info) < 0) {
+		ERROR_ERRNO("Failed to get status64 for loop device %s", loop_dev);
+		goto error;
+	}
+
+	// Verify that autoclear is set
+	if (!(info.lo_flags & LO_FLAGS_AUTOCLEAR)) {
+		ERROR("Autoclear not successfully set");
+		goto error;
+	}
+
+	close(img_fd);
+	return loop_dev;
+
+error:
+	if (img_fd >= 0)
+		close(img_fd);
+
+	if (*loop_fd >= 0) {
+		ioctl(*loop_fd, LOOP_CLR_FD, 0);
+		close(*loop_fd);
+	}
+	return NULL;
 }
 
 void
 loopdev_free(char *dev)
 {
 	mem_free0(dev);
-}
-
-int
-loopdev_wait(const char *dev, unsigned timeout)
-{
-	unsigned i;
-
-	for (i = 0; i < timeout; i++) {
-		struct stat st;
-
-		DEBUG("Checking loop device %s (%i/%i)", dev, i, timeout);
-		if (stat(dev, &st) || !S_ISBLK(st.st_mode))
-			NANOSLEEP(0, 1000000)
-		else
-			return 0;
-	}
-
-	return -1;
-}
-
-/*
- * Taken from:
- * http://stackoverflow.com/questions/11295154/how-do-i-loop-mount-programmatically
- */
-
-int
-loopdev_setup_device(const char *img, const char *dev)
-{
-	struct loop_info64 info;
-	mem_memset(&info, 0, sizeof(info));
-	int img_fd, dev_fd = -1;
-
-	img_fd = open(img, O_RDWR | O_EXCL);
-	if (img_fd < 0) {
-		ERROR_ERRNO("Could not open image file %s", img);
-		goto error;
-	}
-
-	dev_fd = open(dev, O_RDWR);
-	if (dev_fd < 0) {
-		ERROR_ERRNO("Could not open device %s", dev);
-		goto error;
-	}
-
-	if (ioctl(dev_fd, LOOP_SET_FD, img_fd) < 0) {
-		ERROR_ERRNO("Failed to set fd of loop device %s", dev);
-		goto error;
-	}
-
-	if (ioctl(dev_fd, LOOP_GET_STATUS64, &info) < 0) {
-		ERROR_ERRNO("Failed to get status64 for loop device %s", dev);
-		goto error;
-	}
-
-	/* so we do not need a detach of the loop device after umount */
-	info.lo_flags |= LO_FLAGS_AUTOCLEAR;
-
-	if (ioctl(dev_fd, LOOP_SET_STATUS64, &info) < 0) {
-		ERROR_ERRNO("Failed to set AUTOCLEAR for loop device %s", dev);
-		goto error;
-	}
-
-	close(img_fd);
-	return dev_fd;
-
-error:
-	if (img_fd >= 0)
-		close(img_fd);
-
-	if (dev_fd >= 0) {
-		ioctl(dev_fd, LOOP_CLR_FD, 0);
-		close(dev_fd);
-	}
-	return -1;
 }

commit 952e354eed8aa81eca11b97f278359da99590ac7

common/dm: Fixes a length parameter for strncpy in dm_ioctl_init
Use sizeof(io->uuid) instead of sizeof(io->name) while coping
uuid string in dm_ioctl_init.

Fixes: 3e431b6ec666 ("common: Refactoring")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/dm.c
@@ -101,7 +101,7 @@ dm_ioctl_init(struct dm_ioctl *io, enum dm_cmd_index idx, size_t data_size, cons
 		strncpy(io->name, name, sizeof(io->name) - 1);
 
 	if (uuid)
-		strncpy(io->uuid, uuid, sizeof(io->name) - 1);
+		strncpy(io->uuid, uuid, sizeof(io->uuid) - 1);
 
 	return 0;
 }