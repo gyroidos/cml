gyroidos/cml

commit d7dc100915132abdab5b7f8622f3dd95a4a17667

tpm2d: process pcr indices as bitmap for attestation type advanced
Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

tpm2d/rcontrol.c
@@ -45,6 +45,8 @@
 
 #include <google/protobuf-c/protobuf-c-text.h>
 
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x)[0])
+
 struct tpm2d_rcontrol {
 	int sock; // listen ip socket fd
 };
@@ -92,12 +94,13 @@ tpm2d_rcontrol_handle_message(const RemoteToTpm2d *msg, int fd, tpm2d_rcontrol_t
 	switch (msg->code) {
 	case REMOTE_TO_TPM2D__CODE__ATTESTATION_REQ: {
 		Pcr **out_pcrs = NULL;
-		int pcr_regs = 0;
-		int pcr_indices = 0;
 		tpm2d_pcr_t **pcr_array = NULL;
 		tpm2d_quote_t *quote = NULL;
 		uint8_t *attestation_cert = NULL;
 		size_t att_cert_len = 0;
+		uint8_t pcr_bitmap[3];
+		int pcr_regs = 0;
+		int index = 0;
 
 		TPMI_DH_OBJECT att_key_handle = tpm2d_get_as_key_handle();
 		if (att_key_handle == TPM_RH_NULL)
@@ -110,32 +113,53 @@ tpm2d_rcontrol_handle_message(const RemoteToTpm2d *msg, int fd, tpm2d_rcontrol_t
 		case IDS_ATTESTATION_TYPE__BASIC:
 			TRACE("atype BASIC");
 			pcr_regs = 12;
+			for (int i = 0; i < pcr_regs; ++i) {
+				pcr_bitmap[i / 8] |= 1 << (i % 8);
+			}
 			break;
 		case IDS_ATTESTATION_TYPE__ALL:
 			TRACE("atype ALL");
 			pcr_regs = 24;
+			for (int i = 0; i < pcr_regs; ++i) {
+				pcr_bitmap[i / 8] |= 1 << (i % 8);
+			}
 			break;
 		case IDS_ATTESTATION_TYPE__ADVANCED:
-			TRACE("atype ADVACE");
-			pcr_regs = (msg->has_pcrs) ? msg->pcrs : 0;
+			TRACE("atype ADVANCED");
+			memcpy(pcr_bitmap, &msg->pcrs, sizeof(pcr_bitmap));
+			for (size_t i = 0; i < ARRAY_SIZE(pcr_bitmap); i++) {
+				for (size_t j = 0; j < 8; j++) {
+					if (pcr_bitmap[i] & (1 << j)) {
+						pcr_regs++;
+					}
+				}
+			}
 			break;
 		default:
 			goto err_att_req;
 			break;
 		}
-		pcr_indices = pcr_regs - 1;
 
 		pcr_array = mem_alloc0(
 			MUL_WITH_OVERFLOW_CHECK((size_t)sizeof(tpm2d_pcr_t *), pcr_regs));
-		for (int i = 0; i < pcr_regs; ++i) {
-			pcr_array[i] = tpm2_pcrread_new(i, TPM2D_HASH_ALGORITHM);
-
-			IF_NULL_GOTO_ERROR(pcr_array[i], err_att_req);
-			INFO("PCR%d: size %zu", i, pcr_array[i]->pcr_size);
+		index = 0;
+		for (int i = 0; i < (int)ARRAY_SIZE(pcr_bitmap); ++i) {
+			for (int j = 0; j < 8; j++) {
+				if (pcr_bitmap[i] & (1 << j)) {
+					int pcr_num = i * 8 + j;
+					pcr_array[index] =
+						tpm2_pcrread_new(pcr_num, TPM2D_HASH_ALGORITHM);
+					IF_NULL_GOTO_ERROR(pcr_array[index], err_att_req);
+					INFO("PCR%d: size %zu", pcr_num,
+					     pcr_array[index]->pcr_size);
+					index++;
+				}
+			}
 		}
 
-		quote = tpm2_quote_new(pcr_indices, att_key_handle, TPM2D_ATT_KEY_PW,
-				       msg->qualifyingdata.data, msg->qualifyingdata.len);
+		quote = tpm2_quote_new(pcr_bitmap, sizeof(pcr_bitmap), att_key_handle,
+				       TPM2D_ATT_KEY_PW, msg->qualifyingdata.data,
+				       msg->qualifyingdata.len);
 		IF_NULL_GOTO_ERROR(quote, err_att_req);
 
 		// add device certificate to quote
@@ -165,15 +189,21 @@ tpm2d_rcontrol_handle_message(const RemoteToTpm2d *msg, int fd, tpm2d_rcontrol_t
 
 		Pcr out_pcr = PCR__INIT;
 		out_pcrs = mem_new(Pcr *, pcr_regs);
-		for (int i = 0; i < pcr_regs; ++i) {
-			out_pcr.has_value = true;
-			out_pcr.value.data = pcr_array[i]->pcr_value;
-			out_pcr.value.len = pcr_array[i]->pcr_size;
-			INFO("pcr: %zu", out_pcr.value.len);
-			out_pcr.has_number = true;
-			out_pcr.number = i;
-			out_pcrs[i] = mem_alloc(sizeof(Pcr));
-			memcpy(out_pcrs[i], &out_pcr, sizeof(Pcr));
+		index = 0;
+		for (int i = 0; i < (int)ARRAY_SIZE(pcr_bitmap); ++i) {
+			for (int j = 0; j < 8; j++) {
+				if (pcr_bitmap[i] & (1 << j)) {
+					out_pcr.has_value = true;
+					out_pcr.value.data = pcr_array[index]->pcr_value;
+					out_pcr.value.len = pcr_array[index]->pcr_size;
+					out_pcr.has_number = true;
+					out_pcr.number = (i * 8) + j;
+					INFO("PCR_%d: %zu", index, out_pcr.value.len);
+					out_pcrs[index] = mem_alloc(sizeof(Pcr));
+					memcpy(out_pcrs[index], &out_pcr, sizeof(Pcr));
+					index++;
+				}
+			}
 		}
 
 		out.has_atype = true;

tpm2d/tpm2_commands.c
@@ -841,8 +841,8 @@ tpm2_pcrextend(TPMI_DH_PCR pcr_index, TPMI_ALG_HASH hash_alg, const uint8_t *dat
 }
 
 tpm2d_quote_t *
-tpm2_quote_new(TPMI_DH_PCR pcr_indices, TPMI_DH_OBJECT sig_key_handle, const char *sig_key_pwd,
-	       uint8_t *qualifying_data, size_t qualifying_data_len)
+tpm2_quote_new(uint8_t *pcr_bitmap, size_t size_pcr_bitmap, TPMI_DH_OBJECT sig_key_handle,
+	       const char *sig_key_pwd, uint8_t *qualifying_data, size_t qualifying_data_len)
 {
 	TPM_RC rc = TPM_RC_SUCCESS;
 	Quote_In in;
@@ -852,17 +852,14 @@ tpm2_quote_new(TPMI_DH_PCR pcr_indices, TPMI_DH_OBJECT sig_key_handle, const cha
 
 	IF_NULL_RETVAL_ERROR(tss_context, NULL);
 
-	if (pcr_indices > 23) {
+	if (size_pcr_bitmap > PCR_SELECT_MAX) {
 		ERROR("Exceeded maximum available PCR registers!");
 		return NULL;
 	}
 
-	in.PCRselect.pcrSelections[0].sizeofSelect = 3;
-	in.PCRselect.pcrSelections[0].pcrSelect[0] = 0;
-	in.PCRselect.pcrSelections[0].pcrSelect[1] = 0;
-	in.PCRselect.pcrSelections[0].pcrSelect[2] = 0;
-	for (size_t i = 0; i < pcr_indices; ++i) {
-		in.PCRselect.pcrSelections[0].pcrSelect[pcr_indices / 8] |= 1 << (pcr_indices % 8);
+	in.PCRselect.pcrSelections[0].sizeofSelect = size_pcr_bitmap;
+	for (size_t i = 0; i < size_pcr_bitmap; i++) {
+		in.PCRselect.pcrSelections[0].pcrSelect[i] = pcr_bitmap[i];
 	}
 
 	in.signHandle = sig_key_handle;

tpm2d/tpm2d.h
@@ -194,8 +194,8 @@ TPM_RC
 tpm2_pcrextend(TPMI_DH_PCR pcr_index, TPMI_ALG_HASH hash_alg, const uint8_t *data, size_t data_len);
 
 tpm2d_quote_t *
-tpm2_quote_new(TPMI_DH_PCR pcr_indices, TPMI_DH_OBJECT sig_key_handle, const char *sig_key_pwd,
-	       uint8_t *qualifying_data, size_t qualifying_data_len);
+tpm2_quote_new(uint8_t *pcr_bitmap, size_t size_pcr_bitmap, TPMI_DH_OBJECT sig_key_handle,
+	       const char *sig_key_pwd, uint8_t *qualifying_data, size_t qualifying_data_len);
 
 void
 tpm2_quote_free(tpm2d_quote_t *quote);

commit d4cc168b2a5ab190d8a01723e55af6994628a304

tpm2d/rcontrol: Initialize pcr_bitmap[] array with zero
The bits of the pcr_bitmap array are set later by '|='.
Thus, we need a proper initialization here.

Fixes: d7dc10091513 ("tpm2d: process pcr indices as bitmap for attestation type advanced")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

tpm2d/rcontrol.c
@@ -101,7 +101,7 @@ tpm2d_rcontrol_handle_message(const RemoteToTpm2d *msg, int fd, tpm2d_rcontrol_t
 		tpm2d_quote_t *quote = NULL;
 		uint8_t *attestation_cert = NULL;
 		size_t att_cert_len = 0;
-		uint8_t pcr_bitmap[3];
+		uint8_t pcr_bitmap[3] = { 0 };
 		int pcr_regs = 0;
 		int index = 0;
 