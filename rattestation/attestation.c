gyroidos/cml

commit 3d0184747e04759427709dc40d5a6d001e1336a3

rattestation: verify aggregated pcr value from quote
Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

rattestation/attestation.c
@@ -134,25 +134,30 @@ attestation_verify_resp(Tpm2dToRemote *resp, const char *config_file, uint8_t *n
 	// The pcrs are configured as hex strings
 	size_t pcr_string_len = config->halg * 2;
 
+	bool ret_pcr = true;
 	for (size_t i = 0; i < config->n_pcr_values; i++) {
 		if (strlen(pcr_strings[i]) != pcr_string_len) {
 			ERROR("Length of configured PCR value %zu invalid (%zu,	must be %zu)", i,
 			      strlen(pcr_strings[i]), pcr_string_len);
-			ret = false;
-			goto err;
+			ret_pcr = false;
 		}
 		if (strlen(config->pcr_values[i]->value) != pcr_string_len) {
 			ERROR("Length of received PCR value %zu invalid (%zu, must be %zu)", i,
 			      strlen(config->pcr_values[i]->value), pcr_string_len);
-			ret = false;
-			goto err;
+			ret_pcr = false;
 		}
 		if (strncmp(pcr_strings[i], config->pcr_values[i]->value, pcr_string_len)) {
-			DEBUG("PCR_%zu: %s - VERIFICATION FAILED", i, pcr_strings[i]);
-			ret = false;
-			goto err;
+			ERROR("PCR_%zu: %s - VERIFICATION FAILED", i, pcr_strings[i]);
+			ret_pcr = false;
+		} else {
+			DEBUG("PCR_%zu: %s - VERIFICATION SUCCESSFUL", i, pcr_strings[i]);
 		}
-		DEBUG("PCR_%zu: %s - VERIFICATION SUCCESSFUL", i, pcr_strings[i]);
+	}
+
+	if (!ret_pcr) {
+		ERROR("PCR VERIFICATION FAILED");
+		ret = false;
+		goto err;
 	}
 
 	// The quote is sent as a TPMS_ATTEST and has to be unmarshalled
@@ -174,6 +179,10 @@ attestation_verify_resp(Tpm2dToRemote *resp, const char *config_file, uint8_t *n
 	      ret_nonce ? "VERIFICATION FAILED" : "VERIFICATION SUCCESSFUL");
 	mem_free(nonce_str);
 	mem_free(rcv_nonce_str);
+	if (ret_nonce) {
+		ret = false;
+		goto err;
+	}
 
 	// The signature is sent as a TPMT_SIGNATURE and has to be unmarshalled
 	TPMT_SIGNATURE tpmt_signature;
@@ -198,6 +207,25 @@ attestation_verify_resp(Tpm2dToRemote *resp, const char *config_file, uint8_t *n
 	} else {
 		INFO("VERIFY QUOTE SIGNATURE SUCCESSFUL");
 	}
+
+	// Verify aggregated PCR value
+	char *pcr_digest = convert_bin_to_hex_new(tpms_attest.attested.quote.pcrDigest.t.buffer,
+						  tpms_attest.attested.quote.pcrDigest.t.size);
+	DEBUG("Quote PCR Digest: %s\n", pcr_digest);
+	mem_free(pcr_digest);
+	SHA256_CTX ctx;
+	SHA256_Init(&ctx);
+	uint8_t pcr_calc[SHA256_DIGEST_LENGTH] = { 0 };
+	for (size_t i = 0; i < resp->n_pcr_values; i++) {
+		SHA256_Update(&ctx, resp->pcr_values[i]->value.data, SHA256_DIGEST_LENGTH);
+	}
+	SHA256_Final(pcr_calc, &ctx);
+	if (memcmp(tpms_attest.attested.quote.pcrDigest.t.buffer, pcr_calc, SHA256_DIGEST_LENGTH)) {
+		ERROR("VERIFY AGGREGATED PCR FAILED");
+		ret = false;
+		goto err;
+	}
+	INFO("VERIFY AGGREGATED PCR SUCCESSFUL");
 
 	// PCR10 kernel module verification (from /sys/kernel/security/ima/binary_runtime_measuremts)
 	hash_algo_t hash_algo = size_to_hash_algo((int)resp->halg);

rattestation/hash.c
@@ -76,7 +76,7 @@ hash_algo_to_string(hash_algo_t hash_algo)
 }
 
 void
-sha1(uint8_t *digest, uint8_t *data, size_t len)
+hash_sha1(uint8_t *digest, uint8_t *data, size_t len)
 {
 	ASSERT(digest);
 	ASSERT(data);
@@ -89,7 +89,7 @@ sha1(uint8_t *digest, uint8_t *data, size_t len)
 }
 
 void
-sha256(uint8_t *digest, uint8_t *data, size_t len)
+hash_sha256(uint8_t *digest, uint8_t *data, size_t len)
 {
 	ASSERT(digest);
 	ASSERT(data);
@@ -99,4 +99,24 @@ sha256(uint8_t *digest, uint8_t *data, size_t len)
 	EVP_DigestUpdate(c, data, len);
 	EVP_DigestFinal(c, digest, NULL);
 	EVP_MD_CTX_free(c);
+}
+
+void
+hash_extend(hash_algo_t hash_algo, uint8_t *pcr_value, uint8_t *pcr_extend)
+{
+	if (hash_algo == HASH_ALGO_SHA1) {
+		SHA_CTX ctx;
+		SHA1_Init(&ctx);
+		SHA1_Update(&ctx, pcr_value, SHA_DIGEST_LENGTH);
+		SHA1_Update(&ctx, pcr_extend, SHA_DIGEST_LENGTH);
+		SHA1_Final(pcr_value, &ctx);
+	} else if (hash_algo == HASH_ALGO_SHA256) {
+		SHA256_CTX ctx;
+		SHA256_Init(&ctx);
+		SHA256_Update(&ctx, pcr_value, SHA256_DIGEST_LENGTH);
+		SHA256_Update(&ctx, pcr_extend, SHA256_DIGEST_LENGTH);
+		SHA256_Final(pcr_value, &ctx);
+	} else {
+		printf("Error: Hash algo not supported\n");
+	}
 }

rattestation/hash.h
@@ -58,9 +58,12 @@ hash_algo_t
 size_to_hash_algo(int size);
 
 void
-sha1(uint8_t *digest, uint8_t *data, size_t len);
+hash_sha1(uint8_t *digest, uint8_t *data, size_t len);
 
 void
-sha256(uint8_t *digest, uint8_t *data, size_t len);
+hash_sha256(uint8_t *digest, uint8_t *data, size_t len);
+
+void
+hash_extend(hash_algo_t hash_algo, uint8_t *pcr_value, uint8_t *pcr_extend);
 
 #endif // HASH_ALGO_H_

rattestation/ima_verify.c
@@ -316,7 +316,7 @@ verify_template_hash(struct event *template)
 {
 	uint8_t digest[SHA_DIGEST_LENGTH];
 
-	sha1(digest, template->template_data, template->template_data_len);
+	hash_sha1(digest, template->template_data, template->template_data_len);
 	if (memcmp(digest, template->header.digest, sizeof digest) == 0) {
 		return 0;
 	}
@@ -372,7 +372,8 @@ ima_verify_binary_runtime_measurements(uint8_t *buf, size_t size, const char *ce
 			// instead, the SHA256 hash of the template must be manually calculated to
 			// extend the simulated PCR
 			uint8_t template_hash[SHA256_DIGEST_LENGTH];
-			sha256(template_hash, template.template_data, template.template_data_len);
+			hash_sha256(template_hash, template.template_data,
+				    template.template_data_len);
 
 			EVP_MD_CTX *c_256 = EVP_MD_CTX_new();
 			EVP_DigestInit(c_256, EVP_sha256());

commit 01e9f9a5425202a68d241e07ac2225d6d5e8b4fe

rattestation/attestation: Fix sha1 PCR calculation
In attestation_verify_resp(), use config->halg to decide which
EVP_sha*() function should be used instead of using EVP_sha256
unconditionally. Otherwise, sha1 PCR calculation would be broken.

Fixes: 3d0184747e04 ("rattestation: verify aggregated pcr value from quote")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

rattestation/attestation.c
@@ -199,14 +199,23 @@ attestation_verify_resp(Tpm2dToRemote *resp, RAttestationConfig *config, uint8_t
 	DEBUG_HEXDUMP(tpms_attest.attested.quote.pcrDigest.t.buffer,
 		      tpms_attest.attested.quote.pcrDigest.t.size, "Quote PCR Digest");
 	EVP_MD_CTX *ctx = EVP_MD_CTX_new();
-	EVP_DigestInit(ctx, EVP_sha256());
+	switch (config->halg) {
+	case SHA256_DIGEST_LENGTH:
+		EVP_DigestInit(ctx, EVP_sha256());
+		break;
+	case SHA_DIGEST_LENGTH:
+		EVP_DigestInit(ctx, EVP_sha1());
+		break;
+	default: // should not happen, see ASSERT above
+		goto err;
+	}
 	uint8_t pcr_calc[SHA256_DIGEST_LENGTH] = { 0 };
 	for (size_t i = 0; i < resp->n_pcr_values; i++) {
-		EVP_DigestUpdate(ctx, resp->pcr_values[i]->value.data, SHA256_DIGEST_LENGTH);
+		EVP_DigestUpdate(ctx, resp->pcr_values[i]->value.data, config->halg);
 	}
 	EVP_DigestFinal(ctx, pcr_calc, NULL);
 	EVP_MD_CTX_free(ctx);
-	if (memcmp(tpms_attest.attested.quote.pcrDigest.t.buffer, pcr_calc, SHA256_DIGEST_LENGTH)) {
+	if (memcmp(tpms_attest.attested.quote.pcrDigest.t.buffer, pcr_calc, config->halg)) {
 		ERROR("VERIFY AGGREGATED PCR FAILED");
 		ret = false;
 		goto err;