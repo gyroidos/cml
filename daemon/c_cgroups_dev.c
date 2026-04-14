gyroidos/cml

commit 1081253e8827f5b00a3ea181461ed9a025132783

daemon/c_cgroups_dev: implement device_set_access handler
Provide actual implementation of device access handling by
c_cgroups_dev_device_set_access(). Register this at the container
module as container_device_set_access handler function.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_cgroups_dev.c
@@ -1009,6 +1009,26 @@ c_cgroups_dev_device_deny(void *cgroups_devp, char type, int major, int minor)
 	return ret;
 }
 
+static int
+c_cgroups_dev_device_set_access(void *cgroups_devp, const char *rule)
+{
+	c_cgroups_dev_t *cgroups_dev = cgroups_devp;
+	ASSERT(cgroups_dev);
+
+	int ret;
+
+	/*
+	 * check if last char is either rwm, otherwise no access should
+	 * be given and we just call deny
+	 */
+	if (rule[strlen(rule)] == 'r' || rule[strlen(rule)] == 'w' || rule[strlen(rule) - 1] == 'm')
+		ret = c_cgroups_dev_allow(cgroups_dev, rule);
+	else
+		ret = c_cgroups_dev_deny(cgroups_dev, rule);
+
+	return ret;
+}
+
 static compartment_module_t c_cgroups_dev_module = {
 	.name = MOD_NAME,
 	.compartment_new = c_cgroups_dev_new,
@@ -1037,5 +1057,6 @@ c_cgroups_dev_init(void)
 	// register relevant handlers implemented by this module
 	container_register_device_allow_handler(MOD_NAME, c_cgroups_dev_device_allow);
 	container_register_device_deny_handler(MOD_NAME, c_cgroups_dev_device_deny);
+	container_register_device_set_access_handler(MOD_NAME, c_cgroups_dev_device_set_access);
 	container_register_is_device_allowed_handler(MOD_NAME, c_cgroups_dev_is_dev_allowed);
 }

commit 24d9442b47c75605531e9e2dd6695fdacd7b5611

daemon/c_cgroups_dev: Fix of by one in device_set_access()
In c_cgroups_dev_device_set_access() the last char of rule string
should be checked to be either r, w or m but only the check for 'm'
used the correct offset for the last char namely 'strlen(rule)-1'.
For 'r' and 'w' strlen(rule) without -1 was used. Also fix this to
'strlen(rule)-1' to prevent reading '\0' instead of 'r' or 'w'.

Fixes: 1081253e8827 ("daemon/c_cgroups_dev: implement device_set_access handler")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_cgroups_dev.c
@@ -1028,7 +1028,8 @@ c_cgroups_dev_device_set_access(void *cgroups_devp, const char *rule)
 	 * check if last char is either rwm, otherwise no access should
 	 * be given and we just call deny
 	 */
-	if (rule[strlen(rule)] == 'r' || rule[strlen(rule)] == 'w' || rule[strlen(rule) - 1] == 'm')
+	if (rule[strlen(rule) - 1] == 'r' || rule[strlen(rule) - 1] == 'w' ||
+	    rule[strlen(rule) - 1] == 'm')
 		ret = c_cgroups_dev_allow(cgroups_dev, rule);
 	else
 		ret = c_cgroups_dev_deny(cgroups_dev, rule);