gyroidos/cml

commit 4c7aef362fee24c87d25b0d6df6278c801ce165b

common/list: added list_join() function to concatenate two lists

common/list.c
@@ -41,6 +41,18 @@ list_append(list_t *list, void *data)
 	return tail ? list : e; // return head of list
 }
 
+list_t *
+list_join(list_t *list, list_t *list_append)
+{
+	IF_NULL_RETVAL(list, list_append);
+	IF_NULL_RETVAL(list_append, list);
+
+	list_t *l_tail = list_tail(list);
+	l_tail->next = list_append;
+
+	return list;
+}
+
 bool
 list_contains(const list_t *list, const list_t *elem)
 {

common/list.h
@@ -59,6 +59,17 @@ list_append(list_t *list, void *data)
 ;
 
 /**
+ * Joins two lists by appending list_append to list. If one of
+ * the lists is NULL, the head of the respectiv other list is returned.
+ *
+ * @param list The head of list; may be NULL.
+ * @param list The head of the list to be appended; may be NULL.
+ * @return The head of new joind list.
+ */
+list_t *
+list_join(list_t *list, list_t *list_append);
+
+/**
  * Returns true if and only if the list contains the element.
  *
  * @param list The head of the list.

commit 111ce48fdca654458b71de4e501b53a4b812096a

common/list: Fix list_join
Experimentation with CBMC [1] on selected libcommon modules revealed that
list_join breaks the doubly linked list forgetting to set the prev pointer.

[1] https://github.com/diffblue/cbmc

Fixes: 4c7aef362 ("common/list: added list_join() function to concatenate two lists")
Signed-off-by: Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>

common/list.c
@@ -51,6 +51,7 @@ list_join(list_t *list, list_t *list_append)
 
 	list_t *l_tail = list_tail(list);
 	l_tail->next = list_append;
+	list_append->prev = l_tail;
 
 	return list;
 }