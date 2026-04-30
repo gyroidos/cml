gyroidos/cml

commit 0a33ba40047631c985d27166f3d1b3bdf9c0c6f8

common/proc: Provide helper to read current cap_last_cap
Provide function proc_cap_last_cap() to read last_cap which is
implemented by running kernel from procfs.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/proc.c
@@ -249,3 +249,19 @@ proc_fork_and_execvp(const char *const *argv)
 	}
 	return -1;
 }
+
+int
+proc_cap_last_cap(void)
+{
+	int cap;
+	const char *file_cap_last_cap = "/proc/sys/kernel/cap_last_cap";
+
+	char *str_cap_last_cap = file_read_new(file_cap_last_cap, 24);
+	if (sscanf(str_cap_last_cap, "%d", &cap) <= 0) {
+		ERROR_ERRNO("Can't read cap from '%s'", file_cap_last_cap);
+		cap = -1;
+	}
+
+	mem_free(str_cap_last_cap);
+	return cap;
+}

common/proc.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of trust|me
- * Copyright(c) 2013 - 2017 Fraunhofer AISEC
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
  * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -61,5 +61,12 @@ proc_find(pid_t ppid, const char *name);
 
 int
 proc_fork_and_execvp(const char *const *argv);
+
+/**
+ * Returns the last cap from the running kernel
+ * @return last cap of running kernel, -1 on error;
+ */
+int
+proc_cap_last_cap(void);
 
 #endif /* PROC_H */

commit 23f0abc54b4886a90fe0447023f70a2d20c9395d

common/proc: fixed error check for scanf in proc_cap_last_cap
In proc_cap_last_cap, the error check of scanf return values was
'<= 0'. However, we have to check the exact number of scanned values.
We switched this check to '!= 1'.

This fixes "CWE-252: Unchecked Return Value" and "CWE-253: Incorrect
Check of Function Return Value" detected by CodeQL cpp/missing-check-scanf

Fixes: 0a33ba400476 ("common/proc: Provide helper to read current cap_last_cap")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/proc.c
@@ -292,7 +292,9 @@ proc_cap_last_cap(void)
 	const char *file_cap_last_cap = "/proc/sys/kernel/cap_last_cap";
 
 	char *str_cap_last_cap = file_read_new(file_cap_last_cap, 24);
-	if (sscanf(str_cap_last_cap, "%d", &cap) <= 0) {
+	IF_NULL_RETVAL(str_cap_last_cap, -1);
+
+	if (sscanf(str_cap_last_cap, "%d", &cap) != 1) {
 		ERROR_ERRNO("Can't read cap from '%s'", file_cap_last_cap);
 		cap = -1;
 	}