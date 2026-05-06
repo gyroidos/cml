gyroidos/cml

commit d191c1fb0631209732f30c2f4361ae275f49c395

daemon/audit: explicitly check for va_args terminator
In audit_log_event() the parameter metas should be used to indicate
the following va_args. However, since we do often set this to 0
if no metas are used at all. That is why CodeQL thinks we have
0 as a terminator for while loop checking the va args until 0.
We now additionally introduce an explicit NULL sentinel for the
va_args. This is appended to all calls of audit_log_event() which
use variable meta parameters.

This silences warning about CWE-121 "Stack-based Buffer Overflow"
complaint by CodeQL cpp/unterminated-variadic-call:

   "Warning Unterminated variadic call
   Calls to audit_log_event should use the value 0 as a terminator
   (89 of 107 calls do)." [CodeQL]

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -670,9 +670,11 @@ audit_log_event(const uuid_t *uuid, AUDIT_CATEGORY category, AUDIT_COMPONENT com
 		AUDIT_EVENTCLASS evclass, const char *evtype, const char *subject_id,
 		int meta_count, ...)
 {
+	va_list ap;
 	AuditRecord *record = NULL;
 	AuditRecord__Meta **metas = NULL;
 	int ret = 0;
+	int i = 0;
 
 	if (!AUDIT_STORAGE) {
 		TRACE("Attempt to log audit message but AUDIT_STORAGE is zero, skipping...");
@@ -685,18 +687,21 @@ audit_log_event(const uuid_t *uuid, AUDIT_CATEGORY category, AUDIT_COMPONENT com
 			return -1;
 		}
 
-		va_list ap;
-
 		va_start(ap, meta_count);
 
 		metas = mem_alloc0((meta_count / 2) * sizeof(AuditRecord__Meta *));
-		for (int i = 0; i < meta_count / 2; i++) {
+		for (i = 0; i < meta_count / 2; i++) {
 			metas[i] = mem_alloc0(sizeof(AuditRecord__Meta));
 
 			audit_record__meta__init(metas[i]);
+			const char *key = va_arg(ap, const char *);
+			IF_NULL_GOTO(key, err_va);
 
-			metas[i]->key = mem_strdup(va_arg(ap, const char *));
-			metas[i]->value = mem_strdup(va_arg(ap, const char *));
+			const char *value = va_arg(ap, const char *);
+			IF_NULL_GOTO(value, err_va);
+
+			metas[i]->key = mem_strdup(key);
+			metas[i]->value = mem_strdup(value);
 		}
 
 		va_end(ap);
@@ -738,6 +743,21 @@ out:
 	protobuf_free_message((ProtobufCMessage *)record);
 
 	return ret;
+
+err_va:
+	ERROR("Early va_args terminator reached, aborting...");
+	for (int j = i; j >= 0; j--) {
+		if (metas[j]->key)
+			mem_free0(metas[j]->key);
+		if (metas[j]->value)
+			mem_free0(metas[j]->value);
+		mem_free0(metas[j]);
+	}
+	mem_free0(metas);
+
+	va_end(ap);
+
+	return -1;
 }
 
 static void

daemon/c_fifo.c
@@ -205,12 +205,12 @@ c_fifo_create_fifos(list_t *fifo_list, uid_t uid, const char *fifo_dir,
 		}
 
 		audit_log_event(container_uuid, SSA, CMLD, CONTAINER_ISOLATION, "create-fifo",
-				audit_subject, 2, "name", current_fifo);
+				audit_subject, 2, "name", current_fifo, NULL);
 
 		if (chown(fifo_path, uid, uid)) {
 			audit_log_event(container_uuid, FSA, CMLD, CONTAINER_ISOLATION,
 					"prepare-fifo-directory", audit_subject, 2, "path",
-					fifo_path);
+					fifo_path, NULL);
 			ERROR("Failed to chown fifo dir to %d", uid);
 
 			mem_free0(fifo_path);
@@ -218,7 +218,8 @@ c_fifo_create_fifos(list_t *fifo_list, uid_t uid, const char *fifo_dir,
 		}
 
 		audit_log_event(container_uuid, SSA, CMLD, CONTAINER_ISOLATION,
-				"prepare-fifo-directory", audit_subject, 2, "path", fifo_path);
+				"prepare-fifo-directory", audit_subject, 2, "path", fifo_path,
+				NULL);
 		DEBUG("Chowned FIFO at %s to %d", fifo_path ? fifo_path : "NULL", uid);
 
 		mem_free0(fifo_path);

daemon/c_seccomp/seccomp.c
@@ -293,7 +293,8 @@ c_seccomp_fetch_vm_new(c_seccomp_t *seccomp, int pid, void *rbuf, uint64_t size)
 	if (bytes_read < 0) {
 		char *pid_str = mem_printf("%d", pid);
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-vm-access-failed",
-				compartment_get_name(seccomp->compartment), 2, "pid", pid_str);
+				compartment_get_name(seccomp->compartment), 2, "pid", pid_str,
+				NULL);
 
 		ERROR_ERRNO("Failed to access memory of remote process, bytes read: %zd",
 			    bytes_read);
@@ -325,7 +326,8 @@ c_seccomp_send_vm(c_seccomp_t *seccomp, int pid, void *lbuf, void *rbuf, uint64_
 	if (bytes_written < 0) {
 		char *pid_str = mem_printf("%d", pid);
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-vm-access-failed",
-				compartment_get_name(seccomp->compartment), 2, "pid", pid_str);
+				compartment_get_name(seccomp->compartment), 2, "pid", pid_str,
+				NULL);
 
 		ERROR_ERRNO("Failed to access memory of remote process, bytes written: %zd",
 			    bytes_written);
@@ -372,7 +374,7 @@ c_seccomp_handle_notify(int fd, unsigned events, UNUSED event_io_t *io, void *da
 
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-rcv-next",
 				compartment_get_name(seccomp->compartment), 2, "errno",
-				strerror(errno));
+				strerror(errno), NULL);
 		mem_free0(req);
 		mem_free0(resp);
 		return;
@@ -435,7 +437,7 @@ c_seccomp_handle_notify(int fd, unsigned events, UNUSED event_io_t *io, void *da
 		syscall_str = mem_printf("_NR: %d", req->data.nr);
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-unexpected-syscall",
 				compartment_get_name(seccomp->compartment), 2, "syscall",
-				syscall_str);
+				syscall_str, NULL);
 
 		ERROR("Got syscall not handled by us: %d", req->data.nr);
 
@@ -447,13 +449,13 @@ c_seccomp_handle_notify(int fd, unsigned events, UNUSED event_io_t *io, void *da
 	if (-1 == ret_syscall) {
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-emulation-failed",
 				compartment_get_name(seccomp->compartment), 2, "syscall",
-				syscall_str);
+				syscall_str, NULL);
 	}
 
 	if (-1 == seccomp_ioctl(fd, SECCOMP_IOCTL_NOTIF_SEND, resp)) {
 		audit_log_event(NULL, FSA, CMLD, CONTAINER_ISOLATION, "seccomp-send-response",
 				compartment_get_name(seccomp->compartment), 2, "errno",
-				strerror(errno));
+				strerror(errno), NULL);
 		ERROR_ERRNO("Failed to send seccomp notify response");
 	} else {
 		TRACE("Successfully handled seccomp notification");

daemon/c_vol.c
@@ -865,7 +865,7 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t *mntent)
 			audit_log_event(container_get_uuid(vol->container), FSA, CMLD,
 					CONTAINER_MGMT, "setup-crypted-volume-no-key",
 					uuid_string(container_get_uuid(vol->container)), 2, "label",
-					label);
+					label, NULL);
 			ERROR("Trying to mount encrypted volume without key...");
 			mem_free0(label);
 			goto error;
@@ -895,7 +895,7 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t *mntent)
 				audit_log_event(container_get_uuid(vol->container), FSA, CMLD,
 						CONTAINER_MGMT, "setup-crypted-volume",
 						uuid_string(container_get_uuid(vol->container)), 2,
-						"label", label);
+						"label", label, NULL);
 				ERROR("Setting up cryptfs volume %s for %s failed", label, dev);
 				mem_free0(label);
 				goto error;
@@ -903,7 +903,7 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t *mntent)
 			audit_log_event(container_get_uuid(vol->container), SSA, CMLD,
 					CONTAINER_MGMT, "setup-crypted-volume",
 					uuid_string(container_get_uuid(vol->container)), 2, "label",
-					label);
+					label, NULL);
 		}
 
 		mem_free0(label);
@@ -1420,19 +1420,20 @@ c_vol_verify_mount_entries_bg(const c_vol_t *vol)
 						      "image file is corrupted",
 						      mount_entry_get_img(mntent));
 
-						audit_log_event(
-							container_get_uuid(vol->container), FSA,
-							CMLD, CONTAINER_MGMT, "verify-image",
-							uuid_string(
-								container_get_uuid(vol->container)),
-							2, "name", mount_entry_get_img(mntent));
+						audit_log_event(container_get_uuid(vol->container),
+								FSA, CMLD, CONTAINER_MGMT,
+								"verify-image",
+								uuid_string(container_get_uuid(
+									vol->container)),
+								2, "name",
+								mount_entry_get_img(mntent), NULL);
 						_exit(-1);
 					}
 					audit_log_event(
 						container_get_uuid(vol->container), SSA, CMLD,
 						CONTAINER_MGMT, "verify-image",
 						uuid_string(container_get_uuid(vol->container)), 2,
-						"name", mount_entry_get_img(mntent));
+						"name", mount_entry_get_img(mntent), NULL);
 					_exit(0);
 				} else { // parent
 					INFO("dm-verity active for image %s, "

daemon/cmld.c
@@ -1578,7 +1578,7 @@ cmld_init_stage_unit(const char *path)
 	INFO("time initialized.");
 
 	char *btime = mem_printf("%lld", (long long)time_cml(NULL));
-	audit_log_event(NULL, SSA, CMLD, GENERIC, "boot-time", NULL, 2, "time", btime);
+	audit_log_event(NULL, SSA, CMLD, GENERIC, "boot-time", NULL, 2, "time", btime, NULL);
 	mem_free0(btime);
 
 	if (device_config_get_tpm_enabled(device_config)) {

daemon/guestos_mgr.c
@@ -521,7 +521,7 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 
 			audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-old-version",
 					os_name, 4, "installed_version", installed_str,
-					"update_version", update_str);
+					"update_version", update_str, NULL);
 
 			mem_free0(installed_str);
 			mem_free0(update_str);

commit fe8706f3364aed6da54bc59226d23ad699ac71c5

daemon/audit: add NULL sentinel to audit_log_event calls
In commit d191c1fb0631, we missed 2 statements. Also add the
corresponding NULL sentinel to those two calls of audit_log_event().

This silences warning about CWE-121 "Stack-based Buffer Overflow"
complaint by CodeQL cpp/unterminated-variadic-call:

   "Warning Unterminated variadic call
   Calls to audit_log_event should use the value 0 as a terminator
   (105 of 107 calls do)." [CodeQL]

Fixes: d191c1fb0631 ("daemon/audit: explicitly check for va_args terminator")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -821,7 +821,7 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 
 		char *record_type = mem_printf("type=%hu", type);
 		audit_log_event(uuid, (strstr(res, "success") || res[0] == '1') ? SSA : FSA, CMLD,
-				KAUDIT, record_type, uuid_str, 2, "msg", log_record);
+				KAUDIT, record_type, uuid_str, 2, "msg", log_record, NULL);
 		mem_free0(record_type);
 		TRACE("audit: type=%d %s", type, log_record);
 	} else if (type == AUDIT_DM_EVENT) {
@@ -865,7 +865,7 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 			sector_str = mem_printf("%llu", sector);
 			audit_log_event(uuid, (res == 1) ? SSA : FSA, CMLD, CONTAINER_MGMT,
 					"dm-audit", uuid_str, 6, "op", op_buf, "label", dev_name,
-					"sector", sector_str);
+					"sector", sector_str, NULL);
 
 			mem_free0(sector_str);
 			mem_free0(dev_name);