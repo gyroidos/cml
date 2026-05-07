gyroidos/cml

commit 68dd4a0b851f41d007afd519cd234fff00021641

common/dir: Provide dir_chown_folder() helper for recursively chown
The dir_chown_folder() helper can be used to recursively chown all
contents of a directory to a uid and gid provided as parameters.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/dir.c
@@ -302,3 +302,71 @@ dir_copy_folder(const char *source, const char *target,
 	dir_copy_params_free(params);
 	return ret;
 }
+
+struct chown_data {
+	uid_t uid;
+	gid_t gid;
+};
+
+static int
+dir_chown_contents_cb(const char *path, const char *file, void *data)
+{
+	struct stat s;
+	int ret = 0;
+	struct chown_data *cb_data = data;
+	ASSERT(cb_data);
+
+	char *file_to_chown = mem_printf("%s/%s", path, file);
+	if (lstat(file_to_chown, &s) == -1) {
+		mem_free0(file_to_chown);
+		return -1;
+	}
+
+	uid_t uid = cb_data->uid;
+	gid_t gid = cb_data->gid;
+
+	if (file_is_dir(file_to_chown)) {
+		TRACE("Path %s is dir", file_to_chown);
+		if (dir_foreach(file_to_chown, &dir_chown_contents_cb, cb_data) < 0) {
+			ERROR_ERRNO("Could not chown all dir contents in '%s' to (%d:%d)",
+				    file_to_chown, uid, gid);
+			ret--;
+		}
+		if (chown(file_to_chown, uid, gid) < 0) {
+			ERROR_ERRNO("Could not chown dir '%s' to (%d:%d)", file_to_chown, uid, gid);
+			ret--;
+		}
+	} else {
+		if (lchown(file_to_chown, uid, gid) < 0) {
+			ERROR_ERRNO("Could not chown file '%s' to (%d:%d)", file_to_chown, uid,
+				    gid);
+			ret--;
+		}
+	}
+	TRACE("Chown file '%s' to (%d:%d)", file_to_chown, uid, gid);
+
+	// chown .
+	if (chown(path, uid, gid) < 0) {
+		ERROR_ERRNO("Could not chown dir '%s' to (%d:%d)", path, uid, gid);
+		ret--;
+	}
+	mem_free0(file_to_chown);
+	return ret;
+}
+
+int
+dir_chown_folder(const char *path, uid_t uid, gid_t gid)
+{
+	struct stat s;
+	IF_TRUE_RETVAL(stat(path, &s), -1);
+
+	int ret = 0;
+
+	struct chown_data cb_data = { .uid = uid, .gid = gid };
+	if (dir_foreach(path, &dir_chown_contents_cb, &cb_data) < 0) {
+		ERROR("Could not chown %s to uid:gid (%d:%d)", path, uid, gid);
+		ret--;
+	}
+
+	return ret;
+}

common/dir.h
@@ -67,4 +67,15 @@ int
 dir_copy_folder(const char *source, const char *target,
 		bool (*filter)(const char *file, void *data), void *filter_data);
 
+/**
+ * Chown all files and subdirectories in a directory recursively. 
+ *
+ * @param path The path of the directory.
+ * @param uid uid to be set in the directory.
+ * @param gid gid to be set in the directory.
+ * @returns 0 on success, -1 on error.
+ */
+int
+dir_chown_folder(const char *path, uid_t uid, gid_t gid);
+
 #endif /* DIR_H */

commit a6c2a3d7078f5fed62e680d0346653fc408962f2

common/dir: Fix TOCTOU and symlink-following in dir_chown_folder()
Replace file_is_dir() with S_ISDIR(s.st_mode) to use the already
available lstat result, eliminating a TOCTOU window between two
successive lstat calls. Use lchown uniformly instead of chown to
prevent following symlinks that may have been swapped in between
stat and chown. In dir_chown_folder, use lstat instead of stat for
the same reason.

Fixes: 68dd4a0b851f ("common/dir: Provide dir_chown_folder() helper for recursively chown")
Assisted-by: Claude:claude-opus-4-6
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/dir.c
@@ -329,23 +329,17 @@ dir_chown_contents_cb(const char *path, const char *file, void *data)
 	if (cb_data->id_adjust_cb)
 		cb_data->id_adjust_cb(&s, &uid, &gid);
 
-	if (file_is_dir(file_to_chown)) {
+	if (S_ISDIR(s.st_mode)) {
 		TRACE("Path %s is dir", file_to_chown);
 		if (dir_foreach(file_to_chown, &dir_chown_contents_cb, cb_data) < 0) {
 			ERROR_ERRNO("Could not chown all dir contents in '%s' to (%d:%d)",
 				    file_to_chown, uid, gid);
 			ret--;
 		}
-		if (chown(file_to_chown, uid, gid) < 0) {
-			ERROR_ERRNO("Could not chown dir '%s' to (%d:%d)", file_to_chown, uid, gid);
-			ret--;
-		}
-	} else {
-		if (lchown(file_to_chown, uid, gid) < 0) {
-			ERROR_ERRNO("Could not chown file '%s' to (%d:%d)", file_to_chown, uid,
-				    gid);
-			ret--;
-		}
+	}
+	if (lchown(file_to_chown, uid, gid) < 0) {
+		ERROR_ERRNO("Could not chown '%s' to (%d:%d)", file_to_chown, uid, gid);
+		ret--;
 	}
 	TRACE("Chown file '%s' to (%d:%d)", file_to_chown, uid, gid);
 
@@ -358,7 +352,7 @@ dir_chown_folder(const char *path, uid_t uid, gid_t gid,
 		 void (*id_adjust_cb)(struct stat *, uid_t *, gid_t *))
 {
 	struct stat s;
-	IF_TRUE_RETVAL(stat(path, &s), -1);
+	IF_TRUE_RETVAL(lstat(path, &s), -1);
 
 	uid_t _uid = uid;
 	gid_t _gid = gid;
@@ -367,7 +361,7 @@ dir_chown_folder(const char *path, uid_t uid, gid_t gid,
 		id_adjust_cb(&s, &_uid, &_gid);
 
 	// chown .
-	if (chown(path, _uid, _gid) < 0) {
+	if (lchown(path, _uid, _gid) < 0) {
 		ERROR_ERRNO("Could not chown dir '%s' to (%d:%d)", path, _uid, _gid);
 		return -1;
 	}