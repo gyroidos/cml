gyroidos/cml

commit 52f1c5fa77cb72c2411b2fa271e639ed0e754b50

daemon/oci, c_oci: Initial module for oci compatible containers
This module implements a protobuf control interface (oci_control)
which receives commands from an external oci wrapper binary (rung,
see next commit). The runtme binary implements a oci runtime command
line interface and wraps those commands to oci_control messages.
Including to transfer the json-based con fig file. The oci module makes
use of the libcoispec to parse that config and extracts relevant parts
which are mapped to a cml container. 'c_oci' implements the corresponding
compartment module which pauses the counter start during oci create.
The oci CREATE command should start the namespace but not the child
process, those this corresponds to the cml contaienr state BOOTING.
At that point we use the pause() syscall to pause execution of the
container child just before execve of init. After a oci START command
is received the oci module sends an interrupt to the child process of
the container and continues execution.

Note that this is module only provide rudimentary oci complainant
functionality and does not yet fulfil the oci spec completely.
Especially, hooks are not yet implemented. Further, we enfore safe
defaults, hard set namespaces, cgroups and capabilities and do ignore
these parts of the oci josn-config. Further, do we implement a
writable 'confidential' layer over the oci rootfs which is only
accessible in the CML's mount namespace.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/oci.h
@@ -1,0 +1,54 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2022 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "container.h"
+
+/**
+ * Data structure containing the variables associated to a oci control socket.
+ */
+typedef struct oci_control oci_control_t;
+
+/**
+ * Data structure containing the variables associated to a oci container.
+ */
+typedef struct oci_container oci_container_t;
+
+/**
+ * Frees the oci_container data structure
+ */
+void
+oci_container_free(oci_container_t *oci_container);
+
+/**
+ * Get the oci_container data structure by its corresponding container reference
+ *
+ * container and oci_container always have a one-by-one relationship
+ */
+oci_container_t *
+oci_get_oci_container_by_container(const container_t *container);
+
+/**
+ * Create a control socket receiving the OCI wrapper messages
+ */
+oci_control_t *
+oci_control_new(char *path);

daemon/Makefile
@@ -27,6 +27,7 @@ AGGRESSIVE_WARNINGS ?= y
 SANITIZERS ?= n
 CC_MODE ?= n
 WCAST_ALIGN ?= y
+OCI ?= n
 
 TRUSTME_HARDWARE := x86
 
@@ -128,6 +129,15 @@ SRC_CMODULES := \
 	c_cap.c \
 	c_uevent.c
 
+ifeq ($(OCI),y)
+    LOCAL_CFLAGS += -DOCI -I${HOME}/rcs/libocispec/src
+    LDLIBS += -locispec -L${HOME}/rcs/libocispec/.libs -lyajl
+    PROTO_SRC += oci_control.pb-c.c
+    SRC_FILES += oci.c
+    SRC_CMODULES += c_oci.c
+endif
+
+
 ifeq ($(CC_MODE),y)
 protobuf: container.proto control.proto guestos.proto common/logf.proto device.proto scd.proto common/audit.proto c_service.proto
 	$(MAKE) -C cc_mode
@@ -145,7 +155,7 @@ protobuf: container.proto control.proto guestos.proto common/logf.proto device.p
 	$(MAKE) -C common protobuf
 
 else
-protobuf: container.proto control.proto guestos.proto common/logf.proto device.proto scd.proto common/audit.proto c_service.proto
+protobuf: container.proto control.proto guestos.proto common/logf.proto device.proto scd.proto common/audit.proto c_service.proto oci_control.proto
 	protoc-c --c_out=. container.proto
 	protoc-c --c_out=. control.proto
 	protoc-c --c_out=. guestos.proto
@@ -154,6 +164,7 @@ protobuf: container.proto control.proto guestos.proto common/logf.proto device.p
 	protoc-c --c_out=. attestation.proto
 	protoc-c --c_out=. tpm2d.proto
 	protoc-c --c_out=. c_service.proto
+	protoc-c --c_out=. oci_control.proto
 	$(MAKE) -C common protobuf
 
 endif

daemon/c_oci.c
@@ -1,0 +1,157 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2022 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "oci.h"
+
+#include "container.h"
+#include "hardware.h"
+
+#include "common/file.h"
+#include "common/macro.h"
+#include "common/mem.h"
+#include "common/hex.h"
+
+#include <unistd.h>
+
+#define MOD_NAME "c_oci"
+
+#define TOKEN_KEY_LEN 96 // actual encryption key + hmac key
+
+typedef struct c_oci {
+	container_t *container; //!< container which the c_user struct is associated to
+	char *key_file;		//!< file for generated container key
+} c_oci_t;
+
+static int
+c_oci_start_pre_clone(void *ocip)
+{
+	c_oci_t *oci = ocip;
+	ASSERT(oci);
+
+	// TODO decide location of key e.g. in hardware TPM
+	char *ascii_key = NULL;
+	if (file_exists(oci->key_file)) {
+		ascii_key = file_read_new(oci->key_file, file_size(oci->key_file));
+	} else {
+		unsigned char key[TOKEN_KEY_LEN];
+		int keylen = hardware_get_random(key, sizeof(key));
+		ascii_key = convert_bin_to_hex_new(key, keylen);
+		mem_memset0(key, sizeof(key));
+		// include terminating null byte into file
+		file_write(oci->key_file, ascii_key, strlen(ascii_key) + 1);
+	}
+
+	if (ascii_key) {
+		container_set_key(oci->container, ascii_key);
+
+		// delete key from RAM
+		mem_memset0(ascii_key, strlen(ascii_key));
+		mem_free0(ascii_key);
+		return 0;
+	}
+
+	ERROR("Failed to execute start early clone hook for oci");
+	return -COMPARTMENT_ERROR_VOL;
+}
+
+static int
+c_oci_start_pre_exec_child(void *ocip)
+{
+	c_oci_t *oci = ocip;
+	ASSERT(oci);
+
+	// pause container after OCI_CREATE util OCI_START
+	pause();
+
+	DEBUG("%s %s", CSERVICE_TARGET, file_exists(CSERVICE_TARGET) ? "exists" : "does not exist");
+	return 0;
+}
+
+/**
+ * This function allocates a new c_oci_t instance, associated to a specific container object.
+ * @return the c_oci_t user structure which holds oci spcific information for a container.
+ */
+static void *
+c_oci_new(compartment_t *compartment)
+{
+	ASSERT(compartment);
+	IF_NULL_RETVAL(compartment_get_extension_data(compartment), NULL);
+
+	c_oci_t *oci = mem_new0(c_oci_t, 1);
+	oci->container = compartment_get_extension_data(compartment);
+
+	oci->key_file = mem_printf("%s-oci.key", container_get_images_dir(oci->container));
+
+	return oci;
+}
+
+static void
+c_oci_free(void *ocip)
+{
+	c_oci_t *oci = ocip;
+	ASSERT(oci);
+
+	oci_container_t *oci_container = oci_get_oci_container_by_container(oci->container);
+	// free oci wrapping structure
+	oci_container_free(oci_container);
+
+	if (oci->key_file)
+		mem_free0(oci->key_file);
+
+	mem_free0(oci);
+}
+
+static void
+c_oci_destroy(void *ocip)
+{
+	c_oci_t *oci = ocip;
+	ASSERT(oci);
+
+	if (unlink(oci->key_file))
+		WARN("Could not remove %s!", oci->key_file);
+}
+
+static compartment_module_t c_oci_module = {
+	.name = MOD_NAME,
+	.compartment_new = c_oci_new,
+	.compartment_free = c_oci_free,
+	.compartment_destroy = c_oci_destroy,
+	.start_post_clone_early = NULL,
+	.start_child_early = NULL,
+	.start_pre_clone = c_oci_start_pre_clone,
+	.start_post_clone = NULL,
+	.start_pre_exec = NULL,
+	.start_post_exec = NULL,
+	.start_child = NULL,
+	.start_pre_exec_child = c_oci_start_pre_exec_child,
+	.stop = NULL,
+	.cleanup = NULL,
+	.join_ns = NULL,
+};
+
+static void INIT
+c_oci_init(void)
+{
+	// register this module in compartment.c
+	compartment_register_module(&c_oci_module);
+}

daemon/cmld.c
@@ -59,6 +59,7 @@
 #include "container_config.h"
 #include "container.h"
 #include "input.h"
+#include "oci.h"
 
 #include <inttypes.h>
 #include <stdio.h>
@@ -120,6 +121,13 @@ static bool cmld_signed_configs = false;
 static bool cmld_device_provisioned = false;
 
 static enum command cmld_device_reboot = POWER_OFF;
+
+#ifdef OCI
+// clang-format off
+#define CMLD_OCI_CONTROL_SOCKET SOCK_PATH(oci-control)
+// clang-format on
+static oci_control_t *cmld_oci_control_cml = NULL;
+#endif
 
 /******************************************************************************/
 
@@ -1417,6 +1425,14 @@ cmld_init(const char *path)
 		FATAL("Could not init cmld_cli control socket");
 	}
 	INFO("created control socket.");
+
+#ifdef OCI
+	cmld_oci_control_cml = oci_control_new(CMLD_OCI_CONTROL_SOCKET);
+	if (!cmld_oci_control_cml) {
+		FATAL("Could not init cmld_oci control socket");
+	}
+	INFO("created oci control socket.");
+#endif
 
 	char *guestos_path = mem_printf("%s/%s", path, CMLD_PATH_GUESTOS_DIR);
 	bool allow_locally_signed = device_config_get_locally_signed_images(device_config);

daemon/oci_control.proto
@@ -1,0 +1,82 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2022 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+syntax = "proto2";
+
+option java_package = "de.fraunhofer.aisec.gyroidos";
+
+/**
+ * OCI Command message sent to and processed by the cml-daemon on the device.
+ * mapped oci runtime commands to gyroidos oci forwarder (rung)
+ */
+message OciCommand {
+	enum Operation {
+		// query information of a container
+		STATE = 1;
+
+		// Creates a container, answers with CONTAINER_CONFIGS
+		CREATE = 2;
+
+		// Starts a container.
+		START = 3;
+
+		// Stops/Kills a container.
+		KILL = 4;
+
+		// Removes container config and destroys created container image files
+		DELETE = 5;
+	}
+	required Operation operation = 1;
+
+
+	/////////////////////////////
+	// Command-specific params //
+	/////////////////////////////
+
+	required string container_id = 10; // container id, may be used as input for cmld contaier UUID
+	optional int32 signal = 20;	// used for kill operation
+	optional string bundle_path = 30; // used for create (location of bundle, containing config.json)
+	optional bytes oci_config_file = 31; // config.json allready read to buffer
+}
+
+/**
+ * Control message sent from the cml-daemon on the device to the backend/cmdline tool/etc.
+ */
+message OciResponse {
+	enum Code {
+		RESPONSE = 1;		// -> [response]
+		STATE = 2;		// answer to state request
+
+	}
+	enum Response {
+		CMD_OK = 1;
+		CMD_FAILED = 2;
+	}
+
+	required Code code = 1;
+
+	optional Response response = 2; 	// generic response
+	optional string state = 3;		// container oci state as json string
+	optional int32 pid = 4;			// pid of corresponding container
+	optional string status = 5;		// status of corresponding container
+}

commit 5503ed3e905c21ea39b8f653c16a059b56df69d7

daemon/oci: Expose oci_control_free in header
oci_control_free() is defined in oci.c but was never declared in oci.h,
so external callers cannot use it. Add the missing prototype.

Fixes: 52f1c5fa77cb ("daemon/oci, c_oci: Initial module for oci compatible containers")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/oci.h
@@ -111,3 +111,12 @@ oci_control_new(int socket);
  */
 oci_control_t *
 oci_control_local_new(const char *path);
+
+/**
+ * Releases an oci_control_t object: removes pending IO events, closes connected
+ * client sockets, and frees the struct.
+ *
+ * @param oci_control oci_control_t to release
+ */
+void
+oci_control_free(oci_control_t *oci_control);