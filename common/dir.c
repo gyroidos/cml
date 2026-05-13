gyroidos/cml

commit 1af5083a91458ac820d188a6860431fd5f410efd

common/dir: Fix TOCTOU and symlink-following in dir_delete_folder()
Use lstat instead of stat so symlinks are not followed when deciding
whether an entry is a directory. Check lstat failure explicitly
instead of falling through to the directory branch. This prevents
recursive deletion of a symlink target's contents if an attacker
places a symlink in the tree being deleted.

Fixes: ed54281027e7 ("trustme github release (based on Android 5.1.1)")
Assisted-by: Claude:claude-opus-4-6
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/dir.c
@@ -116,13 +116,12 @@ dir_unlink_folder_contents_cb(const char *path, const char *name, UNUSED void *d
 	struct stat stat_buffer;
 	int ret = 0;
 	char *file_to_remove = mem_printf("%s/%s", path, name);
-	if (stat(file_to_remove, &stat_buffer) == 0 && !S_ISDIR(stat_buffer.st_mode)) {
-		TRACE("Unlinking file %s", file_to_remove);
-		if (unlink(file_to_remove) == -1) {
-			ERROR_ERRNO("Could not delete file %s", file_to_remove);
-			ret--;
-		}
-	} else {
+	if (lstat(file_to_remove, &stat_buffer) == -1) {
+		ERROR_ERRNO("Could not lstat %s", file_to_remove);
+		mem_free0(file_to_remove);
+		return -1;
+	}
+	if (S_ISDIR(stat_buffer.st_mode)) {
 		TRACE("Path %s is dir", file_to_remove);
 		if (dir_foreach(file_to_remove, &dir_unlink_folder_contents_cb, NULL) < 0) {
 			ERROR_ERRNO("Could not delete all dir contents in %s", file_to_remove);
@@ -131,6 +130,12 @@ dir_unlink_folder_contents_cb(const char *path, const char *name, UNUSED void *d
 		TRACE("Removing now empty dir %s", file_to_remove);
 		if (rmdir(file_to_remove) < 0) {
 			ERROR_ERRNO("Could not delete dir %s", file_to_remove);
+			ret--;
+		}
+	} else {
+		TRACE("Unlinking %s", file_to_remove);
+		if (unlink(file_to_remove) == -1) {
+			ERROR_ERRNO("Could not delete %s", file_to_remove);
 			ret--;
 		}
 	}

commit ba2f3b0780d40977453f87f9c103d33779890bac

common/dir: Use fstatat/unlinkat in dir_delete_folder()
The previous fix replaced stat with lstat and file_is_dir with
S_ISDIR but a TOCTOU remained between lstat and the subsequent
rmdir/unlink calls. Use fstatat with AT_SYMLINK_NOFOLLOW and
unlinkat relative to a parent dirfd opened with O_NOFOLLOW to
anchor all operations to the same directory inode.

Fixes: 1af5083a9145 ("common/dir: Fix TOCTOU and symlink-following in dir_delete_folder()")
Assisted-by: Claude:claude-opus-4-6
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/dir.c
@@ -30,6 +30,7 @@
 
 #include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -115,31 +116,41 @@ dir_unlink_folder_contents_cb(const char *path, const char *name, UNUSED void *d
 {
 	struct stat stat_buffer;
 	int ret = 0;
-	char *file_to_remove = mem_printf("%s/%s", path, name);
-	if (lstat(file_to_remove, &stat_buffer) == -1) {
-		ERROR_ERRNO("Could not lstat %s", file_to_remove);
-		mem_free0(file_to_remove);
+
+	int dirfd = open(path, O_RDONLY | O_DIRECTORY | O_NOFOLLOW);
+	if (dirfd < 0) {
+		ERROR_ERRNO("Could not open parent dir %s", path);
 		return -1;
 	}
+
+	if (fstatat(dirfd, name, &stat_buffer, AT_SYMLINK_NOFOLLOW) == -1) {
+		ERROR_ERRNO("Could not fstatat %s/%s", path, name);
+		close(dirfd);
+		return -1;
+	}
+
 	if (S_ISDIR(stat_buffer.st_mode)) {
-		TRACE("Path %s is dir", file_to_remove);
-		if (dir_foreach(file_to_remove, &dir_unlink_folder_contents_cb, NULL) < 0) {
-			ERROR_ERRNO("Could not delete all dir contents in %s", file_to_remove);
+		char *subdir = mem_printf("%s/%s", path, name);
+		TRACE("Path %s is dir", subdir);
+		if (dir_foreach(subdir, &dir_unlink_folder_contents_cb, NULL) < 0) {
+			ERROR_ERRNO("Could not delete all dir contents in %s", subdir);
 			ret--;
 		}
-		TRACE("Removing now empty dir %s", file_to_remove);
-		if (rmdir(file_to_remove) < 0) {
-			ERROR_ERRNO("Could not delete dir %s", file_to_remove);
+		mem_free0(subdir);
+		TRACE("Removing dir %s/%s", path, name);
+		if (unlinkat(dirfd, name, AT_REMOVEDIR) < 0) {
+			ERROR_ERRNO("Could not delete dir %s/%s", path, name);
 			ret--;
 		}
 	} else {
-		TRACE("Unlinking %s", file_to_remove);
-		if (unlink(file_to_remove) == -1) {
-			ERROR_ERRNO("Could not delete %s", file_to_remove);
+		TRACE("Unlinking %s/%s", path, name);
+		if (unlinkat(dirfd, name, 0) < 0) {
+			ERROR_ERRNO("Could not delete %s/%s", path, name);
 			ret--;
 		}
 	}
-	mem_free0(file_to_remove);
+
+	close(dirfd);
 	return ret;
 }
 