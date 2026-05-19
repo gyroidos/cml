gyroidos/cml

commit 2bde7c654478b49bf3f514f91dadcc63ffb4b900

common/network: Added helper functions for mac handling
Introduce some converters between string and byte representation
of network mac addresses. Further, functions to find the corresponding
interface name to a mac address and vice versa are provided.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/network.c
@@ -652,3 +652,76 @@ msg_err:
 	nl_sock_free(nl_sock);
 	return -1;
 }
+
+int
+network_str_to_mac_addr(const char *mac_str, uint8_t mac[6])
+{
+	int ret =
+		sscanf(mac_str,
+		       "%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8,
+		       &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+
+	IF_TRUE_RETVAL((ret == EOF || ret < 6), -1);
+
+	return 0;
+}
+
+char *
+network_mac_addr_to_str_new(uint8_t mac[6])
+{
+	return mem_printf("%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8 ":%02" SCNx8
+			  ":%02" SCNx8,
+			  mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
+
+int
+network_get_mac_by_ifname(const char *ifname, uint8_t mac[6])
+{
+	IF_NULL_RETVAL(ifname, -1);
+	IF_NULL_RETVAL(mac, -1);
+
+	char *dev_addr_path = mem_printf("/sys/class/net/%s/address", ifname);
+	char *mac_str = file_read_new(dev_addr_path, 128);
+	mem_free(dev_addr_path);
+
+	IF_NULL_RETVAL(mac_str, -1);
+
+	memset(mac, 0, 6);
+	int ret = network_str_to_mac_addr(mac_str, mac);
+
+	mem_free(mac_str);
+	return ret;
+}
+
+char *
+network_get_ifname_by_addr_new(uint8_t mac[6])
+{
+	IF_NULL_RETVAL(mac, NULL);
+
+	struct if_nameindex *if_ni, *i;
+	if_ni = if_nameindex();
+
+	IF_NULL_RETVAL(if_ni, NULL);
+
+	uint8_t mac_i[6];
+
+	for (i = if_ni; i->if_index != 0 || i->if_name != NULL; i++) {
+		char *dev_addr_path = mem_printf("/sys/class/net/%s/address", i->if_name);
+		char *mac_str = file_read_new(dev_addr_path, 128);
+		mem_free(dev_addr_path);
+		if (mac_str == NULL)
+			continue;
+
+		memset(mac_i, 0, 6);
+
+		if ((0 == network_str_to_mac_addr(mac_str, mac_i)) &&
+		    (0 == memcmp(mac, mac_i, 6))) {
+			mem_free(mac_str);
+			return mem_strdup(i->if_name);
+		}
+
+		mem_free(mac_str);
+	}
+
+	return NULL;
+}

common/network.h
@@ -188,4 +188,40 @@ network_rtnet_move_ns(const char *ifi_name, const pid_t pid);
 int
 network_rename_ifi(const char *old_ifi_name, const char *new_ifi_name);
 
+/**
+ * Convert a String representing a mac address ,e.g., "00:11:22:33:44:55"
+ * to the corresponding byte array.
+ * @param mac_str String representing the mac
+ * @param mac buffer for the resulting byte array
+ * @return 0 on success, -1 on error
+ */
+int
+network_str_to_mac_addr(const char *mac_str, uint8_t mac[6]);
+
+/**
+ * Constructs a String representation for a mac address.
+ * @param mac array to be converted
+ * @return The string representing the mac, NULL on error
+ */
+char *
+network_mac_addr_to_str_new(uint8_t mac[6]);
+
+/**
+ * Walk through sysfs to find the if name, e.g., shown by ip addr, to the corresponding
+ * hardware (mac) address.
+ * @param mac array containing the mac address
+ * @return The name of the interface
+ */
+char *
+network_get_ifname_by_addr_new(uint8_t mac[6]);
+
+/**
+ * Get mac address for network interface given by name.
+ * @param ifname network interface name
+ * @param mac buffer for the resulting byte array
+ * @return 0 on success, -1 on error
+ */
+int
+network_get_mac_by_ifname(const char *ifname, uint8_t mac[6]);
+
 #endif /* NETWORK_H */

commit 86fb887a923e7b18d9bd5a6ab51f969f84efd64b

common/network: Free if_nameindex in network_get_ifname_by_addr_new
The function calls if_nameindex(3) but never paired it with
if_freenameindex(3) on either exit path: the early return on a
matching interface and the fall-through return at the end both leak
the netlink-allocated nameindex array. Detected by AddressSanitizer
(6 direct leaks attributed to if_nameindex_netlink).

Restructure the function to store the result in a local, break out of
the loop on match, and call if_freenameindex() unconditionally before
returning.

Fixes: 2bde7c654478 ("common/network: Added helper functions for mac handling")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

common/network.c
@@ -1134,6 +1134,7 @@ network_get_ifname_by_addr_new(uint8_t mac[MAC_ADDR_LEN])
 	IF_NULL_RETVAL(if_ni, NULL);
 
 	uint8_t mac_i[MAC_ADDR_LEN];
+	char *ifname = NULL;
 
 	for (i = if_ni; i->if_index != 0 || i->if_name != NULL; i++) {
 		char *dev_addr_path, *dev_bridge_path, *mac_str;
@@ -1161,13 +1162,15 @@ network_get_ifname_by_addr_new(uint8_t mac[MAC_ADDR_LEN])
 		if ((0 == network_str_to_mac_addr(mac_str, mac_i)) &&
 		    (0 == memcmp(mac, mac_i, MAC_ADDR_LEN))) {
 			mem_free0(mac_str);
-			return mem_strdup(i->if_name);
+			ifname = mem_strdup(i->if_name);
+			break;
 		}
 
 		mem_free0(mac_str);
 	}
 
-	return NULL;
+	if_freenameindex(if_ni);
+	return ifname;
 }
 
 char *