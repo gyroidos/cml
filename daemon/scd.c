gyroidos/cml

commit 8b63acca54fbdb7c23f491123c158941ea6b6f8d

daemon/scd: start scd as unit with auto reconnect
Use new unit module to run scd inside of an unprivileged compartment.
Since scd terminates during provisioning, we set restart paramter
of the unit to true. Thus, the scd unit is restarted in case of
disconnection of the scd socket and termination of the scd.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/scd.c
@@ -26,21 +26,16 @@
 #include "scd.h"
 #include "scd_shared.h"
 #include "cmld.h"
+#include "unit.h"
 
 #include "common/macro.h"
 #include "common/logf.h"
 #include "common/event.h"
 #include "common/fd.h"
 #include "common/file.h"
-#include "common/sock.h"
 #include "common/proc.h"
 #include "common/protobuf.h"
 #include "common/mem.h"
-
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 // clang-format off
 #define SCD_CONTROL_SOCKET "scd_control"
@@ -50,37 +45,11 @@
 #define SCD_BINARY_NAME "scd"
 #endif
 
+#define SCD_UUID "00000000-0000-0000-0000-000000000001"
+
 char *scd_sock_path = NULL;
 
-static pid_t scd_pid;
-
-static pid_t
-scd_fork_and_exec(void)
-{
-	TRACE("Starting scd..");
-
-	int status;
-	pid_t pid = fork();
-	char *const param_list[] = { SCD_BINARY_NAME, NULL };
-
-	switch (pid) {
-	case -1:
-		ERROR_ERRNO("Could not fork for %s", SCD_BINARY_NAME);
-		return -1;
-	case 0:
-		execvp((const char *)param_list[0], param_list);
-		FATAL_ERRNO("Could not execvp %s", SCD_BINARY_NAME);
-		return -1;
-	default:
-		// Just check if the child is alive but do not wait
-		if (waitpid(pid, &status, WNOHANG) != 0) {
-			ERROR("Failed to start %s", SCD_BINARY_NAME);
-			return -1;
-		}
-		return pid;
-	}
-	return -1;
-}
+static unit_t *scd_unit;
 
 static int
 scd_register_listener(int fd)
@@ -164,64 +133,49 @@ connection_err:
 	event_io_free(io);
 	if (close(fd) < 0)
 		WARN_ERRNO("Failed to close connected scd event socket");
-	// reconnect
-	scd_init(false);
 	return;
 }
 
-int
-scd_init(bool start_daemon)
+static void
+scd_on_connect_cb(int sock, const char *sock_path)
 {
-	int sock = -1;
-
-	// In hosted mode, the init scripts should take care of correct scd initialization and start-up
-	if (start_daemon) {
-		// if device.cert is not present, start scd to initialize device (provisioning mode)
-		if (!file_exists(DEVICE_CERT_FILE)) {
-			INFO("Starting scd in Provisioning / Installing Mode");
-			// Start the SCD in provisioning mode
-			const char *const args[] = { SCD_BINARY_NAME, NULL };
-			IF_FALSE_RETVAL_TRACE(proc_fork_and_execvp(args) == 0, -1);
-		}
-
-		// Start SCD and wait for control interface
-		scd_pid = scd_fork_and_exec();
-		IF_TRUE_RETVAL_TRACE(scd_pid == -1, -1);
-	} else {
-		DEBUG("Skipping scd launch as requested");
-		scd_pid = -1;
-	}
-
-	scd_sock_path = sock_get_path_new(SCD_CONTROL_SOCKET);
-	size_t retries = 0;
-	do {
-		NANOSLEEP(0, 500000000)
-		sock = sock_unix_create_and_connect(SOCK_SEQPACKET, scd_sock_path);
-		retries++;
-		TRACE("Retry %zu connecting to scd", retries);
-	} while (sock < 0 && retries < 10);
-
-	if (sock < 0) {
-		ERROR("Failed to connect to scd");
-		close(sock);
-		return -1;
-	}
-
+	ASSERT(sock_path);
 	if (scd_register_listener(sock) < 0) {
 		ERROR("Failed to register event listener for scd events");
+		if (scd_sock_path)
+			mem_free0(scd_sock_path);
 		close(sock);
-		return -1;
+		return;
 	}
+
+	if (scd_sock_path)
+		mem_free0(scd_sock_path);
+	scd_sock_path = mem_strdup(sock_path);
 
 	/* register socket for receiving data */
 	fd_make_non_blocking(sock);
 
-	event_io_t *event = event_io_new(sock, EVENT_IO_READ, scd_event_cb_recv_message, NULL);
+	event_io_t *event = event_io_new(sock, EVENT_IO_READ, &scd_event_cb_recv_message, NULL);
 	event_add_io(event);
 
-	// allow access from namespaced child before chroot and execv of init
-	if (chmod(scd_sock_path, 00777))
-		WARN("could not change access rights for scd control socket");
+	if (cmld_init_stage_container() < 0)
+		FATAL("Could not init cmld (container stage)!");
+}
+
+int
+scd_init(void)
+{
+	// if device.cert is not present, scd will die. Hence, we set autorestart in unit_new
+	scd_unit = unit_new(uuid_new(SCD_UUID), "SCD", SCD_BINARY_NAME, NULL, NULL, 0, false,
+			    SCD_CONTROL_SOCKET, &scd_on_connect_cb, true);
+
+	IF_NULL_RETVAL(scd_unit, -1);
+
+	if (unit_start(scd_unit)) {
+		unit_free(scd_unit);
+		ERROR("Could nor start unit for scd!");
+		return -1;
+	}
 
 	return 0;
 }
@@ -229,9 +183,8 @@ scd_init(bool start_daemon)
 void
 scd_cleanup(void)
 {
-	IF_TRUE_RETURN_TRACE(scd_pid == -1);
-	DEBUG("Stopping %s process with pid=%d!", SCD_BINARY_NAME, scd_pid);
-	mem_free(scd_sock_path);
-	scd_sock_path = NULL;
-	kill(scd_pid, SIGTERM);
+	unit_kill(scd_unit);
+	if (scd_sock_path)
+		mem_free0(scd_sock_path);
+	unit_free(scd_unit);
 }

daemon/cmld.c
@@ -1455,7 +1455,6 @@ cmld_init_stage_reached(cmld_init_stage_t stage)
 	case CMLD_INIT_STAGE_UNIT:
 		INFO("CMLD_INIT_STAGE_UNIT completed.");
 		cmld_init_stage = stage;
-		cmld_init_stage_container();
 		break;
 	case CMLD_INIT_STAGE_CONTAINER:
 		INFO("CMLD_INIT_STAGE_CONTAINER completed.");
@@ -1523,7 +1522,7 @@ cmld_init_stage_unit(const char *path)
 	audit_log_event(NULL, SSA, CMLD, GENERIC, "boot-time", NULL, 2, "time", btime);
 	mem_free0(btime);
 
-	if (scd_init(!cmld_is_hostedmode_active()) < 0)
+	if (scd_init() < 0)
 		FATAL("Could not init scd module");
 	INFO("scd initialized.");
 	if (atexit(&scd_cleanup))

daemon/scd.h
@@ -28,11 +28,10 @@
 
 /**
  * Initializes the scd subsystem (starts the corresponding daemon)
- * @param start_daemon Fork and execute the scd daemon
  * @return 0 on success, -1 on error
  */
 int
-scd_init(bool start_daemon);
+scd_init(void);
 
 /**
  * Cleans up the scd subsystem (stops the corresponding daemon)

commit 5c630203fad514fc363d6b7c59a3ddfe46407e10

daemon/scd: Release stale event io on reconnect
scd_on_connect_cb() overwrites the file-scope scd_event_io
unconditionally on every (re-)connect of the scd unit. If the new
connection is established while the previous connection's event io is
still registered, i.e. its EVENT_IO_EXCEPT error event has not been
processed yet, the old handle is untracked from that point on. It is
still freed if its error event fires later, but if cmld exits first,
scd_cleanup() only releases the new handle and the old event_io_t,
its list_t node and the connected socket fd are leaked.

Release a still-set scd_event_io before creating the new one:
event_remove_io()/event_io_free() the handle and close its socket fd,
mirroring the connection_err path of scd_event_cb_recv_message().

Fixes: 8b63acca54fb ("daemon/scd: start scd as unit with auto reconnect")
Assisted-by: Claude:claude-fable-5
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/scd.c
@@ -167,6 +167,19 @@ scd_on_connect_cb(int sock, const char *sock_path)
 	/* register socket for receiving data */
 	fd_make_non_blocking(sock);
 
+	/*
+	 * release a stale event io of a previous connection whose error
+	 * event has not been handled yet, otherwise it would be leaked by
+	 * the overwrite below
+	 */
+	if (scd_event_io) {
+		int old_fd = event_io_get_fd(scd_event_io);
+		event_remove_io(scd_event_io);
+		event_io_free(scd_event_io);
+		if (close(old_fd) < 0)
+			WARN_ERRNO("Failed to close stale scd event socket");
+	}
+
 	scd_event_io = event_io_new(sock, EVENT_IO_READ, &scd_event_cb_recv_message, NULL);
 	event_add_io(scd_event_io);
 