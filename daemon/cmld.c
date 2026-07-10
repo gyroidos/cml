gyroidos/cml

commit 3a72fa9d8b1712301c93d45c68a7674b2792188b

daemon: Reload container on config update
If a config update is performed, reload the container
after the config has been rewritten to make the changes
effective, if the container is stopped. Otherwise,
mark the container as unsynced with the file system.
Trigger reload of config on container stop if out of sync.

Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

daemon/cmld.c
@@ -56,6 +56,8 @@
 #include "lxcfs.h"
 #include "audit.h"
 #include "time.h"
+#include "container_config.h"
+#include "container.h"
 
 #include <stdio.h>
 #include <dirent.h>
@@ -95,6 +97,7 @@
 	"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
 static const char *cmld_path = DEFAULT_BASE_PATH;
+static const char *cmld_container_path = NULL;
 
 static list_t *cmld_containers_list = NULL; // usually first element is c0
 
@@ -407,6 +410,56 @@ cmld_container_token_init(container_t *container)
 	return 0;
 }
 
+int
+cmld_reload_container(const uuid_t *uuid, const char *path)
+{
+	ASSERT(uuid);
+	ASSERT(path);
+
+	int ret = -1;
+
+	// Will be destroyed on container_free
+	uuid_t *uuid_tmp = uuid_new(uuid_string(uuid));
+
+	container_t *c = cmld_container_get_by_uuid(uuid);
+	if (c) {
+		container_state_t state = container_get_state(c);
+		if (state != CONTAINER_STATE_STOPPED) {
+			DEBUG("Refusing to reload already created and not stopped container %s.",
+			      container_get_name(c));
+			goto cleanup;
+		}
+		DEBUG("Removing outdated created container %s for config update",
+		      container_get_name(c));
+
+		if (smartcard_scd_token_remove_block(c)) {
+			ERROR("Reloading container %s failed, cannot remove token",
+			      uuid_string(uuid_tmp));
+			goto cleanup;
+		}
+
+		cmld_containers_list = list_remove(cmld_containers_list, c);
+		container_free(c);
+	}
+	c = container_new(path, uuid_tmp, NULL, 0, NULL, 0, NULL, 0);
+	if (!c) {
+		WARN("Could not create new container object");
+		goto cleanup;
+	}
+
+	DEBUG("Loaded config for container %s", container_get_name(c));
+	cmld_container_token_init(c);
+	cmld_containers_list = list_append(cmld_containers_list, c);
+
+	container_set_sync_state(c, true);
+	ret = 0;
+
+cleanup:
+	mem_free0(uuid_tmp);
+
+	return ret;
+}
+
 static int
 cmld_load_containers_cb(const char *path, const char *name, UNUSED void *data)
 {
@@ -432,31 +485,17 @@ cmld_load_containers_cb(const char *path, const char *name, UNUSED void *data)
 	}
 
 	uuid = uuid_new(prefix);
-	if (uuid) {
-		container_t *c = cmld_container_get_by_uuid(uuid);
-		if (c) {
-			container_state_t state = container_get_state(c);
-			if (state != CONTAINER_STATE_STOPPED) {
-				DEBUG("Not loading %s for already created and not stopped container %s.",
-				      name, container_get_name(c));
-				goto cleanup;
-			}
-			DEBUG("Removing outdated created container %s for config update",
-			      container_get_name(c));
-			cmld_containers_list = list_remove(cmld_containers_list, c);
-			container_free(c);
-		}
-		c = container_new(path, uuid, NULL, 0, NULL, 0, NULL, 0);
-		if (c) {
-			DEBUG("Loaded config for container %s from %s", container_get_name(c),
-			      name);
-			cmld_container_token_init(c);
-			cmld_containers_list = list_append(cmld_containers_list, c);
-			res = 1;
-			goto cleanup;
-		}
+	if (!uuid) {
+		WARN("Failed to retrieve uuid for container");
+		goto cleanup;
 	}
-	WARN("Could not create new container object from %s", name);
+
+	if (cmld_reload_container((const uuid_t *)uuid, path) != 0) {
+		WARN("Failed to reload container");
+		goto cleanup;
+	}
+
+	res = 1;
 
 cleanup:
 	if (uuid)
@@ -991,6 +1030,7 @@ cmld_init(const char *path)
 {
 	INFO("Storage path is %s", path);
 	cmld_path = path;
+	cmld_container_path = mem_printf("%s/%s", cmld_path, CMLD_PATH_CONTAINERS_DIR);
 
 	if (mount_private_tmp())
 		FATAL("Could not setup private tmp!");
@@ -1501,4 +1541,80 @@ cmld_reboot_device(void)
 
 	// stopping c0 also stops other containers.
 	cmld_container_stop(c0);
+}
+
+static bool
+cmld_container_has_token_changed(container_t *container, container_config_t *conf)
+{
+	ASSERT(container);
+
+	container_token_type_t token_type = container_get_token_type(container);
+
+	if (container_config_get_token_type(conf) != token_type) {
+		TRACE("Container token type changed: %d -> %d", token_type,
+		      container_config_get_token_type(conf));
+		return true;
+	}
+
+	if (token_type != CONTAINER_TOKEN_TYPE_USB) {
+		TRACE("Token did not change: Container is not a USB token container");
+		return false;
+	}
+
+	char *serial_new = container_config_get_usbtoken_serial(conf);
+	if (!serial_new) {
+		ERROR("Failed to retrieve container config USB token serial");
+		return true;
+	}
+
+	char *serial_old = container_get_usbtoken_serial(container);
+	if (!serial_old) {
+		ERROR("Failed to retrieve container USB token serial");
+		return true;
+	}
+
+	if (strcmp(serial_new, serial_old)) {
+		TRACE("Container USB token serial changed");
+		return true;
+	}
+
+	TRACE("Container USB token serial did not change");
+	return false;
+}
+
+int
+cmld_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t *sig_buf,
+		   size_t sig_len, uint8_t *cert_buf, size_t cert_len)
+{
+	ASSERT(container);
+	int ret = -1;
+	container_config_t *conf =
+		container_config_new(container_get_config_filename(container), buf, buf_len,
+				     sig_buf, sig_len, cert_buf, cert_len);
+	if (conf) {
+		ret = container_config_write(conf);
+		container_set_sync_state(container, false);
+
+		// Wipe container if USB token serial changed
+		if (cmld_container_has_token_changed(container, conf)) {
+			if (container_wipe(container)) {
+				ERROR("Failed to wipe user data. Setting container state to ZOMBIE");
+				container_set_state(container, CONTAINER_STATE_ZOMBIE);
+			}
+			if ((container_get_token_type(container) == CONTAINER_TOKEN_TYPE_USB) &&
+			    smartcard_release_pairing(container)) {
+				ERROR("Failed to remove token paired file. Setting container state to ZOMBIE");
+				container_set_state(container, CONTAINER_STATE_ZOMBIE);
+			}
+		}
+
+		container_config_free(conf);
+	}
+	return ret;
+}
+
+const char *
+cmld_get_containers_dir(void)
+{
+	return cmld_container_path;
 }

daemon/cmld.h
@@ -79,6 +79,16 @@ int
 cmld_reload_containers(void);
 
 /**
+ * Reloads the specified container
+ *
+ * @param uuid The uuid of the container
+ * @param path  The path of the container
+ * @return 0 on success, -1 on error
+ */
+int
+cmld_reload_container(const uuid_t *uuid, const char *path);
+
+/**
  * Create a container by cloning from another one.
  *
  * @param container The container object to clone from.
@@ -317,5 +327,17 @@ cmld_token_detach(container_t *container);
  */
 void
 cmld_reboot_device(void);
+/**
+ * This function updates the containers config on storage with the provided
+ * protobuf config in buf. A reload is triggerd if the container is stopped
+ * to update cmld's view. If the container is not in stopped state a manual
+ * reload is necessary.
+ */
+int
+cmld_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t *sig_buf,
+		   size_t sig_len, uint8_t *cert_buf, size_t cert_len);
+
+const char *
+cmld_get_containers_dir(void);
 
 #endif /* CMLD_H */

daemon/container.c
@@ -26,7 +26,7 @@
 
 #include "container.h"
 
-#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
+//#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
 #include "common/macro.h"
 #include "common/mem.h"
 #include "common/uuid.h"
@@ -171,6 +171,9 @@ struct container {
 	container_token_config_t token;
 
 	bool usb_pin_entry;
+
+	// indicate if the container is synced with its config
+	bool is_synced;
 };
 
 struct container_callback {
@@ -402,6 +405,7 @@ container_new_internal(const uuid_t *uuid, const char *name, container_type_t ty
 	}
 
 	container->usb_pin_entry = usb_pin_entry;
+	container->is_synced = true;
 
 	return container;
 
@@ -592,6 +596,13 @@ void
 container_free(container_t *container)
 {
 	ASSERT(container);
+
+	/* unregister usb tokens from uevent subsystem */
+	for (list_t *l = container_get_usbdev_list(container); l; l = l->next) {
+		uevent_usbdev_t *usbdev = l->data;
+		if (UEVENT_USBDEV_TYPE_TOKEN == uevent_usbdev_get_type(usbdev))
+			uevent_unregister_usbdevice(container, usbdev);
+	}
 
 	container_free_key(container);
 
@@ -809,6 +820,20 @@ container_setuid0(const container_t *container)
 {
 	ASSERT(container);
 	return c_user_setuid0(container->user);
+}
+
+bool
+container_get_sync_state(const container_t *container)
+{
+	ASSERT(container);
+	return container->is_synced;
+}
+
+void
+container_set_sync_state(container_t *container, bool state)
+{
+	ASSERT(container);
+	container->is_synced = state;
 }
 
 int
@@ -1049,6 +1074,15 @@ container_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 			if (errno == ECHILD) {
 				DEBUG("Process group of container %s terminated completely",
 				      container_get_description(container));
+
+				if (!container_get_sync_state(container)) {
+					DEBUG("Container is out of sync with its config. Reloading..");
+					if (cmld_reload_container(container_get_uuid(container),
+								  cmld_get_containers_dir()) != 0) {
+						ERROR("Failed to reload container on config update");
+					}
+				}
+
 			} else {
 				audit_log_event(container_get_uuid(container), FSA, CMLD,
 						CONTAINER_MGMT, "container-observer-error",
@@ -2515,70 +2549,6 @@ list_t *
 container_get_vnet_runtime_cfg_new(container_t *container)
 {
 	return c_net_get_interface_mapping_new(container->net);
-}
-
-static bool
-container_has_token_changed(container_t *container, container_config_t *conf)
-{
-	if (container_config_get_token_type(conf) != container->token.type) {
-		TRACE("Container token type changed: %d -> %d", container->token.type,
-		      container_config_get_token_type(conf));
-		return true;
-	}
-
-	if (container->token.type != CONTAINER_TOKEN_TYPE_USB) {
-		TRACE("Token did not change: Container is not a USB token container");
-		return false;
-	}
-
-	char *serial_new = container_config_get_usbtoken_serial(conf);
-	if (!serial_new) {
-		ERROR("Failed to retrieve container config USB token serial");
-		return true;
-	}
-
-	char *serial_old = container_get_usbtoken_serial(container);
-	if (!serial_old) {
-		ERROR("Failed to retrieve container USB token serial");
-		return true;
-	}
-
-	if (strcmp(serial_new, serial_old)) {
-		TRACE("Container USB token serial changed");
-		return true;
-	}
-
-	TRACE("Container USB token serial did not change");
-	return false;
-}
-
-int
-container_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t *sig_buf,
-			size_t sig_len, uint8_t *cert_buf, size_t cert_len)
-{
-	ASSERT(container);
-	int ret = -1;
-	container_config_t *conf = container_config_new(container->config_filename, buf, buf_len,
-							sig_buf, sig_len, cert_buf, cert_len);
-	if (conf) {
-		ret = container_config_write(conf);
-		container_config_free(conf);
-	}
-
-	// Wipe container if USB token serial changed
-	if (container_has_token_changed(container, conf)) {
-		if (container_wipe(container)) {
-			ERROR("Failed to wipe user data. Setting container state to ZOMBIE");
-			container_set_state(container, CONTAINER_STATE_ZOMBIE);
-		}
-		if ((container->token.type == CONTAINER_TOKEN_TYPE_USB) &&
-		    smartcard_release_pairing(container)) {
-			ERROR("Failed to remove token paired file. Setting container state to ZOMBIE");
-			container_set_state(container, CONTAINER_STATE_ZOMBIE);
-		}
-	}
-
-	return ret;
 }
 
 int

daemon/container.h
@@ -663,6 +663,12 @@ container_has_setup_mode(const container_t *container);
 int
 container_setuid0(const container_t *container);
 
+bool
+container_get_sync_state(const container_t *container);
+
+void
+container_set_sync_state(container_t *container, bool state);
+
 /**
  * Initialize a container_vnet_cfg_t data structure and allocate needed memory
  */
@@ -698,16 +704,6 @@ container_pnet_cfg_new(const char *if_name, bool mac_filter, list_t *mac_whiteli
  */
 void
 container_pnet_cfg_free(container_pnet_cfg_t *pnet_cfg);
-
-/**
- * This function updates the containers config on storage with the provided
- * protobuf config in buf. A reload is triggerd if the container is stopped
- * to update cmld's view. If the container is not in stopped state a manual
- * reload is necessary.
- */
-int
-container_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t *sig_buf,
-			size_t sig_len, uint8_t *cert_buf, size_t cert_len);
 
 /**
  * Get the list of usb devices which are set in container config.

daemon/control.c
@@ -1134,16 +1134,15 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 			break;
 		}
 		if (msg->has_container_config_signature && msg->has_container_config_certificate) {
-			res = container_update_config(container, msg->container_config_file.data,
-						      msg->container_config_file.len,
-						      msg->container_config_signature.data,
-						      msg->container_config_signature.len,
-						      msg->container_config_certificate.data,
-						      msg->container_config_certificate.len);
+			res = cmld_update_config(container, msg->container_config_file.data,
+						 msg->container_config_file.len,
+						 msg->container_config_signature.data,
+						 msg->container_config_signature.len,
+						 msg->container_config_certificate.data,
+						 msg->container_config_certificate.len);
 		} else {
-			res = container_update_config(container, msg->container_config_file.data,
-						      msg->container_config_file.len, NULL, 0, NULL,
-						      0);
+			res = cmld_update_config(container, msg->container_config_file.data,
+						 msg->container_config_file.len, NULL, 0, NULL, 0);
 		}
 		if (res) {
 			if (protobuf_send_message(fd, (ProtobufCMessage *)&out) < 0)
@@ -1168,8 +1167,16 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 		}
 		// reload configs if container is in state
 		container_state_t state = container_get_state(container);
-		if (state == CONTAINER_STATE_STOPPED)
-			cmld_reload_containers();
+		if (state == CONTAINER_STATE_STOPPED) {
+			// Reload container to make changes effective
+			TRACE("Update: Reloading container %s from %s",
+			      uuid_string(container_get_uuid(container)),
+			      cmld_get_containers_dir());
+			if (cmld_reload_container(container_get_uuid(container),
+						  cmld_get_containers_dir()) != 0) {
+				ERROR("Failed to reload container on config update");
+			}
+		}
 
 		// build and send response message to controller
 		out.n_container_configs = 1;

daemon/smartcard.c
@@ -1091,10 +1091,13 @@ smartcard_free(smartcard_t *smartcard)
 int
 smartcard_release_pairing(container_t *container)
 {
+	int ret = 0;
 	char *path = smartcard_token_paired_file_new(container);
-	int ret = unlink(path);
-	if (ret != 0) {
-		ERROR_ERRNO("Failed to remove file %s", path);
+	if (file_exists(path)) {
+		ret = unlink(path);
+		if (ret != 0) {
+			ERROR_ERRNO("Failed to remove file %s", path);
+		}
 	}
 	mem_free0(path);
 	return ret;

commit 47dfaf806317b3b7362e50aacac815c55104b367

daemon/cmld: Use uuid_free for uuid_tmp in reload_container
cmld_reload_container_internal() builds a local uuid_t via
uuid_new(uuid_string(uuid)) at the top, passes it by const pointer to
cmld_container_new() (which deep-copies into its own uuid), and
released the local on the cleanup path with mem_free0(uuid_tmp). That
only freed the uuid_t struct itself, not the mem_strdup'd uuid->string
inside, leaking ~37 B per reload via uuid_new() at uuid.c:135.

Switch to uuid_free(), which mem_free0()s both string and struct.

Fixes: 3a72fa9d8b17 ("daemon: Reload container on config update")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -671,7 +671,7 @@ cmld_reload_container_internal(const uuid_t *uuid, const char *path, container_c
 	container_set_sync_state(c, true);
 
 cleanup:
-	mem_free0(uuid_tmp);
+	uuid_free(uuid_tmp);
 
 	return c;
 }