gyroidos/cml

commit 7a3594226d8273ce5cc7a8cc5773fe3636328597

daemon/audit: Generate audit messages
This commit introduces audit message generation
of key steps in container creation, start, stop,
removal and token handling.

Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -28,6 +28,7 @@
 #include "cmld.h"
 #include "download.h"
 #include "smartcard.h"
+#include "audit.h"
 
 #include "common/macro.h"
 #include "common/list.h"
@@ -91,8 +92,9 @@ guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED 
 	guestos_verify_result_t guestos_verified = GUESTOS_UNSIGNED;
 
 	char *dir = mem_printf("%s/%s", path, name);
-	if (!file_is_dir(dir))
+	if (!file_is_dir(dir)) {
 		goto cleanup;
+	}
 
 	char *cfg_file = guestos_get_cfg_file_new(dir);
 	char *sig_file = guestos_get_sig_file_new(dir);
@@ -104,17 +106,24 @@ guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED 
 	switch (verify_result) {
 	case VERIFY_GOOD:
 		guestos_verified = GUESTOS_SIGNED;
+		audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "verify-good", name, 0);
 		INFO("Signature of GuestOS OK (GOOD)");
 		break;
 	case VERIFY_LOCALLY_SIGNED:
 		guestos_verified = GUESTOS_LOCALLY_SIGNED;
 		if (guestos_mgr_allow_locally_signed) {
+			audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "verify-locally-signed",
+					name, 0);
 			INFO("Signature of GuestOS OK (locally signed)");
 			break;
 		}
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-locally-signed", name, 0);
 		// fallthrough
 	default:
 		guestos_verified = GUESTOS_UNSIGNED;
+
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-failed", name, 0);
+
 		ERROR("Signature verification failed (%d) while loading GuestOS config %s, skipping.",
 		      verify_result, cfg_file);
 		res = 1;
@@ -122,8 +131,11 @@ guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED 
 	}
 
 	if (guestos_mgr_add_from_file(cfg_file, guestos_verified) < 0) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "load-os-failed-to-add", cfg_file,
+				0);
 		WARN("Could not add guest operating system from file %s.", cfg_file);
 	} else {
+		audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "load-os", cfg_file, 0);
 		res = 1;
 	}
 
@@ -231,16 +243,25 @@ download_complete_cb(bool complete, unsigned int count, guestos_t *os, void *dat
 
 	control_message_t resp = CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_FAILED;
 
-	IF_NULL_GOTO_ERROR(os, out);
+	if (!os) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "download-os-failed", NULL, 0);
+		goto out;
+	}
 
 	if (complete && count > 0) {
 		if (guestos_images_flash(os) < 0) {
+			audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "download-os-flash-failed",
+					guestos_get_name(os), 0);
 			WARN("%s %s", GUESTOS_MGR_UPDATE_TITLE, GUESTOS_MGR_UPDATE_FLASH_FAILED);
 		} else {
+			audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "download-os-flash-success",
+					guestos_get_name(os), 0);
 			INFO("%s %s", GUESTOS_MGR_UPDATE_TITLE, GUESTOS_MGR_UPDATE_SUCCESS);
 			resp = CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_COMPLETED;
 		}
 	} else {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "download-os-failed",
+				guestos_get_name(os), 0);
 		WARN("%s %s", GUESTOS_MGR_UPDATE_TITLE, GUESTOS_MGR_UPDATE_FAILED);
 	}
 out:
@@ -264,14 +285,21 @@ guestos_mgr_download_latest(const char *name, int resp_fd)
 	if (!guestos_images_are_complete(os, false)) {
 		int *cb_resp_fd = mem_new(int, 1);
 		*cb_resp_fd = resp_fd;
-		if (!guestos_images_download(os, download_complete_cb, cb_resp_fd))
+		if (!guestos_images_download(os, download_complete_cb, cb_resp_fd)) {
 			resp = CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_WAITING;
-		else
+			audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "download-os-start", name,
+					0);
+		} else {
+			audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "download-os-start", name,
+					0);
 			return;
+		}
 	}
 	if (guestos_images_flash(os) < 0) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "flash-os", name, 0);
 		WARN("%s %s", GUESTOS_MGR_UPDATE_TITLE, GUESTOS_MGR_UPDATE_FLASH_FAILED);
 	} else {
+		audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "flash-os", name, 0);
 		INFO("%s %s", GUESTOS_MGR_UPDATE_TITLE, GUESTOS_MGR_UPDATE_SUCCESS);
 		resp = CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_COMPLETED;
 	}
@@ -326,29 +354,37 @@ push_config_verify_buf_cb(smartcard_crypto_verify_result_t verify_result, unsign
 	int *resp_fd = data;
 	ASSERT(resp_fd);
 
+	guestos_t *os = guestos_new_from_buffer(cfg_buf, cfg_buf_len, guestos_basepath);
+	const char *os_name = NULL;
+	if (!os) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-invalid", guestos_basepath,
+				0);
+		ERROR("Could not instantiate GuestOS from buffer");
+		goto err;
+	}
+	os_name = guestos_get_name(os);
+
 	switch (verify_result) {
 	case VERIFY_GOOD:
+		audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "verify-good", os_name, 0);
 		INFO("Signature of GuestOS OK (GOOD)");
 		break;
 	case VERIFY_LOCALLY_SIGNED:
 		if (guestos_mgr_allow_locally_signed) {
+			audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "verify-locally-signed",
+					os_name, 0);
 			INFO("Signature of GuestOS OK (locally signed)");
 			break;
 		}
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-locally-signed", os_name, 0);
 		// fallthrough
 	default:
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-failed", os_name, 0);
 		ERROR("Signature verification failed (%d) for pushed GuestOS config buffer, skipping.",
 		      verify_result);
 		goto err;
 	}
 
-	guestos_t *os = guestos_new_from_buffer(cfg_buf, cfg_buf_len, guestos_basepath);
-	if (!os) {
-		ERROR("Could not instantiate GuestOS from buffer");
-		goto err;
-	}
-
-	const char *os_name = guestos_get_name(os);
 	uint64_t os_ver = guestos_get_version(os);
 	bool cml_update = !strcmp(os_name, GUESTOS_MGR_CML_UPDATE_FAKE_OS_NAME);
 	const guestos_t *old_os = guestos_mgr_get_latest_by_name(os_name, false);
@@ -356,6 +392,16 @@ push_config_verify_buf_cb(smartcard_crypto_verify_result_t verify_result, unsign
 		// existing os of same name => verify and update
 		uint64_t old_ver = guestos_get_version(old_os);
 		if (os_ver <= old_ver) {
+			char *installed_str = mem_printf(PRIu64, old_ver);
+			char *update_str = mem_printf(PRIu64, os_ver);
+
+			audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-old-version",
+					os_name, 4, "installed_version", installed_str,
+					"update_version", update_str);
+
+			mem_free(installed_str);
+			mem_free(update_str);
+
 			WARN("Skipping update of GuestOS %s version %" PRIu64
 			     " to older/same version %" PRIu64 ".",
 			     os_name, old_ver, os_ver);
@@ -371,22 +417,30 @@ push_config_verify_buf_cb(smartcard_crypto_verify_result_t verify_result, unsign
 	// 1. create new guestos folder
 	const char *dir = guestos_get_dir(os);
 	if (mkdir(dir, 00755) < 0 && errno != EEXIST) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-prepare-dir",
+				guestos_basepath, 0);
 		ERROR_ERRNO("Could not mkdir GuestOS directory %s", dir);
 		goto cleanup_purge;
 	}
 
 	// 2. save pushed config, signature and cert
 	if (file_write(guestos_get_cfg_file(os), (char *)cfg_buf, cfg_buf_len) < 0) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-store-config",
+				guestos_basepath, 0);
 		ERROR_ERRNO("Failed to write GuestOS config from buffer to %s",
 			    guestos_get_cfg_file(os));
 		goto cleanup_purge;
 	}
 	if (file_write(guestos_get_sig_file(os), (char *)sig_buf, sig_buf_len) < 0) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-store-signature",
+				guestos_basepath, 0);
 		ERROR_ERRNO("Failed to write GuestOS config signature from buffer to %s",
 			    guestos_get_sig_file(os));
 		goto cleanup_purge;
 	}
 	if (file_write(guestos_get_cert_file(os), (char *)cert_buf, cert_buf_len) < 0) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-store-certificate",
+				guestos_basepath, 0);
 		ERROR_ERRNO("Failed to write GuestOS config certificate from buffer to %s",
 			    guestos_get_cert_file(os));
 		goto cleanup_purge;
@@ -394,6 +448,8 @@ push_config_verify_buf_cb(smartcard_crypto_verify_result_t verify_result, unsign
 
 	// 3. register new os instance
 	guestos_list = list_append(guestos_list, os);
+
+	audit_log_event(NULL, SSA, CMLD, GUESTOS_MGMT, "push-os", guestos_basepath, 0);
 
 	//INFO("%s: %s", GUESTOS_MGR_UPDATE_TITLE,
 	//     cmld_is_wifi_active() ? GUESTOS_MGR_UPDATE_DOWNLOAD :
@@ -416,6 +472,8 @@ cleanup_purge:
 cleanup_os:
 	guestos_free(os);
 err:
+	audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "update-start", os_name, 0);
+
 	if (control_send_message(CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_FAILED, *resp_fd) < 0)
 		WARN("Could not send response to fd=%d", *resp_fd);
 	mem_free(resp_fd);
@@ -431,7 +489,16 @@ guestos_mgr_push_config(unsigned char *cfg, size_t cfglen, unsigned char *sig, s
 	int *cb_resp_fd = mem_new0(int, 1);
 	*cb_resp_fd = resp_fd;
 
-	if (cfg && sig && cert) {
+	if (!cfg) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-missing-config",
+				guestos_basepath, 0);
+	} else if (!sig) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-missing-signature",
+				guestos_basepath, 0);
+	} else if (!cert) {
+		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-missing-certificate",
+				guestos_basepath, 0);
+	} else {
 		res = smartcard_crypto_verify_buf(cfg, cfglen, sig, siglen, cert, certlen,
 						  GUESTOS_MGR_VERIFY_HASH_ALGO,
 						  push_config_verify_buf_cb, cb_resp_fd);
@@ -520,6 +587,8 @@ guestos_mgr_get_latest_by_name(const char *name, bool complete)
 			uint64_t version = guestos_get_version(os);
 			// TODO cache image complete result in guestos instance and get rid of check here?
 			if (complete && !guestos_images_are_complete(os, false)) {
+				audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "broken-update",
+						guestos_get_name(os), 0);
 				DEBUG("GuestOS %s v%" PRIu64
 				      " is incomplete (missing images) or broken, skipping.",
 				      guestos_get_name(os), version);

daemon/c_fifo.c
@@ -25,6 +25,7 @@
 #include "c_fifo.h"
 #include "cmld.h"
 #include "container.h"
+#include "audit.h"
 
 #include "common/macro.h"
 #include "common/mem.h"
@@ -100,17 +101,32 @@ c_fifo_create_fifos(c_fifo_t *fifo, container_t *container)
 		if (0 != mkfifo(fifo_path, 666)) {
 			ERROR_ERRNO("Failed to create fifo at %s", fifo_path);
 			mem_free(fifo_path);
+			audit_log_event(container_get_uuid(fifo->container), FSA, CMLD,
+					CONTAINER_ISOLATION, "create-fifo",
+					uuid_string(container_get_uuid(fifo->container)), 2, "name",
+					current_fifo, 0);
 			goto error;
 		}
 
+		audit_log_event(container_get_uuid(fifo->container), SSA, CMLD, CONTAINER_ISOLATION,
+				"create-fifo", uuid_string(container_get_uuid(fifo->container)), 2,
+				"name", current_fifo);
 		TRACE("Created FIFO at %s", fifo_path);
 
 		if (chown(fifo_path, uid, uid)) {
+			audit_log_event(container_get_uuid(fifo->container), FSA, CMLD,
+					CONTAINER_ISOLATION, "prepare-fifo-directory",
+					uuid_string(container_get_uuid(fifo->container)), 2, "path",
+					fifo_path);
 			ERROR("Failed to chown fifo dir to %d", uid);
 			mem_free(fifo_path);
 			goto error;
 		}
 
+		audit_log_event(container_get_uuid(fifo->container), SSA, CMLD, CONTAINER_ISOLATION,
+				"prepare-fifo-directory",
+				uuid_string(container_get_uuid(fifo->container)), 2, "path",
+				fifo_path);
 		DEBUG("Chowned FIFO at %s to %d", fifo_path ? fifo_path : "NULL", uid);
 
 		mem_free(fifo_path);

daemon/c_vol.c
@@ -46,6 +46,7 @@
 #include "guestos.h"
 #include "smartcard.h"
 #include "lxcfs.h"
+#include "audit.h"
 
 #include <unistd.h>
 #include <string.h>
@@ -721,13 +722,18 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t *mntent)
 	if (encrypted) {
 		char *label, *crypt;
 
-		if (!container_get_key(vol->container)) {
-			ERROR("Trying to mount encrypted volume without key...");
-			goto error;
-		}
-
 		label = mem_printf("%s-%s", uuid_string(container_get_uuid(vol->container)),
 				   mount_entry_get_img(mntent));
+
+		if (!container_get_key(vol->container)) {
+			audit_log_event(container_get_uuid(vol->container), FSA, CMLD,
+					CONTAINER_MGMT, "setup-crypted-volume-no-key",
+					uuid_string(container_get_uuid(vol->container)), 2, "label",
+					label);
+			ERROR("Trying to mount encrypted volume without key...");
+			mem_free(label);
+			goto error;
+		}
 
 		crypt = cryptfs_get_device_path_new(label);
 		if (file_is_blk(crypt)) {
@@ -740,10 +746,18 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t *mntent)
 							 container_get_key(vol->container));
 
 			if (!crypt) {
+				audit_log_event(container_get_uuid(vol->container), FSA, CMLD,
+						CONTAINER_MGMT, "setup-crypted-volume",
+						uuid_string(container_get_uuid(vol->container)), 2,
+						"label", label);
 				ERROR("Setting up cryptfs volume %s for %s failed", label, dev);
 				mem_free(label);
 				goto error;
 			}
+			audit_log_event(container_get_uuid(vol->container), SSA, CMLD,
+					CONTAINER_MGMT, "setup-crypted-volume",
+					uuid_string(container_get_uuid(vol->container)), 2, "label",
+					label);
 		}
 
 		mem_free(label);

daemon/cmld.c
@@ -54,6 +54,7 @@
 #include "uevent.h"
 #include "time.h"
 #include "lxcfs.h"
+#include "audit.h"
 
 #include <stdio.h>
 #include <dirent.h>
@@ -150,7 +151,7 @@ cmld_container_get_c_root_netns()
 }
 
 container_t *
-cmld_container_get_by_uuid(uuid_t *uuid)
+cmld_container_get_by_uuid(const uuid_t *uuid)
 {
 	ASSERT(uuid);
 
@@ -232,6 +233,9 @@ cmld_container_stop_cb(container_t *container, container_callback_t *cb, void *d
 
 	/* unregister observer */
 	container_unregister_observer(container, cb);
+
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
+			"container-stopped", uuid_string(container_get_uuid(container)), 0);
 
 	/* execute on_all_stopped, if all containers are stopped now */
 	if (cmld_containers_are_all_stopped()) {
@@ -811,6 +815,9 @@ int
 cmld_container_start(container_t *container)
 {
 	if (!container) {
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-start-not-existing",
+				uuid_string(container_get_uuid(container)), 0);
 		WARN("Container does not exists!");
 		return -1;
 	}
@@ -825,20 +832,32 @@ cmld_container_start(container_t *container)
 
 		// We only support "background-start"...
 		if (!guestos_get_feature_bg_booting(container_get_guestos(container))) {
+			audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+					"container-start",
+					uuid_string(container_get_uuid(container)), 0);
 			WARN("Guest OS of the container %s does not support background booting",
 			     container_get_description(container));
 			return -1;
 		}
 		if (container_start(container)) {
+			audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+					"container-start",
+					uuid_string(container_get_uuid(container)), 0);
 			WARN("Start of background container %s failed",
 			     container_get_description(container));
 			return -1;
 		}
 		time_register_clock_check();
 	} else {
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-start-already-running",
+				uuid_string(container_get_uuid(container)), 0);
 		DEBUG("Container %s has been already started",
 		      container_get_description(container));
 	}
+
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT, "container-start",
+			uuid_string(container_get_uuid(container)), 0);
 	return 0;
 }
 
@@ -851,8 +870,19 @@ cmld_container_change_pin(control_t *control, container_t *container, const char
 	ASSERT(passwd);
 	ASSERT(newpasswd);
 
-	return smartcard_container_change_pin(cmld_smartcard, control, container, passwd,
-					      newpasswd);
+	int rc = smartcard_container_change_pin(cmld_smartcard, control, container, passwd,
+						newpasswd);
+
+	if (!rc)
+		audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
+				"container-change-pin", uuid_string(container_get_uuid(container)),
+				0);
+	else
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-change-pin", uuid_string(container_get_uuid(container)),
+				0);
+
+	return rc;
 }
 
 int
@@ -956,6 +986,9 @@ cmld_shutdown_container_cb(container_t *container, container_callback_t *cb, UNU
 	/* all containers are down, so shut down */
 	DEBUG("Device shutdown: last container down; shutdown now");
 
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT, "shutdown",
+			uuid_string(container_get_uuid(container)), 0);
+
 #ifndef TRUSTME_DEBUG
 	reboot_reboot(POWER_OFF);
 	// should never arrive here, but in case the shutdown fails somehow, we exit
@@ -978,6 +1011,8 @@ cmld_shutdown_a0_cb(container_t *a0, container_callback_t *cb, UNUSED void *data
 	      a0_state == CONTAINER_STATE_ZOMBIE)) {
 		return;
 	}
+	audit_log_event(container_get_uuid(a0), SSA, CMLD, CONTAINER_MGMT, "shutdown-c0-start",
+			uuid_string(container_get_uuid(a0)), 0);
 
 	DEBUG("Device shutdown: a0 went down or shutting down, checking others before shutdown");
 
@@ -1010,6 +1045,8 @@ cmld_shutdown_a0_cb(container_t *a0, container_callback_t *cb, UNUSED void *data
 	if (shutdown_now && !cmld_hostedmode) {
 		/* all containers are down, so shut down */
 		DEBUG("Device shutdown: all containers already down; shutdown now");
+		audit_log_event(container_get_uuid(a0), SSA, CMLD, CONTAINER_MGMT, "shutdown",
+				uuid_string(container_get_uuid(a0)), 0);
 #ifndef TRUSTME_DEBUG
 		reboot_reboot(POWER_OFF);
 		// should never arrive here, but in case the shutdown fails somehow, we exit
@@ -1098,20 +1135,30 @@ cmld_start_a0(container_t *new_a0)
 	}
 
 	container_set_key(new_a0, A0_KEY);
-	if (container_start(new_a0))
+	if (container_start(new_a0)) {
+		audit_log_event(container_get_uuid(new_a0), FSA, CMLD, CONTAINER_MGMT, "c0-start",
+				uuid_string(container_get_uuid(new_a0)), 0);
 		FATAL("Could not start management container");
+	}
 
 	/* register an observer to capture the shutdown command for the special container a0 */
 	if (!container_register_observer(new_a0, &cmld_shutdown_a0_cb, NULL)) {
 		WARN("Could not register observer shutdown callback for a0");
+
+		audit_log_event(container_get_uuid(new_a0), FSA, CMLD, CONTAINER_MGMT, "c0-start",
+				uuid_string(container_get_uuid(new_a0)), 0);
 		return -1;
 	}
 	/* register an observer to capture the reboot command for the special container a0 */
 	if (!container_register_observer(new_a0, &cmld_reboot_a0_cb, NULL)) {
 		WARN("Could not register observer reboot callback for a0");
+		audit_log_event(container_get_uuid(new_a0), FSA, CMLD, CONTAINER_MGMT, "c0-start",
+				uuid_string(container_get_uuid(new_a0)), 0);
 		return -1;
 	}
 
+	audit_log_event(container_get_uuid(new_a0), SSA, CMLD, CONTAINER_MGMT, "c0-start",
+			uuid_string(container_get_uuid(new_a0)), 0);
 	return 0;
 }
 
@@ -1168,8 +1215,9 @@ cmld_init(const char *path)
 
 	const char *device_path = DEFAULT_CONF_BASE_PATH "/" CMLD_PATH_DEVICE_CONF;
 	device_config_t *device_config = device_config_new(device_path);
-	if (!device_config)
-		WARN("Could not initialize device config");
+
+	// set max audit log file size
+	audit_set_size(device_config_get_audit_size(device_config));
 
 	// set hostedmode, which disables some configuration
 	cmld_hostedmode = device_config_get_hostedmode(device_config);
@@ -1331,16 +1379,23 @@ cmld_container_create_from_config(const uint8_t *config, size_t config_len, uint
 		container_new(path, NULL, config, config_len, sig, sig_len, cert, cert_len);
 	if (c) {
 		if (0 != cmld_container_token_init(c)) {
+			audit_log_event(container_get_uuid(c), FSA, CMLD, CONTAINER_MGMT,
+					"container-create-token-uninit",
+					uuid_string(container_get_uuid(c)), 0);
 			ERROR("Could not initialize token associated with container %s (uuid=%s). Aborting creation",
 			      container_get_name(c), uuid_string(container_get_uuid(c)));
 			cmld_container_destroy(c);
 			c = NULL;
 		} else {
 			cmld_containers_list = list_append(cmld_containers_list, c);
+			audit_log_event(container_get_uuid(c), SSA, CMLD, CONTAINER_MGMT,
+					"container-create", uuid_string(container_get_uuid(c)), 0);
 			INFO("Created container %s (uuid=%s).", container_get_name(c),
 			     uuid_string(container_get_uuid(c)));
 		}
 	} else {
+		audit_log_event(container_get_uuid(c), FSA, CMLD, CONTAINER_MGMT,
+				"container-create", uuid_string(container_get_uuid(c)), 0);
 		WARN("Could not create new container object from config");
 	}
 	mem_free(path);
@@ -1366,10 +1421,19 @@ cmld_container_destroy_cb(container_t *container, container_callback_t *cb, UNUS
 	/* remove keyfile */
 	if (0 != smartcard_remove_keyfile(cmld_smartcard, container)) {
 		ERROR("Failed to remove keyfile. Continuing to remove container anyway.");
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-remove-keyfile",
+				uuid_string(container_get_uuid(container)), 0);
+	} else {
+		audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
+				"container-remove-keyfile",
+				uuid_string(container_get_uuid(container)), 0);
 	}
 
 	/* destroy the container */
 	if (container_destroy(container) < 0) {
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-remove", uuid_string(container_get_uuid(container)), 0);
 		ERROR("Could not destroy container");
 		container_set_state(container, CONTAINER_STATE_ZOMBIE);
 		return;
@@ -1377,6 +1441,8 @@ cmld_container_destroy_cb(container_t *container, container_callback_t *cb, UNUS
 
 	/* cleanup container */
 	cmld_containers_list = list_remove(cmld_containers_list, container);
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
+			"container-remove", uuid_string(container_get_uuid(container)), 0);
 	container_free(container);
 }
 
@@ -1394,6 +1460,9 @@ cmld_container_destroy(container_t *container)
 
 		/* Register observer to wait for completed container_stop */
 		if (!container_register_observer(container, &cmld_container_destroy_cb, NULL)) {
+			audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+					"container-remove",
+					uuid_string(container_get_uuid(container)), 0);
 			DEBUG("Could not register destroy callback");
 			return -1;
 		}
@@ -1416,6 +1485,10 @@ cmld_container_stop(container_t *container)
 	      (container_get_state(container) == CONTAINER_STATE_SETUP))) {
 		ERROR("Container %s not running, unable to stop",
 		      container_get_description(container));
+
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"container-stop-failed", uuid_string(container_get_uuid(container)),
+				0);
 		return -1;
 	}
 

daemon/cmld.h
@@ -159,7 +159,7 @@ void
 cmld_wipe_device();
 
 container_t *
-cmld_container_get_by_uuid(uuid_t *uuid);
+cmld_container_get_by_uuid(const uuid_t *uuid);
 
 container_t *
 cmld_container_get_by_token_serial(const char *serial);

daemon/container.c
@@ -1074,13 +1074,29 @@ container_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 			event_signal_free(sig);
 			/* cleanup and set states accordingly to notify observers */
 			container_cleanup(container, rebooting);
+
+			if (rebooting) {
+				audit_log_event(container_get_uuid(container), SSA, CMLD,
+						CONTAINER_MGMT, "reboot",
+						uuid_string(container_get_uuid(container)), 0);
+				container_set_state(container, CONTAINER_STATE_REBOOTING);
+			} else {
+				audit_log_event(container_get_uuid(container), SSA, CMLD,
+						CONTAINER_MGMT, "stop",
+						uuid_string(container_get_uuid(container)), 0);
+				container_set_state(container, CONTAINER_STATE_STOPPED);
+			}
 		} else if (pid == -1) {
-			if (errno == ECHILD)
+			if (errno == ECHILD) {
 				DEBUG("Process group of container %s terminated completely",
 				      container_get_description(container));
-			else
+			} else {
+				audit_log_event(container_get_uuid(container), FSA, CMLD,
+						CONTAINER_MGMT, "container-observer-error",
+						uuid_string(container_get_uuid(container)), 0);
 				WARN_ERRNO("waitpid failed for container %s",
 					   container_get_description(container));
+			}
 			break;
 		} else {
 			DEBUG("Reaped a child with PID %d for container %s", pid,
@@ -1658,6 +1674,8 @@ error_post_clone:
 
 error_pre_clone:
 	container_cleanup(container, false);
+	audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+			"error-preparing-container", uuid_string(container_get_uuid(container)), 0);
 	return ret;
 }
 
@@ -1718,6 +1736,8 @@ container_stop(container_t *container)
 		container_set_setup_mode(container, false);
 
 	/* set state to shutting down (notifies observers) */
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT, "shutting-down",
+			uuid_string(container_get_uuid(container)), 0);
 	container_set_state(container, CONTAINER_STATE_SHUTTING_DOWN);
 
 	/* call stop hooks for c_* modules */
@@ -1725,8 +1745,14 @@ container_stop(container_t *container)
 
 	if (c_service_stop(container->service) < 0) {
 		ret = CONTAINER_ERROR;
+
+		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+				"request-clean-shutdown",
+				uuid_string(container_get_uuid(container)), 0);
 		goto error_stop;
 	}
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT,
+			"request-clean-shutdown", uuid_string(container_get_uuid(container)), 0);
 
 	// When the stop command was emitted, the TrustmeService tries to shut down the container
 	// i.g. to terminate the container's init process.
@@ -1739,6 +1765,8 @@ container_stop(container_t *container)
 error_stop:
 	DEBUG("Modules could not be stopped successfully, killing container.");
 	container_kill(container);
+	audit_log_event(container_get_uuid(container), SSA, CMLD, CONTAINER_MGMT, "force-stop",
+			uuid_string(container_get_uuid(container)), 0);
 	return ret;
 }
 

daemon/control.c
@@ -38,6 +38,7 @@
 #include "smartcard.h"
 #include "input.h"
 #include "uevent.h"
+#include "audit.h"
 
 //#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
 #include "common/macro.h"
@@ -1195,6 +1196,9 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 	} break;
 	case CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_START: {
 		if (NULL == container) {
+			audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
+					"container-start-not-existing",
+					uuid_string(container_get_uuid(container)), 0);
 			WARN("Container does not exist!");
 			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_EEXIST, fd);
 			break;

daemon/device_config.c
@@ -55,7 +55,7 @@ device_config_new(const char *path)
 		DEBUG("Loading device config from \"%s\".", file);
 
 		cfg = (DeviceConfig *)protobuf_message_new_from_textfile(
-			file, &device_config__descriptor, false);
+			file, &device_config__descriptor);
 		if (!cfg) {
 			WARN("Failed loading device config from file \"%s\". Reverting to default values.",
 			     file);

commit 9e18ea7448a5871283d9b1798310d758f53d71e0

daemon/guestos_mgr: Fix use-after-free of os_name in push_config_verify_buf_cb
push_config_verify_buf_cb() borrowed os_name via guestos_get_name(os),
which returns a pointer into the protobuf-unpacked config owned by
os. Any error path falling through cleanup_os calls guestos_free()
which releases those strings, so the audit_log_event() at err reads
a dangling pointer. ASan flagged this as a heap-use-after-free.

Duplicate os_name into a heap-owned local right after
guestos_new_from_buffer() so it survives guestos_free(). When os is
NULL, strdup the placeholder "NA" so the audit event still carries a
valid subject. Release the copy on both exit paths.

Fixes: 7a3594226d82 ("daemon/audit: Generate audit messages")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -481,14 +481,20 @@ push_config_verify_buf_cb(crypto_verify_result_t verify_result, unsigned char *c
 	ASSERT(resp_fd);
 
 	guestos_t *os = guestos_new_from_buffer(cfg_buf, cfg_buf_len, guestos_basepath);
-	const char *os_name = NULL;
+
+	/*
+	 * guestos_get_name() borrows a pointer into the protobuf-unpacked
+	 * config owned by os. Duplicate it so it survives guestos_free() and
+	 * remains valid for the audit_log_event() at the err label.
+	 */
+	char *os_name = mem_strdup(os ? guestos_get_name(os) : "NA");
+
 	if (!os) {
 		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "push-os-invalid", guestos_basepath,
 				0);
 		ERROR("Could not instantiate GuestOS from buffer");
 		goto err;
 	}
-	os_name = guestos_get_name(os);
 
 	switch (verify_result) {
 	case VERIFY_GOOD:
@@ -603,6 +609,7 @@ trigger_download:
 	guestos_mgr_download_latest(os_name, *resp_fd);
 
 	mem_free0(resp_fd);
+	mem_free0(os_name);
 	return;
 
 cleanup_purge:
@@ -617,6 +624,7 @@ err:
 	if (control_send_message(CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_FAILED, *resp_fd) < 0)
 		WARN("Could not send response to fd=%d", *resp_fd);
 	mem_free0(resp_fd);
+	mem_free0(os_name);
 }
 
 int