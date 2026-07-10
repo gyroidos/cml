gyroidos/cml

commit 4977717e536568bba15503a13fd1547fcfae028e

daemon: Moved smartcard module to c_smartcard module
Initial move of smartcard module to c_smartcard. Mostly use function
wrappers for now. Following commits will move corresponding
functionality which is now implemented in cmld.c and container.c to
c_smartcard.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -48,7 +48,7 @@
 #include "control.h"
 #include "guestos_mgr.h"
 #include "guestos.h"
-#include "smartcard.h"
+#include "scd.h"
 #include "tss.h"
 #include "ksm.h"
 #include "uevent.h"
@@ -98,13 +98,12 @@
 
 static const char *cmld_path = DEFAULT_BASE_PATH;
 static const char *cmld_container_path = NULL;
+static const char *cmld_wrapped_keys_path = NULL;
 
 static list_t *cmld_containers_list = NULL; // usually first element is c0
 
 static control_t *cmld_control_gui = NULL;
 static control_t *cmld_control_cml = NULL;
-
-static smartcard_t *cmld_smartcard = NULL;
 
 static char *cmld_device_uuid = NULL;
 static char *cmld_device_update_base_url = NULL;
@@ -396,16 +395,16 @@ cmld_container_token_init(container_t *container)
 		return 0;
 	}
 
-	DEBUG("Invoking smartcard_scd_token_add_block() for container %s",
+	DEBUG("Invoking container_scd_token_add_block() for container %s",
 	      container_get_name(container));
-	if (smartcard_scd_token_add_block(container) != 0) {
+	if (container_scd_token_add_block(container) != 0) {
 		ERROR("Requesting SCD to init token failed");
 		return -1;
 	}
 
 	DEBUG("Initialized token for container %s", container_get_name(container));
 
-	smartcard_update_token_state(container);
+	container_update_token_state(container);
 
 	return 0;
 }
@@ -433,7 +432,7 @@ cmld_reload_container(const uuid_t *uuid, const char *path)
 		      container_get_name(c));
 
 		if (token_type == CONTAINER_TOKEN_TYPE_USB) {
-			if (smartcard_scd_token_remove_block(c)) {
+			if (container_scd_token_remove_block(c)) {
 				ERROR("Reloading container %s failed, cannot remove token",
 				      uuid_string(uuid_tmp));
 				goto cleanup;
@@ -726,8 +725,7 @@ cmld_container_change_pin(container_t *container, int resp_fd, const char *passw
 	ASSERT(passwd);
 	ASSERT(newpasswd);
 
-	return smartcard_container_change_pin(cmld_smartcard, container, resp_fd, passwd,
-					      newpasswd);
+	return container_change_pin(container, resp_fd, passwd, newpasswd);
 }
 
 int
@@ -737,8 +735,14 @@ cmld_container_ctrl_with_smartcard(container_t *container, int resp_fd, const ch
 	ASSERT(container);
 	ASSERT(passwd);
 
-	return smartcard_container_ctrl_handler(cmld_smartcard, container, resp_fd, passwd,
-						container_ctrl);
+	if (container_ctrl == CMLD_CONTAINER_CTRL_START)
+		return container_start_with_smartcard(container, resp_fd, passwd);
+	else if (container_ctrl == CMLD_CONTAINER_CTRL_STOP)
+		return container_stop_with_smartcard(container, resp_fd, passwd);
+
+	ERROR("Unknown container control command %u", container_ctrl);
+	control_send_message(CONTROL_RESPONSE_CONTAINER_CTRL_EINTERNAL, resp_fd);
+	return -1;
 }
 
 /******************************************************************************/
@@ -1101,6 +1105,12 @@ cmld_init(const char *path)
 	audit_log_event(NULL, SSA, CMLD, GENERIC, "boot-time", NULL, 2, "time", btime);
 	mem_free0(btime);
 
+	if (scd_init() < 0)
+		FATAL("Could not init scd module");
+	INFO("scd initialized.");
+	if (atexit(&scd_cleanup))
+		WARN("Could not register on exit cleanup method 'scd_cleanup()'");
+
 	if (uevent_init() < 0)
 		FATAL("Could not init uevent module");
 	INFO("uevent initialized.");
@@ -1151,10 +1161,6 @@ cmld_init(const char *path)
 	}
 	INFO("created control socket.");
 
-	char *tokens_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
-	cmld_smartcard = smartcard_new(tokens_path);
-	mem_free0(tokens_path);
-
 	char *guestos_path = mem_printf("%s/%s", path, CMLD_PATH_GUESTOS_DIR);
 	bool allow_locally_signed = device_config_get_locally_signed_images(device_config);
 	if (guestos_mgr_init(guestos_path, allow_locally_signed) < 0 && !cmld_hostedmode)
@@ -1167,15 +1173,9 @@ cmld_init(const char *path)
 	if (mkdir(containers_path, 0700) < 0 && errno != EEXIST)
 		FATAL_ERRNO("Could not mkdir containers directory %s", containers_path);
 
-	char *keys_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
+	cmld_wrapped_keys_path = mem_printf("%s/%s", path, CMLD_PATH_CONTAINER_KEYS_DIR);
 	if (mkdir(containers_path, 0700) < 0 && errno != EEXIST)
 		FATAL_ERRNO("Could not mkdir container keys directory %s", containers_path);
-	mem_free0(keys_path);
-
-	if (cmld_smartcard == NULL)
-		FATAL("Could not connect to smartcard daemon");
-	else
-		INFO("Connected to smartcard daemon");
 
 	if (cmld_init_c0(containers_path, device_config_get_c0os(device_config)) < 0)
 		FATAL("Could not init c0");
@@ -1246,11 +1246,11 @@ cmld_container_destroy_cb(container_t *container, container_callback_t *cb, UNUS
 		container_unregister_observer(container, cb);
 
 	if (container_get_token_is_init(container)) {
-		smartcard_scd_token_remove_block(container);
+		container_scd_token_remove_block(container);
 	}
 
 	/* remove keyfile */
-	if (0 != smartcard_remove_keyfile(cmld_smartcard, container)) {
+	if (0 != container_remove_keyfile(container)) {
 		ERROR("Failed to remove keyfile. Continuing to remove container anyway.");
 		audit_log_event(container_get_uuid(container), FSA, CMLD, CONTAINER_MGMT,
 				"container-remove-keyfile",
@@ -1494,7 +1494,7 @@ cmld_token_detach(container_t *container)
 		ERROR("Could not stop container after token detachment.");
 	}
 
-	if (smartcard_scd_token_remove_block(container)) {
+	if (container_scd_token_remove_block(container)) {
 		ERROR("Failed to notify scd about token detachment");
 	}
 
@@ -1514,9 +1514,6 @@ cmld_cleanup(void)
 		control_free(cmld_control_gui);
 	if (cmld_control_cml)
 		control_free(cmld_control_cml);
-
-	if (cmld_smartcard)
-		smartcard_free(cmld_smartcard);
 
 	mem_free0(cmld_device_uuid);
 	mem_free0(cmld_device_update_base_url);
@@ -1604,7 +1601,7 @@ cmld_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t
 				container_set_state(container, CONTAINER_STATE_ZOMBIE);
 			}
 			if ((container_get_token_type(container) == CONTAINER_TOKEN_TYPE_USB) &&
-			    smartcard_release_pairing(container)) {
+			    container_scd_release_pairing(container)) {
 				ERROR("Failed to remove token paired file. Setting container state to ZOMBIE");
 				container_set_state(container, CONTAINER_STATE_ZOMBIE);
 			}
@@ -1619,4 +1616,10 @@ const char *
 cmld_get_containers_dir(void)
 {
 	return cmld_container_path;
+}
+
+const char *
+cmld_get_wrapped_keys_dir(void)
+{
+	return cmld_wrapped_keys_path;
 }

daemon/Makefile
@@ -92,7 +92,7 @@ SRC_FILES := main.c \
 	common/protobuf.c \
 	download.c \
 	crypto.c \
-	smartcard.c \
+	scd.c \
 	tss.c \
 	common/sock.c \
 	common/network.c \
@@ -110,6 +110,7 @@ SRC_FILES := main.c \
 
 # module order is crucial
 SRC_CMODULES := \
+	c_smartcard.c \
 	c_user.c \
 	c_net.c \
 	c_cgroups.c \

daemon/c_vol.c
@@ -53,7 +53,6 @@
 #include "cmld.h"
 #include "hardware.h"
 #include "guestos.h"
-#include "smartcard.h"
 #include "lxcfs.h"
 #include "audit.h"
 
@@ -1329,71 +1328,6 @@ c_vol_get_rootdir(void *volp)
 }
 
 static int
-c_vol_bind_token(c_vol_t *vol)
-{
-	if (CONTAINER_TOKEN_TYPE_USB != container_get_token_type(vol->container)) {
-		DEBUG("Token type is not USB, not binding relay socket");
-		return 0;
-	}
-
-	int ret = -1;
-	uid_t uid = container_get_uid(vol->container);
-
-	char *src_path = mem_printf("%s/%s.sock", SCD_TOKENCONTROL_SOCKET,
-				    uuid_string(container_get_uuid(vol->container)));
-	char *dest_dir = mem_printf("%s/dev/tokens", vol->root);
-	char *dest_path = mem_printf("%s/token.sock", dest_dir);
-
-	DEBUG("Binding token socket to %s", dest_path);
-
-	if (!file_exists(dest_dir)) {
-		if (dir_mkdir_p(dest_dir, 0755)) {
-			ERROR_ERRNO("Failed to create containing directory for %s", dest_path);
-			goto err;
-		}
-
-		if (chown(dest_dir, uid, uid)) {
-			ERROR("Failed to chown token directory at %s to %d", dest_path, uid);
-			goto err;
-		} else {
-			DEBUG("Successfully chowned token directory at %s to %d", dest_path, uid);
-		}
-	} else if (!file_is_dir(dest_dir)) {
-		ERROR("Token path %s exists and is no directory", dest_dir);
-		goto err;
-	}
-
-	if (file_touch(dest_path)) {
-		ERROR_ERRNO("Failed to prepare target file for bind mount at %s", dest_path);
-		goto err;
-	}
-
-	DEBUG("Binding token socket from %s to %s", src_path, dest_path);
-	if (mount(src_path, dest_path, NULL, MS_BIND, NULL)) {
-		ERROR_ERRNO("Failed to bind socket from %s to %s", src_path, dest_path);
-		goto err;
-	} else {
-		DEBUG("Successfully bound token socket to %s", dest_path);
-	}
-
-	if (chown(dest_path, uid, uid)) {
-		ERROR("Failed to chown token socket at %s to %d", dest_path, uid);
-		goto err;
-	} else {
-		DEBUG("Successfully chowned token socket at %s to %d", dest_path, uid);
-	}
-
-	ret = 0;
-
-err:
-	mem_free0(src_path);
-	mem_free0(dest_dir);
-	mem_free0(dest_path);
-
-	return ret;
-}
-
-static int
 c_vol_start_child_early(void *volp)
 {
 	c_vol_t *vol = volp;
@@ -1508,12 +1442,6 @@ c_vol_start_pre_exec(void *volp)
 
 	if (container_shift_ids(vol->container, dev_mnt, false) < 0)
 		WARN("Failed to setup ids for %s in user namespace!", dev_mnt);
-
-	if (c_vol_bind_token(vol) < 0) {
-		ERROR_ERRNO("Failed to bind token to container");
-		mem_free0(dev_mnt);
-		return -CONTAINER_ERROR_VOL;
-	}
 
 	mem_free0(dev_mnt);
 	return 0;

daemon/cmld.h
@@ -340,4 +340,7 @@ cmld_update_config(container_t *container, uint8_t *buf, size_t buf_len, uint8_t
 const char *
 cmld_get_containers_dir(void);
 
+const char *
+cmld_get_wrapped_keys_dir(void);
+
 #endif /* CMLD_H */

daemon/container.c
@@ -46,7 +46,6 @@
 #include "hardware.h"
 #include "uevent.h"
 #include "audit.h"
-#include "smartcard.h"
 
 #include <inttypes.h>
 #include <stdint.h>
@@ -437,6 +436,24 @@ CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(audit_get_loginuid, uint32_t, void *)
 CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(audit_get_loginuid, uint32_t, 0)
 CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(audit_set_loginuid, int, void *, uint32_t)
 CONTAINER_MODULE_FUNCTION_WRAPPER2_IMPL(audit_set_loginuid, int, 0, uint32_t)
+
+/* Functions usually implemented and registered by c_smartcard module */
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(start_with_smartcard, int, void *, int, const char *)
+CONTAINER_MODULE_FUNCTION_WRAPPER3_IMPL(start_with_smartcard, int, -1, int, const char *)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(stop_with_smartcard, int, void *, int, const char *)
+CONTAINER_MODULE_FUNCTION_WRAPPER3_IMPL(stop_with_smartcard, int, -1, int, const char *)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(scd_token_add_block, int, void *)
+CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(scd_token_add_block, int, 0)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(scd_token_remove_block, int, void *)
+CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(scd_token_remove_block, int, 0)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(scd_release_pairing, int, void *)
+CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(scd_release_pairing, int, 0)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(update_token_state, int, void *)
+CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(update_token_state, int, 0)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(change_pin, int, void *, int, const char *, const char *)
+CONTAINER_MODULE_FUNCTION_WRAPPER4_IMPL(change_pin, int, 0, int, const char *, const char *)
+CONTAINER_MODULE_REGISTER_WRAPPER_IMPL(remove_keyfile, int, void *)
+CONTAINER_MODULE_FUNCTION_WRAPPER_IMPL(remove_keyfile, int, 0)
 
 void
 container_free_key(container_t *container)
@@ -2078,7 +2095,7 @@ container_destroy(container_t *container)
 		}
 	mem_free0(file_name_uid);
 
-	if (smartcard_release_pairing(container)) {
+	if (container_scd_release_pairing(container)) {
 		ERROR("Can't remove token paired file!");
 	}
 

daemon/container.h
@@ -157,7 +157,8 @@ enum container_error {
 	CONTAINER_ERROR_USER,
 	CONTAINER_ERROR_FIFO,
 	CONTAINER_ERROR_TIME,
-	CONTAINER_ERROR_AUDIT
+	CONTAINER_ERROR_AUDIT,
+	CONTAINER_ERROR_SMARTCARD
 };
 
 typedef struct container_module {
@@ -1024,7 +1025,8 @@ container_audit_get_last_ack(const container_t *container);
  * Registers the corresponding handler for container_audit_get_last_ack
  */
 void
-container_register_audit_get_last_ack_handler(const char *mod_name , const char * (*handler)(void *data));
+container_register_audit_get_last_ack_handler(const char *mod_name,
+					      const char *(*handler)(void *data));
 
 /**
  * Stores the last ACK hash that has been received for this container.
@@ -1036,7 +1038,8 @@ container_audit_set_last_ack(const container_t *container, const char *last_ack)
  * Registers the corresponding handler for container_audit_set_last_ack
  */
 void
-container_register_audit_set_last_ack_handler(const char *mod_name , int (*handler)(void *data, const char *last_ack));
+container_register_audit_set_last_ack_handler(const char *mod_name,
+					      int (*handler)(void *data, const char *last_ack));
 
 /**
  * Returns wether an ACK is currently being processed for this container
@@ -1048,7 +1051,8 @@ container_audit_get_processing_ack(const container_t *container);
  * Registers the corresponding handler for container_audit_get_processing_ack
  */
 void
-container_register_audit_get_processing_ack_handler(const char *mod_name , bool (*handler)(void *data));
+container_register_audit_get_processing_ack_handler(const char *mod_name,
+						    bool (*handler)(void *data));
 
 /**
  * Stores if an ACK hash is currently being processed for this container
@@ -1060,7 +1064,9 @@ container_audit_set_processing_ack(const container_t *container, bool processing
  * Registers the corresponding handler for container_audit_set_processing_ack
  */
 void
-container_register_audit_set_processing_ack_handler(const char *mod_name , int (*handler)(void *data, bool processing_ack));
+container_register_audit_set_processing_ack_handler(const char *mod_name,
+						    int (*handler)(void *data,
+								   bool processing_ack));
 
 /**
  * Send audit record to container
@@ -1106,7 +1112,8 @@ container_audit_set_loginuid(const container_t *container, uint32_t uid);
  * Registers the corresponding handler for container_audit_set_loginuid
  */
 void
-container_register_audit_set_loginuid_handler(const char *mod_name , int (*handler)(void *data, uint32_t uid));
+container_register_audit_set_loginuid_handler(const char *mod_name,
+					      int (*handler)(void *data, uint32_t uid));
 
 uint32_t
 container_audit_get_loginuid(const container_t *container);
@@ -1115,7 +1122,8 @@ container_audit_get_loginuid(const container_t *container);
  * Registers the corresponding handler for container_audit_get_loginuid
  */
 void
-container_register_audit_get_loginuid_handler(const char *mod_name , uint32_t (*handler)(void *data));
+container_register_audit_get_loginuid_handler(const char *mod_name,
+					      uint32_t (*handler)(void *data));
 
 list_t *
 container_get_pnet_cfg_list(const container_t *container);
@@ -1125,5 +1133,60 @@ container_get_vnet_cfg_list(const container_t *container);
 
 list_t *
 container_get_fifo_list(const container_t *container);
+
+int
+container_start_with_smartcard(const container_t *container, int resp_fd, const char *pw);
+
+int
+container_stop_with_smartcard(const container_t *container, int resp_fd, const char *pw);
+
+int
+container_scd_token_add_block(const container_t *container);
+
+int
+container_scd_token_remove_block(const container_t *container);
+
+int
+container_scd_release_pairing(const container_t *container);
+
+int
+container_update_token_state(const container_t *container);
+
+int
+container_change_pin(const container_t *container, int resp_fd, const char *pw, const char *newpw);
+
+int
+container_remove_keyfile(const container_t *container);
+
+void
+container_register_start_with_smartcard_handler(const char *mod_name,
+						int (*handler)(void *data, int resp_fd,
+							       const char *pw));
+
+void
+container_register_stop_with_smartcard_handler(const char *mod_name,
+					       int (*handler)(void *data, int resp_fd,
+							      const char *pw));
+
+void
+container_register_scd_token_add_block_handler(const char *mod_name, int (*handler)(void *data));
+
+void
+container_register_scd_token_remove_block_handler(const char *mod_name, int (*handler)(void *data));
+
+void
+container_register_scd_release_pairing_handler(const char *mod_name, int (*handler)(void *data));
+
+void
+container_register_update_token_state_handler(const char *mod_name, int (*handler)(void *data));
+
+void
+container_register_change_pin_handler(const char *mod_name,
+				      int (*handler)(void *data, int resp_fd, const char *pw,
+						     const char *newpw));
+
+void
+container_register_remove_keyfile_handler(const char *mod_name, int (*handler)(void *data));
+
 
 #endif /* CONTAINER_H */

daemon/control.c
@@ -31,7 +31,6 @@
 #include "guestos.h"
 #include "cmld.h"
 #include "hardware.h"
-#include "smartcard.h"
 #include "crypto.h"
 #include "input.h"
 #include "uevent.h"

commit 3b55d019b4d8fdd8d622250b244dc26dc8505907

daemon/cmld: Free cmld_wrapped_keys_path in cmld_cleanup
cmld_wrapped_keys_path is allocated via mem_printf() in
cmld_init_stage_container() but was never released, leaking the
asprintf-allocated string on daemon exit.

Change the declaration from const char * to char * to match the
mem_printf() return type (cmld_get_wrapped_keys_dir() keeps its const
char * return type via implicit conversion), and release the buffer in
cmld_cleanup() alongside the other path globals.

Fixes: 4977717e5365 ("daemon: Moved smartcard module to c_smartcard module")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/cmld.c
@@ -110,7 +110,7 @@
 
 static const char *cmld_path = DEFAULT_BASE_PATH;
 static char *cmld_container_path = NULL;
-static const char *cmld_wrapped_keys_path = NULL;
+static char *cmld_wrapped_keys_path = NULL;
 
 static list_t *cmld_containers_list = NULL; // usually first element is c0
 static list_t *cmld_units_list = NULL;
@@ -2079,6 +2079,9 @@ cmld_cleanup(void)
 
 	if (cmld_container_path)
 		mem_free0(cmld_container_path);
+
+	if (cmld_wrapped_keys_path)
+		mem_free0(cmld_wrapped_keys_path);
 
 	if (cmld_device_uuid)
 		mem_free0(cmld_device_uuid);