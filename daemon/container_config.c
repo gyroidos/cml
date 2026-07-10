gyroidos/cml

commit 90274a3154d1e0344dd8642f726d8690b1920d5c

daemon/container: propagate mac filter to c_net for physical netifs
[michael.weiss] This commit was cherry-picked from Christian's fork in
   branch 'feature_mac_filter' "3ef4a01... [WIP] 001" and cleaned out
   a little bit. I will use this as base for my series to complete that
   feature. We start with the propagation of the configuration from
   container_config to c_net extracted from Christian's commit.

Signed-off-by: Christian Epple <christian.epple@aisec.fraunhofer.de>
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/container_config.c
@@ -46,7 +46,6 @@
 
 struct container_config {
 	char *file;
-
 	ContainerConfig *cfg;
 };
 
@@ -390,15 +389,40 @@ container_config_get_net_ifaces_list_new(const container_config_t *config)
 
 	list_t *net_ifaces_list = NULL;
 	for (size_t i = 0; i < config->cfg->n_net_ifaces; i++) {
-		net_ifaces_list =
-			list_append(net_ifaces_list, mem_strdup(config->cfg->net_ifaces[i]->netif));
-		for (size_t i = 0; i < config->cfg->net_ifaces[i]->n_mac_filter; i++) {
-			INFO("MAC Filter on for %s: allow '%s'", config->cfg->net_ifaces[i]->netif,
-			     config->cfg->net_ifaces[i]->mac_filter[i]);
-		}
-		if (config->cfg->net_ifaces[i]->n_mac_filter <= 0)
+		bool mac_filter_enabled = config->cfg->net_ifaces[i]->n_mac_filter > 0;
+		list_t *mac_whitelist = NULL;
+		// check if MAC filtering is configured
+		if (mac_filter_enabled) {
+			// create list of whitelisted MAC adresses
+			for (size_t j = 0; j < config->cfg->net_ifaces[i]->n_mac_filter; j++) {
+				INFO("MAC Filter on for %s: allow '%s'",
+				     config->cfg->net_ifaces[i]->netif,
+				     config->cfg->net_ifaces[i]->mac_filter[j]);
+
+				uint8_t *whitelisted_mac = mem_new(uint8_t, 6);
+				if (0 != network_str_to_mac_addr(
+						 config->cfg->net_ifaces[i]->mac_filter[j],
+						 whitelisted_mac)) {
+					mem_free(whitelisted_mac);
+				} else {
+					mac_whitelist = list_append(mac_whitelist, whitelisted_mac);
+				}
+			}
+		} else {
 			INFO("MAC Filter off for %s: allow any MAC",
 			     config->cfg->net_ifaces[i]->netif);
+		}
+		container_pnet_cfg_t *pnet_cfg = container_pnet_cfg_new(
+			config->cfg->net_ifaces[i]->netif, mac_filter_enabled, mac_whitelist);
+		if (pnet_cfg == NULL) {
+			ERROR("Failed to create container_pnet_cfg for %s",
+			      config->cfg->net_ifaces[i]->netif);
+			continue;
+		}
+
+		// append to net_ifaces_list
+		net_ifaces_list =
+			list_append(net_ifaces_list, pnet_cfg); // append to net_ifaces_list
 	}
 	return net_ifaces_list;
 }

daemon/c_net.c
@@ -106,9 +106,9 @@ struct c_net {
 	container_t *container; //!< container which the c_net struct is associated to
 	bool ns_net;		//!< indicates if the c_net structure has a network namespace
 	list_t *interface_list; //!< contains list of settings for different nw interfaces
-	list_t *interface_mv_name_list; //!< contains list of iff names to be moved into the container
-	char *ns_path;			//!< path for binding netns into filesystem
-	int fd_netns;			//!< fd to keep netns active during reboots
+	list_t *pnet_mv_list; //!< contains list of phyiscal NICs to be bridged via a veth or moved into a container. MAC adress filtering may be applied
+	char *ns_path;	      //!< path for binding netns into filesystem
+	int fd_netns;	      //!< fd to keep netns active during reboots
 };
 
 /**
@@ -501,7 +501,7 @@ c_net_interface_new(const char *if_name, uint8_t if_mac[6], bool configure)
  * @return the c_net_t network structure which holds networking information for a container.
  */
 c_net_t *
-c_net_new(container_t *container, bool net_ns, list_t *vnet_cfg_list, list_t *nw_mv_name_list)
+c_net_new(container_t *container, bool net_ns, list_t *vnet_cfg_list, list_t *pnet_cfg_list)
 {
 	ASSERT(container);
 
@@ -542,8 +542,9 @@ c_net_new(container_t *container, bool net_ns, list_t *vnet_cfg_list, list_t *nw
 	}
 
 	uint8_t mac[6];
-	for (list_t *l = nw_mv_name_list; l; l = l->next) {
-		char *if_name_macstr = l->data;
+	for (list_t *l = pnet_cfg_list; l; l = l->next) {
+		container_pnet_cfg_t *pnet_cfg = l->data;
+		char *if_name_macstr = pnet_cfg->pnet_name;
 		char *if_name = NULL;
 		TRACE("mv_name_list add ifname %s", if_name_macstr);
 		memset(&mac, 0, 6);
@@ -566,8 +567,10 @@ c_net_new(container_t *container, bool net_ns, list_t *vnet_cfg_list, list_t *nw
 		TRACE("mv_name_list add ifname %s", if_name);
 
 		if (cmld_netif_phys_remove_by_name(if_name))
-			net->interface_mv_name_list =
-				list_append(net->interface_mv_name_list, if_name);
+			net->pnet_mv_list = list_append(
+				net->pnet_mv_list,
+				container_pnet_cfg_new(pnet_cfg->pnet_name, pnet_cfg->mac_filter,
+						       pnet_cfg->mac_whitelist));
 	}
 
 	// path to bind netns, compatible to ip netns tool
@@ -717,6 +720,8 @@ c_net_start_pre_clone_interface(c_net_interface_t *ni)
 		goto err;
 	}
 
+	/* TODO: check if physical IF has MAC that is allowed by container config */
+
 	/* Start with second step: create veth pair, set root ns ipv4 add, bring the interface up */
 	DEBUG("Create veth pair %s/%s", ni->veth_cont_name, ni->veth_cmld_name);
 
@@ -828,11 +833,13 @@ c_net_helper_sigchild_cb(UNUSED int signum, event_signal_t *sig, void *data)
 }
 
 /**
- * This function is responisble for moving the container interface to its corresponding namespace.
+ * This function is responsible for moving the container interface to its corresponding namespace.
  *
  * It moves physical interfaces to its configured containers. Furher it creates a new child from
  * cmld and joins this to c0's netns for configuring the network endpoint of container virtual
  * veth's there.
+ * TODO: do not move physical interfaces but rather a veth so that iptables can be applied in the
+ * CML context
  */
 int
 c_net_start_post_clone(c_net_t *net)
@@ -852,12 +859,16 @@ c_net_start_post_clone(c_net_t *net)
 	pid_t pid = container_get_pid(net->container);
 	pid_t pid_c0 = cmld_containers_get_c0() ? container_get_pid(cmld_containers_get_c0()) : 0;
 
-	for (list_t *l = net->interface_mv_name_list; l; l = l->next) {
-		char *iff_name = l->data;
-
-		DEBUG("move phys %s to the ns of this pid: %d", iff_name, pid);
-		if (c_net_move_ifi(iff_name, pid) < 0)
-			return -1;
+	for (list_t *l = net->pnet_mv_list; l; l = l->next) {
+		container_pnet_cfg_t *cfg = l->data;
+		if (false == cfg->mac_filter) { // directly map phys. IF into container
+			DEBUG("move phys %s to the ns of this pid: %d", cfg->pnet_name, pid);
+			if (c_net_move_ifi(cfg->pnet_name, pid) < 0) {
+				return -1;
+			}
+		} else { // pIF should be bridged and MAC filtering applied
+			 // TODO
+		}
 	}
 	if (pid == pid_c0 && container_is_privileged(net->container)) {
 		for (list_t *l = cmld_get_netif_phys_list(); l; l = l->next) {
@@ -1216,10 +1227,11 @@ c_net_free(c_net_t *net)
 		c_net_free_interface(ni);
 	}
 	list_delete(net->interface_list);
-	for (list_t *l = net->interface_mv_name_list; l; l = l->next) {
-		mem_free(l->data);
+	for (list_t *l = net->pnet_mv_list; l; l = l->next) {
+		container_pnet_cfg_t *pnet = l->data;
+		container_pnet_cfg_free(pnet);
 	}
-	list_delete(net->interface_mv_name_list);
+	list_delete(net->pnet_mv_list);
 	mem_free(net->ns_path);
 	mem_free(net);
 }

daemon/container.c
@@ -302,29 +302,18 @@ container_new_internal(const uuid_t *uuid, const char *name, container_type_t ty
 		      (vnet_cfg->configure) ? "configured" : "unconfigured");
 	}
 
-	list_t *nw_mv_name_list = NULL;
-	if (container_uuid_is_c0id(uuid) && ns_net) {
-		nw_mv_name_list = hardware_get_nw_mv_name_list();
-		for (list_t *elem = nw_mv_name_list; elem != NULL; elem = elem->next) {
-			DEBUG("List element in nw_names_list: %s", (char *)(elem->data));
-		}
-	}
-
 	// network interfaces from container config
 	for (list_t *elem = net_ifaces; elem != NULL; elem = elem->next) {
-		char *if_name_macstr = elem->data;
-		nw_mv_name_list = list_append(nw_mv_name_list, mem_strdup(if_name_macstr));
-		DEBUG("List element in net_ifaces: %s", if_name_macstr);
+		container_pnet_cfg_t *pnet_cfg = elem->data;
+		DEBUG("List element in net_ifaces: %s", pnet_cfg->pnet_name);
 	}
 
-	container->net = c_net_new(container, ns_net, vnet_cfg_list, nw_mv_name_list);
+	container->net = c_net_new(container, ns_net, vnet_cfg_list, net_ifaces);
 	if (!container->net) {
 		WARN("Could not initialize net subsystem for container %s (UUID: %s)",
 		     container->name, uuid_string(container->uuid));
 		goto error;
 	}
-	list_delete(nw_mv_name_list);
-	list_delete(net_ifaces);
 
 	container->vol = c_vol_new(container);
 	if (!container->vol) {
@@ -588,6 +577,12 @@ container_new(const char *store_path, const uuid_t *existing_uuid, const uint8_t
 		mem_free(vnet_cfg);
 	}
 	list_delete(vnet_cfg_list);
+
+	for (list_t *l = net_ifaces; l; l = l->next) {
+		container_pnet_cfg_t *pnet_cfg = l->data;
+		container_pnet_cfg_free(pnet_cfg);
+	}
+	list_delete(net_ifaces);
 
 	container_config_free(conf);
 	return c;
@@ -2482,6 +2477,44 @@ container_vnet_cfg_new(const char *if_name, const char *rootns_name, const uint8
 	vnet_cfg->rootns_name = rootns_name ? mem_strdup(rootns_name) : NULL;
 	vnet_cfg->configure = configure;
 	return vnet_cfg;
+}
+
+/**
+ * Create a new container_pnet_cfg_t structure for physical NICs that should be
+ * made accessible to a container.
+ */
+container_pnet_cfg_t *
+container_pnet_cfg_new(const char *if_name_mac, bool mac_filter, list_t *mac_whitelist)
+{
+	container_pnet_cfg_t *pnet_cfg = mem_new0(container_pnet_cfg_t, 1);
+
+	pnet_cfg->pnet_name = mem_strdup(if_name_mac);
+	pnet_cfg->mac_filter = mac_filter;
+	pnet_cfg->mac_whitelist = NULL;
+
+	if (!mac_filter)
+		return pnet_cfg;
+
+	for (list_t *l = mac_whitelist; l; l = l->next) {
+		uint8_t *mac = mem_alloc0(6);
+		memcpy(mac, l->data, 6);
+		pnet_cfg->mac_whitelist = list_append(pnet_cfg->mac_whitelist, mac);
+	}
+
+	return pnet_cfg;
+}
+
+void
+container_pnet_cfg_free(container_pnet_cfg_t *pnet_cfg)
+{
+	IF_NULL_RETURN(pnet_cfg);
+
+	for (list_t *l = pnet_cfg->mac_whitelist; l; l = l->next) {
+		uint8_t *mac = l->data;
+		mem_free(mac);
+	}
+	list_delete(pnet_cfg->mac_whitelist);
+	mem_free(pnet_cfg);
 }
 
 void

daemon/container.h
@@ -97,6 +97,17 @@ typedef struct container_vnet_cfg {
 } container_vnet_cfg_t;
 
 /**
+ * Structure to define a phyiscal NIC that is accesible from inside a container.
+ * The CML bridges or moves the physical IF into the container and enforces
+ * filtering of layer 2 frames based on MAC adresses
+ */
+typedef struct container_pnet_cfg {
+	char *pnet_name;
+	bool mac_filter;
+	list_t *mac_whitelist;
+} container_pnet_cfg_t;
+
+/**
  * Structure to define the configuration of the token associated with the container.
  */
 typedef struct container_token_config {
@@ -671,6 +682,20 @@ container_vnet_cfg_free(container_vnet_cfg_t *vnet_cfg);
  */
 list_t *
 container_get_vnet_runtime_cfg_new(container_t *container);
+
+/**
+ * Initialize a container_pnet_cfg_t data structure and allocate needed memory.
+ * @if_name may be either the name or the MAC of the phyiscal NIC
+ */
+container_pnet_cfg_t *
+container_pnet_cfg_new(const char *if_name, bool mac_filter, list_t *mac_whitelist);
+
+/**
+ * Free all memory used by a container_pnet_cfg_t data structure, including
+ * the internal mac_whitelist.
+ */
+void
+container_pnet_cfg_free(container_pnet_cfg_t *pnet_cfg);
 
 /**
  * This function updates the containers config on storage with the provided

daemon/container.proto
@@ -33,7 +33,7 @@ message ContainerImageSize {
 
 message ContainerVnetConfig {
 	required string if_name = 1; // name of virtual veth endpoint in container
-	required bool configure = 2; // should cmld configure the interface or leav it unconfigured
+	required bool configure = 2; // should cmld configure the interface or leave it unconfigured
 	optional string if_rootns_name = 3; // name of virtual veth endpoint in rootns (will be autogenerated by cmld)
 	optional string if_mac = 4; // mac of virtual veth endpoint inside container (will be autogenerated)
 	// TODO Define configuration, for now just use hardcoded default config in c_net

commit 55d50d5929e8182fdb5c1bf0a5156b4b4465b4d3

daemon/container_config: Release local mac_whitelist after copy
container_config_get_net_ifaces_list_new() builds a local
mac_whitelist of mem_new()'d uint8_t * MAC entries and hands it to
container_pnet_cfg_new(). The new() function deep-copies each entry
into its own list rather than taking ownership, so the local list
(both the list_t nodes and the MAC byte arrays) must be released by
the caller.

Currently the caller drops the list on the floor on every loop
iteration where mac_filter is enabled, leaking one uint8_t * per
whitelisted MAC plus a list_t node per entry.

Walk and free the entries, then list_delete() the list, after every
container_pnet_cfg_new() call (regardless of whether it returned a
valid pnet_cfg).

Fixes: 90274a3154d1 ("daemon/container: propagate mac filter to c_net for physical netifs")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/container_config.c
@@ -422,6 +422,14 @@ container_config_get_net_ifaces_list_new(const container_config_t *config)
 		}
 		container_pnet_cfg_t *pnet_cfg = container_pnet_cfg_new(
 			config->cfg->net_ifaces[i]->netif, mac_filter_enabled, mac_whitelist);
+
+		/* container_pnet_cfg_new() deep-copies each entry; release the
+		 * local mac_whitelist (both nodes and entries) regardless of
+		 * whether pnet_cfg creation succeeded. */
+		for (list_t *l = mac_whitelist; l; l = l->next)
+			mem_free0(l->data);
+		list_delete(mac_whitelist);
+
 		if (pnet_cfg == NULL) {
 			ERROR("Failed to create container_pnet_cfg for %s",
 			      config->cfg->net_ifaces[i]->netif);