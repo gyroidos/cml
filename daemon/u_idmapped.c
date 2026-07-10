gyroidos/cml

commit fb6de437245bca3091b2b861f8591bf4b1e3da78

daemon/unit: Introduce new unit module for minimal compartments
The new unit module with its u_user and u_net submodules provides
a minimal environment for services inside the CML. For instate, the
scd as well as the tpm2d will use this in the future to run in
isolated compartments.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_net.c
@@ -1,0 +1,118 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+/**
+ * @file u_net.c
+ *
+ * This module is responsible for a private network without external connectivity for units.
+ */
+
+#define _GNU_SOURCE
+
+#define MOD_NAME "c_net"
+
+#include "common/macro.h"
+#include "common/mem.h"
+#include "compartment.h"
+#include "unit.h"
+
+#include <sys/mount.h>
+
+/* Dummy Network structure */
+typedef struct u_net {
+	unit_t *unit; //!< unit which the u_net struct is associated to
+} u_net_t;
+
+/**
+ * This function allocates a new u_net_t instance, associated to a specific unit object.
+ * @return the u_net_t network structure which holds networking information for an unit.
+ */
+static void *
+u_net_new(compartment_t *compartment)
+{
+	ASSERT(compartment);
+	IF_NULL_RETVAL(compartment_get_extension_data(compartment), NULL);
+
+	u_net_t *net = mem_new0(u_net_t, 1);
+	net->unit = compartment_get_extension_data(compartment);
+
+	return net;
+}
+
+/**
+ * Frees the u_net_t structure
+ */
+static void
+u_net_free(void *netp)
+{
+	u_net_t *net = netp;
+	ASSERT(net);
+
+	mem_free0(net);
+}
+
+/*
+ * Mount sys in namspaced child
+ */
+static int
+u_net_start_child(void *netp)
+{
+	u_net_t *net = netp;
+	ASSERT(net);
+
+	if (!unit_has_netns(net->unit))
+		return 0;
+
+	if (mount("sys", "/sys", "sys", MS_RELATIME | MS_NOSUID, NULL) < 0) {
+		ERROR_ERRNO("Could not remount /sys in unit %s", unit_get_description(net->unit));
+		return -COMPARTMENT_ERROR_NET;
+	}
+
+	return 0;
+}
+
+static compartment_module_t u_net_module = {
+	.name = MOD_NAME,
+	.compartment_new = u_net_new,
+	.compartment_free = u_net_free,
+	.compartment_destroy = NULL,
+	.start_post_clone_early = NULL,
+	.start_child_early = NULL,
+	.start_pre_clone = NULL,
+	.start_post_clone = NULL,
+	.start_pre_exec = NULL,
+	.start_post_exec = NULL,
+	.start_pre_exec_child_early = NULL,
+	.start_child = u_net_start_child,
+	.start_pre_exec_child = NULL,
+	.stop = NULL,
+	.cleanup = NULL,
+	.join_ns = NULL,
+};
+
+static void INIT
+u_net_init(void)
+{
+	// register this module in unit.c
+	unit_register_compartment_module(&u_net_module);
+}

daemon/u_user.c
@@ -1,0 +1,278 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#define MOD_NAME "c_user"
+
+#include "common/macro.h"
+#include "common/mem.h"
+#include "common/file.h"
+#include "common/ns.h"
+#include "unit.h"
+
+#define UID_RANGE 1
+#define UID_RANGES_START 65537
+
+#define MAX_UID_RANGES ((int)((100000 - UID_RANGES_START) / UID_RANGE))
+
+/* Paths for controling mappings */
+#define U_USER_UID_MAP_PATH "/proc/%d/uid_map"
+#define U_USER_GID_MAP_PATH "/proc/%d/gid_map"
+
+#define U_USER_MAP_FORMAT "%d %d %d"
+
+/* User structure with specific usernamespace mappings */
+typedef struct u_user {
+	unit_t *unit;  //!< unit which the u_user struct is associated to
+	int offset;    //!< gives information about the uid mapping to be set
+	int uid_start; //!< this is the start of uids and gids in the root namespace
+} u_user_t;
+
+/**
+ * bool array, which globally holds assigend ranges in order to
+ * determine a new offset for a starting container.
+ * uid_offsets[i]==true means that a container holds this offset to get its
+ * specific uid range
+ */
+static bool *uid_offsets = NULL;
+
+/**
+ * sets the offset at the specified position to false.
+ * indicates that a unit releases its mappings.
+ */
+static void
+u_user_unset_offset(int offset)
+{
+	ASSERT(offset < MAX_UID_RANGES);
+	TRACE("UID offset %d released by an unit", offset);
+	IF_TRUE_RETURN(offset == -1);
+
+	uid_offsets[offset] = false;
+}
+
+/**
+ * determines first free slot and occupies it. Also responsible for allocating the offsets array.
+ * @return failure, return -1, else return first free offset
+ */
+static int
+u_user_set_next_offset(void)
+{
+	ASSERT(MAX_UID_RANGES > 0);
+
+	if (!uid_offsets) {
+		uid_offsets = mem_new0(bool, MAX_UID_RANGES);
+		uid_offsets[0] = true;
+		TRACE("UID offset 0 ocupied by an unit");
+		return 0;
+	}
+
+	for (int i = 0; i < MAX_UID_RANGES; i++) {
+		if (!uid_offsets[i]) {
+			TRACE("UID offset %d occupied by an unit", i);
+			uid_offsets[i] = true;
+			return i;
+		}
+	}
+
+	DEBUG("Unable to provide a valid uid/gid range for u_user");
+	return -1;
+}
+
+/**
+ * This function determines and sets the next available uid range, depending on the unit offset.
+ */
+static int
+u_user_set_next_uid_range_start(u_user_t *user)
+{
+	ASSERT(user);
+
+	int offset = -1;
+
+	offset = u_user_set_next_offset();
+	IF_TRUE_RETVAL(offset < 0, -1);
+	user->offset = offset;
+
+	user->uid_start = UID_RANGES_START + (user->offset * UID_RANGE);
+	DEBUG("Next free uid/gid map start is: %u", user->uid_start);
+
+	return 0;
+}
+
+/**
+ * This function allocates a new u_user_t instance, associated to a specific unit object.
+ * @return the u_user_t user structure which holds user namespace information for a unit.
+ */
+static void *
+u_user_new(compartment_t *compartment)
+{
+	ASSERT(compartment);
+	IF_NULL_RETVAL(compartment_get_extension_data(compartment), NULL);
+
+	u_user_t *user = mem_new0(u_user_t, 1);
+	user->unit = compartment_get_extension_data(compartment);
+
+	user->uid_start = 0;
+
+	TRACE("new u_user struct was allocated");
+
+	return user;
+}
+
+/**
+ * Setup mappings for uids and gids
+ */
+static int
+u_user_setup_mapping(pid_t pid, int uid_start)
+{
+	int ret = -1;
+
+	char *uid_mapping = mem_printf(U_USER_MAP_FORMAT, 0, uid_start, UID_RANGE);
+	INFO("mapping: \n'%s'", uid_mapping);
+
+	char *uid_map_path = mem_printf(U_USER_UID_MAP_PATH, pid);
+	char *gid_map_path = mem_printf(U_USER_GID_MAP_PATH, pid);
+
+	// write mapping to proc
+	if (file_printf(uid_map_path, "%s", uid_mapping) == -1) {
+		ERROR_ERRNO("Failed to write to %s", uid_map_path);
+		goto error;
+	}
+	if (file_printf(gid_map_path, "%s", uid_mapping) == -1) {
+		ERROR_ERRNO("Failed to write to %s", gid_map_path);
+		goto error;
+	}
+
+	ret = 0;
+
+error:
+	mem_free0(uid_mapping);
+	mem_free0(uid_map_path);
+	mem_free0(gid_map_path);
+	return ret;
+}
+
+/**
+ * Cleans up the u_user_t struct.
+ */
+static void
+u_user_cleanup(void *usr, UNUSED bool is_rebooting)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	u_user_unset_offset(user->offset);
+}
+
+/**
+ * Frees the u_user_t structure
+ */
+static void
+u_user_free(void *usr)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	mem_free0(user);
+}
+
+/**
+ * Become root in new userns
+ */
+static int
+u_user_setuid0(void)
+{
+	return namespace_setuid0();
+}
+
+static int
+u_user_start_child(void *usr)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	if (u_user_setuid0() < 0) {
+		return -COMPARTMENT_ERROR_USER;
+	}
+	return 0;
+}
+
+/**
+ * Reserves a mapping for uids and gids of the user namespace in rootns
+ */
+static int
+u_user_start_pre_clone(void *usr)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	// reserve a new mapping
+	if (u_user_set_next_uid_range_start(user)) {
+		ERROR("Reserving uid range for userns for unit %s", unit_get_name(user->unit));
+		return -COMPARTMENT_ERROR_USER;
+	}
+	return 0;
+}
+
+/**
+ * Setup mapping for uids and gids of the user namespace in rootns
+ */
+static int
+u_user_start_post_clone(void *usr)
+{
+	u_user_t *user = usr;
+	ASSERT(user);
+
+	pid_t unit_pid = unit_get_pid(user->unit);
+
+	if (u_user_setup_mapping(unit_pid, user->uid_start) < 0)
+		return -COMPARTMENT_ERROR_USER;
+
+	INFO("uid/gid mapping '%d %d' for %s activated", user->uid_start, UID_RANGE,
+	     unit_get_name(user->unit));
+
+	return 0;
+}
+
+static compartment_module_t u_user_module = {
+	.name = MOD_NAME,
+	.compartment_new = u_user_new,
+	.compartment_free = u_user_free,
+	.compartment_destroy = NULL,
+	.start_post_clone_early = NULL,
+	.start_child_early = NULL,
+	.start_pre_clone = u_user_start_pre_clone,
+	.start_post_clone = u_user_start_post_clone,
+	.start_pre_exec = NULL,
+	.start_post_exec = NULL,
+	.start_child = u_user_start_child,
+	.start_pre_exec_child = NULL,
+	.stop = NULL,
+	.cleanup = u_user_cleanup,
+	.join_ns = NULL,
+};
+
+static void INIT
+u_user_init(void)
+{
+	// register this module in unit.c
+	unit_register_compartment_module(&u_user_module);
+}

daemon/unit.c
@@ -1,0 +1,187 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#include "unit.h"
+
+#include "common/macro.h"
+#include "common/mem.h"
+#include "common/list.h"
+#include "common/uuid.h"
+#include "compartment.h"
+
+#include <stdint.h>
+
+struct unit {
+	compartment_t *compartment;
+};
+
+static char *unit_bin_path[] = { "/usr/sbin", "/sbin", "/usr/bin", "/bin" };
+
+// list of compartment modules for all container objects
+static list_t *compartment_module_list = NULL;
+
+static list_t *
+unit_get_compartment_modules(void)
+{
+	return compartment_module_list;
+}
+
+static void
+unit_set_extension(void *extension_data, compartment_t *compartment)
+{
+	ASSERT(extension_data);
+	ASSERT(compartment);
+
+	unit_t *unit = extension_data;
+	unit->compartment = compartment;
+}
+
+void
+unit_register_compartment_module(compartment_module_t *mod)
+{
+	ASSERT(mod);
+
+	compartment_module_list = list_append(compartment_module_list, mod);
+	DEBUG("Unit module %s registered, nr of hooks: %d)", mod->name,
+	      list_length(compartment_module_list));
+}
+
+unit_t *
+unit_new(const uuid_t *uuid, const char *name, const char *command, char **argv, char **env,
+	 size_t env_len, bool netns)
+{
+	// set type specific flags for compartment
+	uint64_t flags = 0;
+	flags |= COMPARTMENT_FLAG_TYPE_CONTAINER;
+	// set namespace flags for compartment
+	flags |= COMPARTMENT_FLAG_NS_USER;
+	if (netns)
+		flags |= COMPARTMENT_FLAG_NS_NET;
+
+	unit_t *unit = mem_new0(unit_t, 1);
+
+	// initial length of 2 + actual argv size to hold command name + final NULL pointer
+	int argv_len = 2;
+	for (char **arg = argv; arg && *arg; arg++)
+		argv_len++;
+
+	char *command_path = NULL;
+	for (size_t i = 0; i < sizeof(unit_bin_path) / sizeof(unit_bin_path[0]); i++) {
+		command_path = mem_printf("%s/%s", unit_bin_path[i], command);
+		if (access(command_path, F_OK) == 0) {
+			DEBUG("Found command '%s' in path '%s'", command, unit_bin_path[i]);
+			break;
+		}
+	}
+	if (command_path == NULL) {
+		ERROR("Binary '%s' not found in valid path.", command);
+		unit_free(unit);
+		return NULL;
+	}
+
+	char **init_argv = mem_new0(char *, argv_len);
+	init_argv[0] = command_path;
+
+	size_t i = 1;
+	for (char **arg = argv; arg && *arg; arg++, i++)
+		init_argv[i] = mem_strdup(*arg);
+
+	// create internal compartment object with unit as extension data
+	compartment_extension_t *extension =
+		compartment_extension_new(unit_set_extension, unit_get_compartment_modules, unit);
+	unit->compartment = compartment_new(uuid, name, flags, init_argv[0], init_argv, env,
+					    env_len, extension);
+
+	if (!unit->compartment) {
+		ERROR("Could not create internal compartment object");
+		compartment_extension_free(extension);
+		unit_free(unit);
+		return NULL;
+	}
+
+	return unit;
+}
+
+void
+unit_free(unit_t *unit)
+{
+	ASSERT(unit);
+
+	/*
+	 * free compartment first, as c_*_free() may access
+	 * unit resources through extension pointer
+	 */
+	if (unit->compartment)
+		compartment_free(unit->compartment);
+
+	mem_free0(unit);
+}
+
+const char *
+unit_get_name(const unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_get_name(unit->compartment);
+}
+
+const char *
+unit_get_description(const unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_get_description(unit->compartment);
+}
+
+bool
+unit_has_netns(const unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_get_flags(unit->compartment) & COMPARTMENT_FLAG_NS_NET;
+}
+
+pid_t
+unit_get_pid(const unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_get_pid(unit->compartment);
+}
+
+int
+unit_start(unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_start(unit->compartment);
+}
+
+int
+unit_stop(unit_t *unit)
+{
+	ASSERT(unit);
+	return compartment_stop(unit->compartment);
+}
+
+void
+unit_kill(unit_t *unit)
+{
+	ASSERT(unit);
+	compartment_kill(unit->compartment);
+}

daemon/unit.h
@@ -1,0 +1,65 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+#ifndef UNIT_H
+#define UNIT_H
+
+#include "common/uuid.h"
+#include "compartment.h"
+
+#include <sys/types.h>
+
+typedef struct unit unit_t;
+
+void
+unit_register_compartment_module(compartment_module_t *mod);
+
+unit_t *
+unit_new(const uuid_t *uuid, const char *name, const char *command, char **argv, char **env,
+	 size_t env_len, bool netns);
+
+void
+unit_free(unit_t *unit);
+
+const char *
+unit_get_name(const unit_t *unit);
+
+const char *
+unit_get_description(const unit_t *unit);
+
+bool
+unit_has_netns(const unit_t *unit);
+
+pid_t
+unit_get_pid(const unit_t *unit);
+
+int
+unit_start(unit_t *unit);
+
+int
+unit_stop(unit_t *unit);
+
+void
+unit_kill(unit_t *unit);
+
+#endif /* UNIT_H */

daemon/Makefile
@@ -98,6 +98,7 @@ PROTO_SRC := \
 	c_service.pb-c.c
 
 SRC_FILES := main.c \
+	unit.c \
 	cmld.c \
 	hotplug.c \
 	container.c \
@@ -119,6 +120,10 @@ SRC_FILES := main.c \
 	lxcfs.c \
 	input.c \
 	audit.c
+
+SRC_UMODULES += \
+	u_user.c \
+	u_net.c
 
 # module order is crucial
 SRC_CMODULES := \
@@ -233,7 +238,7 @@ else
 endif
 
 cmld: libcommon $(PROTO_SRC) $(SRC_FILES) $(SRC_CMODULES)
-	$(CC) $(LOCAL_CFLAGS) $(SRC_FILES) $(SRC_CMODULES) $(PROTO_SRC) $(LDLIBS) -o cmld
+	$(CC) $(LOCAL_CFLAGS) $(SRC_FILES) $(SRC_UMODULES) $(SRC_CMODULES) $(PROTO_SRC) $(LDLIBS) -o cmld
 
 
 .PHONY: clean

commit 5e79d8a77d5db8411e01854715c2d4be45c86a6a

daemon/unit: Release compartment_module_list on daemon exit
unit.c keeps a static list of registered unit compartment modules.
Modules are registered at INIT time via unit_register_compartment_module()
and the list_t nodes were never freed at shutdown.

Add a paired unit_unregister_compartment_module() that list_remove()s
the module from the list. Each u_* module gets a matching
DEINIT-marked u_*_deinit() that calls unregister, so the registration
and the corresponding cleanup live next to each other in the same
module file. Once every module has unregistered at destructor time
the list head is NULL and the list_t nodes are gone with no central
sweep needed.

Fixes: fb6de437245b ("daemon/unit: Introduce new unit module for minimal compartments")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_idmapped.c
@@ -251,3 +251,9 @@ u_idmapped_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_idmapped_module);
 }
+
+static void DEINIT
+u_idmapped_deinit(void)
+{
+	unit_unregister_compartment_module(&u_idmapped_module);
+}

daemon/u_net.c
@@ -116,3 +116,9 @@ u_net_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_net_module);
 }
+
+static void DEINIT
+u_net_deinit(void)
+{
+	unit_unregister_compartment_module(&u_net_module);
+}

daemon/u_perm.c
@@ -309,3 +309,9 @@ u_perm_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_perm_module);
 }
+
+static void DEINIT
+u_perm_deinit(void)
+{
+	unit_unregister_compartment_module(&u_perm_module);
+}

daemon/u_service.c
@@ -222,3 +222,9 @@ u_service_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_service_module);
 }
+
+static void DEINIT
+u_service_deinit(void)
+{
+	unit_unregister_compartment_module(&u_service_module);
+}

daemon/u_time.c
@@ -126,3 +126,9 @@ u_time_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_time_module);
 }
+
+static void DEINIT
+u_time_deinit(void)
+{
+	unit_unregister_compartment_module(&u_time_module);
+}

daemon/u_usb.c
@@ -224,3 +224,9 @@ u_usb_init(void)
 	// register this module in unit.c
 	unit_register_compartment_module(&u_usb_module);
 }
+
+static void DEINIT
+u_usb_deinit(void)
+{
+	unit_unregister_compartment_module(&u_usb_module);
+}

daemon/u_user.c
@@ -373,3 +373,9 @@ u_user_init(void)
 	if (chmod(LOGFILE_DIR, mode))
 		WARN_ERRNO("Could not chmod %s (%o)", LOGFILE_DIR, mode);
 }
+
+static void DEINIT
+u_user_deinit(void)
+{
+	unit_unregister_compartment_module(&u_user_module);
+}

daemon/unit.c
@@ -87,6 +87,16 @@ unit_register_compartment_module(compartment_module_t *mod)
 	      list_length(compartment_module_list));
 }
 
+void
+unit_unregister_compartment_module(compartment_module_t *mod)
+{
+	ASSERT(mod);
+
+	compartment_module_list = list_remove(compartment_module_list, mod);
+	DEBUG("Unit module %s unregistered, nr of hooks: %d)", mod->name,
+	      list_length(compartment_module_list));
+}
+
 unit_t *
 unit_new(const uuid_t *uuid, const char *name, const char *command, char **argv, const char **env,
 	 size_t env_len, bool netns, const char *data_path, const char *sock_name, int sock_type,

daemon/unit.h
@@ -34,6 +34,9 @@ typedef struct unit unit_t;
 void
 unit_register_compartment_module(compartment_module_t *mod);
 
+void
+unit_unregister_compartment_module(compartment_module_t *mod);
+
 unit_t *
 unit_new(const uuid_t *uuid, const char *name, const char *command, char **argv, const char **env,
 	 size_t env_len, bool netns, const char *data_path, const char *sock_name, int sock_type,