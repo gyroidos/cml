gyroidos/cml

commit 5b3602ec0226b0f6bcf038d618aee15653f75ef0

daemon/u_idmapped: Introduce u_idmapped module
Introduce a new unit submodule u_idmapped. The u_idmapped module
uses the lately introduced mount_idmapped() implementation to mount
data dir as well as log dir with uid mapping in units.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_idmapped.c
@@ -1,0 +1,243 @@
+/*
+ * This file is part of GyroidOS
+ * Copyright(c) 2025 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+ */
+
+/**
+ * @file u_idmapped.c
+ *
+ * This module is responsible for uid mapping of some mounts inside units.
+ */
+
+#define _GNU_SOURCE
+
+#define MOD_NAME "c_idmapped"
+
+#include "common/macro.h"
+#include "common/mem.h"
+#include "common/ns.h"
+#include "common/sock.h"
+#include "compartment.h"
+#include "unit.h"
+#include "cmld.h"
+#include "mount.h"
+
+#include <fcntl.h>
+
+typedef struct u_idmapped {
+	unit_t *unit; //!< unit which the u_perm struct is associated to
+	list_t *mnt_list;
+} u_idmapped_t;
+
+typedef struct u_idmapped_mnt {
+	char *src;
+	char *dst;
+} u_idmapped_mnt_t;
+
+static u_idmapped_mnt_t *
+u_idmapped_mnt_new(const char *src, const char *dst)
+{
+	u_idmapped_mnt_t *mnt = mem_new0(u_idmapped_mnt_t, 1);
+	mnt->src = mem_strdup(src);
+	mnt->dst = mem_strdup(dst);
+
+	return mnt;
+}
+
+void
+u_idmapped_mnt_free(u_idmapped_mnt_t *mnt)
+{
+	ASSERT(mnt);
+
+	mem_free0(mnt->src);
+	mem_free0(mnt->dst);
+
+	mem_free0(mnt);
+}
+
+int
+u_idmapped_add_mount(u_idmapped_t *idmapped, const char *src, const char *dst)
+{
+	ASSERT(idmapped);
+
+	IF_FALSE_RETVAL(mount_is_idmapping_supported(), -1);
+
+	IF_FALSE_RETVAL(src, -1);
+	IF_FALSE_RETVAL(dst, -1);
+
+	idmapped->mnt_list = list_append(idmapped->mnt_list, u_idmapped_mnt_new(src, dst));
+
+	return 0;
+}
+
+/**
+ * This function allocates a new u_idmapped_t instance, associated to a specific unit object.
+ * @return the u_idmapped_t idmappedission structure which holds idmappedission access rules for an unit.
+ */
+static void *
+u_idmapped_new(compartment_t *compartment)
+{
+	ASSERT(compartment);
+	IF_NULL_RETVAL(compartment_get_extension_data(compartment), NULL);
+
+	u_idmapped_t *idmapped = mem_new0(u_idmapped_t, 1);
+	idmapped->unit = compartment_get_extension_data(compartment);
+
+	return idmapped;
+}
+
+/**
+ * Frees the u_idmapped_t structure
+ */
+static void
+u_idmapped_free(void *idmappedp)
+{
+	u_idmapped_t *idmapped = idmappedp;
+	ASSERT(idmapped);
+
+	mem_free0(idmapped);
+}
+
+struct do_mount_data {
+	int userns_fd;
+	u_idmapped_t *idmapped;
+};
+
+int
+u_idmapped_do_mount(const void *data)
+{
+	const struct do_mount_data *datap = data;
+	ASSERT(datap && datap->idmapped);
+
+	int ret = -1;
+
+	for (list_t *l = datap->idmapped->mnt_list; l; l = l->next) {
+		u_idmapped_mnt_t *mnt = l->data;
+		if (mount_idmapped(mnt->src, mnt->dst, datap->userns_fd) < 0) {
+			ERROR("Could mount %s -> %s with uid mapping in unit %s!", mnt->src,
+			      mnt->dst, unit_get_description(datap->idmapped->unit));
+			goto out;
+		} else {
+			DEBUG("Mounted %s -> %s with uid mapping in unit %s.", mnt->src, mnt->dst,
+			      unit_get_description(datap->idmapped->unit));
+		}
+	}
+
+	ret = 0;
+out:
+	return ret;
+}
+
+static int
+u_idmapped_start_pre_clone(void *idmappedp)
+{
+	u_idmapped_t *idmapped = idmappedp;
+	ASSERT(idmapped);
+
+	// do not add mounts if idmapped mounts are unsupported
+	if (!mount_is_idmapping_supported())
+		return 0;
+
+	IF_TRUE_RETVAL(u_idmapped_add_mount(idmapped, CMLD_SOCKET_DIR, CMLD_SOCKET_DIR) < 0,
+		       -COMPARTMENT_ERROR_USER);
+	IF_TRUE_RETVAL(u_idmapped_add_mount(idmapped, LOGFILE_DIR, LOGFILE_DIR) < 0,
+		       -COMPARTMENT_ERROR_USER);
+	IF_TRUE_RETVAL(u_idmapped_add_mount(idmapped, unit_get_data_path(idmapped->unit),
+					    unit_get_data_path(idmapped->unit)) < 0,
+		       -COMPARTMENT_ERROR_USER);
+
+	return 0;
+}
+
+static int
+u_idmapped_start_pre_exec(void *idmappedp)
+{
+	u_idmapped_t *idmapped = idmappedp;
+	ASSERT(idmapped);
+
+	if (!mount_is_idmapping_supported())
+		return 0;
+
+	int ret = -COMPARTMENT_ERROR_USER;
+
+	char *userns_path = mem_printf("/proc/%d/ns/user", unit_get_pid(idmapped->unit));
+	int userns_fd = open(userns_path, O_RDONLY);
+	if (userns_fd < 0) {
+		ERROR_ERRNO("Failed to open userns fd for %s",
+			    unit_get_description(idmapped->unit));
+		goto out;
+	}
+
+	struct do_mount_data data = { .userns_fd = userns_fd, .idmapped = idmapped };
+
+	if (namespace_exec(unit_get_pid(idmapped->unit), CLONE_NEWNS, 0, 0, u_idmapped_do_mount,
+			   &data))
+		goto out;
+
+	ret = 0;
+out:
+	mem_free0(userns_path);
+	close(userns_fd);
+	return ret;
+}
+
+/**
+ * Cleans up the u_idmapped_t struct.
+ */
+static void
+u_idmapped_cleanup(void *idmappedp, UNUSED bool is_rebooting)
+{
+	u_idmapped_t *idmapped = idmappedp;
+	ASSERT(idmapped);
+
+	for (list_t *l = idmapped->mnt_list; l; l = l->next) {
+		u_idmapped_mnt_t *mnt = l->data;
+		u_idmapped_mnt_free(mnt);
+	}
+	list_delete(idmapped->mnt_list);
+	idmapped->mnt_list = NULL;
+}
+
+static compartment_module_t u_idmapped_module = {
+	.name = MOD_NAME,
+	.compartment_new = u_idmapped_new,
+	.compartment_free = u_idmapped_free,
+	.compartment_destroy = NULL,
+	.start_post_clone_early = NULL,
+	.start_child_early = NULL,
+	.start_pre_clone = u_idmapped_start_pre_clone,
+	.start_post_clone = NULL,
+	.start_pre_exec = u_idmapped_start_pre_exec,
+	.start_post_exec = NULL,
+	.start_pre_exec_child_early = NULL,
+	.start_child = NULL,
+	.start_pre_exec_child = NULL,
+	.stop = NULL,
+	.cleanup = u_idmapped_cleanup,
+	.join_ns = NULL,
+};
+
+static void INIT
+u_idmapped_init(void)
+{
+	// register this module in unit.c
+	unit_register_compartment_module(&u_idmapped_module);
+}

daemon/Makefile
@@ -126,6 +126,7 @@ SRC_FILES := main.c \
 
 SRC_UMODULES += \
 	u_user.c \
+	u_idmapped.c \
 	u_service.c \
 	u_net.c \
 	u_time.c

commit c236e0d355f9f36cfeeb61245ac4712c7ef992e7

daemon/u_idmapped: Free mnt_list entries in u_idmapped_free
u_idmapped_add_mount() appends a freshly-allocated u_idmapped_mnt_t
(struct + two mem_strdup'd paths) to idmapped->mnt_list for every
mount the unit is configured with (e.g. for SCD: /run/socket,
/data/logs, /data/cml/tokens). u_idmapped_free() released only the
u_idmapped_t wrapper, leaving every mount entry, both strdups and the
list_t nodes leaked on every compartment teardown.

Walk mnt_list and call u_idmapped_mnt_free() per entry, then
list_delete() the list before freeing the wrapper.

Fixes: 5b3602ec0226 ("daemon/u_idmapped: Introduce u_idmapped module")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/u_idmapped.c
@@ -123,6 +123,10 @@ u_idmapped_free(void *idmappedp)
 	u_idmapped_t *idmapped = idmappedp;
 	ASSERT(idmapped);
 
+	for (list_t *l = idmapped->mnt_list; l; l = l->next)
+		u_idmapped_mnt_free(l->data);
+	list_delete(idmapped->mnt_list);
+
 	mem_free0(idmapped);
 }
 