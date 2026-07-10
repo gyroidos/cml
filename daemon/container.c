gyroidos/cml

commit beb7f2c76afdd9053f2249f9909f931ea823cffb

whitelist based configuration rules to allow containers (exclusive or non-exclusive) access to hardware devices

daemon/container.c
@@ -153,6 +153,12 @@ struct container {
 	/* list of enabled features checked against during overlay mounts */
 	list_t *feature_enabled_list;
 
+	// list of allowed devices (rules)
+	char **device_allowed_list;
+
+	// list of exclusively assigned devices (rules)
+	char **device_assigned_list;
+
 	char *dns_server;
 	time_t time_started;
 	time_t time_created;
@@ -232,7 +238,9 @@ container_new_internal(
 	bool allow_autostart,
 	list_t *feature_enabled,
 	const char *dns_server,
-	list_t *net_ifaces
+	list_t *net_ifaces,
+	char **allowed_devices,
+	char **assigned_devices
 )
 {
 	container_t *container = mem_new0(container_t, 1);
@@ -311,7 +319,7 @@ container_new_internal(
 	for (list_t* elem = net_ifaces; elem != NULL; elem = elem->next) {
                 DEBUG("List element in net_ifaces: %s", (char*)(elem->data));
 		nw_mv_name_list = list_append(nw_mv_name_list, mem_strdup(elem->data));
-        } 
+        }
 
 	container->net = c_net_new(container, ns_net, nw_name_list, nw_mv_name_list, adb_port);
 	if (!container->net) {
@@ -350,6 +358,8 @@ container_new_internal(
 
 	container->time_started = -1;
 	container->time_created = container_get_creation_time_from_file(container);
+	container->device_allowed_list = allowed_devices;
+	container->device_assigned_list = assigned_devices;
 
 	return container;
 
@@ -398,6 +408,8 @@ container_new(const char *store_path, const uuid_t *existing_uuid, const char *c
 	uint64_t new_guestos_version;
 	bool allow_autostart;
 	bool priv;
+	char** allowed_devices;
+	char** assigned_devices;
 
 	if (!existing_uuid) {
 		uuid = uuid_new(NULL);
@@ -482,8 +494,11 @@ container_new(const char *store_path, const uuid_t *existing_uuid, const char *c
 
 	const char* dns_server = (container_config_get_dns_server(conf)) ? container_config_get_dns_server(conf) : cmld_get_device_host_dns();
 
+	allowed_devices = container_config_get_dev_allow_list_new(conf);
+	assigned_devices = container_config_get_dev_assign_list_new(conf);
 	container_t *c = container_new_internal(uuid, name, ns_usr, ns_net, priv, os, config_filename,
-			images_dir, mnt, ram_limit, color, adb_port, allow_autostart, feature_enabled, dns_server, net_ifaces);
+			images_dir, mnt, ram_limit, color, adb_port, allow_autostart, feature_enabled,
+			dns_server, net_ifaces, allowed_devices, assigned_devices);
 	if (c)
 		container_config_write(conf);
 
@@ -547,7 +562,8 @@ container_free(container_t *container) {
 		mem_free(container->phone_number);
 	if (container->dns_server)
 		mem_free(container->dns_server);
-
+	mem_free(container->device_allowed_list);
+	mem_free(container->device_assigned_list);
 	mem_free(container);
 }
 
@@ -1888,4 +1904,14 @@ container_remove_net_iface(container_t *container, const char *iface, bool persi
 	container_config_write(conf);
 	container_config_free(conf);
 	return 0;
+}
+
+const char **
+container_get_dev_allow_list(const container_t *container){
+	return container->device_allowed_list;
+}
+
+const char **
+container_get_dev_assign_list(const container_t *container){
+	return container->device_assigned_list;
 }

daemon/c_cgroups.c
@@ -54,6 +54,15 @@
 
 #define CGROUPS_FREEZER_RETRIES		CGROUPS_FREEZER_TIMEOUT/CGROUPS_FREEZER_RETRY_INTERVAL
 
+ /* List of 2-element int arrays, representing maj:min of devices allowed to be used in the running containers.
+  * wildcard '*' is mapped to -1 */
+list_t* global_allowed_devs_list = NULL;
+
+/* List of 2-element int arrays, representing maj:min of devices exclusively assigned to the running containers.
+  * wildcard '*' is mapped to -1 */
+list_t* global_assigned_devs_list = NULL;
+
+
 struct c_cgroups {
 	container_t *container; // weak reference
 	char *cgroup_path;
@@ -61,6 +70,10 @@ struct c_cgroups {
 	event_inotify_t *inotify_freezer_state;
 	event_timer_t *freeze_timer; /* timer to handle a container freeze timeout */
 	int freezer_retries;
+	list_t* assigned_devs; /* list of 2 element int arrays, representing maj:min of exclusively assigned devices.
+				  wildcard '*' is mapped to -1 */
+	list_t* allowed_devs; /* list of 2 element int arrays, representing maj:min of devices allowed to be accessed.
+				  wildcard '*' is mapped to -1 */
 };
 
 c_cgroups_t *
@@ -72,7 +85,8 @@ c_cgroups_new(container_t *container)
 
 	cgroups->inotify_freezer_state = NULL;
 	cgroups->freeze_timer = NULL;
-
+	cgroups->assigned_devs = NULL;
+	cgroups->allowed_devs = NULL;
 	return cgroups;
 }
 
@@ -158,24 +172,126 @@ static const char *c_cgroups_devices_generic_whitelist[] = {
 	NULL
 };
 
+/* extracts major and minor device numbers from a rule as an int array {major, minor} */
+static int* c_cgroups_dev_from_rule(const char* rule){
+	int* ret = mem_new0(int, 2);
+	char* rule_cp = mem_strdup(rule);
+	char* pointer;
+	char* type;
+	char* dev;
+
+	type = strtok_r(rule_cp, " ", &pointer);
+	dev = strtok_r(NULL, " ", &pointer);
+	pointer = NULL;
+	char* maj_str = strtok_r(dev, ":", &pointer);
+	char* min_str = strtok_r(NULL, ":", &pointer);
+	ret[0] = -1;
+	ret[1] = -1;
+	if (strncmp("*", maj_str, 1)) ret[0] = atoi(maj_str);
+	if (strncmp("*", min_str, 1)) ret[1] = atoi(min_str);
+	mem_free(rule_cp);
+	return ret;
+}
+
+static void
+c_cgroups_list_add(list_t **list, const int* dev)
+{
+	int* dev_copy = mem_new0(int, 2);
+        memcpy(dev_copy, dev, sizeof(int) * 2);
+	*list = list_append(*list, dev_copy);
+}
+
+static void
+c_cgroups_list_remove(list_t **list, const int* dev)
+{
+	for (list_t* elem = *list; elem != NULL; elem = elem->next) {
+		int* dev_elem = (int*) elem->data;
+		if ( (dev_elem[0] == dev[0]) && (dev_elem[1] == dev[1]) ) {
+			*list = list_unlink(*list, elem);
+			break;
+		}
+	}
+}
+
+static int
+c_cgroups_list_contains_match(const list_t* list, const int* dev)
+{
+	for (const list_t* elem = list;  elem != NULL; elem = elem->next) {
+		const int* dev_elem = (const int*) elem->data;
+		if ( (dev_elem[0] == -1) || (dev[0] == -1) ) return 1;
+		if ( dev_elem[0] != dev[0] ) continue;
+		if ( (dev[1] == -1) || (dev_elem[1] == -1) || (dev[1] == dev_elem[1]) ) return 1;
+	}
+	return 0;
+}
+
+static void
+c_cgroups_add_allowed(c_cgroups_t *cgroups, const int* dev)
+{
+	c_cgroups_list_add(&global_allowed_devs_list, dev);
+	c_cgroups_list_add(&cgroups->allowed_devs, dev);
+}
+
+static void
+c_cgroups_add_assigned(c_cgroups_t *cgroups, const int* dev)
+{
+	c_cgroups_list_add(&global_assigned_devs_list, dev);
+	c_cgroups_list_add(&cgroups->assigned_devs, dev);
+}
+
+static int
+c_cgroups_allow_rule(c_cgroups_t *cgroups, const char *rule)
+{
+	char *path = mem_printf("%s/devices.allow", cgroups->cgroup_path);
+	if (file_write(path, rule, -1) == -1) {
+		ERROR_ERRNO("Failed to write to %s", path);
+		mem_free(path);
+		return -1;
+	}
+
+	mem_free(path);
+	return 0;
+}
+
 int
 c_cgroups_devices_allow(c_cgroups_t *cgroups, const char *rule)
 {
 	ASSERT(cgroups);
 	ASSERT(rule);
 
-	char *path = mem_printf("%s/devices.allow", cgroups->cgroup_path);
+	int* dev = c_cgroups_dev_from_rule(rule);
+	if ( c_cgroups_list_contains_match(global_assigned_devs_list, dev) ){
+		WARN("Unable to allow rule %s: device busy (already assigned to another container)", rule);
+		mem_free(dev);
+		return 0;
+	}
+	c_cgroups_add_allowed(cgroups, dev);
+	mem_free(dev);
+	return c_cgroups_allow_rule(cgroups, rule);
+}
 
-	if (file_write(path, rule, -1) == -1) {
-		ERROR_ERRNO("Failed to write to %s", path);
-		goto error;
+int
+c_cgroups_devices_assign(c_cgroups_t *cgroups, const char *rule)
+{
+	ASSERT(cgroups);
+	ASSERT(rule);
+
+	int* dev = c_cgroups_dev_from_rule(rule);
+	if ( c_cgroups_list_contains_match(global_allowed_devs_list, dev) ){
+		ERROR("Unable to exclusively assign device according to rule %s: device busy (already available to another container)", rule);
+		mem_free(dev);
+		return -1;
 	}
 
-	mem_free(path);
+	if ( c_cgroups_allow_rule(cgroups, rule) < 0 ) {
+		mem_free(dev);
+		return -1;
+	}
+
+	c_cgroups_add_allowed(cgroups, dev);
+	c_cgroups_add_assigned(cgroups, dev);
+	mem_free(dev);
 	return 0;
-error:
-	mem_free(path);
-	return -1;
 }
 
 int
@@ -234,6 +350,20 @@ c_cgroups_devices_deny_list(c_cgroups_t *cgroups, const char **list)
 	return 0;
 }
 
+static int
+c_cgroups_devices_assign_list(c_cgroups_t *cgroups, const char** list)
+{
+	if (!list) return 0;
+
+	for (int i = 0; list[i]; i++) {
+		if ( c_cgroups_devices_assign(cgroups, list[i]) < 0) {
+			return -1;
+		}
+	}
+	return 0;
+}
+
+
 int
 c_cgroups_devices_allow_all(c_cgroups_t *cgroups)
 {
@@ -249,6 +379,7 @@ c_cgroups_devices_deny_all(c_cgroups_t *cgroups)
 
 	return c_cgroups_devices_deny(cgroups, "a");
 }
+
 
 int
 c_cgroups_devices_allow_audio(c_cgroups_t *cgroups)
@@ -322,6 +453,25 @@ c_cgroups_devices_init(c_cgroups_t *cgroups)
 			return -1;
 		}
 	}
+
+
+	/* allow container specific device whitelist */
+	const char** container_dev_whitelist = container_get_dev_allow_list(cgroups->container);
+	if (c_cgroups_devices_allow_list(cgroups, container_dev_whitelist) < 0) {
+		ERROR("Could not initialize container specific device whitelist for container %s",
+			container_get_description(cgroups->container));
+		return -1;
+	}
+	DEBUG("Applied containers whitelist");
+
+	/* apply container specific exclusive device assignment */
+	const char** container_dev_assignlist = container_get_dev_assign_list(cgroups->container);
+	if (c_cgroups_devices_assign_list(cgroups, container_dev_assignlist) < 0) {
+		ERROR("Could not initialize container specific device assignmet list for container %s",
+			container_get_description(cgroups->container));
+		return -1;
+	}
+	DEBUG("Applied containers assign list");
 
 	/* Print out the initialized devices whitelist */
 	char *list_path = mem_printf("%s/devices.list", cgroups->cgroup_path);
@@ -722,5 +872,18 @@ c_cgroups_cleanup(c_cgroups_t *cgroups)
 		}
 		INFO("Successfully removed cgroup for container %s", container_get_description(cgroups->container));
 	}
-}
 
+	/* free assigned devices */
+	for (list_t* elem = cgroups->assigned_devs; elem != NULL; elem = elem->next) {
+		int* dev_elem = (int*) elem->data;
+		c_cgroups_list_remove(&global_assigned_devs_list, dev_elem);
+	}
+	/* free allowed devices */
+	for (list_t* elem = cgroups->allowed_devs; elem != NULL; elem = elem->next) {
+		int* dev_elem = (int*) elem->data;
+		c_cgroups_list_remove(&global_allowed_devs_list, dev_elem);
+	}
+
+
+	list_delete(cgroups->assigned_devs);
+}

daemon/cmld.c
@@ -739,7 +739,7 @@ cmld_init_a0(const char *path, const char *c0os)
 
 	container_t *new_a0 = container_new_internal(a0_uuid, "a0", false, a0_ns_net, privileged, a0_os, NULL,
 			      a0_images_folder, a0_mnt, a0_ram_limit, 0xffffff00, 0, false, NULL,
-			      cmld_get_device_host_dns(), NULL);
+			      cmld_get_device_host_dns(), NULL, NULL, NULL);
 
 	/* depending on the storage of the a0 pointer, do ONE of the following: */
 	/* store a0 as first element of the cmld_containers_list */

daemon/container.h
@@ -138,7 +138,9 @@ container_new_internal(
 	bool allow_autostart,
 	list_t *feature_enabled,
 	const char *dns_server,
-	list_t *net_ifaces
+	list_t *net_ifaces,
+	char **allowed_devices,
+	char **assigned_devices
 );
 
 /**
@@ -607,6 +609,10 @@ container_add_net_iface(container_t *container, const char *iface, bool persiste
  */
 int
 container_remove_net_iface(container_t *container, const char *iface, bool persistent);
+
+const char **container_get_dev_allow_list(const container_t *container);
+
+const char **container_get_dev_assign_list(const container_t *container);
 
 time_t
 container_get_uptime(const container_t *container);

daemon/container.proto
@@ -64,6 +64,12 @@ message ContainerConfig {
 
 	// a list of network interfaces assigned to this container
 	repeated string net_ifaces = 23;
+
+	// a list of devices explicitely allowed for this container
+	repeated string allow_dev = 25;
+
+	// a list of devices exclusively assigned to this container
+	repeated string assign_dev = 26;
 }
 
 /**

daemon/container_config.c
@@ -429,6 +429,34 @@ container_config_get_net_ifaces_list_new(const container_config_t *config)
 	}
 	return net_ifaces_list;
 }
+
+char**
+container_config_get_dev_allow_list_new(const container_config_t *config)
+{
+	ASSERT(config);
+	ASSERT(config->cfg);
+
+	char** dev_whitelist = mem_new0(char *, config->cfg->n_allow_dev + 1);
+	for (size_t i = 0; i < config->cfg->n_allow_dev; i++) {
+		dev_whitelist[i] = mem_strdup(config->cfg->allow_dev[i]);
+	}
+	dev_whitelist[config->cfg->n_allow_dev] = NULL;
+	return dev_whitelist;
+}
+
+char**
+container_config_get_dev_assign_list_new(const container_config_t *config)
+{
+	ASSERT(config);
+	ASSERT(config->cfg);
+	char** dev_assign_list = mem_new0(char *, config->cfg->n_assign_dev + 1);
+	for (size_t i = 0; i < config->cfg->n_assign_dev; i++) {
+		dev_assign_list[i] = mem_strdup(config->cfg->assign_dev[i]);
+	}
+	dev_assign_list[config->cfg->n_assign_dev] = NULL;
+	return dev_assign_list;
+}
+
 #if 0
 bool
 container_config_get_autostart(container_config_t *config)

daemon/container_config.h
@@ -180,6 +180,20 @@ list_t*
 container_config_get_net_ifaces_list_new(const container_config_t *config);
 
 /**
+ * Provides the list of hardware devices explicitely allowed for the container from the container's config file
+ */
+
+char **
+container_config_get_dev_allow_list_new(const container_config_t *config);
+
+/**
+ * Provides the list of hardware devices exclusively assigned to the container from the container's config file
+ */
+
+char **
+container_config_get_dev_assign_list_new(const container_config_t *config);
+
+/**
  * Get the dns_server ip addr set for the container.
  */
 const char *
@@ -202,5 +216,6 @@ void container_config_append_net_ifaces(const container_config_t *config, const 
  */
 void
 container_config_remove_net_ifaces(const container_config_t *config, const char *iface);
+
 
 #endif /* C_CONFIG_H */

daemon/hw_ppc.c
@@ -39,24 +39,21 @@
 
 /******************************************************************************/
 static const char *hw_ppc_devices_whitelist_base[] = {
-	"a *:* rwm", // allow all FIXME
-	NULL
+	NULL // deny all
 };
 
 /**
  * List of devices allowed additionally in privileged containers.
  */
 static const char *hw_ppc_devices_whitelist_priv[] = {
-	"a *:* rwm", // allow all FIXME
-	NULL
+	NULL // deny all
 };
 
 /**
  * List of audio devices
  */
 static const char *hw_ppc_devices_whitelist_audio[] = {
-	"a *:* rwm", // allow all FIXME
-	NULL
+	NULL // deny all
 };
 
 static char *hw_ppc_serial_number = "00000000";

commit 595b18da0e44dfc9f07b5a3e56b38619e18dd441

daemon/container: Free device_allowed/assigned_list entries on cleanup
container_config_get_dev_allow_list_new() and
container_config_get_dev_assign_list_new() return NULL-terminated
char ** arrays where both the outer array and every entry are
heap-allocated (mem_new0 + mem_strdup per element).

container_free() only released the outer array via mem_free0(), so
every per-entry mem_strdup() (one per rule in <allow_dev> /
<assign_dev>) leaked when the container was destroyed.

Walk each list and mem_free0() the entry strings before releasing the
array. The trailing NULL terminator placed by the _new() helpers makes
the for-loop bound implicit.

Fixes: beb7f2c76afd ("whitelist based configuration rules to allow containers (exclusive or non-exclusive) access to hardware devices")
Assisted-by: Claude:claude-opus-4-7
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/container.c
@@ -254,10 +254,16 @@ container_free(container_t *container)
 	}
 	list_delete(container->module_allowed_list);
 
-	if (container->device_allowed_list)
+	if (container->device_allowed_list) {
+		for (char **entry = container->device_allowed_list; *entry; entry++)
+			mem_free0(*entry);
 		mem_free0(container->device_allowed_list);
-	if (container->device_assigned_list)
+	}
+	if (container->device_assigned_list) {
+		for (char **entry = container->device_assigned_list; *entry; entry++)
+			mem_free0(*entry);
 		mem_free0(container->device_assigned_list);
+	}
 
 	for (list_t *l = container->usbdev_list; l; l = l->next) {
 		container_usbdev_t *usbdev = l->data;