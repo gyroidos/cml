gyroidos/cml

commit 5296d61cc43e471e6626e0c140c9b8ad0fd70e0c

daemon/crypto: corrected HASH_BUF related protobuf messages handling
We missed to set the filed has_hash_buf of the corresponding protobuf
message which is sent to the scd. This lead to empty hash buffer to
be transmitted. Further, in the response handler for HASH_OK and
HASH_ERR call the correct has complete callback depending on
hash_buf_complete or hash_complete is set in the crypto_callback_task.

This fixes following error in scd:

  [6] <FATAL> ssl_util.c+1151: Assertion `buf_to_hash' failed
  [6] <FATAL> ssl_util.c+1151: Aborting...
  [1] <WARN>  control.c+531: asyn crypto handler reurned with error

Fixes: 814f6a8e1f0a ("daemon/crypto: provide crypto_hash_buf for hashing buffers")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/crypto.c
@@ -271,8 +271,13 @@ crypto_cb(int fd, unsigned events, event_io_t *io, void *data)
 
 				TRACE("Received hash for file %s: %s",
 				      task->hash_file ? task->hash_file : "<empty>", hash);
-				task->hash_complete(hash, task->hash_file, task->hash_algo,
-						    task->data);
+				if (task->hash_complete)
+					task->hash_complete(hash, task->hash_file, task->hash_algo,
+							    task->data);
+				if (task->hash_buf_complete)
+					task->hash_buf_complete(hash, task->hash_buf,
+								task->hash_buf_len, task->hash_algo,
+								task->data);
 				if (hash != NULL) {
 					mem_free0(hash);
 				}
@@ -281,6 +286,12 @@ crypto_cb(int fd, unsigned events, event_io_t *io, void *data)
 			ERROR("Missing hash_value in CRYPTO_HASH_OK response!"); // fallthrough
 		case TOKEN_TO_DAEMON__CODE__CRYPTO_HASH_ERROR:
 			task->hash_complete(NULL, task->hash_file, task->hash_algo, task->data);
+			if (task->hash_complete)
+				task->hash_complete(NULL, task->hash_file, task->hash_algo,
+						    task->data);
+			if (task->hash_buf_complete)
+				task->hash_buf_complete(NULL, task->hash_buf, task->hash_buf_len,
+							task->hash_algo, task->data);
 			break;
 
 		// deal with CRYPTO_VERIFY_* cases
@@ -459,10 +470,11 @@ crypto_hash_buf(const unsigned char *buf, size_t buf_len, crypto_hashalgo_t hash
 	out.code = DAEMON_TO_TOKEN__CODE__CRYPTO_HASH_BUF;
 	out.has_hash_algo = true;
 	out.hash_algo = crypto_hashalgo_to_proto(hashalgo);
+	out.has_hash_buf = true;
 	out.hash_buf.data = task->hash_buf;
 	out.hash_buf.len = task->hash_buf_len;
 
-	TRACE("Requesting scd to hash file at %s", task->hash_file);
+	TRACE("Requesting scd to hash buf of len %zu", task->hash_buf_len);
 
 	if (crypto_send_msg(&out, task) < 0) {
 		crypto_callback_task_free(task);

commit 4c3f7126b5e75c2f8c11c066c34b3cb5963b23aa

daemon/crypto: Fix Use-After-Free Bug in cryto_cb
In crypto_cb on a CRYPTO_HASH_ERROR, we call task->hash_complete.
task->hash_complete is called twice, first without null check,
then with null check. After the first call of task->hash_complete,
task->data is freed and should no longer be used.
task->hash_complete is then called again,
yet due to task->data being freed causing a SEGV.
Therefore remove the first call to task->hash_complete.

Fixes: 5296d61cc43e ("daemon/crypto: corrected HASH_BUF related protobuf messages handling")
Signed-off-by: Fabian Rauschecker <fabian.rauschecker@aisec.fraunhofer.de>

daemon/crypto.c
@@ -285,7 +285,6 @@ crypto_cb(int fd, unsigned events, event_io_t *io, void *data)
 			}
 			ERROR("Missing hash_value in CRYPTO_HASH_OK response!"); // fallthrough
 		case TOKEN_TO_DAEMON__CODE__CRYPTO_HASH_ERROR:
-			task->hash_complete(NULL, task->hash_file, task->hash_algo, task->data);
 			if (task->hash_complete)
 				task->hash_complete(NULL, task->hash_file, task->hash_algo,
 						    task->data);