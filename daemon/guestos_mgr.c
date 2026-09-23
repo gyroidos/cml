gyroidos/cml

commit 20c13ff0d68305bbf4fd20580b7e52b3bab46e9e

daemon: extended control interface by container trust level
This patch transmits a trust level (SIGNED|LOCALLY_SIGNED|UNSIGNED)
based on the verify_result of the guestos signature verification
process.

daemon/guestos_mgr.c
@@ -85,6 +85,7 @@ static int
 guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED void *data)
 {
 	int res = 0; // counter
+	guestos_verify_result_t guestos_verified = GUESTOS_UNSIGNED;
 
 	char *dir = mem_printf("%s/%s", path, name);
 	if (!file_is_dir(dir))
@@ -99,22 +100,25 @@ guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED 
 
 	switch (verify_result) {
 	case VERIFY_GOOD:
+		guestos_verified = GUESTOS_SIGNED;
 		INFO("Signature of GuestOS OK (GOOD)");
 		break;
 	case VERIFY_LOCALLY_SIGNED:
+		guestos_verified = GUESTOS_LOCALLY_SIGNED;
 		if (guestos_mgr_allow_locally_signed) {
 			INFO("Signature of GuestOS OK (locally signed)");
 			break;
 		}
 		// fallthrough
 	default:
+		guestos_verified = GUESTOS_UNSIGNED;
 		ERROR("Signature verification failed (%d) while loading GuestOS config %s, skipping.",
 				verify_result, cfg_file);
 		res = 1;
 		goto cleanup_files;
 	}
 
-	if (guestos_mgr_add_from_file(cfg_file) < 0) {
+	if (guestos_mgr_add_from_file(cfg_file, guestos_verified) < 0) {
 		WARN("Could not add guest operating system from file %s.", cfg_file);
 	} else  {
 		res = 1;
@@ -166,7 +170,7 @@ guestos_mgr_init(const char *path, bool allow_locally_signed)
 }
 
 int
-guestos_mgr_add_from_file(const char *file)
+guestos_mgr_add_from_file(const char *file, guestos_verify_result_t verify_result)
 {
 	ASSERT(file);
 
@@ -175,6 +179,7 @@ guestos_mgr_add_from_file(const char *file)
 		return -1;
 	}
 
+	guestos_set_verify_result(os, verify_result);
 	guestos_list = list_append(guestos_list, os);
 
 	return 0;

daemon/container.proto
@@ -116,6 +116,12 @@ enum ContainerConnectivity {
 	MOBILE_AND_WIFI = 4;
 }
 
+enum ContainerTrust {
+	SIGNED = 1;
+	LOCALLY_SIGNED = 2;
+	UNSIGNED = 3;
+}
+
 /**
  * Represents the status of a single container.
  */
@@ -126,5 +132,7 @@ message ContainerStatus {
 	required ContainerState state = 4;
 	required uint64 uptime = 5;
 	required uint64 created = 6;
+	required string guestos = 7;
+	required ContainerTrust trust_level = 8;
 	/* TBD more state values */
 }

daemon/control.c
@@ -283,6 +283,19 @@ control_container_status_new(const container_t *container)
 	c_status->state = control_container_state_to_proto(container_get_state(container));
 	c_status->uptime = container_get_uptime(container);
 	c_status->created = container_get_creation_time(container);
+	c_status->guestos = mem_strdup(guestos_get_name(container_get_guestos(container)));
+
+	switch (guestos_get_verify_result(container_get_guestos(container))) {
+	case GUESTOS_SIGNED:
+		c_status->trust_level = CONTAINER_TRUST__SIGNED;
+		break;
+	case GUESTOS_LOCALLY_SIGNED:
+		c_status->trust_level = CONTAINER_TRUST__LOCALLY_SIGNED;
+		break;
+	default:
+		c_status->trust_level = CONTAINER_TRUST__UNSIGNED;
+	}
+
 	return c_status;
 }
 
@@ -298,6 +311,7 @@ control_container_status_free(ContainerStatus *c_status)
 	IF_NULL_RETURN(c_status);
 	mem_free(c_status->name);
 	mem_free(c_status->uuid);
+	mem_free(c_status->guestos);
 	mem_free(c_status);
 }
 

daemon/guestos.c
@@ -46,6 +46,7 @@ struct guestos {
 	char *sig_file;     ///< config signature file name
 	char *cert_file;     ///< config certificate file name
 	guestos_config_t *cfg;  ///< pointer to GuestOS config struct
+	guestos_verify_result_t verify_result; ///< result of guestos signature verification
 
 	bool downloading;   ///< indicates download in progress
 };
@@ -1103,4 +1104,19 @@ guestos_get_feature_install_guest(const guestos_t *os)
 {
 	ASSERT(os);
 	return guestos_config_get_feature_install_guest(os->cfg);
+}
+
+void
+guestos_set_verify_result(guestos_t *os,
+			guestos_verify_result_t verify_result)
+{
+	ASSERT(os);
+	os->verify_result = verify_result;
+}
+
+guestos_verify_result_t
+guestos_get_verify_result(const guestos_t *os)
+{
+	ASSERT(os);
+	return os->verify_result;
 }

daemon/guestos.h
@@ -48,7 +48,6 @@
  */
 typedef struct guestos guestos_t;
 
-
 /******************************************************************************/
 
 /**
@@ -280,6 +279,22 @@ guestos_get_feature_install_guest(const guestos_t *os);
 
 bool
 guestos_is_privileged(const guestos_t *os);
+
+/**
+ * Result of guestos config verification process.
+ */
+typedef enum guestos_verify_result {
+	GUESTOS_SIGNED,
+	GUESTOS_LOCALLY_SIGNED,
+	GUESTOS_UNSIGNED,
+} guestos_verify_result_t;
+
+void
+guestos_set_verify_result(guestos_t *os,
+		guestos_verify_result_t verify_result);
+
+guestos_verify_result_t
+guestos_get_verify_result(const guestos_t *os);
 
 #endif /* GUESTOS_H */
 

daemon/guestos_mgr.h
@@ -48,12 +48,15 @@ int
 guestos_mgr_init(const char *path, bool allow_locally_signed);
 
 /**
- * Add an operating system WITHOUT checking its signature.
+ * Add an operating system WITHOUT checking its signature. The verification
+ * should already made elsewhere and the result must be provided by parameter
+ * verify_result.
  * @param file The name of the guest OS config file.
+ * @param verify_result The result of the pre-required verification process.
  * @return 0 if the guest OS was successfully added, -1 on error.
  */
 int
-guestos_mgr_add_from_file(const char *file);
+guestos_mgr_add_from_file(const char *file, guestos_verify_result_t verify_result);
 
 /**
  * Delete an operating system persistently from disk, i.e. remove its configuration and

daemon/main.c
@@ -36,7 +36,6 @@
 #include "cmld.h"
 #include "hardware.h"
 #include "power.h"
-#include "cmld.h"
 
 #include <sys/stat.h>
 #include <sys/types.h>

commit 624d6f15693f5e3e5ecbc19a7d518c958b05c3ab

daemon/guestos_mgr: Drop redundant guestos_verified assignment
guestos_verified is initialized to GUESTOS_UNSIGNED and the error
branch jumps to the cleanup before the value is ever read, so the
assignment was redundant. Just drop it.

Fixes following warning from clang static analyzer:

  guestos_mgr.c:109:3: warning: Value stored to 'guestos_verified' \
    is never read [deadcode.DeadStores]
    109 |                 guestos_verified = GUESTOS_UNSIGNED;
        |                 ^                  ~~~~~~~~~~~~~~~~

Fixes: 20c13ff0d683 ("daemon: extended control interface by container trust level")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/guestos_mgr.c
@@ -106,8 +106,6 @@ guestos_mgr_load_operatingsystems_cb(const char *path, const char *name, UNUSED 
 		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-locally-signed", name, 0);
 		// fallthrough
 	default:
-		guestos_verified = GUESTOS_UNSIGNED;
-
 		audit_log_event(NULL, FSA, CMLD, GUESTOS_MGMT, "verify-failed", name, 0);
 
 		ERROR("Signature verification failed (%d) while loading GuestOS config %s, skipping.",