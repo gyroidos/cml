gyroidos/cml

commit 2f18e7e7f27d205238db7e8875fd13af0ce20b70

c_run subsystem

daemon/c_run.c
@@ -1,0 +1,367 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pty.h>
+#include <sys/wait.h>
+#include <sys/syscall.h>
+#include <sys/socket.h>
+
+//#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
+#include <macro.h>
+
+#include "common/mem.h"
+#include "common/dir.h"
+#include "common/file.h"
+#include "common/fd.h"
+#include "hardware.h"
+#include "container.h"
+#include "c_run.h"
+
+struct c_run {
+	container_t *container;
+	int console_sock_cmld;
+	int console_sock_container;
+	pid_t active_exec_pid;
+};
+
+c_run_t *
+c_run_new(container_t *container)
+{
+	c_run_t *run = mem_new0(c_run_t, 1);
+	run->container = container;
+	run->console_sock_cmld = -1;
+	run->console_sock_container = -1;
+	run->active_exec_pid = -1;
+	return run;
+}
+
+void
+c_run_free(c_run_t *run)
+{
+	ASSERT(run);
+	mem_free(run);
+}
+
+void
+c_run_cleanup(c_run_t *run)
+{
+	ASSERT(run);
+
+	if (run->active_exec_pid != -1) {
+		if (! kill(run->active_exec_pid, SIGKILL)) {
+			// Happens if called from sigchld_handler
+			DEBUG("Failed to kill process inside container");
+		}
+
+		shutdown(run->console_sock_container, SHUT_WR);
+		shutdown(run->console_sock_cmld, SHUT_WR);
+		run->active_exec_pid = -1;
+	}
+}
+
+int
+c_run_get_console_sock_cmld(const c_run_t * run)
+{
+	ASSERT(run);
+	return run->console_sock_cmld;
+}
+
+int
+c_run_get_active_exec_pid(const c_run_t * run)
+{
+	ASSERT(run);
+	return run->active_exec_pid;
+}
+
+int
+c_run_write_exec_input(c_run_t *run, char *exec_input)
+{
+	if (run->active_exec_pid != -1) {
+		return write(run->console_sock_cmld, exec_input, strlen(exec_input));
+	} else {
+		return -1;
+	}
+}
+
+static int
+setns(int fd, int nstype)
+{
+	return syscall(__NR_setns, fd, nstype);
+}
+
+#define MAX_NS 10
+int fd[MAX_NS] = { 0 };
+
+static int
+setns_cb(const char *path, const char *file, void *data)
+{
+	int *i = data;
+
+	char *ns_file = mem_printf("%s%s", path, file);
+	TRACE("Opening namespace file %s", ns_file);
+
+	if (*i >= MAX_NS) {
+		ERROR("Too many namespace files found in %s", path);
+		goto error;
+	}
+
+	fd[*i] = open(ns_file, O_RDONLY);
+	if (fd[*i] == -1) {
+		ERROR_ERRNO("Could not open namespace file %s", ns_file);
+		goto error;
+	}
+
+	*i = *i + 1;
+
+	mem_free(ns_file);
+	return EXIT_SUCCESS;
+
+error:
+	mem_free(ns_file);
+	exit(EXIT_FAILURE);
+}
+
+static int
+c_run_set_namespaces(pid_t pid)
+{
+	char *pid_string = mem_printf("%d", pid);
+
+	TRACE("Setting namespaces for pid %s", pid_string);
+
+	// set namespaces
+	char *folder = mem_printf("/proc/%d/ns/", pid);
+
+	int i = 0;
+	if (dir_foreach(folder, &setns_cb, &i)) {
+		FATAL("Could not traverse PID dir in procfs, wrong PID?");
+		goto error;
+	}
+
+	for (int j = 0; j < i; j++) {
+		if (setns(fd[j], 0) == -1) {	/* Join that namespace */
+			FATAL_ERRNO("Could not join namespace");
+			goto error;
+		}
+	}
+
+	TRACE("Successfully joined all namespaces");
+
+	mem_free(pid_string);
+	mem_free(folder);
+	return 0;
+
+error:
+	mem_free(pid_string);
+	mem_free(folder);
+	exit(EXIT_FAILURE);
+}
+
+static int
+do_exec (c_run_t *run, char *cmd, char **argv, int fd)
+{
+	if (-1 == dup2(fd, STDIN_FILENO)) {
+		ERROR("Failed to redirect stdin to cmld socket. Exiting...");
+		exit(EXIT_FAILURE);
+	}
+
+	if (-1 == dup2(fd, STDOUT_FILENO)) {
+		ERROR ("Failed to redirect stdout to cmld socket. Exiting...");
+		exit(EXIT_FAILURE);
+	}
+
+	if (-1 == dup2(fd, STDERR_FILENO)) {
+		ERROR("Failed to redirect stderr to cmld. Exiting...");
+		exit(EXIT_FAILURE);
+	}
+
+	container_add_pid_to_cgroups(run->container, getpid());
+
+	c_run_set_namespaces(container_get_pid(run->container));
+
+	container_set_cap_current_process(run->container);
+
+	TRACE("[EXEC: Executing command %s]", cmd);
+	execve(cmd, argv, NULL);
+
+	ERROR_ERRNO("Failed to execve");
+
+	exit(EXIT_FAILURE);
+}
+
+static void
+readloop(int from_fd, int to_fd)
+{
+	int count = 0;
+	char buf[1024];
+
+	while (1) {
+		if (0 < (count = read(from_fd, &buf, sizeof(buf)-1))) {
+			buf[count] = 0;
+			write(to_fd, buf, count+1);
+		} else {
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+static int
+c_run_prepare_pty(c_run_t *run, int create_pty, char *cmd, char **argv)
+{
+	//create new PTY
+	if (create_pty) {
+		TRACE("[EXEC] Starting to create new pty");
+
+		int pty_master = 0;
+
+		if (-1 == (pty_master = posix_openpt(O_RDWR))) {
+			ERROR("[EXEC] Failed to get new PTY master fd\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (0 != grantpt(pty_master)) {
+			printf("Failed to grantpt()\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (0 != unlockpt(pty_master)) {
+			printf("Failed to unlockpt()\n");
+			exit(EXIT_FAILURE);;
+		}
+
+		char pty_slave_name[50];
+		ptsname_r(pty_master, pty_slave_name, sizeof(pty_slave_name));
+		TRACE("Created new pty with fd: %i, slave name: %s\n", pty_master, pty_slave_name);
+
+		//fork childs for reading/writing PTY master fd
+		int pid = fork();
+
+		if (pid == -1) {
+			ERROR("Failed to fork(), exiting...\n");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			TRACE("[EXEC] Forked PTY master output reading process, PID: %d", getpid());
+			readloop(pty_master, run->console_sock_container);
+		} else {
+			// fork child to execute command
+			int pid2 = fork();
+
+			if (pid2 < 0) {
+				ERROR("[EXEC] Failed to fork child to excute command.");
+				exit(EXIT_FAILURE);
+			} else if (pid2 == 0) {
+				TRACE("[EXEC] Command executing child forked, PID: %d", getpid());
+				int pty_slave_fd = -1;
+
+				// open PTY slave
+				if (-1 == (pty_slave_fd = open(pty_slave_name, O_RDWR))) {
+						ERROR("Failed to open pty slave: %s\n", pty_slave_name);
+					shutdown(pty_master, SHUT_WR);
+					exit(EXIT_FAILURE);
+				}
+
+				const char *current_pty = ctermid(NULL);
+				DEBUG("[EXEC] Current controlling PTY is: %s\n", current_pty);
+
+				setsid();
+
+				if (-1 == ioctl(STDIN_FILENO, TIOCNOTTY)) {
+					TRACE("[EXEC] Failed to release current controlling pty.\n");
+				}
+
+				if (-1 == ioctl(pty_slave_fd, TIOCSCTTY, NULL)) {
+				ERROR("[EXEC] Failed to set controlling pty slave\n");
+					exit(EXIT_FAILURE);
+				}
+
+				// attach executed process to new PTY slave
+				do_exec(run, cmd, argv, pty_slave_fd);
+			} else {
+				while (1) {
+					TRACE("[EXEC] Starting console socket read loop in process %d", getpid());
+					readloop(run->console_sock_container, pty_master);
+				}
+			}
+
+			exit(EXIT_SUCCESS);
+		}
+
+		 exit(EXIT_SUCCESS);
+	} else {
+		// attach executed process directly to console socket
+		TRACE("Executing without PTY");
+		int ret = do_exec(run, cmd, argv, run->console_sock_container);
+		exit(ret);
+	}
+}
+
+int
+c_run_exec_process(c_run_t *run, int create_pty, char *cmd, ssize_t argc, char **argv)
+{
+	TRACE("Trying to excute command \"%s\" inside container", cmd);
+	ASSERT(cmd);
+
+	if (run->active_exec_pid != -1) {
+		TRACE("[C_RUN] Already executing command. Aborting request.");
+		return -1;
+	}
+
+	TRACE("Trying to excute command inside container");
+
+	/* Create a socketpair for communication with the console task */
+	int cfd[2];
+
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, cfd)) {
+		WARN("Could not create socketpair for communication with console task!");
+		exit(EXIT_FAILURE);
+	}
+
+	run->console_sock_cmld = cfd[0];
+	run->console_sock_container = cfd[1];
+
+	TRACE("Making cmld console socket nonblocking");
+	fd_make_non_blocking(run->console_sock_cmld);
+
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	} else if ( pid == 0 ) {
+		TRACE("Exec child forked, pid: %d", getpid());
+		//Add NULL pointer to end of argv
+		char ** exec_args = NULL;
+
+		if (argc > 0 && argv != NULL) {
+			ssize_t i = 0;
+			exec_args = mem_alloc(sizeof(char *) * (argc + 1));
+
+			while (i < argc)
+			{
+				TRACE("Got argument: %s", argv[i]);
+				exec_args[i] = mem_strdup(argv[i]);
+				i++;
+			}
+
+			exec_args[i] = NULL;
+		}
+
+		if (setpgid(0, container_get_pid(run->container)) < 0) {
+			ERROR("Failed to set GID of exec child to container PID: %d", container_get_service_pid(run->container));
+			exit(EXIT_FAILURE);
+		}
+
+		// close the cmld end of the console task sockets
+		close(run->console_sock_cmld);
+
+		c_run_prepare_pty(run, create_pty, cmd, exec_args);
+		// free argv
+		mem_free_array((void *) exec_args, argc);
+		exit(EXIT_FAILURE);
+	}
+
+	TRACE("Storing PID of active container: %d", pid);
+	run->active_exec_pid = pid;
+
+	return 0;
+}

common/mem.c
@@ -106,3 +106,22 @@ mem_free(void *mem)
 {
 	free(mem);
 }
+
+void
+mem_free_array(void **array, size_t size)
+{
+	if (array != NULL) {
+		size_t i = 0;
+		while (i < size) {
+			if (array[i] != NULL) {
+				DEBUG("[MEM] Freeing element %lu", i);
+				mem_free(array[i]);
+			}
+
+			i++;
+		}
+
+		DEBUG("[MEM] Freeing array");
+		mem_free(array);
+	}
+}

common/mem.h
@@ -122,6 +122,14 @@ void
 mem_free(void *mem);
 
 /**
+ * Frees the allocated memory of each array element and the array itself.
+ * @param array Array to be freed.
+ * @param size Array size.
+ */
+void
+mem_free_array(void **array, size_t size);
+
+/**
  * Convenience wrapper macro for mem_alloc which calculates
  * the correct size to be allocated and casts accordingly.
  */

control/control.c
@@ -27,16 +27,21 @@
 #include "control.pb-c.h"
 #endif
 
+//#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
 #include "common/macro.h"
 #include "common/mem.h"
 #include "common/protobuf.h"
 #include "common/sock.h"
 #include "common/file.h"
+#include "common/mem.h"
 
 #include <getopt.h>
 #include <stdbool.h>
+#include <termios.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define CONTROL_SOCKET SOCK_PATH(control)
 #define RUN_PATH "run"
@@ -327,17 +332,54 @@ int main(int argc, char *argv[])
                         msg.command = CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_UNASSIGNIFACE;
 		} else
 			ASSERT(false); // should never be reached
-	} else if (!strcasecmp(command, "run") ) {
-		if ( optind > argc-2 )
+	} else if (!strcasecmp(command, "run")) {
+		if (optind > argc-2)
 			print_usage(argv[0]);
+
 		has_response = true;
-		msg.command = CONTROLLER_TO_DAEMON__COMMAND__GET_CONTAINER_PID;
-		optind = argc-1;
+		msg.command = CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_EXEC_CMD;
+
+		int nopty = 0, argcount = 0;
+
+
+		msg.has_exec_pty = true;
+
+		if (! strcmp(argv[optind], "nopty")) {
+			TRACE("Got nopty option");
+			msg.exec_pty = 0;
+			nopty = 1;
+			optind++;
+		} else {
+			msg.exec_pty = 1;
+		}
+
+		if (optind > argc-2)
+			print_usage(argv[0]);
+
+		msg.exec_command = argv[optind];
+
+		if (optind < argc - 1) {
+			TRACE("[CLIENT] Allocating %d bytes for arguments", sizeof(char *) * argc);
+			msg.exec_args = mem_alloc(sizeof(char *) * argc);
+
+			while (optind < argc - 1) {
+				TRACE("[CLIENT] Parsing command arguments at index %d, optind: %d: %s",
+					argcount,optind, argv[optind]);
+				msg.exec_args[argcount] = mem_strdup(argv[optind]);
+
+				optind++;
+				argcount++;
+			}
+		}
+
+		TRACE("[CLIENT] Done parsing arguments, got %d argsuments", argcount);
+		msg.n_exec_args = argcount;
+		TRACE("after set n_exec_args");
 	} else
 		print_usage(argv[0]);
 
 	// need exactly one more argument (i.e. container string)
-	if (optind != argc-1)
+	if (optind != argc - 1)
 		print_usage(argv[0]);
 
 	msg.n_container_uuids = 1;
@@ -348,33 +390,118 @@ send_message:
 	sock = sock_connect(socket_file);
 	send_message(sock, &msg);
 
+	struct termios termios_before;
+	tcgetattr(STDIN_FILENO, &termios_before);
+
+	if (!strcasecmp(command, "run")) {
+		TRACE("[CLIENT] Processing response for run command");
+
+		if(msg.exec_pty) {
+			TRACE("[CLIENT] Setting termios for PTY");
+			struct termios termios_run = termios_before;
+			termios_run.c_cflag &= ~(ICRNL | IXON | IXOFF );
+			termios_run.c_oflag &= ~(OPOST);
+			termios_run.c_lflag &= ~(ISIG | ICANON | ECHO | ECHOCTL);
+			tcsetattr(STDIN_FILENO, TCSANOW, &termios_run);
+		}
+
+		//free exec arguments
+		TRACE("[CLIENT] Freeing %d args at %p", msg.n_exec_args, msg.exec_args);
+		mem_free_array((void *) msg.exec_args, msg.n_exec_args);
+		TRACE("[CLIENT] after free ");
+
+		int pid = fork();
+
+		if (pid == -1) {
+			ERROR("[CLIENT] Failed to fork(), exiting...\n");
+			goto exit;
+		} else if (pid == 0) {
+			TRACE("[CLIENT] User input reading child forked, PID: %i", getpid());
+
+			char buf[128];
+			unsigned int count;
+
+			while (1) {
+				TRACE("[CLIENT] Trying to read input for exec'ed process");
+
+				if ((count = read(STDIN_FILENO, buf, 127)) > 0) {
+
+					buf[count] = 0;
+
+					TRACE("[CLIENT] Got input for exec'ed process: %s", buf);
+
+					ControllerToDaemon inputmsg = CONTROLLER_TO_DAEMON__INIT;
+					inputmsg.container_uuids = mem_new(char*, 1);
+					inputmsg.container_uuids[0] = argv[optind];
+					inputmsg.n_container_uuids = 1;
+					inputmsg.command =
+					CONTROLLER_TO_DAEMON__COMMAND__CONTAINER_EXEC_INPUT;
+					inputmsg.exec_input = buf;
+
+					TRACE("[CLIENT] Sending input for exec'ed process in container %s", argv[optind]);
+
+					send_message(sock, &inputmsg);
+					mem_free(inputmsg.container_uuids);
+					TRACE("[CLIENT] Sent input to cmld");
+				}
+			}
+		} else {
+			TRACE("[CLIENT] Exec'ed process outputreceiving  child forked, PID: %i", getpid());
+
+			while (1) {
+				TRACE("[CLIENT] Waiting for command output message from cmld");
+				DaemonToController *resp = recv_message(sock);
+
+				TRACE("[CLIENT] Got message from exec'ed process\n");
+
+				unsigned int count = 0;
+				size_t written = 0, current = 0;
+
+				if (resp->code == DAEMON_TO_CONTROLLER__CODE__EXEC_OUTPUT) {
+					TRACE("[CLIENT] Message length; %d\n", resp->exec_output.len);
+					while (written < resp->exec_output.len) {
+						TRACE("[CLIENT] Writing exec output to stdout");
+						if(current = write(STDOUT_FILENO, resp->exec_output.data + written,
+									resp->exec_output.len - written)) {
+							written += current;
+						}
+
+						fflush(stdout);
+					}
+
+				} else if (resp->code == DAEMON_TO_CONTROLLER__CODE__EXEC_END) {
+					TRACE("[CLIENT] Got notification of command termination. Exiting...");
+
+					kill(pid, SIGTERM);
+					waitpid(pid, NULL, 0);
+					goto exit;
+				} else {
+					ERROR("Detected unexpected message from cmld. Exiting");
+					goto exit;
+				}
+			}
+		}
+		ERROR_ERRNO("[CLIENT] command \"run\" failed");
+	}
+
 	// recv response if applicable
-	if (has_response){
+	if (has_response) {
+		TRACE("[CLIENT] Awaiting response");
+
 		DaemonToController *resp = recv_message(sock);
 
-		// do command-specific response processing
-		if (!strcasecmp(command, "run") ){
-			pid_t pid = resp->container_pid;
-		        int run_argc = argc - 1;
-			char **run_argv = mem_new(char *, run_argc + 1);
-			run_argv[0] = mem_strdup(RUN_PATH);
-			run_argv[1] = mem_printf("%u", pid);
-			for ( int i = 2; i < run_argc; i++ ){
-				run_argv[i] = mem_strdup(argv[i]);
-			}
-			run_argv[run_argc] = NULL;
+		TRACE("[CLIENT] Got response. Processing");
 
-			// execute command
-			execvp(run_argv[0], run_argv);
-			ERROR_ERRNO("run failed");
-		} else {
-			// TODO for now just dump the response in text format
-			protobuf_dump_message(STDOUT_FILENO, (ProtobufCMessage *) resp);
-			protobuf_free_message((ProtobufCMessage *) resp);
-		}
+		// do command-specific response processing
+		// TODO for now just dump the response in text format
+		protobuf_dump_message(STDOUT_FILENO, (ProtobufCMessage *) resp);
+		protobuf_free_message((ProtobufCMessage *) resp);
 	}
+
+exit:
 	close(sock);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_before);
+	mem_free(msg.container_uuids);
 
 	return 0;
 }
-

daemon/c_cap.c
@@ -39,7 +39,7 @@
 } while (0)
 
 int
-c_cap_start_child(const container_t *container)
+c_cap_set_current_process(const container_t *container)
 {
 	///* 1 */ C_CAP_DROP(CAP_DAC_OVERRIDE); /* does NOT work properly */
 	///* 2 */ C_CAP_DROP(CAP_DAC_READ_SEARCH);
@@ -84,4 +84,10 @@ c_cap_start_child(const container_t *container)
 	}
 
 	return 0;
+}
+
+int
+c_cap_start_child(const container_t *container)
+{
+	return c_cap_set_current_process(container);
 }

daemon/c_cap.h
@@ -27,6 +27,9 @@
 #include "container.h"
 
 int
+c_cap_set_current_process(const container_t *container);
+
+int
 c_cap_start_child(const container_t *container);
 
 #endif /* C_CAP_H */

daemon/c_cgroups.c
@@ -966,6 +966,40 @@ error:
 }
 
 int
+c_cgroups_add_pid(c_cgroups_t *cgroups, pid_t pid)
+{
+	ASSERT(cgroups);
+
+	// temporarily add systemd to list
+	cgroups->active_cgroups = list_prepend(cgroups->active_cgroups, "systemd");
+
+	for (list_t* l = cgroups->active_cgroups; l; l = l->next) {
+		char *subsys = l->data;
+		char *subsys_child_path = mem_printf("%s/%s/%s/child", CGROUPS_FOLDER, subsys,
+				uuid_string(container_get_uuid(cgroups->container)));
+		char *cgroup_tasks = mem_printf("%s/tasks", subsys_child_path);
+
+		/* assign the container to the cgroup */
+		if (file_printf(cgroup_tasks, "%d", pid) == -1) {
+			ERROR_ERRNO("Could not add container %s to its cgroup under %s",
+				container_get_description(cgroups->container), subsys_child_path);
+			mem_free(cgroup_tasks);
+			mem_free(subsys_child_path);
+			goto error;
+		}
+	}
+
+	// remove temporarily added head
+	cgroups->active_cgroups = list_unlink(cgroups->active_cgroups, cgroups->active_cgroups);
+
+	return 0;
+error:
+	// remove temporarily added head
+	cgroups->active_cgroups = list_unlink(cgroups->active_cgroups, cgroups->active_cgroups);
+	return -1;
+}
+
+int
 c_cgroups_start_child(c_cgroups_t *cgroups)
 {
 	ASSERT(cgroups);

daemon/c_cgroups.h
@@ -75,6 +75,9 @@ int
 c_cgroups_start_pre_exec(c_cgroups_t *cgroups);
 
 int
+c_cgroups_add_pid(c_cgroups_t *cgroups, pid_t pid);
+
+int
 c_cgroups_start_child(c_cgroups_t *cgroups);
 
 void

commit 99d04460eac22abf0a2042c0edc2a1c4705ab612

daemon/c_run: Pass ctermid() directly to TRACE()
TRACE() only evaluates its arguments at runtime when tracing is
enabled, so pass ctermid(NULL) directly to the log macro instead of
storing it in a single-use variable.

Fixes following warning from clang static analyzer

  c_run.c:445:14: warning: Value stored to 'current_pty' during its \
   initialization is never read [deadcode.DeadStores]
    445 |         const char *current_pty = ctermid(NULL);
        |                     ^~~~~~~~~~~   ~~~~~~~~~~~~~

Fixes: 2f18e7e7f27d ("c_run subsystem")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_run.c
@@ -442,8 +442,7 @@ do_pty_exec(void *data)
 		goto error;
 	}
 
-	const char *current_pty = ctermid(NULL);
-	TRACE("[EXEC] Current controlling PTY is: %s\n", current_pty);
+	TRACE("[EXEC] Current controlling PTY is: %s\n", ctermid(NULL));
 
 	if (-1 == ioctl(STDIN_FILENO, TIOCNOTTY)) {
 		TRACE("[EXEC] Failed to release current controlling pty.\n");