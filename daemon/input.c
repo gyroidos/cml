gyroidos/cml

commit 8d33a890dfc047b09224c500c4eb35ddc0deb708

daemon: Implemented reading container pin from usb pin reader
Added file input.c with functionality to retrieve the device input file
of a usb device from vendor and product id and to read in a pin
from that device. Implemented the read-in of the pin in a callback
during container start.

Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

daemon/input.c
@@ -1,0 +1,197 @@
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+#include <linux/ioctl.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include "fcntl.h"
+
+#include "common/macro.h"
+#include "common/mem.h"
+#include "container.h"
+
+// The following defines represent parts of the syntax of /proc/bus/input/devices
+#define IFACE_PREFIX "I:"
+#define HANDLER_PREFIX "H: Handlers="
+#define EVENT "event"
+#define SYSRQ_EVENT "sysrq"
+
+/**
+ * Converts a keyboard scan code value into its ASCII digit representation
+ * 
+ * @param int in    The input keyboard scan code value
+ * @param char* out Its ASCII digit representation
+ * @return          0 in case of success, -1 if the scan code value was not a digit code
+ */
+static int
+input_event_code_to_ascii_num(int in, char *out)
+{
+	if (in == KEY_1 || in == KEY_KP1) {
+		*out = '1';
+		return 0;
+	} else if (in == KEY_2 || in == KEY_KP2) {
+		*out = '2';
+		return 0;
+	} else if (in == KEY_3 || in == KEY_KP3) {
+		*out = '3';
+		return 0;
+	} else if (in == KEY_4 || in == KEY_KP4) {
+		*out = '4';
+		return 0;
+	} else if (in == KEY_5 || in == KEY_KP5) {
+		*out = '5';
+		return 0;
+	} else if (in == KEY_6 || in == KEY_KP6) {
+		*out = '6';
+		return 0;
+	} else if (in == KEY_7 || in == KEY_KP7) {
+		*out = '7';
+		return 0;
+	} else if (in == KEY_8 || in == KEY_KP8) {
+		*out = '8';
+		return 0;
+	} else if (in == KEY_9 || in == KEY_KP9) {
+		*out = '9';
+		return 0;
+	} else if (in == KEY_0 || in == KEY_KP0) {
+		*out = '0';
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+char *
+input_get_usb_input_event_file(uint16_t vendor_id, uint16_t product_id)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
+	uint16_t bus;
+	uint16_t vid;
+	uint16_t pid;
+	uint16_t ver;
+	char *event = NULL;
+	char *event_path = NULL;
+
+	fp = fopen("/proc/bus/input/devices", "r");
+	IF_NULL_RETVAL(fp, NULL);
+
+	while ((n = getline(&line, &len, fp)) != -1) {
+		if (strncmp(IFACE_PREFIX, line, strlen(IFACE_PREFIX)) == 0) {
+			sscanf(line, "I: Bus=%04hx Vendor=%04hx Product=%04hx Version=%04hx\n",
+			       &bus, &vid, &pid, &ver);
+		}
+
+		if ((vid == vendor_id) && (pid == product_id) &&
+		    (strncmp(HANDLER_PREFIX, line, strlen(IFACE_PREFIX)) == 0) &&
+		    strstr(line, SYSRQ_EVENT)) {
+			event = strtok(strstr(line, EVENT), " ");
+			event_path = mem_printf("/dev/input/%s", event);
+			break;
+		}
+	}
+
+	fclose(fp);
+	mem_free(line);
+	return event_path;
+}
+
+int
+input_open_usb_input_event_file(char *input_file)
+{
+	int fd = open(input_file, O_RDONLY | O_NONBLOCK);
+	if (fd == -1) {
+		WARN("Failed to open usb input event file for pin reader");
+	}
+	return fd;
+}
+
+char *
+input_read_usb_input_event_file_pin(int fd)
+{
+	char *key = NULL;
+	char *tmp_key = NULL;
+
+	int ret = ioctl(fd, EVIOCGRAB, (void *)1);
+	if (ret != 0) {
+		WARN("Failed to get exclusive access to pin reader");
+		return key;
+	}
+	struct input_event keyboard_event;
+
+	TRACE("Reading pin from pin reader");
+	int index = 0;
+	while (1) {
+		if (read(fd, &keyboard_event, sizeof(keyboard_event)) != -1) {
+			if ((keyboard_event.type == 0x1) &&
+			    (keyboard_event.code == KEY_BACKSPACE) &&
+			    (keyboard_event.value == 0x1)) {
+				// Backspace was pressed, remove last entered digit
+				if (index > 0) {
+					index--;
+				}
+			} else if ((keyboard_event.type == 0x1) &&
+				   ((keyboard_event.code == KEY_ENTER) ||
+				    (keyboard_event.code == KEY_KPENTER)) &&
+				   (keyboard_event.value == 0x1)) {
+				// Enter was pressed, finish
+				break;
+			} else if ((keyboard_event.type == 0x1) && (keyboard_event.value == 0x1)) {
+				// Another key was pressed..
+				char c;
+				// ..Check if pressed key is a digit and store it
+				if (input_event_code_to_ascii_num(keyboard_event.code, &c) == 0) {
+					index++;
+					tmp_key = (char *)mem_realloc(key, index * sizeof(char));
+					if (tmp_key) {
+						key = tmp_key;
+						key[index - 1] = c;
+					} else {
+						WARN("Failed to allocate memory for pin");
+						free(key);
+						return key;
+					}
+				}
+			}
+		}
+	}
+
+	ret = ioctl(fd, EVIOCGRAB, NULL);
+	if (ret != 0) {
+		WARN("Failed to release exclusive access to pin reader");
+	}
+
+	return key;
+}
+
+int
+close_usb_input_event_file(int fd)
+{
+	return close(fd);
+}

daemon/Makefile
@@ -101,7 +101,8 @@ SRC_FILES := main.c \
 	c_run.c \
 	c_fifo.c \
 	c_time.c \
-	hw_$(TRUSTME_HARDWARE).c
+	hw_$(TRUSTME_HARDWARE).c \
+	input.c
 
 protobuf: container.proto control.proto guestos.proto common/logf.proto device.proto scd.proto c_service.proto
 	protoc-c --c_out=. container.proto

daemon/control.c
@@ -36,6 +36,8 @@
 #include "cmld.h"
 #include "hardware.h"
 #include "smartcard.h"
+#include "input.h"
+#include "uevent.h"
 
 //#define LOGF_LOG_MIN_PRIO LOGF_PRIO_TRACE
 #include "common/macro.h"
@@ -487,6 +489,10 @@ control_send_message(control_message_t message, int fd)
 		out.response = DAEMON_TO_CONTROLLER__RESPONSE__CONTAINER_LOCKED_TILL_REBOOT;
 		break;
 
+	case CONTROL_RESPONSE_CONTAINER_USB_PIN_ENTRY_FAIL:
+		out.response = DAEMON_TO_CONTROLLER__RESPONSE__CONTAINER_USB_PIN_ENTRY_FAIL;
+		break;
+
 	case CONTROL_RESPONSE_GUESTOS_MGR_INSTALL_STARTED:
 		out.response = DAEMON_TO_CONTROLLER__RESPONSE__GUESTOS_MGR_INSTALL_STARTED;
 		break;
@@ -633,9 +639,46 @@ control_start_container(control_t *control, container_t *container,
 	// Key is asserted to be the user entered passwd/pin
 	res = cmld_container_start_with_smartcard(control, container, key);
 	if (res != 0) {
-		DEBUG("Token has not been initialized yet");
+		ERROR("Failed to start container %s", container_get_name(container));
 	}
 	return res;
+}
+
+typedef struct {
+	control_t *control;
+	container_t *container;
+	ContainerStartParams *start_params;
+} container_start_cb_data_t;
+
+static void
+control_cb_request_pin_start_container(int fd, UNUSED unsigned events, UNUSED event_io_t *io,
+				       void *data)
+{
+	ASSERT(data);
+
+	char *key = NULL;
+	container_start_cb_data_t *cb_data = data;
+	if (!cb_data->control || !cb_data->container || !cb_data->start_params) {
+		WARN("Invalid parameters for container start callback");
+		goto exit;
+	}
+
+	key = input_read_usb_input_event_file_pin(fd);
+	if (!key) {
+		WARN("Failed to read in pin from pin reader");
+		goto exit;
+	}
+
+	DEBUG("Starting container from Callback, fd");
+	control_start_container(cb_data->control, cb_data->container, cb_data->start_params, key);
+	memset(key, 0x0, strlen(key));
+	mem_free(key);
+
+exit:
+	event_remove_io(io);
+	event_io_free(io);
+	close_usb_input_event_file(fd);
+	mem_free(data);
 }
 
 /**
@@ -645,24 +688,87 @@ static int
 control_handle_container_start(control_t *control, container_t *container,
 			       ContainerStartParams *start_params, int fd)
 {
+	TRACE("Starting container");
 	int res = -1;
-	if (start_params) {
+	char *input_file = NULL;
+
+	// Check if pin should be interactively requested via pin pad reader
+	if (container_get_usb_pin_entry(container)) {
+		TRACE("Searching for USB pin reader for interactive pin entry");
+
+		// Iterate through usb-dev list and look for USB_PIN_ENTRY device
+		uevent_usbdev_t *usbdev_pinreader = NULL;
+		for (list_t *l = container_get_usbdev_list(container); l; l = l->next) {
+			uevent_usbdev_t *usbdev = (uevent_usbdev_t *)l->data;
+			if (uevent_usbdev_get_type(usbdev) == UEVENT_USBDEV_TYPE_PIN_ENTRY) {
+				usbdev_pinreader = usbdev;
+				break;
+			}
+		}
+
+		if (!usbdev_pinreader) {
+			ERROR("Failed to find USB pin reader");
+			control_send_message(CONTROL_RESPONSE_CONTAINER_USB_PIN_ENTRY_FAIL, fd);
+			res = -1;
+			goto exit;
+		}
+
+		TRACE("Found USB pin reader. Device Serial: %s. Vendor:Product: %x:%x",
+		      uevent_usbdev_get_i_serial(usbdev_pinreader),
+		      uevent_usbdev_get_id_vendor(usbdev_pinreader),
+		      uevent_usbdev_get_id_product(usbdev_pinreader));
+
+		input_file = input_get_usb_input_event_file(
+			uevent_usbdev_get_id_vendor(usbdev_pinreader),
+			uevent_usbdev_get_id_product(usbdev_pinreader));
+		if (!input_file) {
+			ERROR("Failed to find USB pin reader input file");
+			control_send_message(CONTROL_RESPONSE_CONTAINER_USB_PIN_ENTRY_FAIL, fd);
+			res = -1;
+			goto exit;
+		}
+
+		TRACE("Found USB pin reader input file: %s", input_file);
+
+		int fd_pin = input_open_usb_input_event_file(input_file);
+		if (fd_pin == -1) {
+			ERROR("Failed to open USB pin reader input file");
+			control_send_message(CONTROL_RESPONSE_CONTAINER_USB_PIN_ENTRY_FAIL, fd);
+			res = -1;
+			goto exit;
+		}
+
+		// Read in pin in asynchronous callback
+		container_start_cb_data_t *cb_data = mem_new0(container_start_cb_data_t, 1);
+		cb_data->control = control;
+		cb_data->container = container;
+		cb_data->start_params = start_params;
+		event_io_t *event = event_io_new(fd_pin, EVENT_IO_READ,
+						 control_cb_request_pin_start_container, cb_data);
+
+		TRACE("Registering callback for pin entry");
+		event_add_io(event);
+
+	} else if (start_params) {
 		char *key = start_params->key;
+		TRACE("Default container start without pin entry chosen. Starting");
 		res = control_start_container(control, container, start_params, key);
 		if (res != 0) {
-			res = control_send_message(CONTROL_RESPONSE_CONTAINER_TOKEN_UNINITIALIZED,
-						   fd);
+			control_send_message(CONTROL_RESPONSE_CONTAINER_TOKEN_UNINITIALIZED, fd);
 		}
 	} else if (container_is_encrypted(container)) {
-		res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_PASSWD_WRONG, fd);
+		res = -1;
+		control_send_message(CONTROL_RESPONSE_CONTAINER_START_PASSWD_WRONG, fd);
 	} else {
 		res = cmld_container_start(container);
 		if (res < 0) {
-			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_EEXIST, fd);
+			control_send_message(CONTROL_RESPONSE_CONTAINER_START_EEXIST, fd);
 		} else {
-			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_OK, fd);
+			control_send_message(CONTROL_RESPONSE_CONTAINER_START_OK, fd);
 		}
 	}
+exit:
+	mem_free(input_file);
 	return res;
 }
 
@@ -1081,10 +1187,14 @@ control_handle_message(control_t *control, const ControllerToDaemon *msg, int fd
 			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_EEXIST, fd);
 			break;
 		}
-		if (container_get_state(container) == CONTAINER_STATE_RUNNING) {
-			WARN("Container is already running!");
-			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_LOCK_FAILED,
-						   fd);
+		container_state_t container_state = container_get_state(container);
+		if ((container_state == CONTAINER_STATE_RUNNING) ||
+		    (container_state == CONTAINER_STATE_BOOTING) ||
+		    (container_state == CONTAINER_STATE_SETUP) ||
+		    (container_state == CONTAINER_STATE_REBOOTING) ||
+		    (container_state == CONTAINER_STATE_STARTING)) {
+			WARN("Container is already running or in the process of starting up!");
+			res = control_send_message(CONTROL_RESPONSE_CONTAINER_START_EEXIST, fd);
 			break;
 		}
 		ContainerStartParams *start_params = msg->container_start_params;

daemon/control.h
@@ -57,6 +57,7 @@ typedef enum {
 	CONTROL_RESPONSE_CONTAINER_CHANGE_PIN_FAILED,
 	CONTROL_RESPONSE_CONTAINER_CHANGE_PIN_SUCCESSFUL,
 	CONTROL_RESPONSE_CONTAINER_LOCKED_TILL_REBOOT,
+	CONTROL_RESPONSE_CONTAINER_USB_PIN_ENTRY_FAIL,
 	CONTROL_RESPONSE_DEVICE_PROVISIONING_ERROR,
 	CONTROL_RESPONSE_DEVICE_CERT_ERROR,
 	CONTROL_RESPONSE_DEVICE_CERT_OK,

daemon/control.proto
@@ -265,8 +265,8 @@ message DaemonToController {
 		CONTAINER_START_TOKEN_UNPAIRED = 19;
 		CONTAINER_CHANGE_PIN_FAILED = 8;
 		CONTAINER_CHANGE_PIN_SUCCESSFUL = 9;
-		CMD_UNSUPPORTED = 7;
 		CONTAINER_LOCKED_TILL_REBOOT = 5;
+		CONTAINER_USB_PIN_ENTRY_FAIL = 20;
 		DEVICE_PROVISIONING_ERROR = 10;
 		DEVICE_CERT_ERROR = 11;
 		DEVICE_CERT_OK = 12;
@@ -274,6 +274,7 @@ message DaemonToController {
 		GUESTOS_MGR_INSTALL_FAILED = 15;
 		GUESTOS_MGR_INSTALL_WAITING = 16;
 		GUESTOS_MGR_INSTALL_COMPLETED = 17;
+		CMD_UNSUPPORTED = 7;
 	}
 
 	required Code code = 1;

daemon/input.h
@@ -1,0 +1,72 @@
+/*
+ * This file is part of trust|me
+ * Copyright(c) 2013 - 2020 Fraunhofer AISEC
+ * Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 (GPL 2), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ * Contact Information:
+ * Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+ */
+
+/**
+ * @file input.h
+ *
+ * This module implements functionality to search for an external usb
+ * input pin reader and to read in pins from that device
+ */
+
+#ifndef DAEMON_INPUT_H_
+#define DAEMON_INPUT_H_
+
+/**
+ * Parses /proc/bus/input/devices to find the /dev/input/eventx input file for the 
+ * usb devices specified by vendor_id and product_id
+ * 
+ * @param vendor_id     The vendor ID of the usb device to get the input file for
+ * @param product_id    The product ID of the usb device to get the input file for
+ * @return              The /dev/input/eventx file of the usb device
+ */
+char *
+input_get_usb_input_event_file(uint16_t vendor_id, uint16_t product_id);
+
+/**
+ * Opens the /dev/input/eventx file to enable reading the keystrokes of the usb device
+ * 
+ * @param char* input_file  The /dev/input/eventx file to be opened
+ * return                   The file descriptor or -1 if an error occurred
+ */
+int
+input_open_usb_input_event_file(char *input_file);
+
+/**
+ * Read a pin from an external usb pin reader device fd points to
+ * 
+ * @param fd    The file descriptor that represents the input file
+ * @return      The read in pin
+ */
+char *
+input_read_usb_input_event_file_pin(int fd);
+
+/**
+ * Closes the usb input file
+ * 
+ * @param int fd    The file descriptor of the file to be closed
+ * @return          0 on success, otherwise -1
+ */
+int
+close_usb_input_event_file(int fd);
+
+#endif // DAEMON_INPUT_H_

commit e545575e86dfe040a09b618de8cf36e596dba8a4

daemon/input: Drop unused getline() return value
The length returned by getline() is not needed, only the -1
end-of-file/error condition. Drop the unused variable and check the
return value directly.

Fixes following warning from clang static analyzer:

  input.c:133:10: warning: Although the value stored to 'n' is \
   used in the enclosing expression, the value is never actually \
   read from 'n' [deadcode.DeadStores]
  133 |         while ((n = getline(&line, &len, fp)) != -1) {
      |                 ^   ~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: 8d33a890dfc0 ("daemon: Implemented reading container pin from usb pin reader")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/input.c
@@ -119,7 +119,6 @@ input_get_usb_input_event_file(uint16_t vendor_id, uint16_t product_id)
 	FILE *fp;
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t n = 0;
 	uint16_t bus = 0;
 	uint16_t vid = 0;
 	uint16_t pid = 0;
@@ -130,7 +129,7 @@ input_get_usb_input_event_file(uint16_t vendor_id, uint16_t product_id)
 	fp = fopen("/proc/bus/input/devices", "r");
 	IF_NULL_RETVAL(fp, NULL);
 
-	while ((n = getline(&line, &len, fp)) != -1) {
+	while (getline(&line, &len, fp) != -1) {
 		if (strncmp(IFACE_PREFIX, line, strlen(IFACE_PREFIX)) == 0) {
 			if (sscanf(line, "I: Bus=%04hx Vendor=%04hx Product=%04hx Version=%04hx\n",
 				   &bus, &vid, &pid, &ver) != 4)