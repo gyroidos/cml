gyroidos/cml

commit a5e11437f156d38ce2fcc9d64ce321f2ab88b794

daemon/compartment: Check compartment pid/early_pid in sigchild_cb
If the container gets terminated the sigchld handler calls cleanup.
The compartment_cleanup() sets internal the compartment->pid and
compartment->early_pid to "-1". In case, another SIGCHLD arrives,
e.g., c_fifo kills its forwarding children, the sigchld handler for
the compartment is triggered again. This lead to a waidpid() on 1
since we do a waitpid() for the process group by -(compartment->pid).
This resulted in a second cleanup run. To prevent this we just
check compartment->pid to be a valid pid before running waitpaid()
on it.

This fixes commit "(1aa1055fb09f) daemon/compartment: Cleanup and
handle helper children properly"

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/compartment.c
@@ -679,11 +679,10 @@ compartment_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 	/* In the start function the childs init process gets set a process group which has
 	 * the same pgid as its pid. We wait for all processes belonging to our compartment's
 	 * process group, but only change the compartments state to stopped if the init exited */
-	pid_t compartment_pid = compartment->pid;
 	pid_t pid = 0;
 	int status = 0;
-	if ((pid = proc_waitpid(-(compartment_pid), &status, WNOHANG))) {
-		if (pid == compartment_pid) {
+	if (compartment->pid > 0 && (pid = proc_waitpid(-(compartment->pid), &status, WNOHANG))) {
+		if (pid == compartment->pid) {
 			bool rebooting = false;
 			if (WIFEXITED(status)) {
 				INFO("Container %s terminated (init process exited with status=%d)",
@@ -720,8 +719,8 @@ compartment_sigchld_cb(UNUSED int signum, event_signal_t *sig, void *data)
 	}
 
 	// check for compartment itself again, e.g., before execv
-	pid = proc_waitpid(compartment_pid, &status, WNOHANG);
-	if (pid == compartment_pid) {
+	pid = proc_waitpid(compartment->pid, &status, WNOHANG);
+	if (pid == compartment->pid) {
 		INFO("Compartment %s reaped before beeing part of pg",
 		     compartment_get_description(compartment));
 		if (WIFEXITED(status)) {
@@ -756,7 +755,8 @@ compartment_sigchld_early_cb(UNUSED int signum, event_signal_t *sig, void *data)
 	TRACE("SIGCHLD handler called for compartment %s early start child with PID %d",
 	      compartment_get_description(compartment), compartment->pid);
 
-	if ((pid = proc_waitpid(compartment->pid_early, &status, WNOHANG)) > 0) {
+	if (compartment->pid_early &&
+	    (pid = proc_waitpid(compartment->pid_early, &status, WNOHANG)) > 0) {
 		TRACE("Reaped early compartment child process: %d", pid);
 		/* remove the sigchld callback for this early child from the event loop */
 		event_remove_signal(sig);

commit daee2b7e2fc182f64d9b439985e530837cfcbbc8

daemon/compartment: Log reaped pid of early child
Actually use the pid returned by proc_waitpid() by including it in
the INFO log message, which is helpful when debugging early
compartment shutdown anyway.

Fixes following warning from clang static analyzer:

  compartment.c:805:7: warning: Although the value stored to 'pid' \
   is used in the enclosing expression, the value is never actually \
   read from 'pid' [deadcode.DeadStores]
    805 |             (pid = proc_waitpid(compartment->pid_early, &status, WNOHANG)) > 0) {
        |              ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: a5e11437f156 ("daemon/compartment: Check compartment pid/early_pid in sigchild_cb")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/compartment.c
@@ -813,7 +813,7 @@ compartment_sigchld_early_cb(UNUSED int signum, event_signal_t *sig, void *data)
 			if (compartment->pid == -1)
 				compartment_cleanup(compartment, false);
 
-			INFO("exit status: %d, %d", WEXITSTATUS(status), status);
+			INFO("pid %d: exit status: %d, %d", pid, WEXITSTATUS(status), status);
 
 			if ((WIFEXITED(status) &&
 			     WEXITSTATUS(status) == COMPARTMENT_ERROR_VOL_CORRUPTED))