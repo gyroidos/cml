gyroidos/cml

commit 87664e9fb964982bcf119f28356f54a8c6fbdb3a

cmld c_vol: allow btrfs for image creation and subvol usage

daemon/c_vol.c
@@ -55,6 +55,7 @@
 #define MAKE_EXT4FS "mkfs.ext4"
 #endif
 #define BTRFSTUNE "btrfstune"
+#define MAKE_BTRFS "mkfs.btrfs"
 
 #define ICC_SHARED_MOUNT "data/trustme-com"
 #define TPM2D_SHARED_MOUNT ICC_SHARED_MOUNT "/tpm2d"
@@ -69,6 +70,34 @@ struct c_vol {
 };
 
 /******************************************************************************/
+
+static int
+c_vol_fork_and_execvp(const char * const *argv)
+{
+	int status;
+	pid_t pid = fork();
+
+	switch (pid) {
+	case -1:
+		ERROR_ERRNO("Could not fork for %s", argv[0]);
+		return -1;
+	case 0:
+		execvp(argv[0], (char * const *)argv);
+		ERROR_ERRNO("Could not execvp %s", argv[0]);
+		return -1;
+	default:
+		if (waitpid(pid, &status, 0) != pid) {
+			ERROR_ERRNO("Could not waitpid for '%s'", argv[0]);
+		} else if (!WIFEXITED(status)) {
+			ERROR("Child '%s' terminated abnormally", argv[0]);
+		} else {
+			TRACE("%s terminated normally", argv[0]);
+			return WEXITSTATUS(status) ? -1 : 0;
+		}
+	}
+	return -1;
+}
+
 
 /**
  * Allocate a new string with the full image path for one mount point.
@@ -179,25 +208,8 @@ c_vol_create_image_empty(c_vol_t *vol, const char *img, const mount_entry_t *mnt
 static int
 c_vol_btrfs_regen_uuid(const char *dev)
 {
-	pid_t pid = fork();
-
-	switch (pid) {
-	case -1:
-		ERROR_ERRNO("Could not fork to regen uuid for %s", dev);
-		return -1;
-	case 0:
-		execlp("/usr/bin/"BTRFSTUNE, BTRFSTUNE, "-f", "-u", dev, (char *) NULL);
-		ERROR_ERRNO("Could not execlp %s", BTRFSTUNE);
-		return -1;
-	default:
-		if (waitpid(pid, NULL, 0) != pid) {
-			ERROR_ERRNO("Could not waitpid for %s", BTRFSTUNE);
-			return -1;
-		}
-		return 0;
-	}
-
-	return 0;
+	const char * const argv_regen[] = { BTRFSTUNE, "-f", "-u", dev, NULL };
+	return c_vol_fork_and_execvp(argv_regen);
 }
 
 static int
@@ -289,32 +301,65 @@ c_vol_create_image(c_vol_t *vol, const char *img, const mount_entry_t *mntent)
 static int
 c_vol_format_image(const char *dev, const char *fs)
 {
-	pid_t pid;
-
-	if (strcmp("ext4", fs)) {
+	const char *mkfs_bin = NULL;
+	if (0 == strcmp("ext4", fs)) {
+		mkfs_bin = MAKE_EXT4FS;
+	} else if (0 == strcmp("btrfs", fs)) {
+		mkfs_bin = MAKE_BTRFS;
+	} else {
 		ERROR("Could not create filesystem of type %s on %s", fs, dev);
 		return -1;
 	}
+	const char * const argv_mkfs[] = {mkfs_bin, dev, NULL};
+	return c_vol_fork_and_execvp(argv_mkfs);
+}
 
-	pid = fork();
+static int
+c_vol_btrfs_create_subvol(const char *dev, const char *mount_data)
+{
+	IF_NULL_RETVAL(mount_data, -1);
 
-	switch (pid) {
-	case -1:
-		ERROR_ERRNO("Could not fork to create filesystem for %s", dev);
+	int ret = 0;
+	char *token = strdup(mount_data);
+	char *subvol = strtok(token, "=");
+	subvol = strtok(NULL, "=");
+	if (NULL == subvol) {
+		mem_free(token);
 		return -1;
-	case 0:
-		execlp("/sbin/"MAKE_EXT4FS, MAKE_EXT4FS, dev, (char *) NULL);
-		ERROR_ERRNO("Could not execlp %s", MAKE_EXT4FS);
-		return -1;
-	default:
-		if (waitpid(pid, NULL, 0) != pid) {
-			ERROR_ERRNO("Could not waitpid for %s", MAKE_EXT4FS);
-			return -1;
-		}
-		return 0;
 	}
 
-	return 0;
+	char *subvol_path = NULL;
+	char *tmp_mount = mem_strdup("/tmp/tmp.XXXXXX");
+	tmp_mount = mkdtemp(tmp_mount);
+	if (NULL == tmp_mount) {
+		ret = -1;
+		goto out;
+	}
+	if (-1 == (ret = mount(dev, tmp_mount, "btrfs", 0, 0))) {
+		ERROR_ERRNO("temporary mount of btrfs root volume %s failed", dev);
+		goto out;
+	}
+	subvol_path = mem_printf("%s/%s", tmp_mount, subvol);
+
+	const char * const argv_list[] = {"btrfs", "subvol", "list", subvol_path, NULL};
+	if (-1 == (ret = c_vol_fork_and_execvp(argv_list))) {
+		const char * const argv_create[] = {"btrfs", "subvol", "create", subvol_path, NULL};
+		if (-1 == (ret = c_vol_fork_and_execvp(argv_create))) {
+			ERROR_ERRNO("Could not create btrfs subvol %s", subvol);
+		} else {
+			INFO("Created new suvol %s on btrfs device %s", subvol, dev);
+		}
+	}
+	if (-1 == (ret = umount(tmp_mount))) {
+		ERROR_ERRNO("Could not umount temporary mount of btrfs root volume %s!", dev);
+	}
+out:
+	unlink(tmp_mount);
+	if (subvol_path)
+		mem_free(subvol_path);
+	mem_free(tmp_mount);
+	mem_free(token);
+	return ret;
 }
 
 /**
@@ -471,14 +516,20 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t* mntent)
 		label = mem_printf("%s-%s", uuid_string(container_get_uuid(vol->container)),
 				mount_entry_get_img(mntent));
 
-		DEBUG("Setting up cryptfs volume %s for %s", label, dev);
+		crypt = cryptfs_get_device_path_new(label);
+		if (file_is_blk(crypt)) {
+			INFO("Using existing mapper device: %s", crypt);
+		} else {
+			DEBUG("Setting up cryptfs volume %s for %s", label, dev);
 
-		crypt = cryptfs_setup_volume_new(label, dev, container_get_key(vol->container));
+			mem_free(crypt);
+			crypt = cryptfs_setup_volume_new(label, dev, container_get_key(vol->container));
 
-		if (!crypt) {
-			ERROR("Setting up cryptfs volume %s for %s failed", label, dev);
-			mem_free(label);
-			goto error;
+			if (!crypt) {
+				ERROR("Setting up cryptfs volume %s for %s failed", label, dev);
+				mem_free(label);
+				goto error;
+			}
 		}
 
 		mem_free(label);
@@ -494,10 +545,13 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t* mntent)
 
 	if (mount_entry_get_type(mntent) == MOUNT_TYPE_SHARED_RW) {
 		// create mountpoints for lower and upper dev
-		overlayfs_mount_dir = mem_printf("/tmp/%s-overlayfs", uuid_string(container_get_uuid(vol->container)));
-		lower_dir = mem_printf("%s/%s/lower", overlayfs_mount_dir, mount_entry_get_img(mntent));
-		upper_dir = mem_printf("%s/%s/upper", overlayfs_mount_dir, mount_entry_get_img(mntent));
-		work_dir = mem_printf("%s/%s/work", overlayfs_mount_dir, mount_entry_get_img(mntent));
+		overlayfs_mount_dir = mem_printf("/tmp/%s-overlayfs/%s%s",
+						uuid_string(container_get_uuid(vol->container)),
+						mount_entry_get_img(mntent),
+						mount_entry_get_dir(mntent));
+		lower_dir = mem_printf("%s/lower", overlayfs_mount_dir);
+		upper_dir = mem_printf("%s/upper", overlayfs_mount_dir);
+		work_dir = mem_printf("%s/work", overlayfs_mount_dir);
 
 		if (dir_mkdir_p(overlayfs_mount_dir, 0755) < 0) {
 			ERROR_ERRNO("Could not mkdir overlayfs dir %s", overlayfs_mount_dir);
@@ -566,12 +620,19 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t* mntent)
 			}
 			DEBUG("Successfully formatted new image %s using %s", img, dev);
 		}
+		if (!strcmp("btrfs", rw_fstype) &&
+			!strncmp("subvol", mount_entry_get_mount_data(mntent), 6)) {
+			c_vol_btrfs_create_subvol(dev, mount_entry_get_mount_data(mntent));
+		}
 
 		/*
 		 * mount rw image for overlayfs lower, upper and work dir
 		 * (at least upper and work need to be on the same fs)
 		 */
-		overlayfs_mount_dir = mem_printf("/tmp/%s-overlayfs", uuid_string(container_get_uuid(vol->container)));
+		overlayfs_mount_dir = mem_printf("/tmp/%s-overlayfs/%s%s",
+						uuid_string(container_get_uuid(vol->container)),
+						mount_entry_get_img(mntent),
+						mount_entry_get_dir(mntent));
 		if (dir_mkdir_p(overlayfs_mount_dir, 0755) < 0) {
 			ERROR_ERRNO("Could not mkdir overlayfs mount dir: %s", overlayfs_mount_dir);
 			goto error;
@@ -583,8 +644,8 @@ c_vol_mount_image(c_vol_t *vol, const char *root, const mount_entry_t* mntent)
 		DEBUG("Successfully mounted %s using %s to %s", img, dev, overlayfs_mount_dir);
 
 		// create mountpoint for upper dev
-		upper_dir = mem_printf("%s/%s-upper", overlayfs_mount_dir, mount_entry_get_img(mntent));
-		work_dir = mem_printf("%s/%s-work", overlayfs_mount_dir, mount_entry_get_img(mntent));
+		upper_dir = mem_printf("%s/upper", overlayfs_mount_dir);
+		work_dir = mem_printf("%s/work", overlayfs_mount_dir);
 		if (dir_mkdir_p(upper_dir, 0755) < 0) {
 			ERROR_ERRNO("Could not mkdir upper dir %s", upper_dir);
 			goto error;

common/cryptfs.c
@@ -88,8 +88,8 @@ get_blkdev_size(int fd)
 	return nr_sec;
 }
 
-static char *
-get_device_path_new(const char *label)
+char *
+cryptfs_get_device_path_new(const char *label)
 {
 	return mem_printf("%s%s", CRYPT_PATH_PREFIX, label);
 }
@@ -272,7 +272,7 @@ create_device_node(const char *name)
 	/* should not be necassery for android */
 	mkdir("/dev/block", 00777);
 
-	char *device = get_device_path_new(name);
+	char *device = cryptfs_get_device_path_new(name);
 	/* we might need to remove this device first */
 	unlink(device);
 	if (mknod(device, S_IFBLK | 00777, io->dev) != 0) {
@@ -321,7 +321,7 @@ cryptfs_delete_blk_dev(const char *name)
 	}
 
 	/* remove device node if necessary */
-	char *device = get_device_path_new(name);
+	char *device = cryptfs_get_device_path_new(name);
 	unlink(device);
 	mem_free(device);
 

common/cryptfs.h
@@ -33,6 +33,9 @@
 #define CRYPTFS_H
 
 char *
+cryptfs_get_device_path_new(const char *label);
+
+char *
 cryptfs_setup_volume_new(const char *label, const char *real_blk_dev, const char *ascii_key);
 
 int

common/file.c
@@ -71,6 +71,14 @@ file_is_dir(const char *file)
 }
 
 bool
+file_is_blk(const char *file)
+{
+	struct stat s;
+
+	return !lstat(file, &s) && S_ISBLK(s.st_mode);
+}
+
+bool
 file_is_mountpoint(const char *file)
 {
 	bool ret;

common/file.h
@@ -46,6 +46,9 @@ bool
 file_is_dir(const char *file);
 
 bool
+file_is_blk(const char *file);
+
+bool
 file_is_mountpoint(const char *file);
 
 bool

rattestation/Makefile
@@ -55,4 +55,4 @@ rattestation: libcommon ${SRC_FILES} ${PROTO_SRC}
 
 .PHONY: clean
 clean:
-	rm -f attestation *.o *.pb-c.*
+	rm -f rattestation *.o *.pb-c.*

commit d5f7773c33f43831707eac47f57e317963ab6c9c

daemon/c_vol: Fix error handling in c_vol_btrfs_create_subvol()
The return value of the btrfs subvol list/create commands was
unconditionally overwritten with the result of the final umount(), so
a successful umount masked a failed subvolume creation and the
function wrongly reported success. Preserve ret and only set it to -1
if the umount itself fails.

Also only continue the tokenizing of mount_data if the first strtok()
returned a token at all; for mount data without a '=' delimiter,
strtok(NULL, ...) would have continued a finished tokenization.

Fixes following warnings from clang static analyzer:

  c_vol.c:397:8: warning: Value stored to 'subvol' during its \
   initialization is never read [deadcode.DeadStores]
    397 |         char *subvol = strtok(token, "=");
        |               ^~~~~~   ~~~~~~~~~~~~~~~~~~
  c_vol.c:418:13: warning: Although the value stored to 'ret' is \
   used in the enclosing expression, the value is never actually \
   read from 'ret' [deadcode.DeadStores]
    418 |         if (-1 == (ret = proc_fork_and_execvp(argv_list))) {
        |                    ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  c_vol.c:421:14: warning: Although the value stored to 'ret' is \
   used in the enclosing expression, the value is never actually \
   read from 'ret' [deadcode.DeadStores]
    421 |                 if (-1 == (ret = proc_fork_and_execvp(argv_create))) {
        |                            ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: 87664e9fb964 ("cmld c_vol: allow btrfs for image creation and subvol usage")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/c_vol.c
@@ -395,7 +395,8 @@ c_vol_btrfs_create_subvol(const char *dev, const char *mount_data)
 	int ret = 0;
 	char *token = mem_strdup(mount_data);
 	char *subvol = strtok(token, "=");
-	subvol = strtok(NULL, "=");
+	if (subvol)
+		subvol = strtok(NULL, "=");
 	if (NULL == subvol) {
 		mem_free0(token);
 		return -1;
@@ -424,8 +425,10 @@ c_vol_btrfs_create_subvol(const char *dev, const char *mount_data)
 			INFO("Created new suvol %s on btrfs device %s", subvol, dev);
 		}
 	}
-	if (-1 == (ret = umount(tmp_mount))) {
+	// preserve ret from btrfs list or create
+	if (-1 == umount(tmp_mount)) {
 		ERROR_ERRNO("Could not umount temporary mount of btrfs root volume %s!", dev);
+		ret = -1;
 	}
 out:
 	if (tmp_mount)