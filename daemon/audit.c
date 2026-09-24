14d4702b8bf00b7b6cdf8d0470d8e567b11d1695
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2026-09-24 09:02:17 +0200

    daemon/audit: Set log_record once for all netlink message types
    The payload pointer of the received netlink message was assigned in
    every type-specific branch of audit_cb_kernel_handle_log(). Move the
    assignment before the type dispatch so it is set only once for all
    branches.
    
    Fixes following warning from clang static analyzer:
    
      audit.c:882:4: warning: Value stored to 'log_record' is never \
       read [deadcode.DeadStores]
        882 |                         log_record = NLMSG_DATA(nlmsg);
            |                         ^            ~~~~~~~~~~~~~~~~~
    
    Fixes: 5bcfba3324ea ("daemon/audit: error-handling in callback")
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- daemon/audit.c
+++ daemon/audit.c
@@ -781,9 +781,9 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 
 		struct nlmsghdr *nlmsg = (struct nlmsghdr *)buf;
 		uint16_t type = nlmsg->nlmsg_type;
+		log_record = NLMSG_DATA(nlmsg);
 
 		if (type == AUDIT_TRUSTED_APP) {
-			log_record = NLMSG_DATA(nlmsg);
 			int uid = -1;
 			int pid = -1;
 			sscanf(log_record, "%*s pid=%d uid=%d %*8970c", &pid, &uid);
@@ -801,7 +801,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 		} else if (type == AUDIT_USER || type == AUDIT_LOGIN || type == AUDIT_DM_CTRL ||
 			   (type >= AUDIT_FIRST_USER_MSG && type <= AUDIT_LAST_USER_MSG) ||
 			   (type >= AUDIT_FIRST_USER_MSG2 && type <= AUDIT_LAST_USER_MSG2)) {
-			log_record = NLMSG_DATA(nlmsg);
 			int uid = -1;
 			int pid = -1;
 			sscanf(log_record, "%*s pid=%d uid=%d %*8970c", &pid, &uid);
@@ -824,7 +823,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 			mem_free0(record_type);
 			TRACE("audit: type=%d %s", type, log_record);
 		} else if (type == AUDIT_DM_EVENT) {
-			log_record = NLMSG_DATA(nlmsg);
 			uuid_t *uuid = NULL;
 			char *dev_file = NULL;
 			char *op_buf = mem_new0(char, MAX_AUDIT_MESSAGE_LENGTH);
@@ -877,7 +875,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 			TRACE("audit: type=%d %s", type, log_record);
 		} else if (type == AUDIT_KERNEL ||
 			   (type >= AUDIT_FIRST_EVENT && type <= AUDIT_INTEGRITY_LAST_MSG)) {
-			log_record = NLMSG_DATA(nlmsg);
 			TRACE("audit: type=%d %s", type, log_record);
 		}
 	}

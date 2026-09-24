gyroidos/cml

commit 5bcfba3324eaa430e3874012d66ae543fe279a05

daemon/audit: error-handling in callback
Signed-off-by: Joana Pecholt <joana.pecholt@aisec.fraunhofer.de>

daemon/audit.c
@@ -643,7 +643,7 @@ out:
 }
 
 static void
-audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, void *data)
+audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void *data)
 {
 	nl_sock_t *audit_sock = data;
 	ASSERT(audit_sock);
@@ -652,11 +652,15 @@ audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, v
 	char *buf = mem_new0(char, MAX_AUDIT_MESSAGE_LENGTH);
 	char *log_record = NULL;
 
+	if (events & EVENT_IO_EXCEPT) {
+		goto out;
+	}
+
 	int msg_len;
 	if ((msg_len = nl_msg_receive_kernel(audit_sock, buf, MAX_AUDIT_MESSAGE_LENGTH, false)) <=
 	    0) {
 		WARN("could not read audit meassge.");
-		goto err;
+		goto out;
 	}
 
 	struct nlmsghdr *nlmsg = (struct nlmsghdr *)buf;
@@ -669,7 +673,7 @@ audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, v
 		sscanf(log_record, "%*s pid=%d uid=%d %*8970c", &pid, &uid);
 		TRACE("scanned pid=%d, uid=%d", pid, uid);
 		char *record_text = strstr(log_record, "msg='");
-		IF_NULL_GOTO(record_text, err);
+		IF_NULL_GOTO(record_text, out);
 		record_text += 5;
 		// remove closing ' char from msg string
 		int record_text_len = strlen(record_text) - 1;
@@ -701,7 +705,7 @@ audit_kernel_handle_log(int fd, UNUSED unsigned events, UNUSED event_io_t *io, v
 		log_record = NLMSG_DATA(nlmsg);
 		TRACE("audit: type=%d %s", type, log_record);
 	}
-err:
+out:
 	mem_free(buf);
 }
 
@@ -727,7 +731,7 @@ audit_init(uint32_t size)
 	}
 
 	event_io_t *audit_io_event = event_io_new(nl_sock_get_fd(audit_sock), EVENT_IO_READ,
-						  &audit_kernel_handle_log, audit_sock);
+						  &audit_cb_kernel_handle_log, audit_sock);
 	event_add_io(audit_io_event);
 
 	/* Register message handler for audit logs */

commit 14d4702b8bf00b7b6cdf8d0470d8e567b11d1695

daemon/audit: Set log_record once for all netlink message types
The payload pointer of the received netlink message was assigned in
every type-specific branch of audit_cb_kernel_handle_log(). Move the
assignment before the type dispatch so it is set only once for all
branches.

Fixes following warning from clang static analyzer:

  audit.c:882:4: warning: Value stored to 'log_record' is never \
   read [deadcode.DeadStores]
    882 |                         log_record = NLMSG_DATA(nlmsg);
        |                         ^            ~~~~~~~~~~~~~~~~~

Fixes: 5bcfba3324ea ("daemon/audit: error-handling in callback")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

daemon/audit.c
@@ -781,9 +781,9 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 
 		struct nlmsghdr *nlmsg = (struct nlmsghdr *)buf;
 		uint16_t type = nlmsg->nlmsg_type;
+		log_record = NLMSG_DATA(nlmsg);
 
 		if (type == AUDIT_TRUSTED_APP) {
-			log_record = NLMSG_DATA(nlmsg);
 			int uid = -1;
 			int pid = -1;
 			sscanf(log_record, "%*s pid=%d uid=%d %*8970c", &pid, &uid);
@@ -801,7 +801,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 		} else if (type == AUDIT_USER || type == AUDIT_LOGIN || type == AUDIT_DM_CTRL ||
 			   (type >= AUDIT_FIRST_USER_MSG && type <= AUDIT_LAST_USER_MSG) ||
 			   (type >= AUDIT_FIRST_USER_MSG2 && type <= AUDIT_LAST_USER_MSG2)) {
-			log_record = NLMSG_DATA(nlmsg);
 			int uid = -1;
 			int pid = -1;
 			sscanf(log_record, "%*s pid=%d uid=%d %*8970c", &pid, &uid);
@@ -824,7 +823,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 			mem_free0(record_type);
 			TRACE("audit: type=%d %s", type, log_record);
 		} else if (type == AUDIT_DM_EVENT) {
-			log_record = NLMSG_DATA(nlmsg);
 			uuid_t *uuid = NULL;
 			char *dev_file = NULL;
 			char *op_buf = mem_new0(char, MAX_AUDIT_MESSAGE_LENGTH);
@@ -877,7 +875,6 @@ audit_cb_kernel_handle_log(int fd, unsigned events, UNUSED event_io_t *io, void 
 			TRACE("audit: type=%d %s", type, log_record);
 		} else if (type == AUDIT_KERNEL ||
 			   (type >= AUDIT_FIRST_EVENT && type <= AUDIT_INTEGRITY_LAST_MSG)) {
-			log_record = NLMSG_DATA(nlmsg);
 			TRACE("audit: type=%d %s", type, log_record);
 		}
 	}